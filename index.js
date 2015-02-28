5c4a36bd7b4d2682432f30bdf13ab235f1bd6b76
Fabian Vogelsteller <fabian@frozeman.de>
2015-02-28 15:55:41 +0100

    working on adding callbacks

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 var web3 = require('./lib/web3');
-web3.providers.HttpSyncProvider = require('./lib/httpsync');
+web3.providers.HttpProvider = require('./lib/httpprovider');
 web3.providers.QtSyncProvider = require('./lib/qtsync');
 web3.eth.contract = require('./lib/contract');
 web3.abi = require('./lib/abi');
