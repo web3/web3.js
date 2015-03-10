38f656734fe39a707c421367f975204212466a40
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-03-10 13:27:16 +0100

    fixed examples

--- example/node-app.js
+++ example/node-app.js
@@ -2,7 +2,7 @@
 
 var web3 = require("../index.js");
 
-web3.setProvider(new web3.providers.HttpSyncProvider('http://localhost:8080'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8080'));
 
 var coinbase = web3.eth.coinbase;
 console.log(coinbase);
