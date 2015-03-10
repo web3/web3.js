1c9f50feb9971af800b500cb593c27bd567b6710
Fabian Vogelsteller <fabian@frozeman.de>
2015-03-10 16:22:16 +0100

    changed topic to topics

--- test/formatters.inputPostFormatter.js
+++ test/formatters.inputPostFormatter.js
@@ -13,14 +13,14 @@ describe('formatters', function () {
                 payload: {test: 'test'},
                 ttl: 200,
                 workToProve: 1000,
-                topic: ['hello','mytopics']
+                topics: ['hello','mytopics']
             }), {
                 from: '0x00000',
                 to: '0x00000',
                 payload: '0x7b2274657374223a2274657374227d',
                 ttl: '0xc8',
                 workToProve: '0x3e8',
-                topic: ['0x68656c6c6f','0x6d79746f70696373']
+                topics: ['0x68656c6c6f','0x6d79746f70696373']
             });
 
         });
