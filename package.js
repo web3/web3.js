26f268f50bc621593f985635f3f87520c6b8dba6
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-03-11 10:25:04 +0100

    upgraded version to 0.1.1

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 /* jshint ignore:start */
 Package.describe({
   name: 'ethereum:js',
-  version: '0.1.0',
+  version: '0.1.1',
   summary: 'Ethereum JavaScript API, middleware to talk to a ethreum node over RPC',
   git: 'https://github.com/ethereum/ethereum.js',
   // By default, Meteor will default to using README.md for documentation.
