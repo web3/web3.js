0b84376721b7cadb011d32d11efa74e616915d5d
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-03-23 18:10:07 +0100

    fix for otto

--- dist/ethereum-light.js
+++ dist/ethereum-light.js
@@ -541,7 +541,7 @@ module.exports = {
 'use strict';
 
 // go env doesn't have and need XMLHttpRequest
-if (typeof XMLHttpRequest === undefined) {
+if (typeof XMLHttpRequest === 'undefined') {
     exports.XMLHttpRequest = {};
 } else {
     exports.XMLHttpRequest = XMLHttpRequest; // jshint ignore:line
