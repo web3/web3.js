2b179a8ef8ffbf3c2cc9edf747219a6f078d00db
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-05-13 11:20:00 +0200

    updated node install test

--- example/node-app.js
+++ example/node-app.js
@@ -2,11 +2,11 @@
 
 var web3 = require("../index.js");
 
-web3.setProvider(new web3.providers.HttpProvider('http://localhost:8080'));
+web3.setProvider(new web3.providers.HttpProvider('http://localhost:8545'));
 
 var coinbase = web3.eth.coinbase;
 console.log(coinbase);
 
 var balance = web3.eth.getBalance(coinbase);
-console.log(balance);
+console.log(balance.toString(10));
 
