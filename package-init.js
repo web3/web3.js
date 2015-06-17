3e49f56971122a80d6039a317296d0d66e2b9a3b
Fabian Vogelsteller <fabian@frozeman.de>
2015-06-17 12:41:25 +0200

    add currentProvidor and mist fixes

--- package-init.js
+++ package-init.js
@@ -1,8 +1,28 @@
 /* jshint ignore:start */
 
-if(typeof web3 === 'undefined') {
-    web3 = require('web3');
-    BigNumber = require('bignumber.js');
+
+// Browser environment
+if(typeof window !== 'undefined') {
+
+    if(typeof window.web3 === 'undefined') {
+        web3 = require('web3');
+        BigNumber = require('bignumber.js');
+    } else {
+        web3 = window.web3;
+        BigNumber = window.BigNumber
+    }
+}
+
+
+// Node environment
+if(typeof global !== 'undefined') {
+    if(typeof global.web3 === 'undefined') {
+        web3 = require('web3');
+        BigNumber = require('bignumber.js');
+    } else {
+        web3 = global.web3;
+        BigNumber = global.BigNumber
+    }
 }
 
 /* jshint ignore:end */
