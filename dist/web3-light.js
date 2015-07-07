75b5c9153949089bec7c01e8b2ce88b0dadb88ee
Marek Kotewicz <marek.kotewicz@gmail.com>
2015-07-07 11:01:28 +0200

    version 0.8.0

--- dist/web3-light.js
+++ dist/web3-light.js
@@ -1398,7 +1398,7 @@ module.exports = {
 
 },{"bignumber.js":"bignumber.js"}],8:[function(require,module,exports){
 module.exports={
-    "version": "0.7.1"
+    "version": "0.8.0"
 }
 
 },{}],9:[function(require,module,exports){
