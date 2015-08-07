aca98050ba323157698d13a4f93fab845b58c933
debris <marek.kotewicz@gmail.com>
2015-08-07 11:19:44 +0200

    updated tests

--- test/iban.fromAddress.js
+++ test/iban.fromAddress.js
@@ -3,7 +3,8 @@ var Iban = require('../lib/web3/iban.js');
 var assert = chai.assert;
 
 var tests = [
-    { address: '00c5496aee77c1ba1f0854206a26dda82a81d6d8', expected: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS'}
+    { address: '00c5496aee77c1ba1f0854206a26dda82a81d6d8', expected: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS'},
+    { address: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8', expected: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS'}
 ];
 
 describe('lib/web3/iban', function () {
