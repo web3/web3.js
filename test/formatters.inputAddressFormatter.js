46fcae76edb0e3eaad36d52407405411f3c2f753
debris <marek.kotewicz@gmail.com>
2015-08-12 08:34:50 +0200

    fixed inputAddressFormatter

--- test/formatters.inputAddressFormatter.js
+++ test/formatters.inputAddressFormatter.js
@@ -3,11 +3,12 @@ var assert = chai.assert;
 var formatters = require('../lib/web3/formatters.js');
 var BigNumber = require('bignumber.js');
 
-var tests = [{
-    input: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8',
-    input: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8',
-    input: '00c5496aee77c1ba1f0854206a26dda82a81d6d8', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8'
-}];
+var tests = [
+    { input: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8' }, 
+    { input: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8'},
+    { input: '00c5496aee77c1ba1f0854206a26dda82a81d6d8', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8'},
+    { input: '0x11f4d0a3c12e86b4b5f39b213f7e19d048276dae', result: '0x11f4d0a3c12e86b4b5f39b213f7e19d048276dae' }
+];
 
 var errorTests = [
     '0x0c5496aee77c1ba1f0854206a26dda82a81d6d8',
