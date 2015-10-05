0a79a520a7d8b0c22ce15bb058900321ce9ab8e2
debris <marek.kotewicz@gmail.com>
2015-10-05 13:46:50 +0200

    added vitaliks test to test/sha3.js

--- test/sha3.js
+++ test/sha3.js
@@ -14,5 +14,6 @@ describe('lib/utils/sha3', function () {
     test('test(int)', 'f4d03772bec1e62fbe8c5691e1a9101e520e8f8b5ca612123694632bf3cb51b1');
     test('0x80', '56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421', { encoding: 'hex' });
     test('0x80', '6b03a5eef7706e3fb52a61c19ab1122fad7237726601ac665bd4def888f0e4a0');
+    test('0x3c9229289a6125f7fdf1885a77bb12c37a8d3b4962d936f7e3084dece32a3ca1', '82ff40c0a986c6a5cfad4ddf4c3aa6996f1a7837f9c398e17e5de5cbd5a12b28', { encoding: 'hex' });
 });
 
