5564011743c3bb51d7f4d2a6cd5d5410670c36ce
debris <marek.kotewicz@gmail.com>
2015-10-08 16:19:43 +0800

    making lint happy

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var Web3 = require('./lib/web3');
-var namereg = require('./lib/web3/namereg');
-
-//Web3.prototype.eth.sendIBANTransaction = require('./lib/web3/transfer');
 
 // dont override global variable
 if (typeof window !== 'undefined' && typeof window.web3 === 'undefined') {
