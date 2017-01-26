b08d97f79b14d43b6bd5bca5ad5df408916779bf
Fabian Vogelsteller <fabian@frozeman.de>
2017-01-26 16:28:06 +0100

    add more address tests

--- test/formatters.inputAddressFormatter.js
+++ test/formatters.inputAddressFormatter.js
@@ -4,6 +4,7 @@ var formatters = require('../packages/web3-core-helpers/src/formatters.js');
 
 var tests = [
     { input: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8' },
+    { input: 'XE75JRZCTTLBSYEQBGAS7GID8DKR7QY0QA3', result: '0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b'},
     { input: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8'},
     { input: '00c5496aee77c1ba1f0854206a26dda82a81d6d8', result: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8'},
     { input: '11f4d0a3c12e86b4b5f39b213f7e19d048276dae', result: '0x11f4d0a3c12e86b4b5f39b213f7e19d048276dae' },
