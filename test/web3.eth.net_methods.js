90ca87ea08eb09b87a8b63d46ee3511aaf5d1d24
Fabian Vogelsteller <fabian@frozeman.de>
2017-02-22 16:06:57 +0100

    add tests for getNetworkType

--- test/web3.eth.net_methods.js
+++ test/web3.eth.net_methods.js
@@ -7,7 +7,7 @@ var eth = new Eth();
 describe('web3.net', function() {
     describe('methods', function() {
         u.methodExists(eth.net, 'getId');
-        u.methodExists(eth.net, 'guessChain');
+        u.methodExists(eth.net, 'getNetworkType');
         u.methodExists(eth.net, 'isListening');
         u.methodExists(eth.net, 'getPeerCount');
     });
