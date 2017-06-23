7c7177c06d7e38ce3746e0af7672c6b3ea6f68cf
Fabian Vogelsteller <fabian@frozeman.de>
2017-06-23 11:28:20 +0200

    added whisper api tests

--- test/web3.shh.post.js
+++ test/web3.shh.post.js
@@ -7,39 +7,24 @@ var method = 'post';
 
 var tests = [{
     args: [{
-        from: '0x123123123',
-        topics: ['hello_world'],
+        symKeyID: '123123123ff',
+        sig: '44ffdd55',
+        topic: '0xffdd11',
         payload: web3.toHex('12345'),
         ttl: 100,
-        workToProve: 101
+        minPow: 0.5,
+        powTarget: 3,
+        padding: '0xffdd4455'
     }],
     formattedArgs: [{
-        from: '0x123123123',
-        topics: [web3.fromAscii('hello_world')],
+        symKeyID: '123123123ff',
+        sig: '44ffdd55',
+        topic: '0xffdd11',
         payload: web3.toHex('12345'),
-        ttl: web3.toHex('100'),
-        workToProve: web3.toHex('101'),
-        priority: '0x0'
-    }],
-    result: true,
-    formattedResult: true,
-    call: 'shh_'+ method
-}, {
-    args: [{
-        from: '0x21312',
-        topics: ['hello_world'],
-        payload: '0x12345',
-        ttl: 0x100,
-        workToProve: 0x101,
-        priority: 0x15
-    }],
-    formattedArgs: [{
-        from: '0x21312',
-        topics: [web3.fromAscii('hello_world')],
-        payload: '0x12345',
-        ttl: '0x100',
-        workToProve: '0x101',
-        priority: '0x15'
+        ttl: 100,
+        minPow: 0.5,
+        powTarget: 3,
+        padding: '0xffdd4455'
     }],
     result: true,
     formattedResult: true,
