aadb7fc74cc0eaea117e7c7cd8dba855e37ec7bc
perissology <30963004+perissology@users.noreply.github.com>
2017-09-19 08:38:55 +0000

    allow empty address arguments when encoding abi params (#1047)
    * allow empty address arguments
    
    * add test for address encoding

--- packages/web3-eth-abi/src/types/address.js
+++ packages/web3-eth-abi/src/types/address.js
@@ -15,7 +15,7 @@ var SolidityType = require('../type');
 var SolidityTypeAddress = function () {
     this._inputFormatter = function(){
         var args = Array.prototype.slice.call(arguments);
-        args[0] = formatters.inputAddressFormatter(args[0]);
+        args[0] = (!args[0] || args[0] === '0x0') ? '' : formatters.inputAddressFormatter(args[0]);
         return f.formatInputInt.apply(this, args);
     };
     this._outputFormatter = f.formatOutputAddress;
