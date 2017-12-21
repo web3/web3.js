de3d64a4a9caa667642784f1aced78d5a89bb325
Fabian Vogelsteller <fabian@frozeman.de>
2017-12-21 10:50:09 +0100

    bump version

--- dist/web3.js
+++ dist/web3.js
@@ -2458,7 +2458,7 @@ module.exports = {
 
 },{"./sha3.js":19,"bignumber.js":"bignumber.js","utf8":85}],21:[function(require,module,exports){
 module.exports={
-    "version": "0.20.2"
+    "version": "0.20.3"
 }
 
 },{}],22:[function(require,module,exports){
