dd55daa6cd54a3907457169425bcd5e29bf32ec4
Fabian Vogelsteller <fabian@frozeman.de>
2018-01-26 10:44:24 +0100

    bumped version

--- dist/web3.js
+++ dist/web3.js
@@ -54651,7 +54651,7 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
       module.exports = {
         "name": "web3",
         "namespace": "ethereum",
-        "version": "1.0.0-beta.28",
+        "version": "1.0.0-beta.29",
         "description": "Ethereum JavaScript API",
         "repository": "https://github.com/ethereum/web3.js/tree/master/packages/web3",
         "license": "LGPL-3.0",
@@ -54683,13 +54683,13 @@ var _typeof = typeof Symbol === "function" && typeof Symbol.iterator === "symbol
           "url": "https://github.com/obscuren"
         }],
         "dependencies": {
-          "web3-bzz": "1.0.0-beta.28",
-          "web3-core": "1.0.0-beta.28",
-          "web3-eth": "1.0.0-beta.28",
-          "web3-eth-personal": "1.0.0-beta.28",
-          "web3-net": "1.0.0-beta.28",
-          "web3-shh": "1.0.0-beta.28",
-          "web3-utils": "1.0.0-beta.28"
+          "web3-bzz": "1.0.0-beta.29",
+          "web3-core": "1.0.0-beta.29",
+          "web3-eth": "1.0.0-beta.29",
+          "web3-eth-personal": "1.0.0-beta.29",
+          "web3-net": "1.0.0-beta.29",
+          "web3-shh": "1.0.0-beta.29",
+          "web3-utils": "1.0.0-beta.29"
         }
       };
     }, {}], "BN": [function (require, module, exports) {
