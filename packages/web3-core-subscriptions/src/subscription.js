9573d226e638c33ea21c83443ba92fbaae2173fd
Gabriel Montes <gabriel@utoctadel.com.ar>
2018-02-13 05:11:29 -0300

    Emit error if subscription fails to start (#1370)
    * Emit error if subscription fails and no callback
    
    * Add comment to the error path

--- packages/web3-core-subscriptions/src/subscription.js
+++ packages/web3-core-subscriptions/src/subscription.js
@@ -295,6 +295,9 @@ Subscription.prototype.subscribe = function() {
         } else if (_.isFunction(_this.callback)) {
             _this.callback(err, null, _this);
             _this.emit('error', err);
+        } else {
+            // emit the event even if no callback was provided
+            _this.emit('error', err);
         }
     });
 
