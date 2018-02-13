b34dcf93281a506bdfd42e02a72bcff9636fafd6
Fabian Vogelsteller <fabian@frozeman.de>
2018-02-13 12:21:20 +0100

    fixes anonymous events

--- test/allevents.decode.js
+++ test/allevents.decode.js
@@ -9,14 +9,15 @@ var name = 'event1';
 var address = '0x1234567890123456789012345678901234567890';
 
 var tests = [{
-    abi: {
+    abi: [{
         name: name,
+        type: 'event',
         inputs: [{
             name: 'a',
             type: 'int',
             indexed: false
         }]
-    },
+    }],
     data: {
         logIndex: '0x1',
         transactionIndex: '0x10',
@@ -30,54 +31,56 @@ var tests = [{
         topics: undefined,
     },
     expected: {
-        logIndex: '0x1',
-        transactionIndex: '0x10',
+        logIndex: 1,
+        transactionIndex: 16,
         transactionHash: '0x1234567890',
         address: address,
         blockHash: '0x1234567890',
-        blockNumber: '0x1',
+        blockNumber: 1,
         data: '0x' +
             '0000000000000000000000000000000000000000000000000000000000000001' +
             '0000000000000000000000000000000000000000000000000000000000000004',
         topics: undefined,
     }
 }, {
-    abi: {
+    abi: [{
         name: name,
-        inputs: [{
-            name: 'a',
-            type: 'int',
-            indexed: false
-        }]
-    },
-    data: {
-        logIndex: '0x1',
-        transactionIndex: '0x10',
-        transactionHash: '0x1234567890',
-        address: address,
-        blockHash: '0x1234567890',
-        blockNumber: '0x1',
-        data: '0x' +
-            '0000000000000000000000000000000000000000000000000000000000000001' +
-            '0000000000000000000000000000000000000000000000000000000000000004',
-        topics: [],
-    },
-    expected: {
-        logIndex: '0x1',
-        transactionIndex: '0x10',
-        transactionHash: '0x1234567890',
-        address: address,
-        blockHash: '0x1234567890',
-        blockNumber: '0x1',
-        data: '0x' +
-            '0000000000000000000000000000000000000000000000000000000000000001' +
-            '0000000000000000000000000000000000000000000000000000000000000004',
-        topics: [],
-    }
-}, {
-    abi: {
-        name: name,
+        type: 'event',
         anonymous: true,
+        inputs: [{
+            name: 'a',
+            type: 'int',
+            indexed: false
+        }]
+    }],
+    data: {
+        logIndex: '0x1',
+        transactionIndex: '0x10',
+        transactionHash: '0x1234567890',
+        address: address,
+        blockHash: '0x1234567890',
+        blockNumber: '0x1',
+        data: '0x' +
+            '0000000000000000000000000000000000000000000000000000000000000001' +
+            '0000000000000000000000000000000000000000000000000000000000000004',
+        topics: [],
+    },
+    expected: {
+        logIndex: 1,
+        transactionIndex: 16,
+        transactionHash: '0x1234567890',
+        address: address,
+        blockHash: '0x1234567890',
+        blockNumber: 1,
+        data: '0x' +
+            '0000000000000000000000000000000000000000000000000000000000000001' +
+            '0000000000000000000000000000000000000000000000000000000000000004',
+        topics: [],
+    }
+}, {
+    abi: [{
+        name: name,
+        type: 'event',
         inputs: [{
             name: 'a',
             type: 'int',
@@ -95,7 +98,7 @@ var tests = [{
             type: 'int',
             indexed: true
         }]
-    },
+    }],
     data: {
         logIndex: '0x1',
         transactionIndex: '0x10',
@@ -107,7 +110,7 @@ var tests = [{
             '0000000000000000000000000000000000000000000000000000000000000001' +
             '0000000000000000000000000000000000000000000000000000000000000004',
         topics: [
-            '0x000000000000000000000000000000000000000000000000000000000000000a',
+            '0x0e99121e9409665e680573fa7da0cb6651dbdcf10c95f585b92b2c9b9702cff9',
             '0x0000000000000000000000000000000000000000000000000000000000000010'
         ]
     },
@@ -115,9 +118,9 @@ var tests = [{
         event: name,
         args: {
             a: new BigNumber(1),
-            b: new BigNumber(10),
+            b: new BigNumber(16),
             c: new BigNumber(4),
-            d: new BigNumber(16)
+            d: new BigNumber(0)
         },
         logIndex: 1,
         transactionIndex: 16,
