9f57a355210b0e4a36da9bc25e65b8b5df3d3b96
Samuel Furter <sam@tokenate.io>
2018-08-22 16:56:31 +0200

    request manager removed

---
+++ packages/web3-provider/src/detectors/ProviderDetector.js
@@ -0,0 +1,78 @@
+/*
+    This file is part of web3.js.
+
+    web3.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    web3.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file ProviderDetector.js
+ * @authors: Samuel Furter <samuel@ethereum.org>
+ * @date 2018
+ */
+
+
+function ProviderDetector() {
+
+}
+
+ProviderDetector.prototype.detect = function () {
+    // // EthereumProvider
+    // if (typeof global.ethereumProvider !== 'undefined') {
+    //     return global.ethereumProvider;
+    //
+    // }
+    //
+    // // Legacy web3.currentProvider
+    // if (typeof global.web3 !== 'undefined' && global.web3.currentProvider) {
+    //
+    //     // if connection is 'ipcProviderWrapper', add subscription support
+    //     if (!global.web3.currentProvider.on &&
+    //         global.web3.currentProvider.connection &&
+    //         global.web3.currentProvider.connection.constructor.name === 'ipcProviderWrapper') {
+    //
+    //         global.web3.currentProvider.on = function (type, callback) {
+    //
+    //             if (typeof callback !== 'function')
+    //                 throw new Error('The second parameter callback must be a function.');
+    //
+    //             switch (type) {
+    //                 case 'data':
+    //                     this.connection.on('data', function (data) {
+    //                         var result = '';
+    //
+    //                         data = data.toString();
+    //
+    //                         try {
+    //                             result = JSON.parse(data);
+    //                         } catch (e) {
+    //                             return callback(new Error('Couldn\'t parse response data' + data));
+    //                         }
+    //
+    //                         // notification
+    //                         if (!result.id && result.method.indexOf('_subscription') !== -1) {
+    //                             callback(null, result);
+    //                         }
+    //
+    //                     });
+    //                     break;
+    //
+    //                 default:
+    //                     this.connection.on(type, callback);
+    //                     break;
+    //             }
+    //         };
+    //     }
+    //
+    //     return global.web3.currentProvider;
+    // }
+};
--- packages/web3-provider/src/resolvers/ProviderAdapterResolver.js
+++ packages/web3-provider/src/resolvers/ProviderAdapterResolver.js
@@ -1,3 +1,26 @@
+/*
+    This file is part of web3.js.
+
+    web3.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    web3.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file ProviderAdapterResolver.js
+ * @authors: Samuel Furter <samuel@ethereum.org>
+ * @date 2018
+ */
+
+"use strict";
 
 var _ = require('underscore');
 var HttpProviderAdapter = require('../adapters/HttpProviderAdapter');
@@ -7,9 +30,9 @@ var WebsocketProvider = require('web3-providers-ws');
 var HttpProvider = require('web3-providers-http');
 var IpcProvider = require('web3-providers-ipc');
 
-function ProviderAdapterResolver() {}
+var ProviderAdapterResolver = {};
 
-ProviderAdapterResolver.prototype.resolve = function (provider, net) {
+ProviderAdapterResolver.resolve = function (provider, net) {
 
     if (typeof provider === 'string') {
         // HTTP
