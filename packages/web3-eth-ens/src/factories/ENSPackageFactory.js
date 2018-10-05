3225c582738b24d2743859c719035bbe6496fc42
Samuel Furter <sam@tokenate.io>
2018-10-05 11:36:28 +0200

    fileDoc in ENSPackageFactory updated

--- packages/web3-eth-ens/src/factories/ENSPackageFactory.js
+++ packages/web3-eth-ens/src/factories/ENSPackageFactory.js
@@ -12,7 +12,7 @@
     along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 /**
- * @file ENS.js
+ * @file ENSPackageFactory.js
  * @author Samuel Furter <samuel@ethereum.org>
  * @date 2018
  */
