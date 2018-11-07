53aa0ebe3313ef94e9a68d652f1a675ac3e3fb05
Samuel Furter <sam@tokenate.io>
2018-11-07 12:20:16 +0100

    basic rollup config added

---
+++ packages/web3-core-helpers/rollup.config.js
@@ -0,0 +1,45 @@
+import babel from 'rollup-plugin-babel';
+import resolve from 'rollup-plugin-node-resolve';
+import json from 'rollup-plugin-json';
+import pkg from './package.json';
+
+export default [
+    {
+        input: 'src/index.js',
+        output: {
+            name: 'Web3CoreHelpers',
+            file: pkg.browser,
+            format: 'umd'
+        },
+        plugins: [
+            resolve(),
+            json(),
+            babel({
+                exclude: 'node_modules/**',
+                plugins: [
+                    '@babel/plugin-proposal-export-default-from',
+                    '@babel/plugin-proposal-export-namespace-from'
+                ]
+            }),
+        ]
+    },
+    {
+        input: 'src/index.js',
+        output: [
+            {
+                file: pkg.main,
+                format: 'cjs',
+                plugins: [
+                    json()
+                ]
+            },
+            {
+                file: pkg.module,
+                format: 'es',
+                plugins: [
+                    json()
+                ]
+            }
+        ]
+    }
+];
