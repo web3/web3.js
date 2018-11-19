549e39727721435d317f469a8e9d96878240d717
Samuel Furter <sam@tokenate.io>
2018-11-19 09:28:29 +0800

    Rollup setup fixed

--- rollup.config.js
+++ rollup.config.js
@@ -4,29 +4,10 @@ import json from 'rollup-plugin-json';
 import autoExternal from 'rollup-plugin-auto-external';
 import commonjs from 'rollup-plugin-commonjs';
 import builtins from 'rollup-plugin-node-builtins';
+import {uglify} from 'rollup-plugin-uglify';
+import minify from 'rollup-plugin-babel-minify';
 
 const config = [
-    {
-        input: 'src/index.js',
-        output: {
-            name: '',
-            file: '',
-            format: 'umd',
-            sourcemap: true,
-            globals: {}
-        },
-        plugins: [
-            babel({
-                exclude: 'node_modules/**',
-                plugins: ['@babel/plugin-proposal-export-default-from', '@babel/plugin-proposal-export-namespace-from']
-            }),
-            autoExternal(),
-            commonjs(),
-            builtins(),
-            resolve(),
-            json()
-        ]
-    },
     {
         input: 'src/index.js',
         output: [
@@ -34,6 +15,38 @@ const config = [
                 file: '',
                 format: 'cjs'
             },
+            {
+                name: '',
+                file: '',
+                format: 'umd',
+                globals: {}
+            }
+        ],
+        plugins: [
+            babel({
+                exclude: 'node_modules/**',
+                babelrc: false,
+                presets: [
+                    [
+                        '@babel/env',
+                        {
+                            modules: false,
+                            targets: {
+                                node: '6',
+                                browsers: 'last 2 versions'
+                            }
+                        }
+                    ]
+                ],
+                plugins: ['@babel/plugin-proposal-export-default-from', '@babel/plugin-proposal-export-namespace-from']
+            }),
+            json(),
+            autoExternal()
+        ]
+    },
+    {
+        input: 'src/index.js',
+        output: [
             {
                 file: '',
                 format: 'es'
@@ -50,12 +63,26 @@ const config = [
     }
 ];
 
+/**
+ * Returns the rollup configuration with globals and names set.
+ *
+ * @param {String} name
+ * @param {String} outputFileName
+ * @param {Object} globals
+ *
+ * @returns {Array}
+ */
 export default (name, outputFileName, globals) => {
-    config[0].output.name = name;
-    config[0].output.file = 'dist/' + outputFileName + '.umd.js';
-    config[0].output.globals = globals;
+    //CJS
+    config[0].output[0].file = 'dist/' + outputFileName + '.umd.js';
+
+    //UMD
+    config[0].output[1].name = name;
+    config[0].output[1].file = 'dist/' + outputFileName + '.umd.js';
+    config[0].output[1].globals = globals;
+
+    //ESM
     config[1].output[0].file = 'dist/' + outputFileName + '.cjs.js';
-    config[1].output[1].file = 'dist/' + outputFileName + '.esm.js';
 
     return config;
 };
