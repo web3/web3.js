1605d7e91a43931eba3607c13d9180e72142de11
Samuel Furter <sam@tokenate.io>
2018-11-27 16:54:44 +0800

    CallMethodCommand test created

--- packages/web3-core-method/src/commands/CallMethodCommand.js
+++ packages/web3-core-method/src/commands/CallMethodCommand.js
@@ -47,6 +47,8 @@ export default class CallMethodCommand {
             if (methodModel.callback) {
                 methodModel.callback(error, null);
             }
+
+            throw error;
         }
     }
 }
--- packages/web3-core-method/tests/commands/CallMethodCommandTest.js
+++ packages/web3-core-method/tests/commands/CallMethodCommandTest.js
@@ -1,11 +1,10 @@
 import CallMethodCommand from '../../src/commands/CallMethodCommand';
 import AbstractMethodModel from '../../lib/models/AbstractMethodModel';
-import {WebsocketProvider, SocketProviderAdapter} from 'web3-providers';
+import {SocketProviderAdapter} from 'web3-providers';
 import {AbstractWeb3Module} from 'web3-core';
 
 // Mocks
 jest.mock('SocketProviderAdapter');
-jest.mock('WebsocketProvider');
 jest.mock('../../lib/models/AbstractMethodModel');
 jest.mock('AbstractWeb3Module');
 
@@ -14,63 +13,76 @@ jest.mock('AbstractWeb3Module');
  */
 describe('CallMethodCommandTest', () => {
     let callMethodCommand,
-        provider,
         providerAdapter,
+        providerAdapterMock,
         moduleInstance,
+        moduleInstanceMock,
         methodModel,
-        methodModelCallbackSpy;
+        methodModelMock;
 
     beforeEach(() => {
-        provider = new WebsocketProvider('ws://127.0.0.1', {});
-        providerAdapter = new SocketProviderAdapter(provider);
-        moduleInstance = new AbstractWeb3Module(providerAdapter, {}, {}, {});
+        providerAdapter = new SocketProviderAdapter({});
+        providerAdapterMock = SocketProviderAdapter.mock.instances[0];
+
+        moduleInstance = new AbstractWeb3Module(providerAdapterMock, {}, {}, {});
+        moduleInstanceMock = AbstractWeb3Module.mock.instances[0];
+
         methodModel = new AbstractMethodModel('', 0, {}, {});
+        methodModelMock = AbstractMethodModel.mock.instances[0];
+        methodModelMock.rpcMethod = 'RPC_METHOD';
+        methodModelMock.parameters = ['FIRST_PARAM'];
+
         callMethodCommand = new CallMethodCommand();
-
-        methodModelCallbackSpy = jest.fn();
-        methodModel.callback = methodModelCallbackSpy;
     });
 
-    it('calls execute', async () => {
-        expect(AbstractMethodModel.beforeExecution)
-            .toHaveBeenNthCalledWith(1, moduleInstance);
+    it('calls execute', async (done) => {
+        providerAdapterMock.send
+            .mockReturnValueOnce(Promise.resolve('response'));
 
-        expect(SocketProviderAdapter.send)
-            .toHaveNthReturnedWith(1, new Promise(resolve => {
-                resolve('response');
-            }));
+        methodModelMock.afterExecution
+            .mockReturnValueOnce('response');
 
-        expect(methodModelCallbackSpy)
-            .toHaveBeenNthCalledWith(
-                1,
-                false,
-                '0x0'
-            );
+        moduleInstanceMock.currentProvider = providerAdapterMock;
 
-        const returnValue = await callMethodCommand.execute(moduleInstance, methodModel);
-        expect(returnValue).toBe('0x0');
+        methodModelMock.callback = (error, response) => {
+            expect(error).toBe(false);
+            expect(response).toBe('response');
+
+            done();
+        };
+
+        await expect(callMethodCommand.execute(moduleInstanceMock, methodModelMock)).resolves.toBe('response');
+
+        expect(methodModelMock.beforeExecution)
+            .toHaveBeenCalledWith(moduleInstanceMock);
+
+        expect(providerAdapterMock.send)
+            .toHaveBeenCalledWith(methodModelMock.rpcMethod, methodModelMock.parameters);
+
+        expect(methodModelMock.afterExecution)
+            .toHaveBeenCalledWith('response');
     });
 
-    it('calls execute and throws error', async () => {
-        expect(AbstractMethodModel.beforeExecution)
-            .toHaveBeenNthCalledWith(1, moduleInstance);
+    it('calls execute and throws error', async (done) => {
+        providerAdapterMock.send
+            .mockReturnValueOnce(Promise.reject(new Error('ERROR')));
 
-        expect(SocketProviderAdapter.send)
-            .toHaveNthReturnedWith(1, new Promise((resolve, reject) => {
-                reject(new Error('error'));
-            }));
+        moduleInstanceMock.currentProvider = providerAdapterMock;
 
-        try {
-            await callMethodCommand.execute(moduleInstance, methodModel);
-        } catch (error) {
-            expect(error).toBe('error');
-        }
+        methodModelMock.callback = (error, response) => {
+            expect(error).toEqual(new Error('ERROR'));
+            expect(response).toBe(null);
 
-        expect(methodModelCallbackSpy)
-            .toHaveBeenNthCalledWith(
-                1,
-                new Error('error'),
-                null
-            );
+            done();
+        };
+
+        await expect(callMethodCommand.execute(moduleInstanceMock, methodModelMock))
+            .rejects.toEqual(new Error('ERROR'));
+
+        expect(methodModelMock.beforeExecution)
+            .toHaveBeenCalledWith(moduleInstanceMock);
+
+        expect(providerAdapterMock.send)
+            .toHaveBeenCalledWith(methodModelMock.rpcMethod, methodModelMock.parameters);
     });
 });
