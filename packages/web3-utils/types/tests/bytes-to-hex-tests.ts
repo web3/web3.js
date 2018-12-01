390f76d08852511f016464eec482481653d6c804
Josh Stevens <joshstevens19@hotmail.co.uk>
2018-12-01 09:18:26 +0000

    `bytesToHex` tests

---
+++ packages/web3-utils/types/tests/bytes-to-hex-tests.ts
@@ -0,0 +1,41 @@
+/*
+    This file is part of web3.js.
+    web3.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+    web3.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+    You should have received a copy of the GNU Lesser General Public License
+    along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file index.d.ts
+ * @author Josh Stevens <joshstevens19@hotmail.co.uk>
+ * @date 2018
+ */
+
+import { BN, bytesToHex } from "utils";
+
+const bigNumber = new BN(3);
+
+bytesToHex([ 72, 101, 108, 108, 111, 33, 36 ]); // $ExpectType string
+
+// $ExpectError
+bytesToHex([ '72', 101, 108, 108, 111, 33, 36 ]);
+// $ExpectError
+bytesToHex(345);
+// $ExpectError
+bytesToHex(bigNumber);
+// $ExpectError
+bytesToHex({});
+// $ExpectError
+bytesToHex(true);
+// $ExpectError
+bytesToHex(['string']);
+// $ExpectError
+bytesToHex(null);
+// $ExpectError
+bytesToHex(undefined);
