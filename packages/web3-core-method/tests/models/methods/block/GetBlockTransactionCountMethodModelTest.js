40721aa94504be48155974ba4cf3f46b44b5ce29
Samuel Furter <sam@tokenate.io>
2018-12-02 19:40:19 +0800

    GetBlockTransactionCountMethodModelTest rewritten

--- packages/web3-core-method/tests/models/methods/block/GetBlockTransactionCountMethodModelTest.js
+++ packages/web3-core-method/tests/models/methods/block/GetBlockTransactionCountMethodModelTest.js
@@ -1,24 +1,19 @@
-import * as sinonLib from 'sinon';
-import utils from 'web3-utils';
+import * as Utils from 'web3-utils';
 import {formatters} from 'web3-core-helpers';
 import GetBlockTransactionCountMethodModel from '../../../../src/models/methods/block/GetBlockTransactionCountMethodModel';
 
-const sinon = sinonLib.createSandbox();
+// Mocks
+jest.mock('Utils');
+jest.mock('formatters');
 
 /**
  * GetBlockTransactionCountMethodModel test
  */
 describe('GetBlockTransactionCountMethodModelTest', () => {
-    let model, utilsMock, formattersMock;
+    let model;
 
     beforeEach(() => {
-        utilsMock = sinon.mock(utils);
-        formattersMock = sinon.mock(formatters);
-        model = new GetBlockTransactionCountMethodModel(utils, formatters);
-    });
-
-    afterEach(() => {
-        sinon.restore();
+        model = new GetBlockTransactionCountMethodModel(Utils, formatters);
     });
 
     it('rpcMethod should return eth_getTransactionByBlockNumberAndIndex', () => {
@@ -32,17 +27,15 @@ describe('GetBlockTransactionCountMethodModelTest', () => {
     it('beforeExecution should call method with block hash as parameter and call inputBlockNumberFormatter', () => {
         model.parameters = ['0x0'];
 
-        formattersMock
-            .expects('inputBlockNumberFormatter')
-            .withArgs(model.parameters[0])
-            .returns('0x0')
-            .once();
+        formatters.inputBlockNumberFormatter
+            .mockReturnValueOnce('0x0');
 
         model.beforeExecution({});
 
         expect(model.parameters[0]).toBe('0x0');
 
-        formattersMock.verify();
+        expect(formatters.inputBlockNumberFormatter)
+            .toHaveBeenCalledWith('0x0');
 
         expect(model.rpcMethod).toBe('eth_getTransactionByBlockHashAndIndex');
     });
@@ -50,30 +43,26 @@ describe('GetBlockTransactionCountMethodModelTest', () => {
     it('beforeExecution should call method with block number as parameter and call inputBlockNumberFormatter', () => {
         model.parameters = [100];
 
-        formattersMock
-            .expects('inputBlockNumberFormatter')
-            .withArgs(model.parameters[0])
-            .returns('0x0')
-            .once();
+        formatters.inputBlockNumberFormatter
+            .mockReturnValueOnce('0x0');
 
         model.beforeExecution({});
 
         expect(model.parameters[0]).toBe('0x0');
 
-        formattersMock.verify();
+        expect(formatters.inputBlockNumberFormatter)
+            .toHaveBeenCalledWith(100);
 
         expect(model.rpcMethod).toBe('eth_getTransactionByBlockNumberAndIndex');
     });
 
     it('afterExecution should map the hex string to a number', () => {
-        utilsMock
-            .expects('hexToNumber')
-            .withArgs('0x0')
-            .returns(100)
-            .once();
+        Utils.hexToNumber
+            .mockReturnValueOnce(100);
 
         expect(model.afterExecution('0x0')).toBe(100);
 
-        utilsMock.verify();
+        expect(Utils.hexToNumber)
+            .toHaveBeenCalledWith('0x0')
     });
 });
