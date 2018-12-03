c9cc92f4f8d9bac36d99bfa8eaf15efcf6e3ecf0
Samuel Furter <sam@tokenate.io>
2018-12-03 07:07:58 +0400

    sthe polyfill option does no longer exist since the last release of @babel/runtime

--- jest.preprocessor.js
+++ jest.preprocessor.js
@@ -1,21 +1,16 @@
 module.exports = require('babel-jest').createTransformer(
     {
-        "env": {
-            "test": {
-                "presets": [
+        'env': {
+            'test': {
+                'presets': [
                     [
-                        "@babel/env"
+                        '@babel/env'
                     ]
                 ],
-                "plugins": [
-                    "@babel/plugin-proposal-export-default-from",
-                    "@babel/plugin-proposal-export-namespace-from",
-                    [
-                        "@babel/plugin-transform-runtime",
-                        {
-                            "polyfill": false
-                        }
-                    ]
+                'plugins': [
+                    '@babel/plugin-proposal-export-default-from',
+                    '@babel/plugin-proposal-export-namespace-from',
+                    '@babel/plugin-transform-runtime'
                 ]
             }
         }
