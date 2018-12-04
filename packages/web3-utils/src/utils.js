79ad19868fd7ca50e6aaffeff2a93e75d8af622a
nujabes403 <nujabes403@gmail.com>
2018-12-04 20:57:45 +0900

    Check isBN first for sha3 before checking hex

--- packages/web3-utils/src/utils.js
+++ packages/web3-utils/src/utils.js
@@ -425,12 +425,12 @@ var isTopic = function (topic) {
 var SHA3_NULL_S = '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470';
 
 var sha3 = function (value) {
-    if (isHexStrict(value) && /^0x/i.test((value).toString())) {
-        value = hexToBytes(value);
+    if (isBN(value)) {
+        value = value.toString();
     }
 
-    if (isBN(value)) {
-        value = value.toString(10);
+    if (isHexStrict(value) && /^0x/i.test((value).toString())) {
+        value = hexToBytes(value);
     }
 
     var returnValue = Hash.keccak256(value); // jshint ignore:line
