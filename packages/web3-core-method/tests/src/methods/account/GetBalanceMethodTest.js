d6a8746f8dee1787b5adba3f942e7834405cc50e
Samuel Furter <sam@tokenate.io>
2018-12-06 19:12:10 +0100

    GetBalanceMethodTest refactored

--- packages/web3-core-method/tests/src/methods/account/GetBalanceMethodTest.js
+++ packages/web3-core-method/tests/src/methods/account/GetBalanceMethodTest.js
@@ -1,31 +1,31 @@
-import {formatters} from 'packages/web3-core-helpers/dist/web3-core-helpers.cjs';
-import GetBalanceMethodModel from '../../../../src/models/methods/account/GetBalanceMethodModel';
+import {formatters} from 'web3-core-helpers';
+import GetBalanceMethod from '../../../../src/methods/account/GetBalanceMethod';
 
 // Mocks
 jest.mock('formatters');
 
 /**
- * GetBalanceMethodModel test
+ * GetBalanceMethod test
  */
-describe('GetBalanceMethodModelTest', () => {
-    let model;
+describe('GetBalanceMethodTest', () => {
+    let method;
 
     beforeEach(() => {
-        model = new GetBalanceMethodModel({}, formatters);
+        method = new GetBalanceMethod({}, {}, formatters);
     });
 
     it('rpcMethod should return eth_getBalance', () => {
-        expect(model.rpcMethod)
+        expect(method.rpcMethod)
             .toBe('eth_getBalance');
     });
 
     it('parametersAmount should return 2', () => {
-        expect(model.parametersAmount)
+        expect(method.parametersAmount)
             .toBe(2);
     });
 
     it('beforeExecution should call inputAddressFormatter and inputDefaultBlockNumberFormatter', () => {
-        model.parameters = ['string', 100];
+        method.parameters = ['string', 100];
 
         formatters.inputAddressFormatter
             .mockReturnValueOnce('0x0');
@@ -33,12 +33,12 @@ describe('GetBalanceMethodModelTest', () => {
         formatters.inputDefaultBlockNumberFormatter
             .mockReturnValueOnce('0x0');
 
-        model.beforeExecution({});
+        method.beforeExecution({});
 
-        expect(model.parameters[0])
+        expect(method.parameters[0])
             .toBe('0x0');
 
-        expect(model.parameters[1])
+        expect(method.parameters[1])
             .toBe('0x0');
 
         expect(formatters.inputAddressFormatter)
@@ -52,7 +52,7 @@ describe('GetBalanceMethodModelTest', () => {
         formatters.outputBigNumberFormatter
             .mockReturnValueOnce({bigNumber: true});
 
-        expect(model.afterExecution({}))
+        expect(method.afterExecution({}))
             .toHaveProperty('bigNumber', true);
 
         expect(formatters.outputBigNumberFormatter)
