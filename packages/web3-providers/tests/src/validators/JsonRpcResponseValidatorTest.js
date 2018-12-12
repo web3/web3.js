2c533b5fdede2958a3ec8fd939be1a674afab161
Samuel Furter <sam@tokenate.io>
2018-12-13 00:20:44 +0100

    JsonRpcResponseValidatorTest created

--- packages/web3-providers/tests/src/validators/JsonRpcResponseValidatorTest.js
+++ packages/web3-providers/tests/src/validators/JsonRpcResponseValidatorTest.js
@@ -1,11 +1,50 @@
-// Mocks
-jest.mock('');
+import JsonRpcResponseValidator from '../../../src/validators/JsonRpcResponseValidator';
 
 /**
  * JsonRpcResponseValidator test
  */
 describe('JsonRpcResponseValidatorTest', () => {
-    it('', () => {
+    it('validate returns true with an array of responses', () => {
+        expect(JsonRpcResponseValidator.validate(
+            [
+                {
+                    id: 0,
+                    jsonrpc: '2.0',
+                    result: ''
+                }
+            ]
+        )).toEqual(true);
+    });
 
+    it('validate returns false with an array of responses', () => {
+        expect(JsonRpcResponseValidator.validate(
+            [
+                {
+                    id: 0,
+                    jsonrpc: '2.0',
+                    result: undefined
+                }
+            ]
+        )).toEqual(false);
+    });
+
+    it('validate returns true with one response', () => {
+        expect(JsonRpcResponseValidator.validate(
+            {
+                id: 0,
+                jsonrpc: '2.0',
+                result: ''
+            }
+        )).toEqual(true);
+    });
+
+    it('validate returns false with one response', () => {
+        expect(JsonRpcResponseValidator.validate(
+            {
+                id: 0,
+                jsonrpc: '2.0',
+                result: undefined
+            }
+        )).toEqual(false);
     });
 });
