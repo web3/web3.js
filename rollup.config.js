0e078f5df4e03d981d91aaea8ae915c1dec885c9
Samuel Furter <sam@tokenate.io>
2019-01-08 14:41:03 +0100

    Rollup root config changed for the web3 module this will be undone later

--- rollup.config.js
+++ rollup.config.js
@@ -88,5 +88,10 @@ export default (name, outputFileName, globals) => {
     // ESM
     config[1].output[0].file = 'dist/' + outputFileName + '.esm.js';
 
+    if (name === 'Web3') {
+        config[0].input = 'src/Web3.js';
+        config[1].input = 'src/Web3.js';
+    }
+
     return config;
 };
