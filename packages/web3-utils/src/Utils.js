12650002d1cc9d325776c4d83035fbfd24ae12e9
Aalok Singh <aalok_2@live.com>
2019-01-10 15:58:37 +0530

    TypeError fix

--- packages/web3-utils/src/Utils.js
+++ packages/web3-utils/src/Utils.js
@@ -500,7 +500,8 @@ export const getSignatureParameters = (signature) => {
     const r = signature.slice( 0, 66 );
     const s = `0x${signature.slice( 66, 130 )}`;
     let v = `0x${signature.slice( 130, 132 )}`;
-    v = this.hexToNumber(v);
+    v = hexToNumber(v);
+    
 
     if ( ![ 27, 28 ].includes( v ) ) v += 27;
 
