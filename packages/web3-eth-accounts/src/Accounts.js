45218a8f9a17331bf4f842a81e163feaeb8810e4
Brendan Asselstine <mail.asselstine@gmail.com>
2019-01-21 10:38:16 -0800

    Added safer localStorage detection so that web3 does not break the JS on the page when turned off.

--- packages/web3-eth-accounts/src/Accounts.js
+++ packages/web3-eth-accounts/src/Accounts.js
@@ -744,18 +744,33 @@ class Wallet {
     }
 }
 
-try {
-    if (typeof localStorage === 'undefined') {
-        delete Wallet.prototype.save;
-        delete Wallet.prototype.load;
+// Taken from https://developer.mozilla.org/en-US/docs/Web/API/Web_Storage_API/Using_the_Web_Storage_API#Feature-detecting_localStorage
+function storageAvailable(type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
     }
-} catch (error) {
-    // code 18 means trying to use local storage in a iframe
-    // with third party cookies turned off
-    // we still want to support using web3 in a iframe
-    // as by default safari turn these off for all iframes
-    // so mask the error
-    if (error.code !== 18) {
-        throw new Error(error);
+    catch(e) {
+      var isDOMException = typeof DOMException !== 'undefined' && e instanceof DOMException
+        return isDOMException && (
+            // everything except Firefox
+            e.code === 22 ||
+            // Firefox
+            e.code === 1014 ||
+            // test name field too, because code might not be present
+            // everything except Firefox
+            e.name === 'QuotaExceededError' ||
+            // Firefox
+            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            // acknowledge QuotaExceededError only if there's something already stored
+            storage.length !== 0;
     }
+}
+
+if (!storageAvailable('localStorage')) {
+    delete Wallet.prototype.save;
+    delete Wallet.prototype.load;
 }
