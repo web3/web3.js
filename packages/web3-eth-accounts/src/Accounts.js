1b890cc0469490872d1d6322b3166081b699bbad
Brendan Asselstine <mail.asselstine@gmail.com>
2019-01-22 10:39:00 -0800

    Fixed name of exception to make unicorn happy

--- packages/web3-eth-accounts/src/Accounts.js
+++ packages/web3-eth-accounts/src/Accounts.js
@@ -753,18 +753,18 @@ function storageAvailable(type) {
         storage.removeItem(x);
         return true;
     }
-    catch(e) {
-      var isDOMException = typeof DOMException !== 'undefined' && e instanceof DOMException
+    catch(error) {
+      var isDOMException = typeof DOMException !== 'undefined' && error instanceof DOMException
         return isDOMException && (
             // everything except Firefox
-            e.code === 22 ||
+            error.code === 22 ||
             // Firefox
-            e.code === 1014 ||
+            error.code === 1014 ||
             // test name field too, because code might not be present
             // everything except Firefox
-            e.name === 'QuotaExceededError' ||
+            error.name === 'QuotaExceededError' ||
             // Firefox
-            e.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
+            error.name === 'NS_ERROR_DOM_QUOTA_REACHED') &&
             // acknowledge QuotaExceededError only if there's something already stored
             storage.length !== 0;
     }
