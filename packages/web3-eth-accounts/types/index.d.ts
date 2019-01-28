8cd748bdfa6bf88c034cc3b9945a9987db7ae339
Josh Stevens <joshstevens19@hotmail.co.uk>
2019-01-28 16:07:57 +0000

    remove redundant comments

--- packages/web3-eth-accounts/types/index.d.ts
+++ packages/web3-eth-accounts/types/index.d.ts
@@ -101,8 +101,6 @@ export interface EncryptedKeystoreV3Json {
     }
 }
 
-/** TODO - MOVE ALL BELOW TO WEB3-CORE ONCE FIXED CONFUSING WITH RETURN TYPES !!! */
-
 export interface SignedTransaction {
     messageHash?: string;
     r: string;
@@ -114,5 +112,3 @@ export interface SignedTransaction {
 export interface Sign extends SignedTransaction {
     message: string;
 }
-
-/** END !!! */
