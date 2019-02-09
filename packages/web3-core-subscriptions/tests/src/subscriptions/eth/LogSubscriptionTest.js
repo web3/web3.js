ea48b991ae201ea738a6047a89e9452ab572ffac
Samuel Furter <sam@tokenate.io>
2019-02-09 12:51:34 +0100

    release commit

--- packages/web3-core-subscriptions/tests/src/subscriptions/eth/LogSubscriptionTest.js
+++ packages/web3-core-subscriptions/tests/src/subscriptions/eth/LogSubscriptionTest.js
@@ -40,9 +40,7 @@ describe('LogSubscriptionTest', () => {
     it('calls subscribe executes GetPastLogsMethod and calls the callback twice because of the past logs', (done) => {
         formatters.inputLogFormatter.mockReturnValueOnce({});
 
-        formatters.outputLogFormatter
-            .mockReturnValueOnce(0)
-            .mockReturnValueOnce('ITEM');
+        formatters.outputLogFormatter.mockReturnValueOnce(0).mockReturnValueOnce('ITEM');
 
         getPastLogsMethodMock.execute = jest.fn((moduleInstance) => {
             expect(moduleInstance).toEqual(moduleInstanceMock);
