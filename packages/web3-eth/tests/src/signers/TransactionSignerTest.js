32aa00270080fc3d7cdd2827382b0b35697ddcc1
Samuel Furter <sam@tokenate.io>
2019-03-26 21:04:34 +0100

    codestyle improved

--- packages/web3-eth/tests/src/signers/TransactionSignerTest.js
+++ packages/web3-eth/tests/src/signers/TransactionSignerTest.js
@@ -36,11 +36,11 @@ describe('TransactionSignerTest', () => {
         await expect(transactionSigner.sign(tx, '3a0ce9a362c73439adb38c595e739539be1e34d19c5e9f04962c101c86bd7616'))
             .resolves.toEqual(
                 {
-                    'messageHash': '91e0ad336c23d84f757aa4cde2d0bb557daf5e1ca0a0b850b6431f3277fc167b',
-                    'r': '0x1fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867',
-                    'rawTransaction': '0xf86303843b9aca0082753094b414031aa4838a69e27cb2ae31e709bcd674f0cb648045a01fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867a01415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
-                    's': '0x1415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
-                    'v': '0x45'
+                    messageHash: '91e0ad336c23d84f757aa4cde2d0bb557daf5e1ca0a0b850b6431f3277fc167b',
+                    rawTransaction: '0xf86303843b9aca0082753094b414031aa4838a69e27cb2ae31e709bcd674f0cb648045a01fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867a01415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
+                    r: '0x1fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867',
+                    s: '0x1415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
+                    v: '0x45'
                 });
     });
 
@@ -62,11 +62,11 @@ describe('TransactionSignerTest', () => {
         await expect(transactionSigner.sign(tx, '0x3a0ce9a362c73439adb38c595e739539be1e34d19c5e9f04962c101c86bd7616'))
             .resolves.toEqual(
                 {
-                    'messageHash': '91e0ad336c23d84f757aa4cde2d0bb557daf5e1ca0a0b850b6431f3277fc167b',
-                    'r': '0x1fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867',
-                    'rawTransaction': '0xf86303843b9aca0082753094b414031aa4838a69e27cb2ae31e709bcd674f0cb648045a01fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867a01415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
-                    's': '0x1415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
-                    'v': '0x45'
+                    messageHash: '91e0ad336c23d84f757aa4cde2d0bb557daf5e1ca0a0b850b6431f3277fc167b',
+                    rawTransaction: '0xf86303843b9aca0082753094b414031aa4838a69e27cb2ae31e709bcd674f0cb648045a01fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867a01415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
+                    r: '0x1fff9fa845437523b0a7f334b7d2a0ab14364a3581f898cd1bba3b5909465867',
+                    s: '0x1415137f53eeddf0687e966f8d59984676d6d92ce88140765ed343db6936679e',
+                    v: '0x45'
                 });
     });
 
