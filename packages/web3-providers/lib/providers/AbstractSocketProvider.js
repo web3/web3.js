68a275f5bc652f95e05e5160fe7925a6bc4479f6
Samuel Furter <sam@tokenate.io>
2019-04-01 15:46:36 +0200

    author updated

--- packages/web3-providers/lib/providers/AbstractSocketProvider.js
+++ packages/web3-providers/lib/providers/AbstractSocketProvider.js
@@ -13,7 +13,7 @@
 */
 /**
  * @file AbstractSocketProvider
- * @author Samuel Furter <samuel@ethereum.org>, Fabian Vogelsteller <fabian@ethereum.org>
+ * @author Samuel Furter <samuel@ethereum.org>
  * @date 2018
  */
 
