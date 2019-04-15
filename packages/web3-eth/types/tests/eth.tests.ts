e3032d1682fb1a1b682d041083c2ae27f570c7f2
princesinha19 <sinhaprince013@gmail.com>
2019-04-15 12:04:42 +0530

    minor fix

--- packages/web3-eth/types/tests/eth.tests.ts
+++ packages/web3-eth/types/tests/eth.tests.ts
@@ -17,7 +17,7 @@
  * @date 2018
  */
 
-import {Log, Transaction, TransactionReceipt, RLPEncodedTransaction, Content} from 'web3-core';
+import {Log, Transaction, TransactionReceipt, RLPEncodedTransaction} from 'web3-core';
 import {Eth, BlockHeader, Syncing, Block} from 'web3-eth';
 
 const eth = new Eth('http://localhost:8545');
