f21cc7ccf7003c6a122e6dcb40543471f5fea8a0
Samuel Furter <sam@tokenate.io>
2019-04-17 22:15:26 +0200

    SendRawTransactionMethodTest and SendTransactionMethodTest updated

--- packages/web3-core-method/tests/src/methods/transaction/SendRawTransactionMethodTest.js
+++ packages/web3-core-method/tests/src/methods/transaction/SendRawTransactionMethodTest.js
@@ -1,5 +1,9 @@
 import AbstractObservedTransactionMethod from '../../../../lib/methods/transaction/AbstractObservedTransactionMethod';
 import SendRawTransactionMethod from '../../../../src/methods/transaction/SendRawTransactionMethod';
+import {formatters} from 'web3-core-helpers';
+
+// Mocks
+jest.mock('web3-core-helpers');
 
 /**
  * SendRawTransactionMethod test
@@ -8,12 +12,20 @@ describe('SendRawTransactionMethodTest', () => {
     let method;
 
     beforeEach(() => {
-        method = new SendRawTransactionMethod(null, null, null, {});
+        method = new SendRawTransactionMethod(null, formatters, null, {});
     });
 
     it('constructor check', () => {
         expect(method).toBeInstanceOf(AbstractObservedTransactionMethod);
 
         expect(method.rpcMethod).toEqual('eth_sendRawTransaction');
+    });
+
+    it('calls afterExecution and returns the expected value', () => {
+        formatters.outputTransactionFormatter.mockReturnValueOnce({status: true});
+
+        expect(method.afterExecution({status: false})).toEqual({status: true});
+
+        expect(formatters.outputTransactionFormatter).toHaveBeenCalledWith({status: false});
     });
 });
