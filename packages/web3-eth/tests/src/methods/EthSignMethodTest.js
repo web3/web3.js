55d1c8f0ededd36c379eebb1e2c2a0716f616f03
Samuel Furter <sam@tokenate.io>
2019-04-26 16:26:20 +0200

    EthSignMethodTest updated

--- packages/web3-eth/tests/src/methods/EthSignMethodTest.js
+++ packages/web3-eth/tests/src/methods/EthSignMethodTest.js
@@ -29,7 +29,6 @@ describe('EthSignMethodTest', () => {
         formatters.inputSignFormatter.mockReturnValue('string');
 
         method = new EthSignMethod(Utils, formatters, moduleInstanceMock);
-        method.callback = jest.fn();
         method.parameters = ['nope', '0x0'];
     });
 
@@ -37,14 +36,12 @@ describe('EthSignMethodTest', () => {
         expect(method).toBeInstanceOf(SignMethod);
     });
 
-    it('calls execute with wallets defined', async () => {
+    it('calls execute with wallets defined and returns a resolved Promise', async () => {
         accountsMock.sign.mockReturnValueOnce('0x00');
 
         const response = await method.execute();
 
         expect(response).toEqual('0x00');
-
-        expect(method.callback).toHaveBeenCalledWith(false, '0x00');
 
         expect(method.parameters[0]).toEqual('0x0');
 
@@ -57,7 +54,31 @@ describe('EthSignMethodTest', () => {
         expect(accountsMock.sign).toHaveBeenCalledWith('string', '0x0');
     });
 
-    it('calls execute with wallets defined but accounts.sign throws an error', async () => {
+    it('calls execute with wallets defined and the callback gets called', (done) => {
+        accountsMock.sign.mockReturnValueOnce('0x00');
+
+        method.callback = jest.fn((error, response) => {
+            expect(error).toEqual(false);
+
+            expect(response).toEqual('0x00');
+
+            expect(method.parameters[0]).toEqual('0x0');
+
+            expect(method.parameters[1]).toEqual('string');
+
+            expect(formatters.inputAddressFormatter).toHaveBeenCalledWith('0x0');
+
+            expect(formatters.inputSignFormatter).toHaveBeenCalledWith('nope');
+
+            expect(accountsMock.sign).toHaveBeenCalledWith('string', '0x0');
+
+            done();
+        });
+
+        const response = method.execute();
+    });
+
+    it('calls execute with wallets defined but accounts.sign returns a rejected Promise', async () => {
         const error = new Error('SIGN ERROR');
         accountsMock.sign = jest.fn(() => {
             throw error;
@@ -68,10 +89,25 @@ describe('EthSignMethodTest', () => {
         } catch (error2) {
             expect(error2).toEqual(error);
 
-            expect(method.callback).toHaveBeenCalledWith(error, null);
-
             expect(accountsMock.sign).toHaveBeenCalledWith('string', '0x0');
         }
+    });
+
+    it('calls execute with wallets defined but accounts.sign throws an error', (done) => {
+        const error = new Error('SIGN ERROR');
+        accountsMock.sign = jest.fn(() => {
+            throw error;
+        });
+
+        method.callback = jest.fn((error, response) => {
+            expect(error).toEqual(error);
+
+            expect(response).toEqual(null);
+
+            done();
+        });
+
+        method.execute();
     });
 
     it('calls execute and the account does not exist in the eth-accounts wallet', async () => {
