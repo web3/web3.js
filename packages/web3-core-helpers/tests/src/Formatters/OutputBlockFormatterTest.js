8ffe36fd7750893893361f074098e635beccfec7
princesinha19 <sinhaprince013@gmail.com>
2019-05-03 12:56:58 +0530

    Conflict resolve

--- packages/web3-core-helpers/tests/src/Formatters/OutputBlockFormatterTest.js
+++ packages/web3-core-helpers/tests/src/Formatters/OutputBlockFormatterTest.js
@@ -52,6 +52,52 @@ describe('OutputBlockFormatterTest', () => {
         });
     });
 
+    it('[Quorum] call outputBlockFormatter with a valid block without a timestamp', () => {
+        const block = {
+            gasLimit: 0x0,
+            gasUsed: 0x0,
+            size: 0x0,
+            number: 0x0,
+            difficulty: 100,
+            totalDifficulty: 100,
+            transactions: [
+                {
+                    blockNumber: 0,
+                    transactionIndex: 0,
+                    gas: 100,
+                    gasPrice: 100,
+                    nonce: 1,
+                    value: 100,
+                    to: '0x03c9a938ff7f54090d0d99e2c6f80380510ea078',
+                    from: '0x03c9a938ff7f54090d0d99e2c6f80380510ea078'
+                }
+            ],
+            miner: '0x03c9a938ff7f54090d0d99e2c6f80380510ea078'
+        };
+
+        expect(outputBlockFormatter(block)).toEqual({
+            gasLimit: 0x0,
+            gasUsed: 0x0,
+            size: 0x0,
+            number: 0x0,
+            difficulty: '100', // Strange some numbers will be handled as string and some as number (gas & nonce)
+            totalDifficulty: '100',
+            transactions: [
+                {
+                    blockNumber: 0,
+                    transactionIndex: 0,
+                    gas: 100,
+                    gasPrice: '100',
+                    nonce: 1,
+                    value: '100',
+                    to: '0x03C9A938fF7f54090d0d99e2c6f80380510Ea078',
+                    from: '0x03C9A938fF7f54090d0d99e2c6f80380510Ea078'
+                }
+            ],
+            miner: '0x03C9A938fF7f54090d0d99e2c6f80380510Ea078'
+        });
+    });
+
     it('call outputBlockFormatter if block number is null', () => {
         const block = {
             gasLimit: 0x0,
@@ -97,7 +143,6 @@ describe('OutputBlockFormatterTest', () => {
             ]
         });
     });
-
 
     it('call outputBlockFormatter if miner is undefined', () => {
         const block = {
