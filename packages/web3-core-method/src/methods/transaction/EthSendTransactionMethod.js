c219d879661a3eaff7e74be2305e8c83ede43908
Samuel Furter <sam@tokenate.io>
2019-05-28 16:12:58 +0200

    nonce conversion removed because it's already doing it in the formatter

--- packages/web3-core-method/src/methods/transaction/EthSendTransactionMethod.js
+++ packages/web3-core-method/src/methods/transaction/EthSendTransactionMethod.js
@@ -186,7 +186,6 @@ export default class EthSendTransactionMethod extends SendTransactionMethod {
         transaction.data = transaction.data || '0x';
         transaction.value = transaction.value || '0x';
         transaction.chainId = this.utils.numberToHex(transaction.chainId);
-        transaction.nonce = this.utils.numberToHex(transaction.nonce);
 
         delete transaction.from;
 
