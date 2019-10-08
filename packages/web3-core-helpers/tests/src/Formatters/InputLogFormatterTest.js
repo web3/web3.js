769cf8b018ea2dc8a4ad8f673851b3424b03d979
nivida <samuel@ethereum.org>
2019-10-08 20:42:06 +0900

    inputLogFormatter fix from 1.x forwarded (#3089) and security updates applied

--- packages/web3-core-helpers/tests/src/Formatters/InputLogFormatterTest.js
+++ packages/web3-core-helpers/tests/src/Formatters/InputLogFormatterTest.js
@@ -20,6 +20,38 @@ describe('InputLogFormatterTest', () => {
         });
     });
 
+    it('call inputLogFormatter with numerical from/to blocks', function() {
+        var log = {
+            fromBlock: 1,
+            toBlock: 2,
+            topics: ['0x0'],
+            address: '0x03C9A938fF7f54090d0d99e2c6f80380510Ea078'
+        };
+
+        expect(inputLogFormatter(log)).toEqual({
+            fromBlock: '0x1',
+            toBlock: '0x2',
+            topics: ['0x0'],
+            address: '0x03c9a938ff7f54090d0d99e2c6f80380510ea078'
+        });
+    });
+
+    it('call inputLogFormatter with zero valued from/to blocks', function() {
+        var log = {
+            fromBlock: 0,
+            toBlock: 0,
+            topics: ['0x0'],
+            address: '0x03C9A938fF7f54090d0d99e2c6f80380510Ea078'
+        };
+
+        expect(inputLogFormatter(log)).toEqual({
+            fromBlock: '0x0',
+            toBlock: '0x0',
+            topics: ['0x0'],
+            address: '0x03c9a938ff7f54090d0d99e2c6f80380510ea078'
+        });
+    });
+
     it('call inputLogFormatter with a array of addresses in the log', () => {
         const log = {
             fromBlock: 'earliest',
