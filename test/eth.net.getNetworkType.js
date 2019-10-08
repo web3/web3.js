f2ff293d9d5d90344b68e12fa9d2cfc9582348f9
Jay Welsh <thealchemicalopus@gmail.com>
2019-10-08 13:28:08 +0200

    feat(Network): add support for net id of Goerli (#3095)
    * feat(Network): add support for net id of Goerli
    
    * eth.net.getNetworkType test extended

--- test/eth.net.getNetworkType.js
+++ test/eth.net.getNetworkType.js
@@ -8,13 +8,21 @@ var tests = [{
     id: 1,
     result: 'main'
 },{
-    hash: '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
-    id: 1,
-    result: 'main'
+    hash: '0cd786a2425d16f152c658316c423e6ce1181e15c3295826d7c9904cba9ce303',
+    id: 2,
+    result: 'morden'
 },{
-    hash: '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
-    id: 1,
-    result: 'main'
+    hash: '0x41941023680923e0fe4d74a34bdac8141f2540e3ae90623718e47d66d1ca4a2d',
+    id: 3,
+    result: 'ropsten'
+},{
+    hash: '0x6341fd3daf94b748c72ced5a5b26028f2474f5f00d824504e4fa37a75767e177',
+    id: 4,
+    result: 'rinkeby'
+},{
+    hash: '0xbf7e331f7f7c1dd2e05159666b3bf8bc7a8a3a9eb1d518969eab529dd9b88c1a',
+    id: 5,
+    result: 'goerli'
 },{
     hash: '0xffe56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
     id: 42,
@@ -27,7 +35,7 @@ var tests = [{
     hash: '0xd4e56740f876aef8c010b86a40d5f56745a118d0906a34e69aec8c0db1cb8fa3',
     id: 42,
     result: 'private'
-}]
+}];
 
 describe('getNetworkType', function () {
     tests.forEach(function (test) {
