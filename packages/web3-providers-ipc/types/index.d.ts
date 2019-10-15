68ecb1537a9556d1b5b539d847784187aae2c4a1
Josh Stevens <joshstevens19@hotmail.co.uk>
2019-10-15 14:34:48 +0100

    feat: typings for web3-providers-ipc

---
+++ packages/web3-providers-ipc/types/index.d.ts
@@ -0,0 +1,66 @@
+import * as net from 'net';
+
+/*
+    This file is part of web3.js.
+
+    web3.js is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Lesser General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    web3.js is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Lesser General Public License for more details.
+
+    You should have received a copy of the GNU Lesser General Public License
+    along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+/**
+ * @file http-provider-test.ts
+ * @author Josh Stevens <joshstevens19@hotmail.co.uk>
+ * @date 2018
+ */
+export class AbstractSocketProvider {
+    constructor(connection: any, timeout?: number);
+
+    host: string;
+    connected: boolean;
+
+    supportsSubscriptions(): boolean;
+
+    registerEventListeners(): void;
+
+    send(method: string, parameters: any[]): Promise<any>;
+
+    sendBatch(methods: any[], moduleInstance: any): Promise<any[]>;
+
+    subscribe(
+        subscribeMethod: string,
+        subscriptionMethod: string,
+        parameters: any[]
+    ): Promise<string>;
+
+    unsubscribe(
+        subscriptionId: string,
+        unsubscribeMethod: string
+    ): Promise<boolean>;
+
+    clearSubscriptions(unsubscribeMethod: string): Promise<boolean>;
+
+    on(type: string, callback: () => void): void;
+
+    removeListener(type: string, callback: () => void): void;
+
+    removeAllListeners(type: string): void;
+
+    reset(): void;
+
+    reconnect(): void;
+
+    disconnect(code: number, reason: string): void;
+}
+
+export class IpcProvider extends AbstractSocketProvider {
+    constructor(path: string, net: net.Server);
+}
