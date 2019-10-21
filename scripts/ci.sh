6d3ef0c6df5900f3a8e3bbdd2ad719fa90ed133d
cgewecke <christophergewecke@gmail.com>
2019-10-21 14:35:00 -0700

    Move build/lint to their own CI job

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -9,13 +9,15 @@ set -o errexit
 
 if [ "$TEST" = "unit" ]; then
 
-  npm run build
   npm run test:unit
+
+if [ "$TEST" = "build_and_lint" ]; then
+
+  npm run build
   npm run dtslint
 
 elif [ "$TEST" = "unit_and_e2e_clients" ]; then
 
-  npm run build
   npm run test:e2e:ganache
   npm run test:e2e:geth:insta
   npm run test:e2e:geth:auto
