93feb45de022a1fe44fb9fdd212180cfc7c588c5
cgewecke <christophergewecke@gmail.com>
2019-10-30 20:58:00 -0700

    Add possible causes to error message

--- packages/web3-eth-abi/src/index.js
+++ packages/web3-eth-abi/src/index.js
@@ -225,8 +225,10 @@ ABICoder.prototype.decodeParameters = function (outputs, bytes) {
     if (outputs.length > 0 && (!bytes || bytes === '0x' || bytes === '0X')) {
         throw new Error(
             'Returned values aren\'t valid, did it run Out of Gas? ' +
-            'Are you using the correct ABI for the contract you are ' +
-            'retrieving data from?'
+            'You might also see this error if you are not using the ' +
+            'correct ABI for the contract you are retrieving data from, ' +
+            'requesting data from a block number that does not exist, ' +
+            'or querying a node which is not fully synced.'
         );
     }
 
