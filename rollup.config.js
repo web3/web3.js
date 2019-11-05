96b3102e4ca95d0cf4c463942151dd90be3cc8a4
nivida <samuel@ethereum.org>
2019-11-05 11:08:33 +0100

    rollup.config.js duplicated babel configs removed

--- rollup.config.js
+++ rollup.config.js
@@ -1,131 +1,13 @@
+import commonjs from 'rollup-plugin-commonjs';
 import babel from 'rollup-plugin-babel';
 import json from 'rollup-plugin-json';
 import autoExternal from 'rollup-plugin-auto-external';
 import cleanup from 'rollup-plugin-cleanup';
-import commonjs from 'rollup-plugin-commonjs';
-import resolve from 'rollup-plugin-node-resolve';
-import builtins from 'rollup-plugin-node-builtins';
 import nodeGlobals from 'rollup-plugin-node-globals';
+import builtins from 'rollup-plugin-node-builtins';
 import {terser} from 'rollup-plugin-terser';
 import bundleSize from 'rollup-plugin-bundle-size';
-
-const config = [
-    {
-        input: 'src/index.js',
-        output: [
-            {
-                file: '',
-                format: 'cjs',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            commonjs(),
-            babel({
-                exclude: 'node_modules/**',
-                babelrc: false,
-                runtimeHelpers: true,
-                presets: [
-                    [
-                        '@babel/preset-env',
-                        {
-                            modules: false,
-                            targets: {
-                                node: '8'
-                            }
-                        }
-                    ]
-                ],
-                plugins: [
-                    ['@babel/plugin-transform-runtime', {useESModules: true}],
-                    'istanbul'
-                ]
-            }),
-            json(),
-            autoExternal(),
-            cleanup()
-        ]
-    },
-    {
-        input: 'src/index.js',
-        output: [
-            {
-                file: '',
-                format: 'es',
-                sourcemap: true
-            }
-        ],
-        plugins: [
-            commonjs(),
-            babel({
-                exclude: 'node_modules/**',
-                babelrc: false,
-                runtimeHelpers: true,
-                presets: [
-                    [
-                        '@babel/preset-env',
-                        {
-                            modules: false,
-                            targets: {
-                                node: '8'
-                            }
-                        }
-                    ]
-                ],
-                plugins: [
-                    ['@babel/plugin-transform-runtime', {useESModules: true}]
-                ]
-            }),
-            json(),
-            autoExternal(),
-            cleanup()
-        ]
-    },
-    {
-        input: 'src/index.js',
-        output: [
-            {
-                exports: 'default',
-                name: '',
-                file: '',
-                format: 'umd',
-                sourcemap: true,
-                globals: {}
-            }
-        ],
-        plugins: [
-            commonjs(),
-            babel(
-                {
-                    exclude: ['node_modules/@babel/runtime/**'],
-                    babelrc: false,
-                    runtimeHelpers: true,
-                    minified: true,
-                    presets: [
-                        [
-                            '@babel/preset-env',
-                            {
-                                forceAllTransforms: true,
-                                modules: false,
-                                targets: {
-                                    browsers: 'last 2 versions'
-                                }
-                            }
-                        ]
-                    ],
-                    plugins: [
-                        ['@babel/plugin-transform-runtime', {useESModules: true, absoluteRuntime: true}]
-                    ]
-                }
-            ),
-            nodeGlobals(),
-            json(),
-            builtins(),
-            terser({sourcemap: true}),
-            bundleSize()
-        ]
-    }
-];
+import resolve from 'path';
 
 /**
  * Returns the rollup configuration with globals and names set.
@@ -139,46 +21,229 @@ const config = [
  * @returns {Array}
  */
 export default (name, outputFileName, globals, dedupe, namedExports) => {
-    var mappedConfig = [];
+    let mappedConfig = [];
 
     // CJS
     if (process.env.CJS === 'true') {
-        config[0].output[0].file = 'dist/' + outputFileName + '.cjs.js';
-
-        if (namedExports) {
-            config[0].output[0].exports = 'named';
-        }
-
-        mappedConfig.push(config[0]);
+        mappedConfig.push(
+            rollupConfig(
+                'src/index.js',
+                'dist/' + outputFileName + '.cjs.js',
+                name,
+                'cjs',
+                namedExports ? 'named' : 'auto',
+                {},
+                {
+                    exclude: 'node_modules/**',
+                    targets: {
+                        node: '8'
+                    }
+                }
+            )
+        );
     }
 
     // ESM
     if (process.env.ESM === 'true') {
-        config[1].output[0].file = 'dist/' + outputFileName + '.esm.js';
-
-        mappedConfig.push(config[1]);
+        mappedConfig.push(
+            rollupConfig(
+                'src/index.js',
+                'dist/' + outputFileName + '.esm.js',
+                name,
+                'es',
+                'auto',
+                {},
+                {
+                    exclude: 'node_modules/**',
+                    targets: {
+                        node: '8'
+                    }
+                }
+            )
+        );
     }
 
     // Minified UMD
     if (process.env.MINIFIED_BUNDLES === 'true') {
-        if (namedExports) {
-            config[2].output[0].exports = 'named';
-        }
-
-        config[2].output[0].name = name;
-        config[2].output[0].file = 'dist/' + outputFileName + '.min.js';
-        config[2].output[0].globals = globals;
-
-        config[2].plugins = [
-            resolve({
-                browser: true,
-                preferBuiltins: true,
-                dedupe: dedupe
-            })
-        ].concat(config[2].plugins);
-
-        mappedConfig.push(config[2]);
+        mappedConfig.push(
+            rollupConfig(
+                'src/index.js',
+                'dist/' + outputFileName + '.min.js',
+                name,
+                'min',
+                namedExports ? 'named' : 'auto',
+                globals,
+                {
+                    forceAllTransforms: true,
+                    exclude: 'node_modules/@babel/runtime/**',
+                    minified: true,
+                    targets: {
+                        browsers: 'last 2 versions'
+                    },
+                    transformRuntimeOptions: {absoluteRuntime: true}
+                },
+                {
+                    dedupe: dedupe
+                }
+            )
+        );
     }
 
     return mappedConfig;
 };
+
+/**
+ * Generates and returns a rollup config item
+ *
+ * @method rollupConfig
+ *
+ * @param {string} input
+ * @param {string} outputFile
+ * @param {string} outputName
+ * @param {string} outputType
+ * @param {string} exports
+ * @param {object} globals
+ * @param {object} babelOptions
+ * @param {object} resolverOptions
+ *
+ * @returns {{output: ({file, sourcemap, exports, globals, name, format}|{file, sourcemap, exports, format}), input: *, plugins: *[]}}
+ */
+function rollupConfig(input, outputFile, outputName, outputType, exports = 'auto', globals = {}, babelOptions = {}, resolverOptions = {}) {
+    return {
+        input: input,
+        output: getOutput(outputFile, outputType, outputName, exports, globals),
+        plugins: getPlugins(outputType, babelOptions, resolverOptions)
+    };
+}
+
+/**
+ * Returns the correct output configuration
+ *
+ * @method getOutput
+ *
+ * @param {string} file
+ * @param {string} type
+ * @param {string} name
+ * @param {string} exports
+ * @param {object} globals
+ *
+ * @returns {{file: *, sourcemap: boolean, exports, format: *}|{file: *, sourcemap: boolean, exports: string, globals: *, name: *, format: string}}
+ */
+function getOutput(file, type, name, exports, globals) {
+    switch (type) {
+        case 'min':
+            return {
+                exports: 'default',
+                name: name,
+                file: file,
+                format: 'umd',
+                sourcemap: true,
+                globals: globals
+            };
+        case 'es':
+        case 'cjs':
+            return {
+                exports: exports,
+                name: name,
+                file: file,
+                format: type,
+                sourcemap: true
+            };
+    }
+}
+
+/**
+ * Returns the correct plugins array
+ *
+ * @method getPlugins
+ *
+ * @param {string} type
+ * @param {object} babelOptions
+ * @param {object} resolverOptions
+ *
+ * @returns {Array}
+ */
+function getPlugins(type, babelOptions, resolverOptions) {
+    const babelPlugin = babel(
+        getBabelConfig(
+            babelOptions.exclude,
+            babelOptions.targets,
+            babelOptions.forceAllTransforms,
+            babelOptions.transformRuntimeOptions,
+            babelOptions.minified
+        )
+    );
+
+    switch (type) {
+        case 'min':
+            return [
+                resolve(
+                    {
+                        browser: true,
+                        preferBuiltins: true
+                    }.assign(resolverOptions)
+                ),
+                commonjs(),
+                babelPlugin,
+                nodeGlobals(),
+                json(),
+                builtins(),
+                terser({sourcemap: true}),
+                bundleSize()
+            ];
+        case 'cjs':
+        case 'es':
+            return [
+                commonjs(),
+                babelPlugin,
+                json(),
+                autoExternal(),
+                cleanup()
+            ];
+    }
+}
+
+
+/**
+ * Returns the correct babel config
+ *
+ * @method getBabelConfig
+ *
+ * @param {Array|string} exclude
+ * @param {object} targets
+ * @param {boolean} forceAllTransforms
+ * @param {object} transformRuntimeOptions
+ * @param {boolean} minified
+ *
+ * @returns {{minified: boolean, presets: *[][], babelrc: boolean, plugins: *[][], runtimeHelpers: boolean, exclude: *}}
+ */
+function getBabelConfig(exclude, targets, forceAllTransforms = false, transformRuntimeOptions = {}, minified = false) {
+    let plugins = [
+        [
+            '@babel/plugin-transform-runtime',
+            Object.assign({useESModules: true}, transformRuntimeOptions)
+        ]
+    ];
+
+    if (process.env.TEST) {
+        plugins.push('istanbul');
+    }
+
+    return {
+        exclude: exclude,
+        babelrc: false,
+        runtimeHelpers: true,
+        minified: minified,
+        presets: [
+            [
+                '@babel/preset-env',
+                {
+                    forceAllTransforms: forceAllTransforms,
+                    modules: false,
+                    targets: targets
+                }
+            ]
+        ],
+        plugins: plugins
+    };
+}
