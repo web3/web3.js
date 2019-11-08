1fbea049c4d98d830dea6bcb4f05021db71ee4f0
nivida <samuel@ethereum.org>
2019-11-08 16:35:56 +0100

    parse-url.js -> helpers.js renamed and btoa export added

--- packages/web3-providers-ws/src/helpers.js
+++ packages/web3-providers-ws/src/helpers.js
@@ -1,7 +1,7 @@
 var isNode = Object.prototype.toString.call(typeof process !== 'undefined' ? process : 0) === '[object process]';
 
 var _btoa = null;
-var parseURL = null;
+var helpers = null;
 if (isNode) {
     _btoa = function(str) {
         return Buffer.from(str).toString('base64');
@@ -10,18 +10,21 @@ if (isNode) {
     if (url.URL) {
         // Use the new Node 6+ API for parsing URLs that supports username/password
         var newURL = url.URL;
-        parseURL = function(url) {
+        helpers = function(url) {
             return new newURL(url);
         };
     } else {
         // Web3 supports Node.js 5, so fall back to the legacy URL API if necessary
-        parseURL = require('url').parse;
+        helpers = require('url').parse;
     }
 } else {
     _btoa = btoa;
-    parseURL = function(url) {
+    helpers = function(url) {
         return new URL(url);
     };
 }
 
-module.exports = parseURL;
+module.exports = {
+    parseURL: helpers,
+    btoa: _btoa
+};
