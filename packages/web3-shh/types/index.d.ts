8fbc75eeceb3e4799fa69adcd14a33986e62b5e0
nivida <samuel@ethereum.org>
2019-11-12 19:10:49 +0100

    constructor type simplified in web3-shh

--- packages/web3-shh/types/index.d.ts
+++ packages/web3-shh/types/index.d.ts
@@ -22,9 +22,7 @@ import { BatchRequest, provider, Providers, Extension } from 'web3-core';
 import { Network } from 'web3-net';
 
 export class Shh {
-    constructor(provider: provider);
-    constructor(provider: provider, net: net.Socket);
-    constructor();
+    constructor(provider: provider, net?: net.Socket);
 
     readonly givenProvider: any;
     static readonly givenProvider: any;
