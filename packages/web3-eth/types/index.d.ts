30b2dc8d03c8da8bfea67a4f49f9e4f455c99407
nivida <samuel@ethereum.org>
2019-11-12 12:00:07 +0100

    eth and eth-contract types updated

--- packages/web3-eth/types/index.d.ts
+++ packages/web3-eth/types/index.d.ts
@@ -34,7 +34,9 @@ import {
     Common,
     hardfork,
     chain,
-    BlockNumber
+    BlockNumber,
+    LogsOptions,
+    PastLogsOptions
 } from 'web3-core';
 import { Subscription } from 'web3-core-subscriptions';
 import { AbiCoder } from 'web3-eth-abi';
@@ -396,16 +398,6 @@ export interface BlockTransactionObject extends BlockTransactionBase {
 
 export interface BlockTransactionString extends BlockTransactionBase {
     transactions: string[];
-}
-
-export interface PastLogsOptions extends LogsOptions {
-    toBlock?: BlockNumber;
-}
-
-export interface LogsOptions {
-    fromBlock?: BlockNumber;
-    address?: string | string[];
-    topics?: Array<string | string[] | null>;
 }
 
 export interface GetProof {
