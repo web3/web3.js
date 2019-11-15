8b7f6b6db967b5b7e95e9ee099d861bc94a9a459
cgewecke <christophergewecke@gmail.com>
2019-11-14 22:44:08 -0800

    Remove log line

--- packages/web3-core-helpers/src/formatters.js
+++ packages/web3-core-helpers/src/formatters.js
@@ -277,7 +277,6 @@ var outputTransactionReceiptFormatter = function (receipt) {
         receipt.status = Boolean(parseInt(receipt.status));
     }
 
-    console.log('In web3 1.2.4!!')
     return receipt;
 };
 
