27a5e7f4faa5443f47220ef0e1548ed794fe2a7c
Lautaro Dragan <lautaro.dragan@gmail.com>
2019-11-18 23:59:53 -0300

    types(web3-core-helpers): fix disconnect args
    This commit makes WebsocketProviderBase.disconnect arguments optional in the type declaration file, since they have a default value in the JavaScript implementation for the 2.x branch and the arguments aren't even present in the 1.x branch.

--- packages/web3-core-helpers/types/index.d.ts
+++ packages/web3-core-helpers/types/index.d.ts
@@ -97,7 +97,7 @@ export class WebsocketProviderBase {
 
     reset(): void;
 
-    disconnect(code: number, reason: string): void;
+    disconnect(code?: number, reason?: string): void;
 }
 
 export class IpcProviderBase {
