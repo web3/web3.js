ea311c34244a57705bc02e0aeb4b7497baed5f04
nivida <samuel@ethereum.org>
2019-11-20 13:06:47 +0100

    test case descriptions updated

--- test/utils.soliditySha3Raw.js
+++ test/utils.soliditySha3Raw.js
@@ -3,7 +3,7 @@ var assert = chai.assert;
 var soliditySha3Raw = require('../packages/web3-utils').soliditySha3Raw;
 
 describe('web3.soliditySha3Raw', function () {
-    it('should return the sha3 hash of null with hex prefix', function() {
+    it('should return the sha3 hash with hex prefix', function() {
         assert.deepEqual(soliditySha3Raw({t: 'string', v: ''}), '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470');
     });
 });
