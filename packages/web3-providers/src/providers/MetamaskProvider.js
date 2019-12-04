dc4f6a0eb3c70ced63d150725b52e96254e63dcc
Jack Huang <hxc20060903@gmail.com>
2019-12-04 15:09:52 +0900

    temporary fix for batchRequest

--- packages/web3-providers/src/providers/MetamaskProvider.js
+++ packages/web3-providers/src/providers/MetamaskProvider.js
@@ -139,8 +139,11 @@ export default class MetamaskProvider extends AbstractSocketProvider {
      * @returns {Promise<any>}
      */
     sendPayload(payload) {
+        const method = Array.isArray(payload)
+            ? 'sendAsync'
+            : 'send';
         return new Promise((resolve, reject) => {
-            this.connection.send(payload, (error, response) => {
+            this.connection[method](payload, (error, response) => {
                 if (!error) {
                     return resolve(response);
                 }
