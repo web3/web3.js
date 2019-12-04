114e5ce588059cd2a17d1bdcbbe94a00a29cde9c
nivida <samuel@ethereum.org>
2019-12-04 15:45:41 +0100

    index files for each module and custom resolver rules are required to keep the imports clean and the module interface visible

---
+++ modules/core/index.js
@@ -0,0 +1,4 @@
+// TODO: Export the interface of a module through a index.js in the root folder and use custom resolver rules to simplify
+// TODO: the import statement. (e.g.: import Method from 'core')
+
+
