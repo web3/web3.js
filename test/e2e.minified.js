775fa15fcb2f19fa06d63d7f7360d7174df8f187
cgewecke <christophergewecke@gmail.com>
2019-12-09 01:59:35 -0800

    Add it block to have one test

--- test/e2e.minified.js
+++ test/e2e.minified.js
@@ -3,7 +3,9 @@ describe('window.Web3 is defined', function () {
 
   // Referencing `process` throws w/out browserify.
   // We want to be skipped during the browserified units tests here.
-  try { if (process) return } catch (e) {}
+  it("loads", function(){
+    try { if (process) return } catch (e) {}
 
-  if (!window.Web3) throw new Error('"window.Web3" was not defined');
+    if (!window.Web3) throw new Error('"window.Web3" was not defined');
+  });
 })
