2c94241cc89e0d0ef6f3f4e7c5193c61a65f30bb
nivida <samuel@ethereum.org>
2019-12-17 15:29:08 +0100

    build scripts in package.json updated and package creation in scripts/publish.local.sh fixed

--- scripts/publish.local.sh
+++ scripts/publish.local.sh
@@ -20,32 +20,53 @@ npm-auth-to-token \
   -e test@test.com \
   -r http://localhost:4873
 
-# `tsc`
-npm run build
+# --------------------------------------------------------------------
+# Prepare package folder
+# --------------------------------------------------------------------
 
 # Creates temporary package folder
+rm -rf ./package
 mkdir package
 
-# Copy internal dist files to package
-cp -r dist/internal package/
+# Copy internal files to package
+cp -r ./internal ./package/
 
-# Copy public dist files to package
-cp -r dist/public/* package/
+# Copy public files to package
+cp -r ./public/* ./package/
 
-# Copy package.json to package
-cp ./package.json package/package.json
+# Copy `package.json` to package
+cp ./package.json ./package/package.json
 
-# Copy README.md to package
-cp ./README.md package/README.md
+# Copy `README.md` to package
+cp ./README.md ./package/README.md
+
+# Copy `tsconfig.prod.json`
+cp ./tsconfig.prod.json ./package/
 
 # Go to package directory
 cd ./package
+
+# Build package
+npm run build:prod
+
+# Remove not required files
+rm -rf ./config
+rm -rf ./core
+rm -rf ./ethereum
+rm -rf ./internal
+rm -rf ./index.js
+rm -rf ./tsconfig.prod.json
+
+# Move `dist` files to root folder and delete the `dist` folder
+mv ./dist/package/* ./
+rm -rf ./dist
+
+# --------------------------------------------------------------------
+# Publish package
+# --------------------------------------------------------------------
 
 # Unpublish the local web3 package
 npm unpublish web3@$(node -p "require('./package.json').version") --registry=http://localhost:4873
 
 # Publish package to local verdaccio registry
 npm publish --registry=http://localhost:4873
-
-# Remove temporary package folder
-rm -rf ./package
