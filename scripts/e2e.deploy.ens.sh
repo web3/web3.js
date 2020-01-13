7772d94a589a0d384d40748f1928f570fe59041f
nivida <samuel@ethereum.org>
2020-01-13 12:20:07 +0100

    e2e.ganache.start removed and e2e.deploy.ens shell script updated

--- scripts/e2e.deploy.ens.sh
+++ scripts/e2e.deploy.ens.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env bash
 
+# Exit immediately on error
+set -o errexit
+
+# -----------------------
+# Set up truffle project
+# -----------------------
+
 # Install truffle and the required ENS npm packages
 npm install truffle
 npm install @ensdomains/ens
@@ -18,13 +25,39 @@ cp ./scripts/js/ens/1_initial_migration.js ./migrations/
 rm -rf ./truffle-config.js
 cp ./scripts/js/ens/truffle-config.js ./
 
+# --------------
 # Start ganache
-sh ./scripts/e2e.ganache.start.sh
+# --------------
 
+# Run cleanup on exit
+trap cleanup EXIT
+
+cleanup() {
+  if [ -n "$client" ]; then
+    kill -9 $client
+  fi
+}
+
+# Launch ganache, track its PID and wait until port is open
+npx ganache-cli --noVMErrorsOnRPCResponse > /dev/null &
+client=$!
+npx wait-port 8545
+
+echo " "
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo "Ganache started                          "
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
+echo " "
+
+# ------------------
 # Migrate contracts
+# ------------------
+
 truffle migrate
 
-# Start ENS e2e tests
+# --------------------
+# Start ENS e2e Tests
+# --------------------
 GANACHE=true nyc --no-clean --silent _mocha -- \
   --reporter spec \
   --fgrep 'ENS [ @E2E ]'
