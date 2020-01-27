5bec023432026e4c391f5594f5e697f088cede6c
nivida <samuel@ethereum.org>
2020-01-27 07:48:56 +0100

    formatters.inputTransactionFormatter extended

--- test/formatters.inputTransactionFormatter.js
+++ test/formatters.inputTransactionFormatter.js
@@ -83,6 +83,21 @@ var tests = [{
         gas: '0x3e8',
         gasPrice: '0x3e8'
     }
+}, {
+    input: {
+        data: '34234bf23bf4234',
+        value: '100',
+        from: 'XE7338O073KYGTWWZN0F2WZ0R8PX5ZPPZS',
+        gas: '1000',
+        gasPrice: '1000'
+    },
+    result: {
+        data: '0x34234bf23bf4234',
+        value: '0x64',
+        from: '0x00c5496aee77c1ba1f0854206a26dda82a81d6d8',
+        gas: '0x3e8',
+        gasPrice: '0x3e8'
+    }
 }];
 
 describe('formatters', function () {
