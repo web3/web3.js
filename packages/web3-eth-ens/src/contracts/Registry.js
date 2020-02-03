cc4702f7fa812c73a379fdaf1ac8d5f80fa4827a
eyalron33 <eyalron33@gmail.com>
2020-02-03 11:01:19 +0100

    update the ENS registry address
    Update the ENS registry address based on [ENS registry update](https://medium.com/the-ethereum-name-service/ens-registry-migration-bug-fix-new-features-64379193a5a)

--- packages/web3-eth-ens/src/contracts/Registry.js
+++ packages/web3-eth-ens/src/contracts/Registry.js
@@ -359,9 +359,9 @@ export default class Registry extends AbstractContract {
      */
     async checkNetwork() {
         const ensAddresses = {
-            main: '0x314159265dD8dbb310642f98f50C066173C1259b',
-            ropsten: '0x112234455c3a32fd11230c42e7bccd4a84e02010',
-            rinkeby: '0xe7410170f87102df0055eb195163a03b7f2bff4a'
+            main: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+            ropsten: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+            rinkeby: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
         };
 
         const block = await this.net.getBlockByNumber('latest', false);
