580ed303173dd6d87b84f6780de1bf466cc41aa3
Manuel Wedler <34456797+manuelwedler@users.noreply.github.com>
2020-02-06 11:41:13 +0100

    Adds the ENS registry for goerli testnet

--- packages/web3-eth-ens/src/contracts/Registry.js
+++ packages/web3-eth-ens/src/contracts/Registry.js
@@ -361,7 +361,8 @@ export default class Registry extends AbstractContract {
         const ensAddresses = {
             main: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
             ropsten: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
-            rinkeby: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
+            rinkeby: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e',
+            goerli: '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e'
         };
 
         const block = await this.net.getBlockByNumber('latest', false);
