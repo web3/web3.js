599dab5a6aa6a2fd4cb8824db143852f523e6fa9
Ryan Ghods <ryan@ryanio.com>
2020-04-09 17:10:37 -0700

    Upgrade devDeps to resolve security warnings (dtslint, definitelytyped-header-parser)

--- packages/web3-providers-ws/types/index.d.ts
+++ packages/web3-providers-ws/types/index.d.ts
@@ -20,6 +20,8 @@
  * @date 2018
  */
 
+// Minimum TypeScript Version: 3.0
+
 import { WebsocketProviderBase } from 'web3-core-helpers';
 
 export class WebsocketProvider extends WebsocketProviderBase { }
