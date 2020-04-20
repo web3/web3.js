27e06422c51b4ebb16e0a13c44bbf45118c781f9
viswanathkgp12 <viswanath.iit@gmail.com>
2020-04-21 00:40:59 +0530

    updated types for web3 http provider #3335

--- packages/web3-providers-http/types/index.d.ts
+++ packages/web3-providers-http/types/index.d.ts
@@ -19,7 +19,48 @@
  * @author Josh Stevens <joshstevens19@hotmail.co.uk>
  * @date 2018
  */
+import * as http from "http";
+import * as https from "https";
 
-import { HttpProviderBase } from 'web3-core-helpers';
+import { HttpProviderBase, JsonRpcResponse } from "web3-core-helpers";
 
-export class HttpProvider extends HttpProviderBase {}
+export interface HttpHeader {
+    name: string;
+    value: string;
+}
+
+export interface HttpProviderAgent {
+    baseUrl?: string;
+    http?: http.Agent;
+    https?: https.Agent;
+}
+
+export interface HttpProviderOptions {
+    withCredentials?: boolean;
+    timeout?: number;
+    headers?: HttpHeader[];
+    agent?: HttpProviderAgent;
+    keepAlive?: boolean;
+}
+
+export class HttpProvider extends HttpProviderBase {
+    host: string;
+
+    withCredentials: boolean;
+    timeout: number;
+    headers?: HttpHeader[];
+    agent?: HttpProviderAgent;
+    connected: boolean;
+
+    constructor(host?: string, options?: HttpProviderOptions);
+
+    send(
+        payload: object,
+        callback?: (
+            error: Error | null,
+            result: JsonRpcResponse | undefined
+        ) => void
+    ): void;
+    disconnect(): boolean;
+    supportsSubscriptions(): boolean;
+}
