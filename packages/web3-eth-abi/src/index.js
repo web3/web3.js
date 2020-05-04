3c62ac0bf3a623aded35d0f55b3e7e66aea14b2b
Ryan Ghods <ryan@ryanio.com>
2020-05-04 12:13:09 -0700

    Handle `Buffer` in encodeParameters and add test

--- packages/web3-eth-abi/src/index.js
+++ packages/web3-eth-abi/src/index.js
@@ -21,6 +21,7 @@
  * @date 2018
  */
 
+var Buffer = require('buffer').Buffer;
 var _ = require('underscore');
 var utils = require('web3-utils');
 
@@ -99,6 +100,9 @@ ABICoder.prototype.encodeParameter = function (type, param) {
  */
 ABICoder.prototype.encodeParameters = function (types, params) {
     const bytesToEvenLength = (param) => {
+        if (Buffer.isBuffer(param)) {
+            param = utils.toHex(param)
+        }
         // bitwise AND operator returns true if odd
         if (param.length & 1) { 
             return '0x0' + param.substring(2)
@@ -106,6 +110,9 @@ ABICoder.prototype.encodeParameters = function (types, params) {
         return param
     }
     const bytes32ToFixedLength = (param) => {
+        if (Buffer.isBuffer(param)) {
+            param = utils.toHex(param)
+        }
         if (param.length < 66) { // 66 includes `0x`
             return utils.rightPad(param, 64)
         }
@@ -116,7 +123,7 @@ ABICoder.prototype.encodeParameters = function (types, params) {
         this.mapTypes(types),
         params.map(function (param, index) {
             let type = types[index]
-            if (typeof type === 'object') {
+            if (typeof type === 'object' && type.type) {
                 // We may get a named type of shape {name, type}
                 type = type.type
             }
@@ -126,19 +133,21 @@ ABICoder.prototype.encodeParameters = function (types, params) {
                 return param.toString(10);
             }
 
+            // Convert odd-length bytes to even
             if (type === 'bytes') {
                 param = bytesToEvenLength(param)
             } else if (type === 'bytes[]') {
                 param = param.map(p => bytesToEvenLength(p))
             }
 
+            // Convert bytes32 to fixed length
             if (type === 'bytes32') {
                 param = bytes32ToFixedLength(param)
             } else if (type === 'bytes32[]') {
                 param = param.map(p => bytes32ToFixedLength(p))
             }
 
-            // Format tuples for above bytes and bytes32 rules
+            // Format tuples for above rules
             if (typeof type === 'string' && type.includes('tuple')) {
                 const coder = ethersAbiCoder._getCoder(ParamType.from(type));
                 const modifyParams = (coder, param) => {
