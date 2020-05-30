823fa78cfb6524db1eb7da5712b62ca5eb3ff7a4
toni.beverin@gmail.com <toni.beverin@gmail.com>
2020-05-30 17:48:08 +0200

    more cleanup

--- packages/web3-tolar/src/index.ts
+++ packages/web3-tolar/src/index.ts
@@ -5,7 +5,6 @@ import { TolarAccounts } from "./tolar-account/tolar-account";
 import Web3 from "web3";
 var _ = require("underscore");
 
-var core = require("web3-core");
 var helpers = require("web3-core-helpers");
 var Subscriptions = require("web3-core-subscriptions").subscriptions;
 var Method = require("web3-core-method");
@@ -14,80 +13,16 @@ var Net = require("web3-net");
 var formatter = helpers.formatters;
 
 export class Tolar {
-    private requestManager: any;
     defaultAccount: string | number = null;
     defaultBlock: string | number = "latest";
 
     public net = new TolarNet(this.web3);
-    public accounts = new TolarAccounts(this.web3);
+    public account = new TolarAccounts(this.web3);
     methods: Method[];
     constructor(private web3: Web3) {
         this.registerMethods();
-        //console.log(this.web3.currentProvider.host);
-        //this.checkProviders();
     }
 
-    checkProviders() {
-        console.log(this.web3);
-    }
-
-    async testAsync(fail = false) {
-        //return this.web3.eth
-        /*
-        try {
-            let res: AxiosResponse<any> = await axios.post(
-                `${this.web3.currentProvider.host}/Client`,
-                {
-                    jsonrpc: "2.0",
-                    id: "curltext",
-                    method: "GetTransactionList",
-                    params: {
-                        addresses: [
-                            "5484c512b1cf3d45e7506a772b7358375acc571b2930d27deb",
-                        ],
-                        limit: "5",
-                        skip: "0",
-                    },
-                },
-                { headers: { "content-type": "text/plain" } }
-            );
-            console.log("Status:", res.status, "\n", "Data:\n", res.data);
-        } catch (e) {
-            console.log(e);
-            //throw e;
-        }*/
-        /*
-        await this.web3._requestManager.provider.send(
-            { jsonrpc: "2.0", id: "curltext", method: "PeerCount", params: [] },
-            async (a: any, b: any, c: any) => {
-                console.log(a, b, c);
-                return a;
-            }
-        );
-        return true;*/
-    }
-
-    setRequestManager(manager: any) {
-        console.log("test");
-        console.log(
-            "manager\n",
-            Object.keys(manager.provider.httpsAgent.protocol)
-        );
-        console.log("manager\n");
-        this.requestManager = manager;
-    }
-    setProvider() {}
-
-    extendWeb3() {
-        this.web3.extend({
-            property: "tolar",
-            methods: this.methods,
-        });
-
-        /* this.web3.extend({
-            methods: this.methods,
-        });*/
-    }
     extendTolar() {
         _.each(this.methods, (method: any) => {
             method.attachToObject(this);
@@ -190,34 +125,8 @@ export class Tolar {
 
     registerMethods() {
         this.setMethods();
-        //this.addDefaults();
+        this.addDefaults();
         this.extendTolar();
-        // this.attachToObject();
-    }
-
-    attachToObject() {
-        this.methods.forEach((method) => {
-            Object.assign(this, {
-                [method.name]: function (...args: any) {
-                    return this.web3._requestManager.provider.send(
-                        {
-                            jsonrpc: "2.0",
-                            id: "curltext",
-                            method: method.call,
-                            params: args,
-                        },
-                        async (error: any, result: any) => {
-                            console.log(error, result);
-                            return result;
-                        }
-                    );
-                },
-            });
-        });
-
-        /*this.methods.forEach((method: Method) => {
-            Object.assign(this, { [method.name]: method });
-        });*/
     }
 }
 
