c5726b4af2c7cfe69cabef9a0e307bea5bb53a64
Ignacio Mazzara <nachomazzara@gmail.com>
2020-06-04 14:52:11 -0300

    feat: remove tests

--- test/e2e.contract.events.js
+++ test/e2e.contract.events.js
@@ -74,51 +74,6 @@ describe('contract.events [ @E2E ]', function() {
         });
     });
 
-
-    it('errors when toBlock is passed to contract.events.<eventName>', function () {
-        return new Promise(async resolve => {
-            try {
-                instance
-                    .events
-                    .BasicEvent({
-                        fromBlock: 0,
-                        toBlock: 'latest'
-                    })
-
-                await instance
-                    .methods
-                    .firesEvent(accounts[0], 1)
-                    .send({ from: accounts[0] });
-            } catch (err) {
-                assert(err.message === 'Invalid option: toBlock. Use getPastEvents for specific range.');
-                resolve();
-                done();
-            }
-        });
-    });
-
-    it('errors when toBlock is passed to contract.events.allEvents', function () {
-        return new Promise(async (resolve, reject) => {
-            try {
-                instance
-                    .events
-                    .allEvents({
-                        fromBlock: 0,
-                        toBlock: 'latest'
-                    })
-
-                await instance
-                    .methods
-                    .firesEvent(accounts[0], 1)
-                    .send({ from: accounts[0] });
-            } catch (err) {
-                assert(err.message === 'Invalid option: toBlock. Use getPastEvents for specific range.');
-                resolve();
-                done();
-            }
-        });
-    });
-
     it('should not hear the error handler when connection.closed() called', function(){
         this.timeout(15000);
 
