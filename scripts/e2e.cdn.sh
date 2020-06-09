bf01d4b1f574079082f81ffe91b5fa93567e4c1e
Ryan Ghods <ryan@ryanio.com>
2020-06-08 17:41:02 -0700

    use env variables available from netlify

--- scripts/e2e.cdn.sh
+++ scripts/e2e.cdn.sh
@@ -19,7 +19,7 @@ echo "Preparing test site for web3-staging.netlify.app                      "
 echo "======================================================================"
 
 # Mark file with current commit and branch
-sed -i "s|__COMMIT_HASH__|$GITHUB_SHA|g" scripts/html/index.html
-sed -i "s|__BRANCH__|$GITHUB_REF|g" scripts/html/index.html
+sed -i "s|__COMMIT_HASH__|$COMMIT_REF|g" scripts/html/index.html
+sed -i "s|__BRANCH__|$BRANCH|g" scripts/html/index.html
 
 cp packages/web3/dist/web3.min.js scripts/html
