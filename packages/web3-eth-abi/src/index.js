61b982e2bf08b79afcd0046faa3cdbfa7903f141
cgewecke <christophergewecke@gmail.com>
2020-06-15 12:38:32 -0700

    Remap Function param types to bytes24

--- packages/web3-eth-abi/src/index.js
+++ packages/web3-eth-abi/src/index.js
@@ -156,6 +156,12 @@ ABICoder.prototype.mapTypes = function (types) {
     var self = this;
     var mappedTypes = [];
     types.forEach(function (type) {
+        // Remap `function` type params to bytes24 since Ethers does not
+        // recognize former type. Solidity docs say `Function` is a bytes24
+        // encoding the contract address followed by the function selector hash.
+        if (typeof type === 'object' && type.type === 'function'){
+            type.type = "bytes24"
+        }
         if (self.isSimplifiedStructFormat(type)) {
             var structName = Object.keys(type)[0];
             mappedTypes.push(
@@ -285,9 +291,9 @@ ABICoder.prototype.formatParam = function (type, param) {
                 param = utils.rightPad(param, size * 2)
             }
         }
-        
+
         // format odd-length bytes to even-length
-        if (param.length % 2 === 1) { 
+        if (param.length % 2 === 1) {
             param = '0x0' + param.substring(2)
         }
     }
