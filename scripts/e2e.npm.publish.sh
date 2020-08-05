545052a3c87b112c2996ea4d8ced96cfd95e637a
Ryan Ghods <ryan@ryanio.com>
2020-08-05 09:42:36 -0700

    CI: Fix restoring empty cache (#3674)
    * add some spacing for easier reading
    
    * move build to only the steps that need it

--- scripts/e2e.npm.publish.sh
+++ scripts/e2e.npm.publish.sh
@@ -23,7 +23,9 @@ fi
 # what it needs here.
 npm install -g verdaccio@4.4.4
 npm install -g npm-auth-to-token@1.0.0
-npm install -g lerna@3.18.3
+npm install -g lerna@^3.20.2
+npm install -g typescript@^3.9.5
+npm install -g webpack@^4.44.1 webpack-cli@^3.3.12 clean-webpack-plugin@^3.0.0
 
 # Launch npm proxy registry and save pid to kill server (req. in Windows env)
 verdaccio --config verdaccio.yml &
@@ -50,7 +52,6 @@ lerna version minor \
   --force-publish=* \
   --no-git-tag-version \
   --no-push \
-  --ignore-scripts \
   --yes
 
 # Set identity prior to publishing (necessary for Windows)
