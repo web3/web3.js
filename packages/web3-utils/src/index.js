fc34684bfa0a67c685d3d422250b59c685d365b2
tclairet <tcl@blockchain-studio.com>
2020-08-25 18:03:31 +0200

    add encodePacked in web utils

--- packages/web3-utils/src/index.js
+++ packages/web3-utils/src/index.js
@@ -334,6 +334,7 @@ module.exports = {
     keccak256: utils.sha3,
     soliditySha3: soliditySha3.soliditySha3,
     soliditySha3Raw: soliditySha3.soliditySha3Raw,
+    encodePacked: soliditySha3.encodePacked,
     isAddress: utils.isAddress,
     checkAddressChecksum: utils.checkAddressChecksum,
     toChecksumAddress: toChecksumAddress,
