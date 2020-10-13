b85bb4e687ee829875b89bd585d88f6aff3ab9d2
Wyatt Barnes <me@wyatt.email>
2020-10-13 11:57:45 -0500

    add providerSuffix

--- packages/web3-eth2-beaconchain/test/constructor.test.ts
+++ packages/web3-eth2-beaconchain/test/constructor.test.ts
@@ -1,11 +1,12 @@
 import { ETH2BeaconChain } from '../src/index'
 
 const provider = 'http://127.0.0.1:9596'
+const providerSuffix = '/v1/beacon/'
 
 it('constructs a ETH2BeaconChain instance with expected properties', () => {
     const eth2BeaconChain = new ETH2BeaconChain(provider)
 
     expect(eth2BeaconChain.name).toBe('eth2-beaconchain')
-    expect(eth2BeaconChain.provider).toBe(provider)
+    expect(eth2BeaconChain.provider).toBe(`${provider}${providerSuffix}`)
     expect(eth2BeaconChain.protectProvider)
 })
