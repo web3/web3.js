481d3865e25fb94fba4d20a6ecd2366470593d3a
Wyatt Barnes <me@wyatt.email>
2020-10-13 11:54:21 -0500

    Remove testing code

--- packages/web3-eth2-beaconchain/src/index.ts
+++ packages/web3-eth2-beaconchain/src/index.ts
@@ -9,25 +9,3 @@ export class ETH2BeaconChain extends ETH2Core implements IETH2BeaconChain {
         super(provider, { protectProvider: true }, schema)
     }
 }
-
-(async () => {
-    try {
-        // const provider = 'http://54.157.182.30:3500'
-        const provider = 'http://127.0.0.1:9596'
-        const eth2BeaconChain = new ETH2BeaconChain(provider)
-
-        console.log(await eth2BeaconChain.getBlockHeader({ blockId: 'head' }))
-
-        // const attestationsParams = {
-        //     // epoch: '9946',
-        //     genesisEpoch: false,
-        //     // pageSize: 3,
-        //     // pageToken: 
-        // }
-        // const result = await eth2BeaconChain.attestations(attestationsParams)
-        // console.log(result)
-
-    } catch (error) {
-        console.log(error)
-    }
-})()
