a7bccb3f5e74d08ef1be6f40ffc5ca431e5f092f
Wyatt Barnes <me@wyatt.email>
2020-10-16 18:32:56 -0500

    Temporarily add ts-ignore to fix imports

--- packages/web3-eth2-beaconchain/src/index.ts
+++ packages/web3-eth2-beaconchain/src/index.ts
@@ -1,10 +1,12 @@
-import { ETH2Core } from '../../web3-eth2-core/src/index'
+// @ts-ignore - types not full implemented yet
+import { ETH2Core } from 'web3-eth2-core'
 import { DefaultSchema } from './schema'
 
 import { IETH2BeaconChain } from '../types/index'
-import { IBaseAPISchema } from '../../web3-eth2-core/src/schema'
-
-import { ETH2BaseOpts } from '../../web3-eth2-core/types/index'
+// @ts-ignore - types not full implemented yet
+import { IBaseAPISchema } from 'web3-eth2-core'
+// @ts-ignore - types not full implemented yet
+import { ETH2BaseOpts } from 'web3-eth2-core'
 
 // @ts-ignore - ETH2BeaconChain incorrectly implements interface IETH2BeaconChain
 export class ETH2BeaconChain extends ETH2Core implements IETH2BeaconChain {
