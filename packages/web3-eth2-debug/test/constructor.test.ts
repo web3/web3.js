9768edbccd43d94b79d64b459975ac0e742ffe2f
Wyatt Barnes <me@wyatt.email>
2020-11-20 13:24:23 -0600

    Update test message"

--- packages/web3-eth2-debug/test/constructor.test.ts
+++ packages/web3-eth2-debug/test/constructor.test.ts
@@ -3,7 +3,7 @@ import { ETH2Debug } from '../src/index'
 const provider = 'http://127.0.0.1:9596'
 const providerSuffix = '/eth/v1/debug/beacon/'
 
-it('constructs a ETH2Config instance with expected properties', () => {
+it('constructs a ETH2Debug instance with expected properties', () => {
     const eth2Debug = new ETH2Debug(provider)
 
     // @ts-ignore - types not full implemented yet
