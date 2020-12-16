78440d093b09fed069e7aaeca5fc8382b84d8094
frankiebee <frankie.diamond@gmail.com>
2020-12-16 00:18:19 -1000

    wip: in the process of moving out generated methods to clean up file

---
+++ packages/web3-eth/methods.js
@@ -0,0 +1,212 @@
+module.exports = [    {
+        name: 'getNodeInfo',
+        call: 'web3_clientVersion'
+    },
+    {
+        name: 'getProtocolVersion',
+        call: 'eth_protocolVersion',
+        params: 0
+    },
+    {
+        name: 'getCoinbase',
+        call: 'eth_coinbase',
+        params: 0
+    },
+    {
+        name: 'isMining',
+        call: 'eth_mining',
+        params: 0
+    },
+    {
+        name: 'getHashrate',
+        call: 'eth_hashrate',
+        params: 0,
+        outputFormatter: utils.hexToNumber
+    },
+    {
+        name: 'isSyncing',
+        call: 'eth_syncing',
+        params: 0,
+        outputFormatter: formatter.outputSyncingFormatter
+    },
+    {
+        name: 'getGasPrice',
+        call: 'eth_gasPrice',
+        params: 0,
+        outputFormatter: formatter.outputBigNumberFormatter
+    },
+    {
+        name: 'getAccounts',
+        call: 'eth_accounts',
+        params: 0,
+        outputFormatter: utils.toChecksumAddress
+    },
+    {
+        name: 'getBlockNumber',
+        call: 'eth_blockNumber',
+        params: 0,
+        outputFormatter: utils.hexToNumber
+    },
+    {
+        name: 'getBalance',
+        call: 'eth_getBalance',
+        params: 2,
+        inputFormatter: [formatter.inputAddressFormatter, formatter.inputDefaultBlockNumberFormatter],
+        outputFormatter: formatter.outputBigNumberFormatter
+    },
+    {
+        name: 'getStorageAt',
+        call: 'eth_getStorageAt',
+        params: 3,
+        inputFormatter: [formatter.inputAddressFormatter, utils.numberToHex, formatter.inputDefaultBlockNumberFormatter]
+    },
+    {
+        name: 'getCode',
+        call: 'eth_getCode',
+        params: 2,
+        inputFormatter: [formatter.inputAddressFormatter, formatter.inputDefaultBlockNumberFormatter]
+    },
+    {
+        name: 'getBlock',
+        call: blockCall,
+        params: 2,
+        inputFormatter: [formatter.inputBlockNumberFormatter, function (val) { return !!val; }],
+        outputFormatter: formatter.outputBlockFormatter
+    },
+    {
+        name: 'getUncle',
+        call: uncleCall,
+        params: 2,
+        inputFormatter: [formatter.inputBlockNumberFormatter, utils.numberToHex],
+        outputFormatter: formatter.outputBlockFormatter,
+
+    },
+    {
+        name: 'getBlockTransactionCount',
+        call: getBlockTransactionCountCall,
+        params: 1,
+        inputFormatter: [formatter.inputBlockNumberFormatter],
+        outputFormatter: utils.hexToNumber
+    },
+    {
+        name: 'getBlockUncleCount',
+        call: uncleCountCall,
+        params: 1,
+        inputFormatter: [formatter.inputBlockNumberFormatter],
+        outputFormatter: utils.hexToNumber
+    },
+    {
+        name: 'getTransaction',
+        call: 'eth_getTransactionByHash',
+        params: 1,
+        inputFormatter: [null],
+        outputFormatter: formatter.outputTransactionFormatter
+    },
+    {
+        name: 'getTransactionFromBlock',
+        call: transactionFromBlockCall,
+        params: 2,
+        inputFormatter: [formatter.inputBlockNumberFormatter, utils.numberToHex],
+        outputFormatter: formatter.outputTransactionFormatter
+    },
+    {
+        name: 'getTransactionReceipt',
+        call: 'eth_getTransactionReceipt',
+        params: 1,
+        inputFormatter: [null],
+        outputFormatter: formatter.outputTransactionReceiptFormatter
+    },
+    {
+        name: 'getTransactionCount',
+        call: 'eth_getTransactionCount',
+        params: 2,
+        inputFormatter: [formatter.inputAddressFormatter, formatter.inputDefaultBlockNumberFormatter],
+        outputFormatter: utils.hexToNumber
+    },
+    {
+        name: 'sendSignedTransaction',
+        call: 'eth_sendRawTransaction',
+        params: 1,
+        inputFormatter: [null],
+        abiCoder: abi
+    },
+    {
+        name: 'signTransaction',
+        call: 'eth_signTransaction',
+        params: 1,
+        inputFormatter: [formatter.inputTransactionFormatter]
+    },
+    {
+        name: 'sendTransaction',
+        call: 'eth_sendTransaction',
+        params: 1,
+        inputFormatter: [formatter.inputTransactionFormatter],
+        abiCoder: abi
+    },
+    {
+        name: 'sign',
+        call: 'eth_sign',
+        params: 2,
+        inputFormatter: [formatter.inputSignFormatter, formatter.inputAddressFormatter],
+        transformPayload: function (payload) {
+            payload.params.reverse();
+            return payload;
+        }
+    },
+    {
+        name: 'call',
+        call: 'eth_call',
+        params: 2,
+        inputFormatter: [formatter.inputCallFormatter, formatter.inputDefaultBlockNumberFormatter],
+        abiCoder: abi
+    },
+    {
+        name: 'estimateGas',
+        call: 'eth_estimateGas',
+        params: 1,
+        inputFormatter: [formatter.inputCallFormatter],
+        outputFormatter: utils.hexToNumber
+    },
+    {
+        name: 'submitWork',
+        call: 'eth_submitWork',
+        params: 3
+    },
+    {
+        name: 'getWork',
+        call: 'eth_getWork',
+        params: 0
+    },
+    {
+        name: 'getPastLogs',
+        call: 'eth_getLogs',
+        params: 1,
+        inputFormatter: [formatter.inputLogFormatter],
+        outputFormatter: formatter.outputLogFormatter
+    },
+    {
+        name: 'getChainId',
+        call: 'eth_chainId',
+        params: 0,
+        outputFormatter: utils.hexToNumber
+    },
+    {
+        name: 'requestAccounts',
+        call: 'eth_requestAccounts',
+        params: 0,
+        outputFormatter: utils.toChecksumAddress
+    },
+    {
+        name: 'getProof',
+        call: 'eth_getProof',
+        params: 3,
+        inputFormatter: [formatter.inputAddressFormatter, formatter.inputStorageKeysFormatter, formatter.inputDefaultBlockNumberFormatter],
+        outputFormatter: formatter.outputProofFormatter
+    },
+    {
+        name: 'getPendingTransactions',
+        call: 'eth_pendingTransactions',
+        params: 0,
+        outputFormatter: formatter.outputTransactionFormatter
+    },
+]
--- packages/web3-eth/src/index.js
+++ packages/web3-eth/src/index.js
@@ -346,301 +346,8 @@ var Eth = function Eth() {
     // add ENS
     this.ens = new ENS(this);
 
-    var methods = [
-        new Method({
-            name: 'getNodeInfo',
-            call: 'web3_clientVersion'
-        }),
-        new Method({
-            name: 'getProtocolVersion',
-            call: 'eth_protocolVersion',
-            params: 0
-        }),
-        new Method({
-            name: 'getCoinbase',
-            call: 'eth_coinbase',
-            params: 0
-        }),
-        new Method({
-            name: 'isMining',
-            call: 'eth_mining',
-            params: 0
-        }),
-        new Method({
-            name: 'getHashrate',
-            call: 'eth_hashrate',
-            params: 0,
-            outputFormatter: utils.hexToNumber
-        }),
-        new Method({
-            name: 'isSyncing',
-            call: 'eth_syncing',
-            params: 0,
-            outputFormatter: formatter.outputSyncingFormatter
-        }),
-        new Method({
-            name: 'getGasPrice',
-            call: 'eth_gasPrice',
-            params: 0,
-            outputFormatter: formatter.outputBigNumberFormatter
-        }),
-        new Method({
-            name: 'getAccounts',
-            call: 'eth_accounts',
-            params: 0,
-            outputFormatter: utils.toChecksumAddress
-        }),
-        new Method({
-            name: 'getBlockNumber',
-            call: 'eth_blockNumber',
-            params: 0,
-            outputFormatter: utils.hexToNumber
-        }),
-        new Method({
-            name: 'getBalance',
-            call: 'eth_getBalance',
-            params: 2,
-            inputFormatter: [formatter.inputAddressFormatter, formatter.inputDefaultBlockNumberFormatter],
-            outputFormatter: formatter.outputBigNumberFormatter
-        }),
-        new Method({
-            name: 'getStorageAt',
-            call: 'eth_getStorageAt',
-            params: 3,
-            inputFormatter: [formatter.inputAddressFormatter, utils.numberToHex, formatter.inputDefaultBlockNumberFormatter]
-        }),
-        new Method({
-            name: 'getCode',
-            call: 'eth_getCode',
-            params: 2,
-            inputFormatter: [formatter.inputAddressFormatter, formatter.inputDefaultBlockNumberFormatter]
-        }),
-        new Method({
-            name: 'getBlock',
-            call: blockCall,
-            params: 2,
-            inputFormatter: [formatter.inputBlockNumberFormatter, function (val) { return !!val; }],
-            outputFormatter: formatter.outputBlockFormatter
-        }),
-        new Method({
-            name: 'getUncle',
-            call: uncleCall,
-            params: 2,
-            inputFormatter: [formatter.inputBlockNumberFormatter, utils.numberToHex],
-            outputFormatter: formatter.outputBlockFormatter,
-
-        }),
-        new Method({
-            name: 'getBlockTransactionCount',
-            call: getBlockTransactionCountCall,
-            params: 1,
-            inputFormatter: [formatter.inputBlockNumberFormatter],
-            outputFormatter: utils.hexToNumber
-        }),
-        new Method({
-            name: 'getBlockUncleCount',
-            call: uncleCountCall,
-            params: 1,
-            inputFormatter: [formatter.inputBlockNumberFormatter],
-            outputFormatter: utils.hexToNumber
-        }),
-        new Method({
-            name: 'getTransaction',
-            call: 'eth_getTransactionByHash',
-            params: 1,
-            inputFormatter: [null],
-            outputFormatter: formatter.outputTransactionFormatter
-        }),
-        new Method({
-            name: 'getTransactionFromBlock',
-            call: transactionFromBlockCall,
-            params: 2,
-            inputFormatter: [formatter.inputBlockNumberFormatter, utils.numberToHex],
-            outputFormatter: formatter.outputTransactionFormatter
-        }),
-        new Method({
-            name: 'getTransactionReceipt',
-            call: 'eth_getTransactionReceipt',
-            params: 1,
-            inputFormatter: [null],
-            outputFormatter: formatter.outputTransactionReceiptFormatter
-        }),
-        new Method({
-            name: 'getTransactionCount',
-            call: 'eth_getTransactionCount',
-            params: 2,
-            inputFormatter: [formatter.inputAddressFormatter, formatter.inputDefaultBlockNumberFormatter],
-            outputFormatter: utils.hexToNumber
-        }),
-        new Method({
-            name: 'sendSignedTransaction',
-            call: 'eth_sendRawTransaction',
-            params: 1,
-            inputFormatter: [null],
-            abiCoder: abi
-        }),
-        new Method({
-            name: 'signTransaction',
-            call: 'eth_signTransaction',
-            params: 1,
-            inputFormatter: [formatter.inputTransactionFormatter]
-        }),
-        new Method({
-            name: 'sendTransaction',
-            call: 'eth_sendTransaction',
-            params: 1,
-            inputFormatter: [formatter.inputTransactionFormatter],
-            abiCoder: abi
-        }),
-        new Method({
-            name: 'sign',
-            call: 'eth_sign',
-            params: 2,
-            inputFormatter: [formatter.inputSignFormatter, formatter.inputAddressFormatter],
-            transformPayload: function (payload) {
-                payload.params.reverse();
-                return payload;
-            }
-        }),
-        new Method({
-            name: 'call',
-            call: 'eth_call',
-            params: 2,
-            inputFormatter: [formatter.inputCallFormatter, formatter.inputDefaultBlockNumberFormatter],
-            abiCoder: abi
-        }),
-        new Method({
-            name: 'estimateGas',
-            call: 'eth_estimateGas',
-            params: 1,
-            inputFormatter: [formatter.inputCallFormatter],
-            outputFormatter: utils.hexToNumber
-        }),
-        new Method({
-            name: 'submitWork',
-            call: 'eth_submitWork',
-            params: 3
-        }),
-        new Method({
-            name: 'getWork',
-            call: 'eth_getWork',
-            params: 0
-        }),
-        new Method({
-            name: 'getPastLogs',
-            call: 'eth_getLogs',
-            params: 1,
-            inputFormatter: [formatter.inputLogFormatter],
-            outputFormatter: formatter.outputLogFormatter
-        }),
-        new Method({
-            name: 'getChainId',
-            call: 'eth_chainId',
-            params: 0,
-            outputFormatter: utils.hexToNumber
-        }),
-        new Method({
-            name: 'requestAccounts',
-            call: 'eth_requestAccounts',
-            params: 0,
-            outputFormatter: utils.toChecksumAddress
-        }),
-        new Method({
-            name: 'getProof',
-            call: 'eth_getProof',
-            params: 3,
-            inputFormatter: [formatter.inputAddressFormatter, formatter.inputStorageKeysFormatter, formatter.inputDefaultBlockNumberFormatter],
-            outputFormatter: formatter.outputProofFormatter
-        }),
-        new Method({
-            name: 'getPendingTransactions',
-            call: 'eth_pendingTransactions',
-            params: 0,
-            outputFormatter: formatter.outputTransactionFormatter
-        }),
-
-        // subscriptions
-        new Subscriptions({
-            name: 'subscribe',
-            type: 'eth',
-            subscriptions: {
-                'newBlockHeaders': {
-                    // TODO rename on RPC side?
-                    subscriptionName: 'newHeads', // replace subscription with this name
-                    params: 0,
-                    outputFormatter: formatter.outputBlockFormatter
-                },
-                'pendingTransactions': {
-                    subscriptionName: 'newPendingTransactions', // replace subscription with this name
-                    params: 0
-                },
-                'logs': {
-                    params: 1,
-                    inputFormatter: [formatter.inputLogFormatter],
-                    outputFormatter: formatter.outputLogFormatter,
-                    // DUBLICATE, also in web3-eth-contract
-                    subscriptionHandler: function (output) {
-                        if(output.removed) {
-                            this.emit('changed', output);
-                        } else {
-                            this.emit('data', output);
-                        }
-
-                        if (_.isFunction(this.callback)) {
-                            this.callback(null, output, this);
-                        }
-                    }
-                },
-                'syncing': {
-                    params: 0,
-                    outputFormatter: formatter.outputSyncingFormatter,
-                    subscriptionHandler: function (output) {
-                        var _this = this;
-
-                        // fire TRUE at start
-                        if(this._isSyncing !== true) {
-                            this._isSyncing = true;
-                            this.emit('changed', _this._isSyncing);
-
-                            if (_.isFunction(this.callback)) {
-                                this.callback(null, _this._isSyncing, this);
-                            }
-
-                            setTimeout(function () {
-                                _this.emit('data', output);
-
-                                if (_.isFunction(_this.callback)) {
-                                    _this.callback(null, output, _this);
-                                }
-                            }, 0);
-
-                            // fire sync status
-                        } else {
-                            this.emit('data', output);
-                            if (_.isFunction(_this.callback)) {
-                                this.callback(null, output, this);
-                            }
-
-                            // wait for some time before fireing the FALSE
-                            clearTimeout(this._isSyncingTimeout);
-                            this._isSyncingTimeout = setTimeout(function () {
-                                if(output.currentBlock > output.highestBlock - 200) {
-                                    _this._isSyncing = false;
-                                    _this.emit('changed', _this._isSyncing);
-
-                                    if (_.isFunction(_this.callback)) {
-                                        _this.callback(null, _this._isSyncing, _this);
-                                    }
-                                }
-                            }, 500);
-                        }
-                    }
-                }
-            }
-        })
-    ];
-
-    methods.forEach(function(method) {
+    methods.forEach(function(methodConfig) {
+        const method = new Method(methodConfig)
         method.attachToObject(_this);
         method.setRequestManager(_this._requestManager, _this.accounts); // second param is the eth.accounts module (necessary for signing transactions locally)
         method.defaultBlock = _this.defaultBlock;
@@ -650,6 +357,86 @@ var Eth = function Eth() {
         method.transactionPollingTimeout = _this.transactionPollingTimeout;
         method.handleRevert = _this.handleRevert;
     });
+
+new Subscriptions({
+        name: 'subscribe',
+        type: 'eth',
+        subscriptions: {
+            'newBlockHeaders': {
+                // TODO rename on RPC side?
+                subscriptionName: 'newHeads', // replace subscription with this name
+                params: 0,
+                outputFormatter: formatter.outputBlockFormatter
+            },
+            'pendingTransactions': {
+                subscriptionName: 'newPendingTransactions', // replace subscription with this name
+                params: 0
+            },
+            'logs': {
+                params: 1,
+                inputFormatter: [formatter.inputLogFormatter],
+                outputFormatter: formatter.outputLogFormatter,
+                // DUBLICATE, also in web3-eth-contract
+                subscriptionHandler: function (output) {
+                    if(output.removed) {
+                        this.emit('changed', output);
+                    } else {
+                        this.emit('data', output);
+                    }
+
+                    if (_.isFunction(this.callback)) {
+                        this.callback(null, output, this);
+                    }
+                }
+            },
+            'syncing': {
+                params: 0,
+                outputFormatter: formatter.outputSyncingFormatter,
+                subscriptionHandler: function (output) {
+                    var _this = this;
+
+                    // fire TRUE at start
+                    if(this._isSyncing !== true) {
+                        this._isSyncing = true;
+                        this.emit('changed', _this._isSyncing);
+
+                        if (_.isFunction(this.callback)) {
+                            this.callback(null, _this._isSyncing, this);
+                        }
+
+                        setTimeout(function () {
+                            _this.emit('data', output);
+
+                            if (_.isFunction(_this.callback)) {
+                                _this.callback(null, output, _this);
+                            }
+                        }, 0);
+
+                        // fire sync status
+                    } else {
+                        this.emit('data', output);
+                        if (_.isFunction(_this.callback)) {
+                            this.callback(null, output, this);
+                        }
+
+                        // wait for some time before fireing the FALSE
+                        clearTimeout(this._isSyncingTimeout);
+                        this._isSyncingTimeout = setTimeout(function () {
+                            if(output.currentBlock > output.highestBlock - 200) {
+                                _this._isSyncing = false;
+                                _this.emit('changed', _this._isSyncing);
+
+                                if (_.isFunction(_this.callback)) {
+                                    _this.callback(null, _this._isSyncing, _this);
+                                }
+                            }
+                        }, 500);
+                    }
+                }
+            }
+        }
+    })
+
 
 };
 
