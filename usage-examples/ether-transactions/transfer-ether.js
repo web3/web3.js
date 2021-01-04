170dfb2dfcdb854068a32f1e53675e08ff8a2c3a
Michael <michael.spengler@sap.com>
2021-01-04 18:08:13 +0100

    add comments

--- usage-examples/ether-transactions/transfer-ether.js
+++ usage-examples/ether-transactions/transfer-ether.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config() // this ensures process.env. ... contains your .env file configuration values
 
 const Web3 = require('web3');
 const Tx = require('ethereumjs-tx').Transaction;
@@ -8,20 +8,23 @@ export const transfer = async () => {
 
     const gasPrice = await web3.eth.getGasPrice()
 
-    const nonce = await web3.eth.getTransactionCount(process.env.SOURCE_WALLET, 'pending')
+    // ensure your .env file contains SENDER_WALLET=<sender_wallet_address>
+    const nonce = await web3.eth.getTransactionCount(process.env.SENDER_WALLET, 'pending')
 
     let txParams = {
         nonce: web3.utils.toHex(nonce),
         gasLimit: web3.utils.toHex(web3.utils.toHex(33000)),
         gasPrice: web3.utils.toHex(gasPrice),
-        from: process.env.SOURCE_WALLET,
-        to: process.env.TARGET_WALLET,
-        value: 0.07 * 1000000000000000000,
+        from: process.env.SENDER_WALLET, // ensure your .env file contains SENDER_WALLET=<sender_wallet_address>
+        to: process.env.TARGET_WALLET, // ensure your .env file contains TARGET_WALLET=<recipient_wallet_address>
+        value: 0.07 * 1000000000000000000, // this would e.g. transfer 0.07 Ether
         chainId: web3.utils.toHex(1),
     }
     let tx = new Tx(txParams);
     
-    tx.sign(Buffer.from(process.env.SOURCE_WALLET_PRIVATE_KEY, 'hex'));
+    // ensure your .env file contains SENDER_WALLET_PRIVATE_KEY=<sender_wallet_private_key>
+    // always ensure not to publish your .env file to github --> .gitignore it
+    tx.sign(Buffer.from(process.env.SENDER_WALLET_PRIVATE_KEY, 'hex'));
     tx = "0x" + tx.serialize().toString('hex')
     
     web3.eth.sendSignedTransaction(tx)
