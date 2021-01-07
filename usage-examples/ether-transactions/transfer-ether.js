c33e99eec80275bfee223a09a212e7628d17924a
Michael <michael.spengler@sap.com>
2021-01-07 23:55:58 +0100

    reduce dependencies + add .env.template

--- usage-examples/ether-transactions/transfer-ether.js
+++ usage-examples/ether-transactions/transfer-ether.js
@@ -1,46 +1,57 @@
+
 require('dotenv').config() // this ensures process.env. ... contains your .env file configuration values
 
 const Web3 = require('web3');
-const Tx = require('ethereumjs-tx').Transaction;
-const web3 = new Web3(new Web3.providers.HttpProvider(`https://mainnet.infura.io/v3/${process.env.INFURA_PROJECT_ID}`));
 
-export const transfer = async () => {
+const web3 = new Web3(new Web3.providers.HttpProvider(process.env.PROVIDER_URL));
 
-    const gasPrice = await web3.eth.getGasPrice()
 
-    // ensure your .env file contains SENDER_WALLET=<sender_wallet_address>
-    const nonce = await web3.eth.getTransactionCount(process.env.SENDER_WALLET, 'pending')
+async function signAndSend(transactionObject, senderPrivateKey) {
 
-    let txParams = {
-        nonce: web3.utils.toHex(nonce),
-        gasLimit: web3.utils.toHex(web3.utils.toHex(33000)),
-        gasPrice: web3.utils.toHex(gasPrice),
-        from: process.env.SENDER_WALLET, // ensure your .env file contains SENDER_WALLET=<sender_wallet_address>
-        to: process.env.TARGET_WALLET, // ensure your .env file contains TARGET_WALLET=<recipient_wallet_address>
-        value: 0.07 * 1000000000000000000, // this would e.g. transfer 0.07 Ether
-        chainId: web3.utils.toHex(1),
-    }
-    let tx = new Tx(txParams);
-    
-    // ensure your .env file contains SENDER_WALLET_PRIVATE_KEY=<sender_wallet_private_key>
-    // always ensure not to publish your .env file to github --> .gitignore it
-    tx.sign(Buffer.from(process.env.SENDER_WALLET_PRIVATE_KEY, 'hex'));
-    tx = "0x" + tx.serialize().toString('hex')
-    
-    web3.eth.sendSignedTransaction(tx)
+    const signedTransaction = await web3.eth.accounts.signTransaction(transactionObject, senderPrivateKey)
+
+    web3.eth.sendSignedTransaction(signedTransaction.rawTransaction)
         .on('transactionHash', (hash) => {
             console.log(hash);
         })
         .on('receipt', (receipt) => {
             console.log(receipt);
-            return "receipt"
         })
         .on('confirmation', (confirmationNumber, receipt) => {
             console.log(confirmationNumber);
             console.log(receipt);
         })
-        .on('error', console.error);
+        .on('error', console.error)
 
 }
 
-transfer()
+
+async function getTransactionObject(from, to, value) {
+    const txCount = await web3.eth.getTransactionCount(from, "pending")
+    const gasPrice = await web3.eth.getGasPrice()
+    
+    return {
+        nonce: web3.utils.numberToHex(txCount),
+        gasLimit: web3.utils.numberToHex(33000),
+        gasPrice: web3.utils.toHex(gasPrice),
+        from,
+        to,
+        value,
+        chainId: '0x1',
+    }
+
+}
+
+
+async function triggerTransferExample() {
+
+    const amountInEtherToBeSent = 0.01 * 1000000000000000000 // this would e.g. transfer 0.01 Ether
+    
+    const transactionObject = await getTransactionObject(process.env.FROM_WALLET_ADDRESS, process.env.TO_WALLET_ADDRESS, amountInEtherToBeSent)
+
+    await signAndSend(transactionObject, process.env.SENDER_PRIVATE_KEY)
+
+}
+
+
+triggerTransferExample()
