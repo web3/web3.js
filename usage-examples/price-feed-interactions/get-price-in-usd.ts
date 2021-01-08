c810babc31456b8720f1b1f3ef83c41943d9198e
Michael <michael.spengler@sap.com>
2021-01-08 01:41:39 +0100

    improve the smart contract interaction example

--- usage-examples/price-feed-interactions/get-price-in-usd.ts
+++ usage-examples/price-feed-interactions/get-price-in-usd.ts
@@ -7,13 +7,13 @@ const web3 = new Web3(new Web3.providers.HttpProvider(process.env.PROVIDER_URL))
 
 const { priceFeedAbi } = require('./contract-abis.json');
 
-async function getPrice(assetName: string) {
+async function getPrice(assetName) {
     
     const priceFeedAddress = '0x922018674c12a7f0d394ebeef9b58f186cde13c1';
-    const priceFeed = new web3.eth.Contract(priceFeedAbi, priceFeedAddress);
-    let priceInUsd = await priceFeed.methods.price(assetName).call();
+    const priceFeedSmartContract = new web3.eth.Contract(priceFeedAbi, priceFeedAddress);
+    let priceInUsd = (await priceFeedSmartContract.methods.price(assetName).call()) / 1000000;
     
-    console.log(`According to the price feed address: The price of ${assetName} is about ${priceInUsd} USD.`)
+    console.log(`According to the price feed address: \nThe price of ${assetName} is about ${priceInUsd} USD.`)
 }
 
 getPrice('ETH')
