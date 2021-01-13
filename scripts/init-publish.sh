64681fb5c87b603ec918ac6bcff63363ce899957
RayL <ray.li.cmd@gmail.com>
2021-01-13 21:28:31 +0800

    fix invalid publish package name

--- scripts/init-publish.sh
+++ scripts/init-publish.sh
@@ -4,7 +4,7 @@ SCRIPTPATH="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
 PREFIX="web3"
 
-PUBLISHES=("$PREFIX-utils" "$PREFIX-ssh" "$PREFIX-eth-iban" "$PREFIX-core-promievent" "$PREFIX-core-helpers" "$PREFIX-providers-ws" "$PREFIX-providers-ipc" "$PREFIX-providers-http" "$PREFIX-bzz" "$PREFIX-core-subscriptions" "$PREFIX-core-requestmanager" "$PREFIX-core-method" "$PREFIX-core" "$PREFIX-net" "$PREFIX-eth-personal" "$PREFIX-eth-abi" "$PREFIX-eth-contract" "$PREFIX-eth-accounts" "$PREFIX-eth-ens" "$PREFIX-eth" "$PREFIX" )
+PUBLISHES=("$PREFIX-utils" "$PREFIX-shh" "$PREFIX-eth-iban" "$PREFIX-core-promievent" "$PREFIX-core-helpers" "$PREFIX-providers-ws" "$PREFIX-providers-ipc" "$PREFIX-providers-http" "$PREFIX-bzz" "$PREFIX-core-subscriptions" "$PREFIX-core-requestmanager" "$PREFIX-core-method" "$PREFIX-core" "$PREFIX-net" "$PREFIX-eth-personal" "$PREFIX-eth-abi" "$PREFIX-eth-contract" "$PREFIX-eth-accounts" "$PREFIX-eth-ens" "$PREFIX-eth" "$PREFIX" )
 
 echo "Publishing ${#PUBLISHES[@]} Packages..."
 
