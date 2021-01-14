a44c4b7dd615c90e9c459dfc8b825d02f4e0ce00
frankiebee <frankie.diamond@gmail.com>
2021-01-14 10:55:34 -1000

    remove old packages and add readme

--- packages/web3-eth2-beaconchain/test/axiosMock.ts
+++
@@ -1,3 +0,0 @@
-import jest from 'jest'
-
-export const get = jest.fn(() => Promise.resolve({ data: {} }))
--- packages/web3-eth2-beaconchain/types/index.d.ts
+++
@@ -1,6 +0,0 @@
-import { Genesis } from '@chainsafe/lodestar-types'
-
-export interface IETH2BeaconChain {
-  getGenesis(): Promise<Genesis>
-  getBlockHeader(): Promise<any>
-}
