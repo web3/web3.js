7877268976774726264ed7e12ecca3a7afe1894c
Wyatt Barnes <me@wyatt.email>
2021-01-29 12:25:55 -0600

    Add npm build step

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -63,6 +63,7 @@ elif [ "$TEST" = "e2e_gnosis_dex" ]; then
 
 elif [ "$TEST" = "eth2" ]; then
 
+  npm run build
   npm --prefix ./packages/web3-eth2-core run test
   npm --prefix ./packages/web3-eth2-beaconchain run test
 
