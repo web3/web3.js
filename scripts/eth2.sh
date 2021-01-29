578cc0b4a2a8f024c63945a66583ee2ac831b83b
Wyatt Barnes <me@wyatt.email>
2021-01-29 16:57:27 -0600

    Debugging docker lighthouse

--- scripts/eth2.sh
+++ scripts/eth2.sh
@@ -11,9 +11,9 @@ set -o errexit
 # Run cleanup on exit
 trap cleanup EXIT
 
-# cleanup(){
-#   docker stop lighthouse
-# }
+cleanup(){
+  docker stop $(docker ps -a -q)
+}
 
 echo " "
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
