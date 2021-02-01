719482bf2982e029dacbd51d1abf52c50650596c
Wyatt Barnes <wyatt@writerof.software>
2021-02-01 14:35:31 -0600

    Revert "Add eth2 github action (#3892)" (#3895)
    This reverts commit 1a252a9bd81dff246ec0c1e8b8cdda8e9cc0c70b.

--- packages/web3-eth2-core/types.ts
+++ packages/web3-eth2-core/types.ts
@@ -1,5 +1,3 @@
- import { AxiosInstance } from 'axios';
-
 export interface ETH2BaseOpts {
     protectProvider?: boolean
 }
@@ -23,15 +21,6 @@ export interface BaseAPISchema {
     methods: BaseAPISchemaMethod[]
 }
 
-export declare class ETH2Core {
-    private _httpClient;
-    [key: string]: ETH2Function | any;
-    name: string;
-    provider: string | undefined;
-    protectProvider: boolean;
-    constructor(provider: string, schema: BaseAPISchema, opts?: ETH2BaseOpts);
-    static createHttpClient(baseUrl: string): AxiosInstance;
-    setProvider(provider: string): void;
-    private routeBuilder;
-    private buildAPIWrappersFromSchema;
+export interface ETH2Core {
+    setProvider(provider: string): void
 }
