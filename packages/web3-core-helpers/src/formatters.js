5cff1144efd4399f769a5f64f0ea8653b262ca5b
Wyatt Barnes <me@wyatt.email>
2021-03-30 19:00:14 -0500

    Check if defined before formatting

--- packages/web3-core-helpers/src/formatters.js
+++ packages/web3-core-helpers/src/formatters.js
@@ -238,9 +238,9 @@ var outputTransactionFormatter = function (tx) {
     if (tx.transactionIndex !== null)
         tx.transactionIndex = utils.hexToNumber(tx.transactionIndex);
     tx.nonce = utils.hexToNumber(tx.nonce);
-    tx.gas = outputBigNumberFormatter(tx.gas);
-    tx.gasPrice = outputBigNumberFormatter(tx.gasPrice);
-    tx.value = outputBigNumberFormatter(tx.value);
+    if (tx.gas) tx.gas = outputBigNumberFormatter(tx.gas);
+    if (tx.gasPrice) tx.gasPrice = outputBigNumberFormatter(tx.gasPrice);
+    if (tx.value) tx.value = outputBigNumberFormatter(tx.value);
 
     if (tx.to && utils.isAddress(tx.to)) { // tx.to could be `0x0` or `null` while contract creation
         tx.to = utils.toChecksumAddress(tx.to);
@@ -271,8 +271,8 @@ var outputTransactionReceiptFormatter = function (receipt) {
         receipt.blockNumber = utils.hexToNumber(receipt.blockNumber);
     if (receipt.transactionIndex !== null)
         receipt.transactionIndex = utils.hexToNumber(receipt.transactionIndex);
-    receipt.cumulativeGasUsed = outputBigNumberFormatter(receipt.cumulativeGasUsed);
-    receipt.gasUsed = outputBigNumberFormatter(receipt.gasUsed);
+    if (receipt.cumulativeGasUsed) receipt.cumulativeGasUsed = outputBigNumberFormatter(receipt.cumulativeGasUsed);
+    if (receipt.gasUsed) receipt.gasUsed = outputBigNumberFormatter(receipt.gasUsed);
 
     if (_.isArray(receipt.logs)) {
         receipt.logs = receipt.logs.map(outputLogFormatter);
@@ -297,10 +297,10 @@ var outputTransactionReceiptFormatter = function (receipt) {
  * @returns {Object}
  */
 var outputBlockFormatter = function (block) {
-
+    
     // transform to number
-    block.gasLimit = outputBigNumberFormatter(block.gasLimit);
-    block.gasUsed = outputBigNumberFormatter(block.gasUsed);
+    if (block.gasLimit) block.gasLimit = outputBigNumberFormatter(block.gasLimit);
+    if (block.gasUsed) block.gasUsed = outputBigNumberFormatter(block.gasUsed);
     block.size = utils.hexToNumber(block.size);
     block.timestamp = utils.hexToNumber(block.timestamp);
     if (block.number !== null)
