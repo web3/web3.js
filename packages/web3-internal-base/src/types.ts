b321c5afd35761a49f898bef7dd7cd0f8f8da75f
Wyatt Barnes <me@wyatt.email>
2021-04-14 19:06:10 +1400

    Init

---
+++ packages/web3-internal-base/src/types.ts
@@ -0,0 +1,37 @@
+import { AxiosInstance } from 'axios';
+
+export interface BaseOpts {
+    protectProvider?: boolean
+}
+
+export type BaseFunction<T = any> = (...args: any[]) => Promise<T>;
+
+export interface BaseAPISchemaMethod {
+    notImplemented?: true,
+    name: string,
+    route: string,
+    restMethod: 'get' | 'post'
+    inputFormatter: any,
+    outputFormatter: any,
+    errors: any
+    errorPrefix: string
+}
+
+export interface BaseAPISchema {
+    packageName: string,
+    routePrefix: string,
+    methods: BaseAPISchemaMethod[]
+}
+
+export declare class BaseCore {
+    private _httpClient;
+    [key: string]: BaseFunction | any;
+    name: string;
+    provider: string | undefined;
+    protectProvider: boolean;
+    constructor(provider: string, schema: BaseAPISchema, opts?: BaseOpts);
+    static createHttpClient(baseUrl: string): AxiosInstance;
+    setProvider(provider: string): void;
+    private routeBuilder;
+    private buildAPIWrappersFromSchema;
+}
---
+++ packages/web3-internal-base/test/unit/constructor.test.ts
@@ -0,0 +1,28 @@
+import { BaseCore } from '../../src/index'
+
+const provider = 'http://127.0.0.1:9596'
+const testAPISchema = {
+    packageName: 'testValue',
+    routePrefix: '/test/route/',
+    methods: [
+        {
+            name: 'testMethod',
+            route: 'additional/route',
+            restMethod: 'get',
+            inputFormatter: null,
+            outputFormatter: null,
+            errors: null,
+            errorPrefix: 'Test error prefix:'
+        }
+    ]
+}
+
+it('constructs a ETH2Core instance with expected properties', () => {
+    // @ts-ignore using a mock schema for test
+    const eth2Core = new ETH2Core(provider, testAPISchema, { protectProvider: true })
+
+    expect(eth2Core.name).toBe(testAPISchema.packageName)
+    expect(eth2Core.provider).toBe(`${provider}${testAPISchema.routePrefix}`)
+    expect(eth2Core.protectProvider)
+    expect(eth2Core.testMethod).toBeDefined()
+})
