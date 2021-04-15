dfb1f78bce44e6eca525b3188b2b4097f491a67f
Wyatt Barnes <me@wyatt.email>
2021-04-16 12:38:57 +1400

    Update blockNumber response and tests

--- packages/web3-eth/src/index.ts
+++ packages/web3-eth/src/index.ts
@@ -1,27 +1,22 @@
 import { Base } from 'web3-internal-base';
-import {
-  BaseAPISchema, BaseOpts, RpcParamsBase, RpcResponse
-} from 'web3-internal-base/types';
-import { DefaultSchema } from './schema';
+import { BaseOpts, RpcParamsBase } from 'web3-internal-base/types';
 
-// import { Web3Eth as IWeb3Eth } from '../types';
+import { FormattedRpcResponse } from '../types';
 
-// @ts-ignore - ETH2BeaconChain incorrectly implements interface IETH2BeaconChain
-// because methods are added during runtime
-// export class Web3Eth extends Base implements IWeb3Eth {
-// export default class Web3Eth extends Base implements IWeb3Eth {
 export default class Web3Eth extends Base {
   constructor(
     provider: string,
-    schema: BaseAPISchema = DefaultSchema,
     opts: BaseOpts = { protectProvider: true },
   ) {
-    super(provider, schema, opts);
+    super('eth', provider, opts);
   }
 
-  async getBlockNumber(rpcParams?: RpcParamsBase): Promise<RpcResponse> {
+  async getBlockNumber(rpcParams?: RpcParamsBase): Promise<FormattedRpcResponse> {
     try {
-      return await this.sendRpc({...rpcParams, method: 'eth_blockNumber', params: []})
+      const response = await this.sendRpc({...rpcParams, method: 'eth_blockNumber', params: []})
+      const bigIntBlockNumber = BigInt(response.result)
+      if (bigIntBlockNumber === undefined) throw Error(`Unable to convert returned result: ${response.result} into a BigInt`)
+      return {...response, result: bigIntBlockNumber}
     } catch (error) {
       throw Error(`Error getting block number: ${error.message}`)
     }
--- packages/web3-eth/test/unit/eth_blockNumber.test.ts
+++ packages/web3-eth/test/unit/eth_blockNumber.test.ts
@@ -1,23 +1,31 @@
 
-import { RpcResponse } from 'web3-internal-base/types'
-
 import Web3Eth from '../../src/index'
+import { FormattedRpcResponse } from '../../types'
 
-const provider = 'http://127.0.0.1:8545';
+const provider = 'http://127.0.0.1:8545'
+const expectedId = 42
+const expectedBlockNumber = BigInt(0)
 
-let web3Eth: Web3Eth;
+let web3Eth: Web3Eth
 
 beforeAll(() => {
     web3Eth = new Web3Eth(provider);
 })
 
 it('[SANITY] constructs a Web3Eth instance with getBlockNumber method', () => {
-    expect(web3Eth.getBlockNumber).not.toBe(undefined);
+    expect(web3Eth.getBlockNumber).not.toBe(undefined)
 })
 
-it('should get block number', async () => {
-    const result: RpcResponse = await web3Eth.getBlockNumber()
-    console.log(result)
-    // { id: 7054039680174109, jsonrpc: '2.0', result: '0x0' }
-    
+it('should get block number - no params', async () => {
+    const result: FormattedRpcResponse = await web3Eth.getBlockNumber()
+    expect(typeof result.id).toBe('number')
+    expect(result.jsonrpc).toBe('2.0')
+    expect(result.result).toBe(expectedBlockNumber)  
+})
+
+it(`should get block number - id param should be ${expectedId}`, async () => {
+    const result: FormattedRpcResponse = await web3Eth.getBlockNumber({id: expectedId})
+    expect(result.id).toBe(expectedId)
+    expect(result.jsonrpc).toBe('2.0')
+    expect(result.result).toBe(expectedBlockNumber)  
 })
