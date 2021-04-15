dcbc233cabccb12a8338eb4c14ca64d2bf7a8824
Wyatt Barnes <me@wyatt.email>
2021-04-16 12:37:31 +1400

    Remove no longer needed check

--- packages/web3-eth/test/unit/constructor.test.ts
+++ packages/web3-eth/test/unit/constructor.test.ts
@@ -2,12 +2,10 @@
 import Web3Eth from '../../src/index'
 
 const provider = 'http://127.0.0.1:8545'
-const methodPrefix = 'eth_'
 
 it('constructs a Web3Eth instance with expected properties', () => {
     const web3Eth = new Web3Eth(provider)
     expect(web3Eth.name).toBe('eth')
     expect(web3Eth.provider).toBe(provider)
     expect(web3Eth.protectProvider)
-    expect(web3Eth.methodPrefix).toBe(methodPrefix)
 })
