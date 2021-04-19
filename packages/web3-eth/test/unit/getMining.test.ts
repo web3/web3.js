9d2567aa62d50f608b0b3f725ac71cc80fd51a74
Wyatt Barnes <me@wyatt.email>
2021-04-20 08:29:29 +1400

    Init

--- packages/web3-eth/test/unit/getMining.test.ts
+++ packages/web3-eth/test/unit/getMining.test.ts
@@ -1,11 +1,10 @@
 
-import { RpcResponseBigInt } from 'web3-internal-base/types'
+import { RpcResponse } from 'web3-internal-base/types'
 
 import Web3Eth from '../../src/index'
 
 const provider = 'http://127.0.0.1:8545'
 const expectedId = 42
-const expectedBlockNumber = BigInt(0)
 
 let web3Eth: Web3Eth
 
@@ -13,20 +12,22 @@ beforeAll(() => {
     web3Eth = new Web3Eth(provider);
 })
 
+// TODO Figure out how to mock mining status
+
 it('[SANITY] constructs a Web3Eth instance with getBlockNumber method', () => {
-    expect(web3Eth.getBlockNumber).not.toBe(undefined)
+    expect(web3Eth.getMining).not.toBe(undefined)
 })
 
-it('should get block number - no params', async () => {
-    const result: RpcResponseBigInt = await web3Eth.getBlockNumber()
+it('should get mining status - no params', async () => {
+    const result: RpcResponse = await web3Eth.getMining()
     expect(typeof result.id).toBe('number')
     expect(result.jsonrpc).toBe('2.0')
-    expect(result.result).toBe(expectedBlockNumber)  
+    expect(result.result).toBe(true)
 })
 
 it(`should get block number - id param should be ${expectedId}`, async () => {
-    const result: RpcResponseBigInt = await web3Eth.getBlockNumber({id: expectedId})
+    const result: RpcResponse = await web3Eth.getMining({id: expectedId})
     expect(result.id).toBe(expectedId)
     expect(result.jsonrpc).toBe('2.0')
-    expect(result.result).toBe(expectedBlockNumber)  
+    expect(result.result).toBe(true)  
 })
