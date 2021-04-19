7fda6608ae579fc6d83eaa3b3d77847bf55745dd
Wyatt Barnes <me@wyatt.email>
2021-04-19 17:20:53 +1400

    Remove outdated code

--- packages/web3-internal-base/test/unit/constructor.test.ts
+++ packages/web3-internal-base/test/unit/constructor.test.ts
@@ -1,28 +1,11 @@
-import { Base } from '../../src/index'
 
-const provider = 'http://127.0.0.1:9596'
-const testAPISchema = {
-    packageName: 'testValue',
-    routePrefix: '/test/route/',
-    methods: [
-        {
-            name: 'testMethod',
-            route: 'additional/route',
-            restMethod: 'get',
-            inputFormatter: null,
-            outputFormatter: null,
-            errors: null,
-            errorPrefix: 'Test error prefix:'
-        }
-    ]
-}
+import Base from '../../src/index'
+
+const provider = 'http://127.0.0.1:8545'
 
 it('constructs a Base instance with expected properties', () => {
-    // @ts-ignore using a mock schema for test
-    const base = new Base(provider, testAPISchema, { protectProvider: true })
-
-    expect(base.name).toBe(testAPISchema.packageName)
-    expect(base.provider).toBe(`${provider}${testAPISchema.routePrefix}`)
+    const base = new Base('eth', provider)
+    expect(base.name).toBe('eth')
+    expect(base.provider).toBe(provider)
     expect(base.protectProvider)
-    expect(base.testMethod).toBeDefined()
 })
