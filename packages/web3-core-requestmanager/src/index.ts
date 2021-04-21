c8b1d2551274966dc4e852cb3dda53a6a29f17a9
Wyatt Barnes <me@wyatt.email>
2021-04-21 17:29:01 +1400

    Init web3-core-requestmanager

---
+++ packages/web3-core-requestmanager/src/index.ts
@@ -0,0 +1,50 @@
+import Web3ProviderHttp, { ProviderOptions } from 'web3-providers-http'
+
+interface RequestManagerOptions {}
+
+enum ProviderProtocol { UNKNOWN, HTTP, WS, IPC }
+
+export default class Web3RequestManager {
+    provider: Web3ProviderHttp | undefined
+    providerProtocol: ProviderProtocol = ProviderProtocol.UNKNOWN
+
+    constructor(providerOptions: ProviderOptions, options?: RequestManagerOptions) {
+        switch (Web3RequestManager.detectProviderProtocol(providerOptions.providerString)) {
+            case ProviderProtocol.HTTP:
+                this.provider = new Web3ProviderHttp(providerOptions)
+                this.providerProtocol = ProviderProtocol.HTTP
+                break;
+            case ProviderProtocol.WS:
+                this.providerProtocol = ProviderProtocol.WS
+                // TODO
+                break;
+            default:
+                // TODO figure out if possible to support generic provider
+                break;
+        }
+    }
+
+    static detectProviderProtocol(providerString: string): ProviderProtocol {
+        try {
+            if (/^http(s)?:\/\//i.test(providerString)) {
+                return ProviderProtocol.HTTP
+            } else if (/^ws(s)?:\/\//i.test(providerString)) {
+                return ProviderProtocol.WS
+            } else {
+                return ProviderProtocol.UNKNOWN
+            }
+        } catch (error) {
+            throw Error(`Error detecting provider protocol: ${error.message}`)
+        }
+    }
+
+    // TODO get rid of anys
+    async send(options: any): Promise<any> {
+        try {
+            if (this.provider === undefined) throw Error('No provider initialized')
+            return this.provider.send(options)
+        } catch (error) {
+            
+        }
+    }
+}
