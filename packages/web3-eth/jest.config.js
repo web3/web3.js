15ff38cd4fa09e018a7fcbd0f62985faa0ef0741
Wyatt Barnes <me@wyatt.email>
2021-04-22 15:52:34 +1400

    Remove web3-internal-base

--- packages/web3-eth/jest.config.js
+++ packages/web3-eth/jest.config.js
@@ -2,7 +2,6 @@ module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
   moduleNameMapper: {
-    'web3-internal-base(.*)$': '<rootDir>/../web3-internal-base/src/$1',
     'web3-utils(.*)$': '<rootDir>/../web3-utils/src/$1'
   },
 };
