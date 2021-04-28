8b6a063ff645ba122e0941b981484125e5f68f93
Wyatt Barnes <me@wyatt.email>
2021-04-28 15:51:06 +1400

    WIP

--- test/eth.accounts.signTransaction.js
+++ test/eth.accounts.signTransaction.js
@@ -15,6 +15,23 @@ var common = {
     harfork: 'petersburg',
 };
 
+var commonBerlin = {
+    baseChain: 'mainnet',
+    customChain: {
+        name: 'custom-network',
+        networkId: 1,
+        chainId: 1,
+    },
+    hardfork: 'berlin'
+};
+
+var accessList = [
+    {
+      address: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
+      storageKeys: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55'
+    }
+];
+
 var clone = function (object) { return object ? _.clone(object) : []; };
 
 // @NOTE eth.account.signTransaction was updated to default gasLimit to provided gas if not
@@ -460,6 +477,27 @@ var tests = [
         },
         error: true
     },
+    // {
+    //     address: '0x2c7536E3605D9C16a7a3D7b1898e529396a65c23',
+    //     privateKey: '0x4c0883a69102937d6231471b5dbb6204fe5129617082792ae468d01a3f362318',
+    //     transaction: {
+    //         chainId: 1,
+    //         type: '0x01',
+    //         nonce: 0,
+    //         gasPrice: "20000000000",
+    //         gas: 21000,
+    //         to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
+    //         value: "1000000000",
+    //         data: "",
+    //         common: commonBerlin,
+    //         accessList: accessList
+    //     },
+    //     // signature from eth_signTransaction
+    //     rawTransaction: "0xf868808504a817c80082520894f0109fc8df283027b6285cc889f5aa624eac1f55843b9aca008026a0afa02d193471bb974081585daabf8a751d4decbb519604ac7df612cc11e9226da04bf1bd55e82cebb2b09ed39bbffe35107ea611fa212c2d9a1f1ada4952077118",
+    //     oldSignature: "0xf868808504a817c80082520894f0109fc8df283027b6285cc889f5aa624eac1f55843b9aca008026a0afa02d193471bb974081585daabf8a751d4decbb519604ac7df612cc11e9226da04bf1bd55e82cebb2b09ed39bbffe35107ea611fa212c2d9a1f1ada4952077118",
+    //     transactionHash: "0xab0f71614c37231d71ae521ce188a9c7c9d5e976124a91f62f9f125348dd0326",
+    //     messageHash: "0x2c7903a33b55caf582d170f21595f1a7e598df3fa61b103ea0cd9d6b2a92565d"
+    // },
 ];
 
 describe("eth", function () {
