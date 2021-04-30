bc771a82c0594001b264f31ceb8e31b64a0372f8
Wyatt Barnes <me@wyatt.email>
2021-04-29 14:46:46 -1000

    eslint fixes

--- packages/web3-providers-base/src/index.ts
+++ packages/web3-providers-base/src/index.ts
@@ -1,23 +1,28 @@
-import { ProviderOptions } from '../types'
+// TODO Fix eslint issues
+// eslint-disable-next-line import/no-unresolved, import/extensions
+import { ProviderOptions } from '../types';
 
 export default class Web3ProviderBase {
   providerString = ''
+
   protectProvider = false
+
   connected = false
+
   supportsSubscriptions = false
 
   constructor(options: ProviderOptions) {
-    this.providerString = options.providerString
-    this.protectProvider = options.protectProvider
-    this.supportsSubscriptions = options.supportsSubscriptions
+    this.providerString = options.providerString;
+    this.protectProvider = options.protectProvider;
+    this.supportsSubscriptions = options.supportsSubscriptions;
   }
 
   setProvider(providerString: string) {
     try {
-      if (this.protectProvider) throw Error('Provider is protected')
-      this.providerString = providerString
+      if (this.protectProvider) throw Error('Provider is protected');
+      this.providerString = providerString;
     } catch (error) {
-      throw Error(`Error setting provider: ${error.message}`)
+      throw Error(`Error setting provider: ${error.message}`);
     }
   }
 }
