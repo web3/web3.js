bdae83510106451b3a0d53ec612c6f9dcf593a94
Wyatt Barnes <me@wyatt.email>
2021-05-18 12:48:02 -1000

    Update tests, init missing tests

--- packages/web3-eth/test/unit/sanityChecks.test.ts
+++ packages/web3-eth/test/unit/sanityChecks.test.ts
@@ -2,62 +2,50 @@ import { HttpRpcResponse } from 'web3-providers-http/types';
 import Web3RequestManager from 'web3-core-requestmanager';
 
 import Web3Eth from '../../src';
-import { blockIdentifier, EthTransaction } from '../../types';
+import { BlockIdentifier, EthTransaction } from '../../types';
 import { testConfig } from './testConfig';
 
-// @ts-ignore undefined was added to test default blockIdentifier set by Web3Eth method
-const blockIdentifiers: blockIdentifier[] = [
-    undefined,
-    'latest',
-    'earliest',
-    'pending',
-    42,
-];
+// const blockIdentifiers: BlockIdentifier[] = [
+//     'latest',
+//     'earliest',
+//     'pending',
+//     42,
+// ];
 
 let web3Eth: Web3Eth;
 let web3RequestManagerSendSpy: jest.SpyInstance;
 
-async function configureWeb3EthCall(
-    methodName: string,
-    parameters: { [key: string]: string | number | BigInt },
-    enumerateBlockIdentifiers: boolean
-): Promise<HttpRpcResponse[]> {
-    if (enumerateBlockIdentifiers) {
-        const methodPromises = [];
-        for (const blockIdentifier of blockIdentifiers) {
-            if (blockIdentifier !== undefined)
-                parameters.block = blockIdentifier;
-            // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
-            methodPromises.push(web3Eth[methodName](parameters));
-        }
-        return await Promise.all(methodPromises);
-    }
-    // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
-    return await web3Eth[methodName](parameters);
-}
+// async function configureWeb3EthCall(
+//     methodName: string,
+//     parameters: (string|number)[],
+//     enumerateBlockIdentifiers: boolean,
+//     rpcIdParameter?: number
+// ): Promise<HttpRpcResponse[]> {
+//     if (enumerateBlockIdentifiers) {
+//         const methodPromises = [];
+//         for (const blockIdentifier of blockIdentifiers) {
+//             const parametersCopy = [...parameters, blockIdentifier]
+//             if (rpcIdParameter) parametersCopy.push(rpcIdParameter)
+//             // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
+//             methodPromises.push(web3Eth[methodName](...parametersCopy));
+//         }
+//         return await Promise.all(methodPromises);
+//     }
+//     if (rpcIdParameter) parameters.push(rpcIdParameter)
+//     // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
+//     return await web3Eth[methodName](...parameters);
+// }
 
 function checkForExpected(
     expectedResult: HttpRpcResponse,
     actualResult: HttpRpcResponse,
     expectedSendParameters: any
 ) {
+    // console.log(expectedSendParameters)
     expect(actualResult).toMatchObject(expectedResult);
     expect(web3RequestManagerSendSpy).toHaveBeenCalledWith(
         expectedSendParameters
     );
-}
-
-function formatTransactionObject(
-    transactionObject: EthTransaction
-): [EthTransaction] {
-    for (const property in transactionObject) {
-        // @ts-ignore
-        if (typeof transactionObject[property] === 'bigint')
-            transactionObject[property] = `0x${transactionObject[
-                property
-            ].toString(16)}`;
-    }
-    return [transactionObject];
 }
 
 for (const method of testConfig.methods) {
@@ -82,20 +70,14 @@ for (const method of testConfig.methods) {
         });
 
         it('should get expected result - default RPC parameters', async () => {
-            const result = await configureWeb3EthCall(
-                method.name,
-                method.parameters || {},
-                method.enumerateBlockIdentifiers || false
+            // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
+            const result = await web3Eth[method.name](
+                ...(method.parameters || [])
             );
             const expectedSendParameters = {
                 method: method.rpcMethod,
                 jsonrpc: '2.0',
-                params: method.parameterIsTransactionObject
-                    ? // @ts-ignore we know this is a EthTransaction because of the flag
-                      formatTransactionObject(method.parameters)
-                    : method.parameters
-                    ? Object.values(method.parameters)
-                    : [],
+                params: method.parameters || [],
             };
             Array.isArray(result)
                 ? result.forEach((methodResult) => {
@@ -113,20 +95,15 @@ for (const method of testConfig.methods) {
         });
 
         it('should get expected result - id RPC parameter', async () => {
-            const result = await configureWeb3EthCall(
-                method.name,
-                { ...method.parameters, id: testConfig.expectedRpcId },
-                method.enumerateBlockIdentifiers || false
+            // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
+            const result = await web3Eth[method.name](
+                ...(method.parameters || []),
+                testConfig.expectedRpcId
             );
             const expectedSendParameters = {
                 method: method.rpcMethod,
                 jsonrpc: '2.0',
-                params: method.parameterIsTransactionObject
-                    ? // @ts-ignore we know this is a EthTransaction because of the flag
-                      formatTransactionObject(method.parameters)
-                    : method.parameters
-                    ? Object.values(method.parameters)
-                    : [],
+                params: method.parameters || [],
             };
             Array.isArray(result)
                 ? result.forEach((methodResult) => {
