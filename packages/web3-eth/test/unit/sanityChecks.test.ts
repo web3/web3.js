e0e5ca749a3a40b6974f2cc2b84a7678529f5d4b
Wyatt Barnes <me@wyatt.email>
2021-05-18 12:59:31 -1000

    remove unused code

--- packages/web3-eth/test/unit/sanityChecks.test.ts
+++ packages/web3-eth/test/unit/sanityChecks.test.ts
@@ -2,46 +2,16 @@ import { HttpRpcResponse } from 'web3-providers-http/types';
 import Web3RequestManager from 'web3-core-requestmanager';
 
 import Web3Eth from '../../src';
-import { BlockIdentifier, EthTransaction } from '../../types';
 import { testConfig } from './testConfig';
-
-// const blockIdentifiers: BlockIdentifier[] = [
-//     'latest',
-//     'earliest',
-//     'pending',
-//     42,
-// ];
 
 let web3Eth: Web3Eth;
 let web3RequestManagerSendSpy: jest.SpyInstance;
-
-// async function configureWeb3EthCall(
-//     methodName: string,
-//     parameters: (string|number)[],
-//     enumerateBlockIdentifiers: boolean,
-//     rpcIdParameter?: number
-// ): Promise<HttpRpcResponse[]> {
-//     if (enumerateBlockIdentifiers) {
-//         const methodPromises = [];
-//         for (const blockIdentifier of blockIdentifiers) {
-//             const parametersCopy = [...parameters, blockIdentifier]
-//             if (rpcIdParameter) parametersCopy.push(rpcIdParameter)
-//             // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
-//             methodPromises.push(web3Eth[methodName](...parametersCopy));
-//         }
-//         return await Promise.all(methodPromises);
-//     }
-//     if (rpcIdParameter) parameters.push(rpcIdParameter)
-//     // @ts-ignore tsc doesn't like that we're arbitrarily calling methods
-//     return await web3Eth[methodName](...parameters);
-// }
 
 function checkForExpected(
     expectedResult: HttpRpcResponse,
     actualResult: HttpRpcResponse,
     expectedSendParameters: any
 ) {
-    // console.log(expectedSendParameters)
     expect(actualResult).toMatchObject(expectedResult);
     expect(web3RequestManagerSendSpy).toHaveBeenCalledWith(
         expectedSendParameters
