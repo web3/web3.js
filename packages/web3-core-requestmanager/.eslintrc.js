e3739267531a19e82aef939b526789da028917e5
Wyatt Barnes <me@wyatt.email>
2021-05-19 11:57:35 -1000

    Install, configure, and run prettier

--- packages/web3-core-requestmanager/.eslintrc.js
+++ packages/web3-core-requestmanager/.eslintrc.js
@@ -1,24 +1,14 @@
 module.exports = {
-  env: {
-    browser: true,
-    es2021: true,
-    node: true,
-  },
-  extends: [
-    'airbnb-base',
-  ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: {
-    ecmaVersion: 12,
-    sourceType: 'module',
-  },
-  plugins: [
-    '@typescript-eslint',
-  ],
-  // TODO Review these rules, disabled because false positives
-  rules: {
-    'import/no-unresolved': 0,
-    'import/extensions': 0,
-    'no-underscore-dangle': 0
-  },
+    env: {
+        browser: true,
+        es2021: true,
+        node: true,
+    },
+    extends: ['prettier'],
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+        ecmaVersion: 12,
+        sourceType: 'module',
+    },
+    plugins: ['@typescript-eslint'],
 };
--- packages/web3-core-requestmanager/test/unit/send.test.ts
+++ packages/web3-core-requestmanager/test/unit/send.test.ts
@@ -1,24 +1,27 @@
-import Web3ProviderHttp from 'web3-providers-http'
+import Web3ProviderHttp from 'web3-providers-http';
 
-import Web3RequestManager from '../../src/index'
+import Web3RequestManager from '../../src/index';
 
 describe('Web3RequestManager.send', () => {
     it('should call Web3ProviderHttp.send', async () => {
         // Replace method so network call isn't made
-        Web3ProviderHttp.prototype.send = jest.fn()
-        const web3ProviderHttpSendSpy = jest.spyOn(Web3ProviderHttp.prototype, 'send')
+        Web3ProviderHttp.prototype.send = jest.fn();
+        const web3ProviderHttpSendSpy = jest.spyOn(
+            Web3ProviderHttp.prototype,
+            'send'
+        );
         const providerOptions = {
-            providerUrl: 'http://127.0.0.1:8545'
-        }
+            providerUrl: 'http://127.0.0.1:8545',
+        };
         const httpRpcOptions = {
             id: 42,
             jsonrpc: '2.0',
             method: 'testing',
-            params: {foo: 'bar'}
-        }
-        const web3RequestManager = new Web3RequestManager(providerOptions)
-        await web3RequestManager.send(httpRpcOptions)
-        expect(web3ProviderHttpSendSpy).toHaveBeenCalledTimes(1)
-        expect(web3ProviderHttpSendSpy).toHaveBeenCalledWith(httpRpcOptions)
-    })
-})
+            params: { foo: 'bar' },
+        };
+        const web3RequestManager = new Web3RequestManager(providerOptions);
+        await web3RequestManager.send(httpRpcOptions);
+        expect(web3ProviderHttpSendSpy).toHaveBeenCalledTimes(1);
+        expect(web3ProviderHttpSendSpy).toHaveBeenCalledWith(httpRpcOptions);
+    });
+});
