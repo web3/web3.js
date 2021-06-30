1b66314cd2b46f2145e018fa1d0a6d61474f88bd
Wyatt Barnes <me@wyatt.email>
2021-06-29 21:25:19 -1000

    Update tests for eip-1559 changes

--- test/eth.accounts.signTransaction.js
+++ test/eth.accounts.signTransaction.js
@@ -595,6 +595,33 @@ describe("eth", function () {
                     var provider = new FakeHttpProvider();
                     var web3 = new Web3(provider);
 
+                    provider.injectResult({
+                        "difficulty": "0x4ea3f27bc",
+                        "extraData": "0x476574682f4c5649562f76312e302e302f6c696e75782f676f312e342e32",
+                        "gasLimit": "0x1388",
+                        "gasUsed": "0x0",
+                        "hash": "0xdc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae",
+                        "logsBloom": "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+                        "miner": "0xbb7b8287f3f0a933474a79eae42cbca977791171",
+                        "mixHash": "0x4fffe9ae21f1c9e15207b1f472d5bbdd68c9595d461666602f2be20daf5e7843",
+                        "nonce": "0x689056015818adbe",
+                        "number": "0x1b4",
+                        "parentHash": "0xe99e022112df268087ea7eafaf4790497fd21dbeeb6bd7a1721df161a6657a54",
+                        "receiptsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+                        "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+                        "size": "0x220",
+                        "stateRoot": "0xddc8b0234c2e0cad087c8b389aa7ef01f7d79b2570bccb77ce48648aa61c904d",
+                        "timestamp": "0x55ba467c",
+                        "totalDifficulty": "0x78ed983323d",
+                        "transactions": [],
+                        "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+                        "uncles": []
+                    });
+                    provider.injectValidation(function (payload) {
+                        assert.equal(payload.jsonrpc, '2.0');
+                        assert.equal(payload.method, 'eth_getBlockByNumber');
+                        assert.deepEqual(payload.params, ['latest', false]);
+                    });
                     provider.injectResult('0x5022');
                     provider.injectValidation(function (payload) {
                         assert.equal(payload.jsonrpc, '2.0');
@@ -694,14 +721,41 @@ describe("eth", function () {
                     provider.injectResult(1);
                     provider.injectValidation(function (payload) {
                         assert.equal(payload.jsonrpc, '2.0');
-                        assert.equal(payload.method, 'eth_gasPrice');
-                        assert.deepEqual(payload.params, []);
+                        assert.equal(payload.method, 'eth_getTransactionCount');
+                        assert.deepEqual(payload.params, [test.address, "latest"]);
+                    });
+                    provider.injectResult({
+                        "difficulty": "0x4ea3f27bc",
+                        "extraData": "0x476574682f4c5649562f76312e302e302f6c696e75782f676f312e342e32",
+                        "gasLimit": "0x1388",
+                        "gasUsed": "0x0",
+                        "hash": "0xdc0818cf78f21a8e70579cb46a43643f78291264dda342ae31049421c82d21ae",
+                        "logsBloom": "0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000",
+                        "miner": "0xbb7b8287f3f0a933474a79eae42cbca977791171",
+                        "mixHash": "0x4fffe9ae21f1c9e15207b1f472d5bbdd68c9595d461666602f2be20daf5e7843",
+                        "nonce": "0x689056015818adbe",
+                        "number": "0x1b4",
+                        "parentHash": "0xe99e022112df268087ea7eafaf4790497fd21dbeeb6bd7a1721df161a6657a54",
+                        "receiptsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+                        "sha3Uncles": "0x1dcc4de8dec75d7aab85b567b6ccd41ad312451b948a7413f0a142fd40d49347",
+                        "size": "0x220",
+                        "stateRoot": "0xddc8b0234c2e0cad087c8b389aa7ef01f7d79b2570bccb77ce48648aa61c904d",
+                        "timestamp": "0x55ba467c",
+                        "totalDifficulty": "0x78ed983323d",
+                        "transactions": [],
+                        "transactionsRoot": "0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421",
+                        "uncles": []
+                    });
+                    provider.injectValidation(function (payload) {
+                        assert.equal(payload.jsonrpc, '2.0');
+                        assert.equal(payload.method, 'eth_getBlockByNumber');
+                        assert.deepEqual(payload.params, ['latest', false]);
                     });
                     provider.injectResult(1);
                     provider.injectValidation(function (payload) {
                         assert.equal(payload.jsonrpc, '2.0');
-                        assert.equal(payload.method, 'eth_getTransactionCount');
-                        assert.deepEqual(payload.params, [test.address, "latest"]);
+                        assert.equal(payload.method, 'eth_gasPrice');
+                        assert.deepEqual(payload.params, []);
                     });
                     provider.injectResult(1);
                     provider.injectValidation(function (payload) {
