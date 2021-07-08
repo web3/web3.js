f06214fa2e0df1e3bb68abaa5ae711e9729a0e7a
Wyatt Barnes <me@wyatt.email>
2021-07-07 18:55:30 -1000

    Debug

--- test/e2e.contract.deploy.js
+++ test/e2e.contract.deploy.js
@@ -86,6 +86,7 @@ describe('contract.deploy [ @E2E ]', function() {
 
                 assert.fail();
             } catch(err){
+                console.log(err)
                 assert(err.message.includes(maxFeeLessThanBaseFee));
                 assert(err.receipt.status === false);
             }
