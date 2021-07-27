1cbace4677fba6c7307b6a45361ee875b6a270c5
jdevcs <junaid@chainsafe.io>
2021-07-27 15:49:02 +0200

    formatting

--- packages/web3-providers-ws/src/index.ts
+++ packages/web3-providers-ws/src/index.ts
@@ -3,7 +3,6 @@ import { WebSocketOptions } from './types';
 import Web3ProviderBase from 'web3-providers-base';
 
 export default class Web3ProviderWS extends Web3ProviderBase {
-
     private _webSocketConnection?: w3cwebsocket;
     private _options: WebSocketOptions;
 
@@ -18,20 +17,31 @@ export default class Web3ProviderWS extends Web3ProviderBase {
 
     connect() {
         try {
-            this._webSocketConnection = new w3cwebsocket(this._options.providerUrl, this._options.protocol, undefined, this._options.headers, this._options.requestOptions, this._options.clientConfig);
+            this._webSocketConnection = new w3cwebsocket(
+                this._options.providerUrl,
+                this._options.protocol,
+                undefined,
+                this._options.headers,
+                this._options.requestOptions,
+                this._options.clientConfig
+            );
         } catch (error) {
             throw Error(`Failed to create WebSocket client: ${error.message}`);
         }
-    };
+    }
 
     private static _validateProviderUrl(providerUrl: string): boolean {
         try {
             return (
                 typeof providerUrl !== 'string' ||
-                /^(wss?:\/\/)([0-9]{1,3}(?:\.[0-9]{1,3}){3}|(?=[^\/]{1,254}(?![^\/]))(?:(?=[a-zA-Z0-9-]{1,63}\.)(?:xn--+)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,63}):([0-9]{1,5})$/i.test(providerUrl)
+                /^(wss?:\/\/)([0-9]{1,3}(?:\.[0-9]{1,3}){3}|(?=[^\/]{1,254}(?![^\/]))(?:(?=[a-zA-Z0-9-]{1,63}\.)(?:xn--+)?[a-zA-Z0-9]+(?:-[a-zA-Z0-9]+)*\.)+[a-zA-Z]{2,63}):([0-9]{1,5})$/i.test(
+                    providerUrl
+                )
             );
         } catch (error) {
-            throw Error(`Failed to validate WebSocket Provider string: ${error.message}`);
+            throw Error(
+                `Failed to validate WebSocket Provider string: ${error.message}`
+            );
         }
     }
 }
--- packages/web3-providers-ws/src/types.ts
+++ packages/web3-providers-ws/src/types.ts
@@ -1,8 +1,8 @@
-import { OutgoingHttpHeaders } from "http";
-import { IClientConfig } from "websocket";
-import { ProviderOptions } from "../../web3-providers-base/lib/types";
+import { OutgoingHttpHeaders } from 'http';
+import { IClientConfig } from 'websocket';
+import { ProviderOptions } from '../../web3-providers-base/lib/types';
 
-export type WebSocketOptions = ProviderOptions  & {
+export type WebSocketOptions = ProviderOptions & {
     protocol?: string;
     headers?: OutgoingHttpHeaders;
     requestOptions?: object;
