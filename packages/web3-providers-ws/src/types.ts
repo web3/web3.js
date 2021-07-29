ce7136f91312e7405d0613b0c712087ba122bec0
jdevcs <junaid@chainsafe.io>
2021-07-29 16:49:36 +0200

    added WSErrors enum modified WebSocketOptions

--- packages/web3-providers-ws/src/types.ts
+++ packages/web3-providers-ws/src/types.ts
@@ -2,21 +2,35 @@ import { OutgoingHttpHeaders } from 'http';
 import { IClientConfig } from 'websocket';
 import { ProviderOptions } from '../../web3-providers-base/lib/types';
 
-export type WebSocketOptions = ProviderOptions & {
+export type ReconnectOptions = {
+    auto: boolean;
+    delay: number;
+    maxAttempts: number;
+    onTimeout: boolean;
+};
+export interface WebSocketOptions extends ProviderOptions {
     protocol?: string;
     headers?: OutgoingHttpHeaders;
     requestOptions?: object;
     clientConfig?: IClientConfig;
-};
+    customTimeout?: number;
+    reconnectOptions?: ReconnectOptions;
+}
 
 export enum WSStatus {
-    DATA = 'data',
-    CLOSE = 'close',
-    ERROR = 'error',
-    CONNECT = 'connect',
-    RECONNECT = 'reconnect',
+    DATA =  'Data ',
+    CLOSE = 'Close ',
+    ERROR = 'Error ',
+    CONNECT = 'Connect ',
+    RECONNECT = 'Reconnect ',
 }
 
 export enum WSErrors {
-    ConnectionNotOpenError = 'ConnectionNotOpenError',
+    ConnectionTimeout =                     'Connection Timeout ',
+    ConnectionNotOpenError =                'Connection Not Open Error ',
+    PendingRequestsOnReconnectingError =    'Pending Requests On Reconnecting Error ',
+    MaxAttemptsReachedOnReconnectingError = 'Max Attempts Reached On Reconnecting Error ',
+    InvalidConnection =                     'Invalid Connection ',
 }
+
+
