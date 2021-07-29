1676447fe958a43e071331e8985e41bdb993ca44
Wyatt Barnes <me@wyatt.email>
2021-07-28 14:07:44 -1000

    Revert git url and tag change

--- scripts/e2e.ganache.core.sh
+++ scripts/e2e.ganache.core.sh
@@ -9,9 +9,9 @@
 set -o errexit
 
 # Install ganache-core
-git clone https://github.com/ChainSafe/ganache-core
+git clone https://github.com/trufflesuite/ganache-core
 cd ganache-core
-git checkout 2.13.2-overflow-fix
+git checkout tags/v2.13.0
 
 # Install via registry and verify
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>"
