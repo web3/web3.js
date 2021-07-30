0c6b8c2880b3a59096f4082898f5fc1d741767aa
jdevcs <junaid@chainsafe.io>
2021-07-30 16:06:21 +0200

    JsonRpcResponse

--- packages/web3-providers-ws/src/types.ts
+++ packages/web3-providers-ws/src/types.ts
@@ -45,5 +45,12 @@ export interface JsonRpcPayload {
     jsonrpc: string;
     method: string;
     params: any[];
-    id?: string | number;
+    id: string | number;
+}
+
+export interface JsonRpcResponse {
+    jsonrpc: string;
+    id: number;
+    result?: any;
+    error?: string;
 }
