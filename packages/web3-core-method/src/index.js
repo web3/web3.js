1b0db3b612ef9a7becf865b9bdaf5014b6d5adfd
Wyatt Barnes <me@wyatt.email>
2021-08-04 00:53:08 -1000

    Debugging e2e_ganache - revert and console.log

--- packages/web3-core-method/src/index.js
+++ packages/web3-core-method/src/index.js
@@ -786,9 +786,7 @@ Method.prototype.buildCall = function () {
                 payload.params[0].type = _handleTxType(payload.params[0]);
 
             _handleTxPricing(method, payload.params[0]).then(txPricing => {
-                for (const txPrice of Object.keys(txPricing)) {
-                    txPricing[txPrice] = utils.toHex(txPrice)
-                }
+                console.log('txPricing', txPricing)
                 payload.params[0] = {...payload.params[0], ...txPricing};
 
                 if (isSendTx) {
