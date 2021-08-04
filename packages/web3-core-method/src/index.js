7e1d3386e37f9e5a0055f4257b25e0f6988b3f48
Wyatt Barnes <me@wyatt.email>
2021-08-04 10:35:08 -1000

    Debug - move deletion of type

--- packages/web3-core-method/src/index.js
+++ packages/web3-core-method/src/index.js
@@ -786,13 +786,12 @@ Method.prototype.buildCall = function () {
             if (typeof payload.params[0].type === 'undefined') 
                 payload.params[0].type = _handleTxType(payload.params[0]);
             console.log('debug2', payload)
-            if (payload.params[0].type === '0x0') delete payload.params[0].type;
-            console.log('debug3', payload)
 
             _handleTxPricing(method, payload.params[0]).then(txPricing => {
                 payload.params[0] = {...payload.params[0], ...txPricing};
+                if (payload.params[0].type === '0x0') delete payload.params[0].type;
 
-                console.log('debug4', payload)
+                console.log('debug3', payload)
 
                 if (isSendTx) {
                     setTimeout(() => {
@@ -802,7 +801,7 @@ Method.prototype.buildCall = function () {
 
                 sendRequest(payload, method);
 
-                console.log('debug5', payload)
+                console.log('debug4', payload)
             })
         } else {
             if (isSendTx) {
