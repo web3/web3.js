ee60b0c14c435afb3d9496cb1345f27909c73ec7
Wyatt Barnes <me@wyatt.email>
2021-08-11 13:13:42 -1000

    Init web3-providers-eip1193 tests

--- packages/web3-providers-eip1193/test/unit/constructor.test.ts
+++ packages/web3-providers-eip1193/test/unit/constructor.test.ts
@@ -1,14 +1,36 @@
-import { ProviderOptions } from 'web3-providers-base/lib/types';
+import {
+    ProviderEventListener,
+    RequestArguments,
+    Web3ProviderEvents,
+} from 'web3-core-types/lib/types';
 
-import Web3ProvidersHttp from '../../src/index';
+import Web3ProvidersEip1193 from '../../src/index';
 
-describe('constructs a Web3ProvidersHttp instance with expected properties', () => {
-    const providerOptions: ProviderOptions = {
-        providerUrl: 'http://127.0.0.1:8545',
-    };
+describe('constructs a Web3ProvidersEip1193 instance with expected properties', () => {
+    it('should instantiate with expected properties', () => {
+        const eip1193Provider = {
+            request: async (args: RequestArguments) => {
+                return {
+                    id: 1,
+                    jsonrpc: '2.0',
+                    result: [],
+                };
+            },
+            on: (
+                web3ProviderEvents: Web3ProviderEvents,
+                listener: ProviderEventListener
+            ) => eip1193Provider,
+        };
+        const web3ProvidersEip1193 = new Web3ProvidersEip1193(eip1193Provider);
+        expect(web3ProvidersEip1193.web3Client).toBe(eip1193Provider);
+    });
 
-    it('should set the correct property values', () => {
-        const web3ProvidersHttp = new Web3ProvidersHttp(providerOptions);
-        expect(web3ProvidersHttp).toMatchObject(providerOptions);
+    it('should fail to instantiate with invalid client error', () => {
+        expect(() => {
+            // @ts-ignore - Ignore invalid type
+            new Web3ProvidersEip1193({});
+        }).toThrowError(
+            'Failed to set web3 client: Invalid EIP-1193 client provided'
+        );
     });
 });
