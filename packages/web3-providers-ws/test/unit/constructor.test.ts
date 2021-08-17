a40326896d2de465f2f2c910acaef2265c385799
jdevcs <junaid@chainsafe.io>
2021-08-17 16:16:16 +0200

    fixed tests

--- packages/web3-providers-ws/test/unit/constructor.test.ts
+++ packages/web3-providers-ws/test/unit/constructor.test.ts
@@ -2,18 +2,9 @@ import Web3ProviderWS from '../../src/index';
 import { WebSocketOptions } from '../../src/types';
 
 describe('Web3ProviderWS Tests', () => {
-    let providerOptions: WebSocketOptions;
-
-    beforeEach(() => {
-        providerOptions = {
-            providerUrl: 'wss://127.0.0.1:8545',
-        };
-    });
-
     it('should error because invalid WS address', () => {
-        providerOptions.providerUrl = 'http://127.0.0.1:8545';
         expect(() => {
-            new Web3ProviderWS(providerOptions);
+            new Web3ProviderWS('http://127.0.0.1:8545');
         }).toThrowError('Invalid WebSocket URL provided');
     });
 });
--- packages/web3-providers-ws/test/unit/request.test.ts
+++ packages/web3-providers-ws/test/unit/request.test.ts
@@ -1,31 +1,35 @@
-import { ProviderOptions, RpcOptions } from 'web3-providers-base/lib/types';
+import { RpcOptions } from 'web3-providers-base/lib/types';
 import { EventEmitter } from 'events';
 
 import Web3ProviderWS from '../../src/index';
-import { JsonRpcResponse, WebSocketOptions, WSStatus } from '../../src/types';
+import { WebSocketOptions } from '../../src/types';
 import { doesNotMatch } from 'assert/strict';
+import {
+    RequestArguments,
+    Web3ProviderEvents,
+} from 'web3-core-types/lib/types';
 
 describe('Web3ProvidersWS.request', () => {
-    const rpcOptions: RpcOptions = {
-        id: 1,
-        jsonrpc: '2.0',
+    const request: RequestArguments = {
+        rpcOptions: { id: 1, jsonrpc: '2.0' },
         method: 'eth_blockNumber',
         params: [],
     };
     jest.setTimeout(30000);
     it('should return RpcResponse', async (done) => {
         //jest.useFakeTimers();
-        const web3ProvidersWS = new Web3ProviderWS('ws://127.0.0.1:8546');
+        const web3ProvidersWS: Web3ProviderWS = new Web3ProviderWS(
+            'ws://127.0.0.1:8546'
+        );
         const callback = jest.fn();
 
-        web3ProvidersWS.on(WSStatus.DATA, (data: any) => {
+        web3ProvidersWS.on(Web3ProviderEvents.Message, (data: any) => {
             console.log(JSON.stringify(data));
             callback();
         });
 
-        await web3ProvidersWS.connect();
         await new Promise((r) => setTimeout(r, 5000));
-        await web3ProvidersWS.request(rpcOptions);
+        await web3ProvidersWS.request(request);
 
         //expect(callback).toBeCalledTimes(1);
 
