837379cc35a1469126007561f6cd3a43d19d767d
Wyatt Barnes <me@wyatt.email>
2021-08-23 20:46:56 -1000

    Remove chainId check from web3-eth tests

--- packages/web3-eth/test/unit/hasParams.test.ts
+++ packages/web3-eth/test/unit/hasParams.test.ts
@@ -10,22 +10,10 @@ for (const testHasParams of testsHasParams) {
         let web3Eth: Web3Eth;
 
         beforeAll(() => {
-            const chainIdResult = {
-                id: 42,
-                jsonrpc: '2.0',
-                result: '0x1',
-            };
-
             Web3ProvidersHttp.prototype.request = jest.fn();
             web3ProvidersHttpRequestSpy = jest.spyOn(
                 Web3ProvidersHttp.prototype,
                 'request'
-            );
-            // Web3ProviderHttp makes a request to get chainId of
-            // connected client upon instantiation, so we mock the reponse
-            // @ts-ignore mockReturnValueOnce added by jest
-            Web3ProvidersHttp.prototype.request.mockReturnValueOnce(
-                chainIdResult
             );
             web3Eth = new Web3Eth({ web3Client: 'http://127.0.0.1:8545' });
         });
