221be617bef1c5c9fb50fb6f50a6b4ad33bb7cf4
jdevcs <junaid@chainsafe.io>
2021-08-25 14:39:10 +0200

    Web3ProviderWS instance and connection test

--- packages/web3-providers-ws/test/unit/constructor.test.ts
+++ packages/web3-providers-ws/test/unit/constructor.test.ts
@@ -1,10 +1,30 @@
+import { doesNotMatch } from 'assert';
+import { Web3ProviderEvents } from 'web3-core-types/lib/types';
 import Web3ProviderWS from '../../src/index';
-import { WebSocketOptions } from '../../src/types';
 
 describe('Web3ProviderWS Tests', () => {
+    let provider: Web3ProviderWS;
     it('should error because invalid WS address', () => {
         expect(() => {
             new Web3ProviderWS('http://127.0.0.1:8545');
         }).toThrowError('Invalid WebSocket URL provided');
     });
+
+    it('should create instance of Web3ProviderWS', async (done) => {
+        
+        let isConnected = (status: boolean) =>{
+            expect(status).toBeTruthy();
+            provider.disconnect(0);
+            done();
+        }
+
+        provider = new Web3ProviderWS('ws://127.0.0.1:8545');
+
+        provider.on(Web3ProviderEvents.Connect, () => {
+            isConnected(true);
+        });
+        
+    });
+    
 });
+
