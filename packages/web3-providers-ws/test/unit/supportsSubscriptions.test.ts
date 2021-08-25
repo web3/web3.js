bcf836c439dd8236fb10d207b7ade6f84ccbc8f6
jdevcs <junaid@chainsafe.io>
2021-08-25 14:40:32 +0200

    supportsSubscriptions test jest fix

--- packages/web3-providers-ws/test/unit/supportsSubscriptions.test.ts
+++ packages/web3-providers-ws/test/unit/supportsSubscriptions.test.ts
@@ -2,8 +2,9 @@ import Web3ProviderWS from '../../src/index';
 import { WebSocketOptions } from '../../src/types';
 
 describe('web3ProvidersWS.supportsSubscriptions', () => {
-    it('should return true', () => {
+    it('should return true', async () => {
         const web3ProvidersWS = new Web3ProviderWS('ws://127.0.0.1:8546');
+        await new Promise((r) => setTimeout(r, 1000));
         expect(web3ProvidersWS.supportsSubscriptions()).toBeTruthy();
     });
 });
