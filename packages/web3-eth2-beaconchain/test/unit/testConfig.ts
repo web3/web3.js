dcff000a29d651dc50a53018796ab17e8f23edd0
Alex <alex.luu@mail.utoronto.ca>
2021-09-09 11:24:13 -0400

    adding tests

--- packages/web3-eth2-beaconchain/test/unit/testConfig.ts
+++ packages/web3-eth2-beaconchain/test/unit/testConfig.ts
@@ -6,7 +6,12 @@ export const testsNoParams = [
         endpoint: 'genesis',
         expectedResult: {
             ...baseExpectedResult,
-            endpoint: 'genesis',
+            data: {
+                genesis_time: '1590832934',
+                genesis_validators_root:
+                    '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                genesis_fork_version: '0x00000000',
+            },
         },
     },
     {
@@ -14,6 +19,16 @@ export const testsNoParams = [
         endpoint: 'pool/voluntary_exits',
         expectedResult: {
             ...baseExpectedResult,
+            data: [
+                {
+                    message: {
+                        epoch: '1',
+                        validator_index: '1',
+                    },
+                    signature:
+                        '0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505',
+                },
+            ],
         },
     },
     {
@@ -21,6 +36,38 @@ export const testsNoParams = [
         endpoint: 'pool/proposer_slashings',
         expectedResult: {
             ...baseExpectedResult,
+            data: [
+                {
+                    signed_header_1: {
+                        message: {
+                            slot: '1',
+                            proposer_index: '1',
+                            parent_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            state_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            body_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                        },
+                        signature:
+                            '0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505',
+                    },
+                    signed_header_2: {
+                        message: {
+                            slot: '1',
+                            proposer_index: '1',
+                            parent_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            state_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            body_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                        },
+                        signature:
+                            '0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505',
+                    },
+                },
+            ],
         },
     },
     {
@@ -28,6 +75,48 @@ export const testsNoParams = [
         endpoint: 'pool/attester_slashings',
         expectedResult: {
             ...baseExpectedResult,
+            data: [
+                {
+                    attestation_1: {
+                        attesting_indices: ['1'],
+                        signature:
+                            '0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505',
+                        data: {
+                            slot: '1',
+                            index: '1',
+                            beacon_block_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            source: {
+                                epoch: '1',
+                                root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            },
+                            target: {
+                                epoch: '1',
+                                root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            },
+                        },
+                    },
+                    attestation_2: {
+                        attesting_indices: ['1'],
+                        signature:
+                            '0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505',
+                        data: {
+                            slot: '1',
+                            index: '1',
+                            beacon_block_root:
+                                '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            source: {
+                                epoch: '1',
+                                root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            },
+                            target: {
+                                epoch: '1',
+                                root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                            },
+                        },
+                    },
+                },
+            ],
         },
     },
 ];
@@ -39,6 +128,9 @@ export const testsHasParams = [
         params: ['head'],
         expectedResult: {
             ...baseExpectedResult,
+            data: {
+                root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+            },
         },
     },
     {
@@ -47,6 +139,11 @@ export const testsHasParams = [
         params: ['head'],
         expectedResult: {
             ...baseExpectedResult,
+            data: {
+                previous_version: '0x00000000',
+                current_version: '0x00000000',
+                epoch: '1',
+            },
         },
     },
     {
@@ -55,6 +152,22 @@ export const testsHasParams = [
         params: ['head', 1],
         expectedResult: {
             ...baseExpectedResult,
+            data: {
+                index: '1',
+                balance: '1',
+                status: 'active_ongoing',
+                validator: {
+                    pubkey: '0x93247f2209abcacf57b75a51dafae777f9dd38bc7053d1af526f220a7489a6d3a2753e5f3e8b1cfe39b56f43611df74a',
+                    withdrawal_credentials:
+                        '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                    effective_balance: '1',
+                    slashed: false,
+                    activation_eligibility_epoch: '1',
+                    activation_epoch: '1',
+                    exit_epoch: '1',
+                    withdrawable_epoch: '1',
+                },
+            },
         },
     },
     {
@@ -63,6 +176,20 @@ export const testsHasParams = [
         params: ['head'],
         expectedResult: {
             ...baseExpectedResult,
+            data: {
+                previous_justified: {
+                    epoch: '1',
+                    root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                },
+                current_justified: {
+                    epoch: '1',
+                    root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                },
+                finalized: {
+                    epoch: '1',
+                    root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                },
+            },
         },
     }, // need to test multiple parameters and optional parameters
     {
@@ -71,6 +198,24 @@ export const testsHasParams = [
         params: ['head'],
         expectedResult: {
             ...baseExpectedResult,
+            data: {
+                root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                canonical: true,
+                header: {
+                    message: {
+                        slot: '1',
+                        proposer_index: '1',
+                        parent_root:
+                            '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                        state_root:
+                            '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                        body_root:
+                            '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+                    },
+                    signature:
+                        '0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505',
+                },
+            },
         },
     },
     {
@@ -79,6 +224,7 @@ export const testsHasParams = [
         params: ['head'],
         expectedResult: {
             ...baseExpectedResult,
+            version: "phase0"
         },
     },
     {
@@ -87,6 +233,9 @@ export const testsHasParams = [
         params: ['head'],
         expectedResult: {
             ...baseExpectedResult,
+            data: {
+                root: "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+              }
         },
     },
     {
@@ -95,6 +244,25 @@ export const testsHasParams = [
         params: ['head'],
         expectedResult: {
             ...baseExpectedResult,
+            data: [
+                {
+                  "aggregation_bits": "0x01",
+                  "signature": "0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505",
+                  "data": {
+                    "slot": "1",
+                    "index": "1",
+                    "beacon_block_root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+                    "source": {
+                      "epoch": "1",
+                      "root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+                    },
+                    "target": {
+                      "epoch": "1",
+                      "root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+                    }
+                  }
+                }
+              ]
         },
     },
     {
@@ -206,6 +374,25 @@ export const testsHasParams = [
         },
         expectedResult: {
             ...baseExpectedResult,
+            data: [
+                {
+                  "aggregation_bits": "0x01",
+                  "signature": "0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505",
+                  "data": {
+                    "slot": "1",
+                    "index": "1",
+                    "beacon_block_root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2",
+                    "source": {
+                      "epoch": "1",
+                      "root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+                    },
+                    "target": {
+                      "epoch": "1",
+                      "root": "0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2"
+                    }
+                  }
+                }
+              ]
         },
     },
     {
