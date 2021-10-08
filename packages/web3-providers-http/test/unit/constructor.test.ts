6eac3ee746ce087f1b665b2b663e825ba97512b8
Wyatt Barnes <me@wyatt.email>
2021-10-07 17:17:22 -1000

    Refactor test files and fixtures. Init remaining tests

--- packages/web3-providers-http/test/unit/constructor.test.ts
+++ packages/web3-providers-http/test/unit/constructor.test.ts
@@ -1,47 +1,41 @@
 import { HttpProvider } from '../../src/index';
-import { httpProviderOptions } from '../fixtures/http_provider_options';
-import { validClients, invalidClients } from '../fixtures/client_urls';
+import { httpProviderOptions, validClients, invalidClients } from '../fixtures/helper_data';
 
-describe('constructs a HttpProvider instance with expected methods', () => {
+describe('HttpProvider', () => {
 	it('should construct with expected methods', () => {
 		const httpProvider = new HttpProvider('http://localhost:8545');
 
-		expect(httpProvider.send).not.toBe(undefined);
-		expect(httpProvider.getStatus).not.toBe(undefined);
-		expect(httpProvider.supportsSubscriptions).not.toBe(undefined);
-		expect(httpProvider.request).not.toBe(undefined);
-		expect(httpProvider.on).not.toBe(undefined);
-		expect(httpProvider.removeListener).not.toBe(undefined);
-		expect(httpProvider.once).not.toBe(undefined);
-		expect(httpProvider.removeAllListeners).not.toBe(undefined);
-		expect(httpProvider.connect).not.toBe(undefined);
-		expect(httpProvider.disconnect).not.toBe(undefined);
-		expect(httpProvider.reset).not.toBe(undefined);
-		expect(httpProvider.reconnect).not.toBe(undefined);
+		expect(httpProvider.send).not.toBeUndefined();
+		expect(httpProvider.getStatus).not.toBeUndefined();
+		expect(httpProvider.supportsSubscriptions).not.toBeUndefined();
+		expect(httpProvider.request).not.toBeUndefined();
+		expect(httpProvider.on).not.toBeUndefined();
+		expect(httpProvider.removeListener).not.toBeUndefined();
+		expect(httpProvider.once).not.toBeUndefined();
+		expect(httpProvider.removeAllListeners).not.toBeUndefined();
+		expect(httpProvider.connect).not.toBeUndefined();
+		expect(httpProvider.disconnect).not.toBeUndefined();
+		expect(httpProvider.reset).not.toBeUndefined();
+		expect(httpProvider.reconnect).not.toBeUndefined();
 	});
-});
 
-describe('Allows for providerOptions to be passed upon instantiation', () => {
-	it('should construct successfully', () => {
-		new HttpProvider('http://localhost:8545', httpProviderOptions);
+	it('Allows for providerOptions to be passed upon instantiation', () => {
+		expect(() => new HttpProvider('http://localhost:8545', httpProviderOptions)).not.toThrow();
 	});
-});
 
-describe('Instantiation with valid clients', () => {
 	for (const validClient of validClients) {
-		it(`should successfully construct instance - ${validClient}`, () => {
-			new HttpProvider(validClient);
+		it(`Instantiation with valid client - ${validClient}`, () => {
+			expect(() => new HttpProvider(validClient)).not.toThrow();
 		});
 	}
-});
 
-describe('Instantiation with invalid clients', () => {
 	for (const invalidClient of invalidClients) {
-		it(`should fail with InvalidClientError - ${invalidClient}`, () => {
-			expect(() => {
-				// @ts-ignore - Purposefully passing invalid types to check validation
-				new HttpProvider(invalidClient);
-			}).toThrowError(`Client URL "${invalidClient}" is invalid.`);
+		it(`Instantiation with invalid client - ${invalidClient.toString()}`, () => {
+			expect(
+				() =>
+					// @ts-expect-error - Purposefully passing invalid types to check validation
+					new HttpProvider(invalidClient),
+			).toThrow(`Client URL "${invalidClient.toString()}" is invalid.`);
 		});
 	}
 });
