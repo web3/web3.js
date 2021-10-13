03ca3344c830ad084a27744f8edc723883023dd6
Wyatt Barnes <me@wyatt.email>
2021-10-12 20:27:04 -1000

    Add mock Beaconchain data

--- packages/web3-providers-http/test/fixtures/test_data.ts
+++ packages/web3-providers-http/test/fixtures/test_data.ts
@@ -44,3 +44,21 @@ export const mockGetBalanceResponse = {
 	jsonrpc: '2.0',
 	result: '0x0234c8a3397aab58', // 158972490234375000
 };
+
+export const mockBeaconBlockHeaderResponse = {
+	data: {
+		root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+		canonical: true,
+		header: {
+			message: {
+				slot: '1',
+				proposer_index: '1',
+				parent_root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+				state_root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+				body_root: '0xcf8e0d4e9587369b2301d0790347320302cc0943d5a1884560367e8208d920f2',
+			},
+			signature:
+				'0x1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505cc411d61252fb6cb3fa0017b679f8bb2305b26a285fa2737f175668d0dff91cc1b66ac1fb663c9bc59509846d6ec05345bd908eda73e670af888da41af171505',
+		},
+	},
+};
