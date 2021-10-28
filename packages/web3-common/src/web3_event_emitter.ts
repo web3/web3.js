273b848238b3a53e315a9c48b0189bc534228e92
Nazar Hussain <nazarhussain@gmail.com>
2021-10-28 16:00:00 +0200

    :sparkles: Add once event listner support

--- packages/web3-common/src/web3_event_emitter.ts
+++ packages/web3-common/src/web3_event_emitter.ts
@@ -5,6 +5,7 @@ export type Web3EventKey<T extends Web3EventMap> = string & keyof T;
 export type Web3EventCallback<T> = (params: T) => void | Promise<void>;
 export interface Web3Emitter<T extends Web3EventMap> {
 	on<K extends Web3EventKey<T>>(eventName: K, fn: Web3EventCallback<T[K]>): void;
+	once<K extends Web3EventKey<T>>(eventName: K, fn: Web3EventCallback<T[K]>): void;
 	off<K extends Web3EventKey<T>>(eventName: K, fn: Web3EventCallback<T[K]>): void;
 	emit<K extends Web3EventKey<T>>(eventName: K, params: T[K]): void;
 }
@@ -15,6 +16,11 @@ export class Web3EventEmitter<T extends Web3EventMap> implements Web3Emitter<T> 
 	public on<K extends Web3EventKey<T>>(eventName: K, fn: Web3EventCallback<T[K]>) {
 		// eslint-disable-next-line @typescript-eslint/no-misused-promises
 		this._emitter.on(eventName, fn);
+	}
+
+	public once<K extends Web3EventKey<T>>(eventName: K, fn: Web3EventCallback<T[K]>) {
+		// eslint-disable-next-line @typescript-eslint/no-misused-promises
+		this._emitter.once(eventName, fn);
 	}
 
 	public off<K extends Web3EventKey<T>>(eventName: K, fn: Web3EventCallback<T[K]>) {
