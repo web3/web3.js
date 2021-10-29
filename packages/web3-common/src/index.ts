8cfcfe517f78c7dd41c7d6764582b19920770cde
Nazar Hussain <nazarhussain@gmail.com>
2021-10-29 22:50:41 +0200

    :art: Update exports for web3-common

--- packages/web3-common/src/index.ts
+++ packages/web3-common/src/index.ts
@@ -4,3 +4,5 @@ export * from './types';
 export * from './web3_base_provider';
 export * from './web3_base_wallet';
 export * from './web3_event_emitter';
+export * from './eth_execution_api';
+export * from './json_rpc';
