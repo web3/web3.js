6ec2cfe9ac42dcfd98b7c651f77234ea47c86f0d
Alex <alex.luu@mail.utoronto.ca>
2021-11-02 01:05:31 -0400

    web3 utils hashing encoding 4.x (#4471)
    * worked on the hashing functions
    
    * adding testcases
    
    * sha3raw test created
    
    * updating testcases
    
    * adding testcases for soliditysha3raw and added working soliditysha3 function
    
    * fixing formatting tests
    
    * adding more tests
    
    * adding more testcases, supports int arrays
    
    * added js-sha3 for testing
    
    * adding js-sha3 testcases
    
    Co-authored-by: Nazar Hussain <nazarhussain@gmail.com>

--- packages/web3-utils/src/hash.ts
+++ packages/web3-utils/src/hash.ts
@@ -1,3 +1,218 @@
-// TODO: Implement later
-// eslint-disable-next-line @typescript-eslint/no-unused-vars
-export const sha3 = (data: string): string => data;
+import { keccak256 } from 'ethereum-cryptography/keccak';
+import { Numbers, typedObject, typedObject2, EncodingTypes } from './types';
+import { leftPad, rightPad, toTwosComplement } from './string_manipulation';
+import { utf8ToHex, hexToBytes, toNumber, bytesToHex } from './converters';
+import { isAddress, isHexStrict } from './validation';
+import {
+	InvalidStringError,
+	InvalidType,
+	InvalidBooleanError,
+	InvalidAddressError,
+	InvalidSizeError,
+	InvalidLargeValueError,
+	InvalidUnsignedIntegerError,
+	InvalidBytesError,
+} from './errors';
+
+const SHA3_NULL = '0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470';
+
+/**
+ *
+ * computes the Keccak-256 hash of the string input and returns a hexstring
+ */
+export const sha3 = (data: string): string | null => {
+	if (typeof data !== 'string') throw new InvalidStringError(data);
+	const newData = isHexStrict(data) ? hexToBytes(data) : data;
+
+	const hash = bytesToHex(keccak256(Buffer.from(newData)));
+
+	// EIP-1052 if hash is equal to c5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470, keccak was given empty data
+	return hash === SHA3_NULL ? null : hash;
+};
+
+/**
+ *Will calculate the sha3 of the input but does return the hash value instead of null if for example a empty string is passed.
+ */
+export const sha3Raw = (data: string): string => {
+	const hash = sha3(data);
+	if (hash === null) {
+		return SHA3_NULL;
+	}
+
+	return hash;
+};
+
+export { keccak256 };
+
+/**
+ * returns type and value
+ */
+const getType = (arg: typedObject | typedObject2 | Numbers): [string, EncodingTypes] => {
+	if (
+		typeof arg === 'object' &&
+		('t' in arg || 'type' in arg) &&
+		('v' in arg || 'value' in arg)
+	) {
+		const type1 = 't' in arg ? arg.t : arg.type;
+		const val = 'v' in arg ? arg.v : arg.value;
+		return [type1, val];
+	}
+	throw new InvalidType(arg);
+};
+
+/**
+ * returns the type with size if uint or int
+ */
+const elementaryName = (name: string): string => {
+	if (name.startsWith('int[')) {
+		return `int256${name.slice(3)}`;
+	}
+	if (name === 'int') {
+		return 'int256';
+	}
+	if (name.startsWith('uint[')) {
+		return `uint256'${name.slice(4)}`;
+	}
+	if (name === 'uint') {
+		return 'uint256';
+	}
+	return name;
+};
+
+/**
+ * returns the size of the value of type 'byte'
+ */
+const parseTypeN = (value: string): number => {
+	const typesize = /^\D+(\d+).*$/.exec(value);
+	return typesize ? parseInt(typesize[1], 10) : 0;
+};
+
+/**
+ * returns the bit length of the value
+ */
+const bitLength = (value: BigInt | number): number => {
+	const updatedVal = value.toString(2);
+	return updatedVal.length;
+};
+
+/**
+ * Pads the value based on size and type
+ * returns a string of the padded value
+ */
+const solidityPack = (type: string, val: EncodingTypes, arraySize?: number): string => {
+	const value = val.toString();
+	if (type === 'string') {
+		if (typeof val === 'string') return utf8ToHex(val);
+		throw new InvalidStringError(val);
+	}
+	if (type === 'bool' || type === 'boolean') {
+		if (typeof val === 'boolean') return val ? '01' : '00';
+		throw new InvalidBooleanError(val);
+	}
+
+	if (type === 'address') {
+		if (!isAddress(value)) {
+			throw new InvalidAddressError(value);
+		}
+		return value;
+	}
+	const name = elementaryName(type);
+	if (type.startsWith('uint')) {
+		const size = parseTypeN(name);
+
+		if (size % 8 || size < 8 || size > 256) {
+			throw new InvalidSizeError(value);
+		}
+		const num = toNumber(value);
+		if (bitLength(num) > size) {
+			throw new InvalidLargeValueError(value);
+		}
+		if (num < BigInt(0)) {
+			throw new InvalidUnsignedIntegerError(value);
+		}
+
+		return size ? leftPad(num.toString(16), (size / 8) * 2) : num.toString(16);
+	}
+
+	if (type.startsWith('int')) {
+		const size = parseTypeN(name);
+		if (size % 8 || size < 8 || size > 256) {
+			throw new InvalidSizeError(type);
+		}
+
+		const num = toNumber(value);
+		if (bitLength(num) > size) {
+			throw new InvalidLargeValueError(value);
+		}
+		if (num < BigInt(0)) {
+			return toTwosComplement(num.toString(), (size / 8) * 2);
+		}
+		return size ? leftPad(num.toString(16), size / 4) : num.toString(16);
+	}
+
+	if (name === 'bytes') {
+		if (value.replace(/^0x/i, '').length % 2 !== 0) {
+			throw new InvalidBytesError(value);
+		}
+		return value;
+	}
+
+	if (type.startsWith('bytes')) {
+		if (value.replace(/^0x/i, '').length % 2 !== 0) {
+			throw new InvalidBytesError(value);
+		}
+		// if no byte size is speciified it will default to 32 bytes
+		const size = arraySize ? 32 : parseTypeN(type);
+
+		if (!size || size < 1 || size > 64 || size < value.replace(/^0x/i, '').length / 2) {
+			throw new InvalidBytesError(value);
+		}
+
+		return rightPad(value, size * 2);
+	}
+	return '';
+};
+
+/**
+ * returns a string of the tightly packed value given based on the type
+ */
+export const processSolidityEncodePackedArgs = (
+	arg: typedObject | typedObject2 | Numbers,
+): string => {
+	const [type, val] = getType(arg);
+
+	// array case
+	if (Array.isArray(val)) {
+		// go through each element of the array and use map function to create new hexarg list
+		const hexArg = val.map((v: Numbers | boolean) => solidityPack(type, v).replace('0x', ''));
+		return hexArg.join('');
+	}
+
+	const hexArg = solidityPack(type, val);
+	return hexArg.replace('0x', '');
+};
+
+/**
+ * Encode packed arguments to a hexstring
+ */
+export const encodePacked = (...values: typedObject[] | typedObject2[]): string => {
+	const args = Array.prototype.slice.call(values);
+
+	const hexArgs = args.map(processSolidityEncodePackedArgs);
+
+	return `0x${hexArgs.join('').toLowerCase()}`;
+};
+
+/**
+ * Will tightly pack values given in the same way solidity would then hash.
+ * returns a hash string, or null if input is empty
+ */
+export const soliditySha3 = (...values: typedObject[] | typedObject2[]): string | null =>
+	sha3(encodePacked(...values));
+
+/**
+ * Will tightly pack values given in the same way solidity would then hash.
+ * returns a hash string, if input is empty will return `0xc5d2460186f7233c927e7db2dcc703c0e500b653ca82273b7bfad8045d85a470`
+ */
+export const soliditySha3Raw = (...values: typedObject[] | typedObject2[]): string =>
+	sha3Raw(encodePacked(...values));
