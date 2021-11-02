86c6a1874b46b2202ae34574c4db23552026875c
Alex <alex.luu@mail.utoronto.ca>
2021-11-02 01:05:31 -0400

    web3 utils hashing encoding 4.x (#4471)
    * worked on the hashing functions
    
    * adding testcases
    
    * sha3raw test created
    
    * updating testcases
    
    * adding testcases for soliditysha3raw and added working soliditysha3 function
    
    * fixing formatting tests
    
    * adding more tests
    
    * adding more testcases, supports int arrays
    
    * added js-sha3 for testing
    
    * adding js-sha3 testcases
    
    Co-authored-by: Nazar Hussain <nazarhussain@gmail.com>

---
+++ packages/web3-utils/test/unit/hash.test.ts
@@ -0,0 +1,106 @@
+import { keccak256 } from 'js-sha3';
+import { sha3, sha3Raw, soliditySha3, soliditySha3Raw, encodePacked } from '../../src/hash';
+import {
+	sha3InvalidData,
+	sha3Data,
+	sha3ValidData,
+	soliditySha3RawValidData,
+	sha3RawValidData,
+	soliditySha3ValidData,
+	soliditySha3InvalidData,
+	compareSha3JSValidData,
+	compareSha3JSRawValidData,
+	encodePackData,
+	encodePackedInvalidData,
+} from '../fixtures/hash';
+/* eslint-disable @typescript-eslint/no-magic-numbers */
+describe('hash', () => {
+	describe('sha3', () => {
+		describe('valid cases', () => {
+			it.each(sha3ValidData)('%s', (input, output) => {
+				expect(sha3(input)).toEqual(output);
+			});
+		});
+
+		describe('invalid cases', () => {
+			it.each(sha3InvalidData)('%s', (input, output) => {
+				expect(() => sha3(input)).toThrow(output);
+			});
+		});
+
+		describe('compare with js-sha3 normal cases', () => {
+			it.each(sha3Data)('%s', input => {
+				// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+				expect(sha3(input)).toEqual(`0x${keccak256(input)}`);
+			});
+		});
+
+		describe('compare with js-sha3 buffer cases', () => {
+			it.each(compareSha3JSValidData)('%s', (input, output) => {
+				// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+				expect(sha3(input)).toEqual(`0x${keccak256(output)}`);
+			});
+		});
+	});
+
+	describe('sha3Raw', () => {
+		describe('valid cases', () => {
+			it.each(sha3RawValidData)('%s', (input, output) => {
+				expect(sha3Raw(input)).toEqual(output);
+			});
+		});
+		describe('comparing with js-sha3 cases', () => {
+			it.each(compareSha3JSRawValidData)('%s', (input, output) => {
+				// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+				expect(sha3Raw(input)).toEqual(`0x${keccak256(output)}`);
+			});
+		});
+		describe('invalid cases', () => {
+			it.each(sha3InvalidData)('%s', (input, output) => {
+				expect(() => sha3Raw(input)).toThrow(output);
+			});
+		});
+	});
+
+	describe('soliditySha3', () => {
+		describe('valid cases', () => {
+			it.each(soliditySha3ValidData)('%s', (input, output) => {
+				// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+				expect(soliditySha3(...input)).toEqual(output);
+			});
+		});
+		describe('invalid cases', () => {
+			it.each(soliditySha3InvalidData)('%s', (input, output) => {
+				expect(() => soliditySha3(input)).toThrow(output);
+			});
+		});
+	});
+
+	describe('soliditySha3Raw', () => {
+		describe('valid cases', () => {
+			it.each(soliditySha3RawValidData)('%s', (input, output) => {
+				// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+				expect(soliditySha3Raw(...input)).toEqual(output);
+			});
+		});
+		describe('invalid cases', () => {
+			it.each(soliditySha3InvalidData)('%s', (input, output) => {
+				expect(() => soliditySha3Raw(input)).toThrow(output);
+			});
+		});
+	});
+
+	describe('encodePacked', () => {
+		describe('valid cases', () => {
+			it.each(encodePackData)('%s', (input, output) => {
+				// eslint-disable-next-line @typescript-eslint/no-magic-numbers
+				expect(encodePacked(...input)).toEqual(output);
+			});
+		});
+		describe('invalid cases', () => {
+			it.each(encodePackedInvalidData)('%s', (input, output) => {
+				expect(() => encodePacked(input)).toThrow(output);
+			});
+		});
+	});
+});
