ab32f63bdd63eaa086b381517749a4a8fa037ce9
Nazar Hussain <nazarhussain@gmail.com>
2021-11-10 12:10:41 +0100

    Create `Web3Context` class to hold all state objects (#4524)
    * :sparkles: Add Web3Context class
    
    * :white_check_mark: Add unit test for Web3Context

--- packages/web3-core/src/index.ts
+++ packages/web3-core/src/index.ts
@@ -3,5 +3,6 @@ export * from './local_storage_wallet';
 export * from './web3_request_manager';
 export * from './web3_subscription_manager';
 export * from './web3_subscriptions';
+export * from './web3_context';
 export * from './utils';
 export * from './types';
