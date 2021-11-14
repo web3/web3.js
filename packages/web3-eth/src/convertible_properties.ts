06b53b7cd456219a3e56554987331bf237194bcf
Wyatt Barnes <me@wyatt.email>
2021-11-13 19:09:01 -1000

    Add additional convertible properties

--- packages/web3-eth/src/convertible_properties.ts
+++ packages/web3-eth/src/convertible_properties.ts
@@ -1,4 +1,4 @@
-import { Block } from 'web3-common';
+import { Block, FeeHistoryResult, ReceiptInfo, TransactionInfo } from 'web3-common';
 
 export const convertibleBlockProperties: (keyof Block)[] = [
 	'difficulty',
@@ -10,4 +10,33 @@ export const convertibleBlockProperties: (keyof Block)[] = [
 	'totalDifficulty',
 	'baseFeePerGas',
 	'size',
+];
+
+// https://stackoverflow.com/a/49402091
+type KeysOfUnion<T> = T extends T ? keyof T : never;
+export const convertibleTransactionInfoProperties: KeysOfUnion<TransactionInfo>[] = [
+	'type',
+	'nonce',
+	'gas',
+	'value',
+	'maxFeePerGas',
+	'maxPriorityFeePerGas',
+	'yParity',
+	'v',
+	'gasPrice',
+	'blockNumber',
+	'transactionIndex',
+];
+
+export const convertibleReceiptInfoProperties: (keyof ReceiptInfo)[] = [
+	'transactionIndex',
+	'blockNumber',
+	'cumulativeGasUsed',
+	'gasUsed',
+	'status',
+];
+
+export const convertibleFeeHistoryResultProperties: (keyof FeeHistoryResult)[] = [
+	'oldestBlock',
+	'baseFeePerGas',
 ];
