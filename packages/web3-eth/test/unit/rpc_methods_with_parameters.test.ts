63ece01214bbd8dc8a0f4c66227975167b939c15
Wyatt Barnes <me@wyatt.email>
2021-11-22 09:13:36 -1000

    Init tests for rpc_methods_with_params

--- packages/web3-eth/test/unit/rpc_methods_with_parameters.test.ts
+++ packages/web3-eth/test/unit/rpc_methods_with_parameters.test.ts
@@ -1,8 +1,67 @@
 import { Web3RequestManager } from 'web3-core';
 
-import { getBalance } from '../../src/rpc_methods';
+import {
+	compileLLL,
+	compileSerpent,
+	compileSolidity,
+	getBalance,
+	getBlockByHash,
+	getBlockByNumber,
+	getBlockTransactionCountByHash,
+	getBlockTransactionCountByNumber,
+	getCode,
+	getFeeHistory,
+	getFilterChanges,
+	getFilterLogs,
+	getStorageAt,
+	getTransactionByBlockHashAndIndex,
+	getTransactionByBlockNumberAndIndex,
+	getTransactionByHash,
+	getTransactionCount,
+	getTransactionReceipt,
+	getUncleByBlockHashAndIndex,
+	getUncleByBlockNumberAndIndex,
+	getUncleCountByBlockHash,
+	getUncleCountByBlockNumber,
+	sendRawTransaction,
+	sign,
+	submitHashrate,
+	submitWork,
+	uninstallFilter,
+} from '../../src/rpc_methods';
+import {
+	compileLLLValidData,
+	compileSerpentValidData,
+	compileSolidityValidData,
+	getBalanceValidData,
+	getBlockByHashValidData,
+	getBlockByNumberValidData,
+	getBlockTransactionCountByHashValidData,
+	getBlockTransactionCountByNumberValidData,
+	getCodeValidData,
+	getFeeHistoryValidData,
+	getFilterChangesValidData,
+	getFilterLogsValidData,
+	getStorageAtValidData,
+	getTransactionByBlockHashAndIndexValidData,
+	getTransactionByBlockNumberAndIndexValidData,
+	getTransactionByHashValidData,
+	getTransactionCountValidData,
+	getTransactionReceiptValidData,
+	getUncleByBlockHashAndIndexValidData,
+	getUncleByBlockNumberAndIndexValidData,
+	getUncleCountByBlockHashValidData,
+	getUncleCountByBlockNumberValidData,
+	sendRawTransactionValidData,
+	signValidData,
+	submitHashrateValidData,
+	submitWorkValidData,
+	uninstallFilterValidData,
+} from '../fixtures/rpc_methods_with_params';
 
-describe('rpc_methods_no_params', () => {
+// Invalid data is not being tested for each method, because that
+// would be retesting the validation methods which are tested in web3-utils
+describe('rpc_methods_with_params', () => {
 	const requestManagerSendSpy = jest.fn();
 
 	let requestManager: Web3RequestManager;
@@ -13,16 +72,276 @@ describe('rpc_methods_no_params', () => {
 		requestManager.send = requestManagerSendSpy;
 	});
 
-	describe('should make call with expected parameters', () => {
-		it('getBalance', async () => {
-			const params: [string, string] = ['0x407d73d8a49eeb85d32cf465507dd71d507100c1', '0x1'];
-
-			await getBalance(requestManager, ...params);
-
+	describe('getBalance', () => {
+		it.each(getBalanceValidData)('%s', async (address, blockNumber) => {
+			await getBalance(requestManager, address, blockNumber);
 			expect(requestManagerSendSpy).toHaveBeenCalledWith({
 				method: 'eth_getBalance',
-				params,
+				params: [address, blockNumber],
 			});
 		});
+	});
+
+	describe('getStorageAt', () => {
+		it.each(getStorageAtValidData)('%s', async (address, storageSlot, blockNumber) => {
+			await getStorageAt(requestManager, address, storageSlot, blockNumber);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getStorageAt',
+				params: [address, storageSlot, blockNumber],
+			});
+		});
+	});
+
+	describe('getTransactionCount', () => {
+		it.each(getTransactionCountValidData)('%s', async (address, block) => {
+			await getTransactionCount(requestManager, address, block);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getTransactionCount',
+				params: [address, block],
+			});
+		});
+	});
+
+	describe('getBlockTransactionCountByHash', () => {
+		it.each(getBlockTransactionCountByHashValidData)('%s', async blockHash => {
+			await getBlockTransactionCountByHash(requestManager, blockHash);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getBlockTransactionCountByHash',
+				params: [blockHash],
+			});
+		});
+	});
+
+	describe('getBlockTransactionCountByNumber', () => {
+		it.each(getBlockTransactionCountByNumberValidData)('%s', async block => {
+			await getBlockTransactionCountByNumber(requestManager, block);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getBlockTransactionCountByNumber',
+				params: [block],
+			});
+		});
+	});
+
+	describe('getUncleCountByBlockHash', () => {
+		it.each(getUncleCountByBlockHashValidData)('%s', async blockHash => {
+			await getUncleCountByBlockHash(requestManager, blockHash);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getUncleCountByBlockHash',
+				params: [blockHash],
+			});
+		});
+	});
+
+	describe('getUncleCountByBlockNumber', () => {
+		it.each(getUncleCountByBlockNumberValidData)('%s', async block => {
+			await getUncleCountByBlockNumber(requestManager, block);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getUncleCountByBlockNumber',
+				params: [block],
+			});
+		});
+	});
+
+	describe('getCode', () => {
+		it.each(getCodeValidData)('%s', async (address, block) => {
+			await getCode(requestManager, address, block);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getCode',
+				params: [address, block],
+			});
+		});
+	});
+
+	describe('sign', () => {
+		it.each(signValidData)('%s', async (address, message) => {
+			await sign(requestManager, address, message);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_sign',
+				params: [address, message],
+			});
+		});
+	});
+
+	describe('sendRawTransaction', () => {
+		it.each(sendRawTransactionValidData)('%s', async transaction => {
+			await sendRawTransaction(requestManager, transaction);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_sendRawTransaction',
+				params: [transaction],
+			});
+		});
+	});
+
+	describe('getBlockByHash', () => {
+		it.each(getBlockByHashValidData)('%s', async (blockHash, hydrated) => {
+			await getBlockByHash(requestManager, blockHash, hydrated);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getBlockByHash',
+				params: [blockHash, hydrated],
+			});
+		});
+	});
+
+	describe('getBlockByNumber', () => {
+		it.each(getBlockByNumberValidData)('%s', async (block, hydrated) => {
+			await getBlockByNumber(requestManager, block, hydrated);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getBlockByNumber',
+				params: [block, hydrated],
+			});
+		});
+	});
+
+	describe('getTransactionByHash', () => {
+		it.each(getTransactionByHashValidData)('%s', async transactionHash => {
+			await getTransactionByHash(requestManager, transactionHash);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getTransactionByHash',
+				params: [transactionHash],
+			});
+		});
+	});
+
+	describe('getTransactionByBlockHashAndIndex', () => {
+		it.each(getTransactionByBlockHashAndIndexValidData)('%s', async (blockHash, index) => {
+			await getTransactionByBlockHashAndIndex(requestManager, blockHash, index);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getTransactionByBlockHashAndIndex',
+				params: [blockHash, index],
+			});
+		});
+	});
+
+	describe('getTransactionByBlockNumberAndIndex', () => {
+		it.each(getTransactionByBlockNumberAndIndexValidData)('%s', async (block, index) => {
+			await getTransactionByBlockNumberAndIndex(requestManager, block, index);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getTransactionByBlockNumberAndIndex',
+				params: [block, index],
+			});
+		});
+	});
+
+	describe('getTransactionReceipt', () => {
+		it.each(getTransactionReceiptValidData)('%s', async transactionHash => {
+			await getTransactionReceipt(requestManager, transactionHash);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getTransactionReceipt',
+				params: [transactionHash],
+			});
+		});
+	});
+
+	describe('getUncleByBlockHashAndIndex', () => {
+		it.each(getUncleByBlockHashAndIndexValidData)('%s', async (blockHash, index) => {
+			await getUncleByBlockHashAndIndex(requestManager, blockHash, index);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getUncleByBlockHashAndIndex',
+				params: [blockHash, index],
+			});
+		});
+	});
+
+	describe('getUncleByBlockNumberAndIndex', () => {
+		it.each(getUncleByBlockNumberAndIndexValidData)('%s', async (block, index) => {
+			await getUncleByBlockNumberAndIndex(requestManager, block, index);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getUncleByBlockNumberAndIndex',
+				params: [block, index],
+			});
+		});
+	});
+
+	describe('compileSolidity', () => {
+		it.each(compileSolidityValidData)('%s', async code => {
+			await compileSolidity(requestManager, code);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_compileSolidity',
+				params: [code],
+			});
+		});
+	});
+
+	describe('compileLLL', () => {
+		it.each(compileLLLValidData)('%s', async code => {
+			await compileLLL(requestManager, code);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_compileLLL',
+				params: [code],
+			});
+		});
+	});
+
+	describe('compileSerpent', () => {
+		it.each(compileSerpentValidData)('%s', async code => {
+			await compileSerpent(requestManager, code);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_compileSerpent',
+				params: [code],
+			});
+		});
+	});
+
+	describe('uninstallFilter', () => {
+		it.each(uninstallFilterValidData)('%s', async filterId => {
+			await uninstallFilter(requestManager, filterId);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_uninstallFilter',
+				params: [filterId],
+			});
+		});
+	});
+
+	describe('getFilterChanges', () => {
+		it.each(getFilterChangesValidData)('%s', async filterId => {
+			await getFilterChanges(requestManager, filterId);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getFilterChanges',
+				params: [filterId],
+			});
+		});
+	});
+
+	describe('getFilterLogs', () => {
+		it.each(getFilterLogsValidData)('%s', async filterId => {
+			await getFilterLogs(requestManager, filterId);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getFilterLogs',
+				params: [filterId],
+			});
+		});
+	});
+
+	describe('submitWork', () => {
+		it.each(submitWorkValidData)('%s', async (nonce, seedHash, difficulty) => {
+			await submitWork(requestManager, nonce, seedHash, difficulty);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_submitWork',
+				params: [nonce, seedHash, difficulty],
+			});
+		});
+	});
+
+	describe('submitHashrate', () => {
+		it.each(submitHashrateValidData)('%s', async (hashRate, id) => {
+			await submitHashrate(requestManager, hashRate, id);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_submitHashrate',
+				params: [hashRate, id],
+			});
+		});
+	});
+
+	describe('getFeeHistory', () => {
+		it.each(getFeeHistoryValidData)(
+			'%s',
+			async (blockCount, newestBlock, rewardPercentiles) => {
+				await getFeeHistory(requestManager, blockCount, newestBlock, rewardPercentiles);
+				expect(requestManagerSendSpy).toHaveBeenCalledWith({
+					method: 'eth_feeHistory',
+					params: [blockCount, newestBlock, rewardPercentiles],
+				});
+			},
+		);
 	});
 });
