38fe0c8a32bebfaaed1cba2876cd6dc4e9f03a81
Alex <alex.luu@mail.utoronto.ca>
2021-11-22 10:00:58 -0500

    updating packages

--- packages/web3-eth-accounts/src/index.ts
+++ packages/web3-eth-accounts/src/index.ts
@@ -1,10 +1,10 @@
 import { getPublicKey } from 'ethereum-cryptography/secp256k1';
-
+import { HexString } from './types'
 const fromPrivate = (privateKey: string) => {
 	const publicKey = getPublicKey(privateKey);
 };
 
-const entropy = (): string => {
+const create = (entropy: HexString ): string => {
 	const a = '';
 	return a;
 };
