02dea66da8b12e2f2902e4e6f316b8fbf30583d9
Nazar Hussain <nazarhussain@gmail.com>
2021-11-24 17:01:05 +0100

    :sparkles: Initialize contract constrution

--- packages/web3-eth-abi/src/index.ts
+++ packages/web3-eth-abi/src/index.ts
@@ -1,4 +1,5 @@
 export * from './types';
+export * from './utils';
 export * from './api/events_api';
 export * from './api/functions_api';
 export * from './api/logs_api';
---
+++ packages/web3-eth-contract/src/index.ts
@@ -0,0 +1,2 @@
+export * from './contract';
+export * from './types';
