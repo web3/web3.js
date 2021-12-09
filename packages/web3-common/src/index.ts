2ea05a8e9b251f618bdc54f0319f0c45896822af
Nazar Hussain <nazarhussain@gmail.com>
2021-12-09 20:17:19 +0100

    :sparkles: Add PromiEvent to web3-common (#4606)

--- packages/web3-common/src/index.ts
+++ packages/web3-common/src/index.ts
@@ -6,5 +6,6 @@ export * from './web3_base_wallet';
 export * from './web3_event_emitter';
 export * from './eth_execution_api';
 export * from './deferred_promise';
+export * from './promi_event';
 export * from './formatters';
 export * as jsonRpc from './json_rpc';
