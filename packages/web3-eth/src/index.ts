f851d61adae1148b4af5a39fcf4f829dc96cb197
Wyatt Barnes <me@wyatt.email>
2021-12-09 07:53:53 -1000

    WIP transaction formatters

--- packages/web3-eth/src/index.ts
+++ packages/web3-eth/src/index.ts
@@ -18,15 +18,18 @@ import {
 	convertObjectPropertiesToValidType,
 	isHexString32Bytes,
 	Filter,
+	isNumbers,
+	numberToHex,
 } from 'web3-utils';
 
-// import { Transaction, BlockFormatted } from './types';
-import { BlockFormatted } from './types';
+import { Transaction, BlockFormatted, ChainNames, HardForks } from './types';
 import {
 	convertibleBlockProperties,
 	convertibleFeeHistoryResultProperties,
 	convertibleReceiptInfoProperties,
+	convertibleTransactionCustomChainProperties,
 	convertibleTransactionInfoProperties,
+	convertibleTransactionProperties,
 } from './convertible_properties';
 
 import * as rpcMethods from './rpc_methods';
@@ -76,6 +79,26 @@ export default class Web3Eth {
 		) as ValidReturnTypes[ReturnType];
 	}
 
+    public async getFeeHistory<ReturnType extends ValidTypes = ValidTypes.HexString>(
+		blockCount: Uint,
+		newestBlock: BlockNumberOrTag = this.web3Context.defaultBlock,
+		rewardPercentiles: number[],
+		returnType?: ReturnType,
+	) {
+		const response = await rpcMethods.getFeeHistory(
+			this.web3Context.requestManager,
+			blockCount,
+			newestBlock,
+			rewardPercentiles,
+		);
+
+		return convertObjectPropertiesToValidType(
+			response,
+			convertibleFeeHistoryResultProperties,
+			returnType ?? this.web3Context.defaultReturnType,
+		);
+	}
+
 	public async getAccounts() {
 		return rpcMethods.getAccounts(this.web3Context.requestManager);
 	}
@@ -91,7 +114,6 @@ export default class Web3Eth {
 		) as ValidReturnTypes[ReturnType];
 	}
 
-	// TODO Discuss the use of multiple optional parameters
 	public async getBalance<ReturnType extends ValidTypes = ValidTypes.HexString>(
 		address: Address,
 		blockNumber: BlockNumberOrTag = this.web3Context.defaultBlock,
@@ -129,7 +151,6 @@ export default class Web3Eth {
 		return rpcMethods.getCode(this.web3Context.requestManager, address, blockNumber);
 	}
 
-	// TODO Discuss the use of multiple optional parameters
 	public async getBlock<ReturnType extends ValidTypes = ValidTypes.HexString>(
 		block: HexString32Bytes | BlockNumberOrTag = this.web3Context.defaultBlock,
 		hydrated = false,
@@ -139,7 +160,6 @@ export default class Web3Eth {
 			? await rpcMethods.getBlockByHash(this.web3Context.requestManager, block, hydrated)
 			: await rpcMethods.getBlockByNumber(this.web3Context.requestManager, block, hydrated);
 
-		// @ts-expect-error Having a problem determining if BlockFormatted<ReturnType> is correct type
 		return convertObjectPropertiesToValidType(
 			response,
 			convertibleBlockProperties,
@@ -147,7 +167,6 @@ export default class Web3Eth {
 		);
 	}
 
-	// TODO Discuss the use of multiple optional parameters
 	public async getBlockTransactionCount<ReturnType extends ValidTypes = ValidTypes.HexString>(
 		block: HexString32Bytes | BlockNumberOrTag = this.web3Context.defaultBlock,
 		returnType?: ReturnType,
@@ -168,7 +187,6 @@ export default class Web3Eth {
 		) as ValidReturnTypes[ReturnType];
 	}
 
-	// TODO Discuss the use of multiple optional parameters
 	public async getBlockUncleCount<ReturnType extends ValidTypes = ValidTypes.HexString>(
 		block: HexString32Bytes | BlockNumberOrTag = this.web3Context.defaultBlock,
 		returnType?: ReturnType,
@@ -200,7 +218,6 @@ export default class Web3Eth {
 					uncleIndex,
 			  );
 
-		// @ts-expect-error Having a problem determining if BlockFormatted<ReturnType> is correct type
 		return convertObjectPropertiesToValidType(
 			response,
 			convertibleBlockProperties,
@@ -221,9 +238,6 @@ export default class Web3Eth {
 			? response
 			: convertObjectPropertiesToValidType(
 					response,
-					// TODO
-					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					// @ts-expect-error
 					convertibleTransactionInfoProperties,
 					returnType ?? this.web3Context.defaultReturnType,
 			  );
@@ -255,9 +269,6 @@ export default class Web3Eth {
 			? response
 			: convertObjectPropertiesToValidType(
 					response,
-					// TODO
-					// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-					// @ts-expect-error
 					convertibleTransactionInfoProperties,
 					returnType ?? this.web3Context.defaultReturnType,
 			  );
@@ -281,7 +292,6 @@ export default class Web3Eth {
 			  );
 	}
 
-	// TODO Discuss the use of multiple optional parameters
 	public async getTransactionCount<ReturnType extends ValidTypes = ValidTypes.HexString>(
 		address: Address,
 		blockNumber: BlockNumberOrTag = this.web3Context.defaultBlock,
@@ -299,10 +309,70 @@ export default class Web3Eth {
 		) as ValidReturnTypes[ReturnType];
 	}
 
-	// TODO Needs to convert input to hex string
-	// public async sendTransaction(transaction: Transaction) {
-	// 	return rpcMethods.sendTransaction(this.web3Context.requestManager, transaction);
-	// }
+	public normalizeTransaction(transaction: Transaction): Partial<TransactionWithSender> {
+		const normalizedTransaction = convertObjectPropertiesToValidType(
+			transaction,
+			convertibleTransactionProperties,
+			ValidTypes.HexString,
+		);
+
+		if (normalizedTransaction.common !== undefined) {
+			if (normalizedTransaction.common.customChain === undefined)
+				throw new Error('customChain needs to be defined');
+			normalizedTransaction.common.customChain = convertObjectPropertiesToValidType(
+				normalizedTransaction.common.customChain,
+				convertibleTransactionCustomChainProperties,
+				ValidTypes.HexString,
+			);
+		}
+
+		return normalizedTransaction;
+	}
+
+	public async fillTransaction(transaction: Transaction): TransactionWithSender {
+		const filledTransaction = { ...transaction };
+
+		if (filledTransaction.from === undefined) {
+			if (this.web3Context.defaultAccount === null)
+				throw new Error('No from address specified, and no default available');
+			filledTransaction.from = this.web3Context.defaultAccount;
+		} else if (isNumbers(filledTransaction.from)) {
+			// TODO index of a local wallet in web3.eth.accounts.wallet
+			// filledTransaction.from = numberToHex(filledTransaction.from);
+			throw new Error('index of account in local wallet not implemented');
+		}
+
+		// TODO Correct calculation
+		if (filledTransaction.type === undefined) filledTransaction.type = '0x0';
+		if (
+			(filledTransaction.type === '0x0' || filledTransaction.type === '0x1') &&
+			filledTransaction.gasPrice === undefined
+		) {
+			filledTransaction.gasPrice = await this.getGasPrice();
+		} else {
+			if (filledTransaction.maxPriorityFeePerGas === undefined)
+				filledTransaction.maxPriorityFeePerGas = numberToHex(1000000000); // 1 Gwei
+			// TODO Correct calculation
+			if (filledTransaction.maxFeePerGas === undefined)
+				filledTransaction.maxFeePerGas = '0x1';
+		}
+
+		if (filledTransaction.chain === undefined) filledTransaction.chain = ChainNames.MAINNET;
+		if (filledTransaction.hardfork === undefined) filledTransaction.hardfork = HardForks.LONDON;
+
+		return filledTransaction;
+	}
+
+	public normalizeAndFillTransaction(transaction: Transaction) {
+		return this.fillTransaction(this.normalizeTransaction(transaction));
+	}
+
+	public async sendTransaction(transaction: Transaction) {
+		return rpcMethods.sendTransaction(
+			this.web3Context.requestManager,
+			this.normalizeAndFillTransaction(transaction),
+		);
+	}
 
 	public async sendSignedTransaction(transaction: HexStringBytes) {
 		return rpcMethods.sendRawTransaction(this.web3Context.requestManager, transaction);
@@ -314,10 +384,12 @@ export default class Web3Eth {
 		return rpcMethods.sign(this.web3Context.requestManager, address, message);
 	}
 
-	// TODO Needs to convert input to hex string
-	// public async signTransaction(transaction: Transaction) {
-	// 	return rpcMethods.signTransaction(this.web3Context.requestManager, transaction);
-	// }
+	public async signTransaction(transaction: Transaction) {
+		return rpcMethods.signTransaction(
+			this.web3Context.requestManager,
+			this.normalizeAndFillTransaction(transaction),
+		);
+	}
 
 	// TODO Decide what to do with transaction.to
 	// https://github.com/ChainSafe/web3.js/pull/4525#issuecomment-982330076
@@ -330,13 +402,13 @@ export default class Web3Eth {
 
 	// TODO Missing param
 	public async estimateGas<ReturnType extends ValidTypes = ValidTypes.HexString>(
-		transaction: Partial<TransactionWithSender>,
+		transaction: Transaction,
 		blockNumber: BlockNumberOrTag = this.web3Context.defaultBlock,
 		returnType?: ReturnType,
 	) {
 		const response = await rpcMethods.estimateGas(
 			this.web3Context.requestManager,
-			transaction,
+			this.normalizeAndFillTransaction(transaction),
 			blockNumber,
 		);
 
@@ -387,24 +459,4 @@ export default class Web3Eth {
 	// // public async getProof() {
 
 	// // }
-
-	public async getFeeHistory<ReturnType extends ValidTypes = ValidTypes.HexString>(
-		blockCount: Uint,
-		newestBlock: BlockNumberOrTag = this.web3Context.defaultBlock,
-		rewardPercentiles: number[],
-		returnType?: ReturnType,
-	) {
-		const response = await rpcMethods.getFeeHistory(
-			this.web3Context.requestManager,
-			blockCount,
-			newestBlock,
-			rewardPercentiles,
-		);
-
-		return convertObjectPropertiesToValidType(
-			response,
-			convertibleFeeHistoryResultProperties,
-			returnType ?? this.web3Context.defaultReturnType,
-		);
-	}
 }
