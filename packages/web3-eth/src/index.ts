1d1366c6f819e85b7387814010296c480e146a2a
Wyatt Barnes <me@wyatt.email>
2021-12-12 20:57:47 -1000

    remove normalize and fill transaction methods

--- packages/web3-eth/src/index.ts
+++ packages/web3-eth/src/index.ts
@@ -358,68 +358,10 @@ export default class Web3Eth {
 		) as ValidReturnTypes[ReturnType];
 	}
 
-	// public normalizeTransaction(transaction: Transaction): Partial<TransactionWithSender> {
-	// 	const normalizedTransaction = convertObjectPropertiesToValidType(
-	// 		transaction,
-	// 		convertibleTransactionProperties,
-	// 		ValidTypes.HexString,
-	// 	);
-
-	// 	if (normalizedTransaction.common !== undefined) {
-	// 		if (normalizedTransaction.common.customChain === undefined)
-	// 			throw new Error('customChain needs to be defined');
-	// 		normalizedTransaction.common.customChain = convertObjectPropertiesToValidType(
-	// 			normalizedTransaction.common.customChain,
-	// 			convertibleTransactionCustomChainProperties,
-	// 			ValidTypes.HexString,
-	// 		);
-	// 	}
-
-	// 	return normalizedTransaction;
-	// }
-
-	// public async fillTransaction(transaction: Transaction): TransactionWithSender {
-	// 	const filledTransaction = { ...transaction };
-
-	// 	if (filledTransaction.from === undefined) {
-	// 		if (this.web3Context.defaultAccount === null)
-	// 			throw new Error('No from address specified, and no default available');
-	// 		filledTransaction.from = this.web3Context.defaultAccount;
-	// 	} else if (isNumbers(filledTransaction.from)) {
-	// 		// TODO index of a local wallet in web3.eth.accounts.wallet
-	// 		// filledTransaction.from = numberToHex(filledTransaction.from);
-	// 		throw new Error('index of account in local wallet not implemented');
-	// 	}
-
-	// 	// TODO Correct calculation
-	// 	if (filledTransaction.type === undefined) filledTransaction.type = '0x0';
-	// 	if (
-	// 		(filledTransaction.type === '0x0' || filledTransaction.type === '0x1') &&
-	// 		filledTransaction.gasPrice === undefined
-	// 	) {
-	// 		filledTransaction.gasPrice = await this.getGasPrice();
-	// 	} else {
-	// 		if (filledTransaction.maxPriorityFeePerGas === undefined)
-	// 			filledTransaction.maxPriorityFeePerGas = numberToHex(1000000000); // 1 Gwei
-	// 		// TODO Correct calculation
-	// 		if (filledTransaction.maxFeePerGas === undefined)
-	// 			filledTransaction.maxFeePerGas = '0x1';
-	// 	}
-
-	// 	if (filledTransaction.chain === undefined) filledTransaction.chain = ChainNames.MAINNET;
-	// 	if (filledTransaction.hardfork === undefined) filledTransaction.hardfork = HardForks.LONDON;
-
-	// 	return filledTransaction;
-	// }
-
-	// public normalizeAndFillTransaction(transaction: Transaction) {
-	// 	return this.fillTransaction(this.normalizeTransaction(transaction));
-	// }
-
 	// public async sendTransaction(transaction: Transaction) {
 	// 	return rpcMethods.sendTransaction(
 	// 		this.web3Context.requestManager,
-	// 		this.normalizeAndFillTransaction(transaction),
+	// 		transaction,
 	// 	);
 	// }
 
@@ -436,7 +378,7 @@ export default class Web3Eth {
 	// public async signTransaction(transaction: Transaction) {
 	// 	return rpcMethods.signTransaction(
 	// 		this.web3Context.requestManager,
-	// 		this.normalizeAndFillTransaction(transaction),
+	// 		transaction,
 	// 	);
 	// }
 
@@ -457,7 +399,7 @@ export default class Web3Eth {
 	// ) {
 	// 	const response = await rpcMethods.estimateGas(
 	// 		this.web3Context.requestManager,
-	// 		this.normalizeAndFillTransaction(transaction),
+	// 		transaction,
 	// 		blockNumber,
 	// 	);
 
