7aeb194ecefc02fd3c4c8231f94e5541e1b48f2c
jdevcs <junaid@chainsafe.io>
2021-12-14 15:05:04 +0100

    it shouldnt log sensitive string

--- packages/web3-common/src/errors.ts
+++ packages/web3-common/src/errors.ts
@@ -398,14 +398,14 @@ export class AbiError extends Web3Error {
 
 export class PrivateKeyLengthError extends Web3Error {
 	public code = ERR_PRIVATE_KEY_LENGTH;
-	public constructor(value: string | Buffer) {
-		super(`Invalid value given "${value.toString()}". Error: Private key must be 32 bytes.`);
+	public constructor() {
+		super(`Error: Private key must be 32 bytes.`);
 	}
 }
 
 export class InvalidPrivateKeyError extends Web3Error {
 	public code = ERR_INVALID_PRIVATE_KEY;
-	public constructor(value: string | Buffer) {
-		super(`Invalid value given "${String(value)}". Error: not a valid string or buffer.`);
+	public constructor() {
+		super(`Error: not a valid string or buffer.`);
 	}
 }
