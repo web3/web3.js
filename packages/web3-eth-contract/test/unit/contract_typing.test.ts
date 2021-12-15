c2060170a3fa400759ed6efae529fb70f6ba4639
Nazar Hussain <nazarhussain@gmail.com>
2021-12-15 16:08:14 +0100

    Add ERC721 type tests

--- packages/web3-eth-contract/test/unit/contract_typing.test.ts
+++ packages/web3-eth-contract/test/unit/contract_typing.test.ts
@@ -3,33 +3,46 @@
 import { expectTypeOf, typecheck } from '@humeris/espresso-shot';
 import { Contract } from '../../src/contract';
 import { erc20Abi, Erc20Interface } from '../fixtures/erc20';
+import { erc721Abi, Erc721Interface } from '../fixtures/erc721';
 
 describe('contract typing', () => {
 	describe('erc20', () => {
-		it('should have valid type interface', () => {
-			const contract = new Contract(erc20Abi);
+		const contract = new Contract(erc20Abi);
 
-			typecheck(
-				'should extend correct interface',
-				() => [
-					// Contract contains all methods
-					expectTypeOf<keyof typeof contract.methods>().toBe<
-						keyof Erc20Interface['methods']
-					>(),
+		typecheck('should contain all methods', () =>
+			expectTypeOf<keyof typeof contract.methods>().toBe<keyof Erc20Interface['methods']>(),
+		);
 
-					// Contract methods compliance with interface
-					expectTypeOf(contract.methods).toExtend<Erc20Interface['methods']>(),
+		typecheck('should have interface compliance methods', () =>
+			expectTypeOf(contract.methods).toExtend<Erc20Interface['methods']>(),
+		);
 
-					// Contract contains all events
-					expectTypeOf<keyof typeof contract.events>().toBe<
-						keyof Erc20Interface['events']
-					>(),
+		typecheck('should have all events', () =>
+			expectTypeOf(contract.events).toExtend<Erc20Interface['events']>(),
+		);
 
-					// Contract events compliance with interface
-					expectTypeOf(contract.events).toExtend<Erc20Interface['events']>(),
-				],
-				{ runInTest: false },
-			);
-		});
+		typecheck('should have interface compliance events', () =>
+			expectTypeOf(contract.events).toExtend<Erc20Interface['events']>(),
+		);
+	});
+
+	describe('erc721', () => {
+		const contract = new Contract(erc721Abi);
+
+		typecheck('should contain all methods', () =>
+			expectTypeOf<keyof typeof contract.methods>().toBe<keyof Erc721Interface['methods']>(),
+		);
+
+		typecheck('should have interface compliance methods', () =>
+			expectTypeOf(contract.methods).toExtend<Erc721Interface['methods']>(),
+		);
+
+		typecheck('should have all events', () =>
+			expectTypeOf(contract.events).toExtend<Erc721Interface['events']>(),
+		);
+
+		typecheck('should have interface compliance events', () =>
+			expectTypeOf(contract.events).toExtend<Erc721Interface['events']>(),
+		);
 	});
 });
