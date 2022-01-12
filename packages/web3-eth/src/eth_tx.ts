30b289d8ef669f11a715f4dd722d703876d9f493
Wyatt Barnes <me@wyatt.email>
2022-01-12 06:02:11 -1000

    WIP eth-tx utils

--- packages/web3-eth/src/eth_tx.ts
+++ packages/web3-eth/src/eth_tx.ts
@@ -1,20 +1,18 @@
-import { TransactionFactory } from '@ethereumjs/tx';
 import { AccessList, EthExecutionAPI } from 'web3-common';
 import { Web3Context } from 'web3-core';
 import {
 	Address,
-	Numbers,
+	convertToValidType,
 	HexString,
+	HexStringBytes,
+	Numbers,
+	ValidReturnTypes,
 	ValidTypes,
-	convertObjectPropertiesToValidType,
-	toHex,
 } from 'web3-utils';
-import { BlockTags } from 'web3-validator';
 
 import Web3Eth from './index';
 
 export type chain = 'goerli' | 'kovan' | 'mainnet' | 'rinkeby' | 'ropsten' | 'sepolia';
-
 export type hardfork =
 	| 'arrowGlacier'
 	| 'berlin'
@@ -32,210 +30,191 @@ export type hardfork =
 	| 'spuriousDragon'
 	| 'tangerineWhistle';
 
-export interface CustomChain<NumberType = Numbers | HexString> {
+export interface CustomChain {
 	name?: string;
-	networkId: NumberType;
-	chainId: NumberType;
+	networkId: Numbers;
+	chainId: Numbers;
 }
 
-export interface Common<NumberType = Numbers | HexString> {
-	customChain: CustomChain<NumberType>;
+export interface Common {
+	customChain: CustomChain;
 	baseChain?: chain;
 	hardfork?: hardfork;
 }
 
-export interface CustomChain<NumberType = Numbers | HexString> {
-	name?: string;
-	networkId: NumberType;
-	chainId: NumberType;
-}
-
-export interface BaseTxData<NumberType = Numbers | HexString> {
+export interface Transaction<NumberType = Numbers> {
 	from?: Address;
-	to?: Address | null;
+	to?: Address;
 	value?: NumberType;
 	gas?: NumberType;
 	gasPrice?: NumberType;
-	data?: HexString;
+	type?: NumberType;
+	maxFeePerGas?: NumberType;
+	maxPriorityFeePerGas?: NumberType;
+	accessList?: AccessList;
+	data?: HexStringBytes;
 	nonce?: NumberType;
-	chainId?: NumberType;
-	common?: Common;
 	chain?: chain;
 	hardfork?: hardfork;
+	chainId?: NumberType;
+	common?: Common;
 	gasLimit?: NumberType;
 	v?: NumberType;
 	r?: NumberType;
 	s?: NumberType;
-	type?: NumberType;
 }
 
-export interface AccessListEIP2930TxData<NumberType = Numbers | HexString>
-	extends BaseTxData<NumberType> {
-	accessList?: AccessList;
-}
+export function formatTransaction<DesiredType extends ValidTypes, ReturnType = ValidReturnTypes[DesiredType]>(
+	transaction: Transaction,
+	desiredType: DesiredType,
+	overrideMethod?: (transaction: Transaction) => Transaction<ReturnType>,
+): Transaction<ReturnType> {
+	if (overrideMethod !== undefined) return overrideMethod(transaction);
+	const formattedTransaction = {
+		...transaction,
+		value: convertToValidType(transaction.value, desiredType),
+		gas: convertToValidType(transaction.gas, desiredType),
+		gasPrice: convertToValidType(transaction.gasPrice, desiredType),
+		type: convertToValidType(transaction.type, desiredType),
+		maxFeePerGas: convertToValidType(transaction.maxFeePerGas, desiredType),
+		maxPriorityFeePerGas: convertToValidType(transaction.maxPriorityFeePerGas, desiredType),
+		nonce: convertToValidType(transaction.nonce, desiredType),
+		chainId: convertToValidType(transaction.chainId, desiredType),
+		gasLimit: convertToValidType(transaction.gasLimit, desiredType),
+		v: convertToValidType(transaction.v, desiredType),
+		r: convertToValidType(transaction.r, desiredType),
+		s: convertToValidType(transaction.s, desiredType),
+		common: {
+			...transaction.common,
+			customChain: {
+				...transaction.common?.customChain,
+				networkId: convertToValidType(
+					transaction.common?.customChain.networkId,
+					desiredType,
+				),
+				chainId: convertToValidType(transaction.common?.customChain.chainId, desiredType),
+			},
+		},
+	};
+	return (formattedTransaction as Transaction<ReturnType>)
+};
 
-export interface FeeMarketEIP1559TxData<NumberType = Numbers | HexString>
-	extends AccessListEIP2930TxData<NumberType> {
-	gasPrice?: never;
-	maxPriorityFeePerGas?: NumberType;
-	maxFeePerGas?: NumberType;
-}
+export const detectTransactionType = (
+	transaction: Transaction,
+	overrideMethod?: (transaction: Transaction) => Numbers | undefined,
+): Numbers | undefined => {
+	if (overrideMethod !== undefined) return overrideMethod(transaction);
+	if (transaction.type !== undefined) return transaction.type;
 
-export type TxData = BaseTxData | AccessListEIP2930TxData | FeeMarketEIP1559TxData;
-const txDataConvertibleProperties: (
-	| keyof BaseTxData
-	| keyof AccessListEIP2930TxData
-	| keyof FeeMarketEIP1559TxData
-)[] = [
-	'nonce',
-	'gasPrice',
-	'gasLimit',
-	'value',
-	'v',
-	'r',
-	's',
-	'type',
-	'chainId',
-	'maxPriorityFeePerGas',
-	'maxFeePerGas',
-];
+	if (transaction.maxFeePerGas !== undefined || transaction.maxPriorityFeePerGas !== undefined)
+		return '0x2';
+	if (transaction.hardfork === 'berlin') return '0x2';
+	if (transaction.common?.hardfork === 'berlin') return '0x2';
 
-export default class Web3EthTx {
-	public static async fromTxData(txData: TxData, web3Context: Web3Context<EthExecutionAPI>) {
-		if (typeof txData !== 'object' || txData === null)
+	if (transaction.accessList !== undefined) return '0x1';
+
+	return undefined;
+};
+
+const validateCustomChainInfo = (transaction: Transaction) => {
+	if (transaction.common !== undefined) {
+		if (transaction.common.customChain === undefined)
 			// TODO - Replace error
-			throw new Error('invalid txData object');
-
-		Web3EthTx._validateCustomChainInfo(txData);
-		const txDataHexString = Web3EthTx._formatTxData(txData);
-		const { defaultedTxData, txOptions } = await Web3EthTx._defaultTxDataAndSeparateTxOptions(
-			txDataHexString,
-			Web3EthTx._detectTxType(txDataHexString),
-			web3Context,
-		);
-		return TransactionFactory.fromTxData(defaultedTxData, txOptions);
-	}
-
-	private static _validateCustomChainInfo(txData: TxData) {
-		if (txData.common !== undefined) {
-			if (txData.common.customChain === undefined)
-				// TODO - Replace error
-				throw new Error('If tx.common is provided it must have tx.common.customChain');
-			if (txData.common.customChain.chainId === undefined)
-				// TODO - Replace error
-				throw new Error(
-					'If tx.common is provided it must have tx.common.customChain and tx.common.customChain.chainId',
-				);
-			if (
-				txData.chainId !== undefined &&
-				txData.chainId !== txData.common.customChain.chainId
-			)
-				// TODO - Replace error
-				throw new Error(
-					'Chain Id doesnt match in tx.chainId tx.common.customChain.chainId',
-				);
-		}
-	}
-
-	private static _formatTxData(txData: TxData) {
-		// TODO Fix TSC error
-		// @ts-ignore - Types of property 'gasPrice' are incompatible. Type 'string | undefined' is not assignable to type 'undefined'
-		const txDataHexString:
-			| BaseTxData<HexString>
-			| AccessListEIP2930TxData<HexString>
-			| FeeMarketEIP1559TxData<HexString> = convertObjectPropertiesToValidType(
-			txData,
-			// TODO Fix TSC error
-			// @ts-ignore - TSC doesn't understand txDataConvertibleProperties contains keys of all possible tx types
-			txDataConvertibleProperties,
-			ValidTypes.HexString,
-		);
-		if (txDataHexString.common !== undefined) {
-			txDataHexString.common.customChain = convertObjectPropertiesToValidType(
-				txDataHexString.common.customChain,
-				['networkId', 'chainId'],
-				ValidTypes.HexString,
-			);
-		}
-		// TODO - Formatting toLowerCase, toChecksumAddress, etc.
-		return txDataHexString;
-	}
-
-	private static _detectTxType(
-		txData:
-			| BaseTxData<HexString>
-			| AccessListEIP2930TxData<HexString>
-			| FeeMarketEIP1559TxData<HexString>,
-	) {
-		const hasEip1559 =
-			(txData as FeeMarketEIP1559TxData<HexString>).maxFeePerGas !== undefined ||
-			(txData as FeeMarketEIP1559TxData<HexString>).maxPriorityFeePerGas !== undefined;
-		if (txData.gasPrice !== undefined && (txData.type === '0x2' || hasEip1559))
-			// TODO - Replace error
-			throw new Error("eip-1559 transactions don't support gasPrice");
-		if ((txData.type === '0x0' || txData.type === '0x1') && hasEip1559)
+			throw new Error('If tx.common is provided it must have tx.common.customChain');
+		if (transaction.common.customChain.chainId === undefined)
 			// TODO - Replace error
 			throw new Error(
-				"pre-eip-1559 transaction don't support maxFeePerGas/maxPriorityFeePerGas",
+				'If tx.common is provided it must have tx.common.customChain and tx.common.customChain.chainId',
 			);
-
-		if (txData.type !== undefined) return txData.type;
-
-		if (hasEip1559) return '0x2';
-		if (txData.hardfork === 'berlin') return '0x2';
-		if (txData.common?.hardfork === 'berlin') return '0x2';
-
-		if ((txData as AccessListEIP2930TxData).accessList !== undefined) return '0x1';
-
-		return '0x0';
+		if (
+			transaction.chainId !== undefined &&
+			transaction.chainId !== transaction.common.customChain.chainId
+		)
+			// TODO - Replace error
+			throw new Error('Chain Id doesnt match in tx.chainId tx.common.customChain.chainId');
 	}
+};
 
-	private static async _defaultTxDataAndSeparateTxOptions(
-		txData:
-			| BaseTxData<HexString>
-			| AccessListEIP2930TxData<HexString>
-			| FeeMarketEIP1559TxData<HexString>,
-		txType: HexString,
-		web3Context: Web3Context<EthExecutionAPI>,
-	) {
-		const defaultedTxData = {
-			...txData,
-            ...await Web3EthTx._handleTxPricing(txData, txType, web3Context),
-			data: txData.data ?? '0x',
-			value: txData.value ?? '0x',
-			gasLimit: txData.gasLimit ?? txData.gas, // TODO txData.gas is optional, need a default if both gasLimit and gas aren't provided
-		};
-		if (txType >= '0x1' && (txData as AccessListEIP2930TxData<HexString>).accessList === undefined)
-			(defaultedTxData as AccessListEIP2930TxData<HexString>).accessList = [];
-	}
-
-	private static async _handleTxPricing(
-		txData:
-			| BaseTxData<HexString>
-			| AccessListEIP2930TxData<HexString>
-			| FeeMarketEIP1559TxData<HexString>,
-		txType: HexString,
-		web3Context: Web3Context<EthExecutionAPI>,
-	) {
-		const web3Eth = new Web3Eth(web3Context.currentProvider);
-		if (txType === '0x2') {
-			const block = await web3Eth.getBlock(BlockTags.LATEST);
-			if (block?.baseFeePerGas === undefined)
-				// TODO - Replace error
-				throw new Error("Network doesn't support eip-1559");
-			const maxPriorityFeePerGas =
-				// TODO - Replace hardcoded value with configurable one
-				(txData as FeeMarketEIP1559TxData<HexString>).maxPriorityFeePerGas ?? '0x9502F900'; // 2.5
-			return {
-				maxPriorityFeePerGas,
-				maxFeePerGas:
-					(txData as FeeMarketEIP1559TxData<HexString>).maxFeePerGas ??
-					toHex(BigInt(block.baseFeePerGas) * BigInt(2) + BigInt(maxPriorityFeePerGas)),
-			};
-		} else if (txType === '0x1' || txType === '0x0') {
-			return { gasPrice: txData.gasPrice ?? await web3Eth.getGasPrice() };
-		} else {
-			throw new Error('txType not specified');
-		}
-	}
+const validateChainInfo = (transaction: Transaction) => {
+	if (
+		transaction.common !== undefined &&
+		transaction.chain !== undefined &&
+		transaction.hardfork !== undefined
+	)
+		// TODO - Replace error
+		throw new Error(
+			'Please provide the @ethereumjs/common object or the chain and hardfork property but not all together.',
+		);
+	if (
+		(transaction.chain !== undefined && transaction.hardfork === undefined) ||
+		(transaction.hardfork !== undefined && transaction.chain === undefined)
+	)
+		// TODO - Replace error
+		throw new Error(
+			`When specifying chain and hardfork, both values must be defined. Received "chain": ${transaction.chain}, "hardfork": ${transaction.hardfork}`,
+		);
 }
+
+const validateGas = (transaction: Transaction<HexString>) => {
+	if (
+		transaction.gas === undefined &&
+		transaction.gasLimit === undefined &&
+		transaction.maxPriorityFeePerGas === undefined &&
+		transaction.maxFeePerGas === undefined
+	)
+		// TODO - Replace error
+		throw new Error('"gas" is missing');
+	if (transaction.gas !== undefined && transaction.gasPrice !== undefined) {
+		// This check is verifying gas and gasPrice aren't less than 0.
+		// transaction's number properties have been converted to HexStrings.
+		// JavaScript doesn't handle negative hex strings e.g. -0x1, but our
+		// numberToHex method does. -0x1 < 0 would result in false, so we must check if
+		// hex string is negative via the inclusion of -
+		if (
+			(transaction.gas as HexString).charAt(0) === '-' ||
+			(transaction.gasPrice as HexString).charAt(0) === '-'
+		)
+			// TODO - Replace error
+			throw new Error('Gas or gasPrice is lower than 0');
+	} else {
+		if (
+			(transaction.maxFeePerGas as HexString).charAt(0) === '-' ||
+			(transaction.maxPriorityFeePerGas as HexString).charAt(0) === '-'
+		)
+			// TODO - Replace error
+			throw new Error('maxPriorityFeePerGas or maxFeePerGas is lower than 0');
+	}
+
+	const hasEip1559 =
+		transaction.maxFeePerGas !== undefined || transaction.maxPriorityFeePerGas !== undefined;
+	if (transaction.gasPrice !== undefined && (transaction.type === '0x2' || hasEip1559))
+		// TODO - Replace error
+		throw new Error("eip-1559 transactions don't support gasPrice");
+	if ((transaction.type === '0x0' || transaction.type === '0x1') && hasEip1559)
+		// TODO - Replace error
+		throw new Error("pre-eip-1559 transaction don't support maxFeePerGas/maxPriorityFeePerGas");
+}
+
+export const validateTransaction = (
+	transaction: Transaction,
+	overrideMethod?: (transaction: Transaction) => void,
+) => {
+	if (overrideMethod !== undefined) return overrideMethod(transaction);
+
+	if (typeof transaction !== 'object' || transaction === null)
+		// TODO - Replace error
+		throw new Error('invalid transaction obejct');
+
+	const formattedTransaction = formatTransaction(transaction, ValidTypes.HexString);
+
+	validateCustomChainInfo(transaction);
+	validateChainInfo(transaction);
+	validateGas(formattedTransaction);
+
+	if (
+		(formattedTransaction.nonce as HexString).charAt(0) === '-' ||
+		(formattedTransaction.chainId as HexString).charAt(0) === '-'
+	)
+		// TODO - Replace error
+		throw new Error('Nonce or chainId is lower than 0');
+};
