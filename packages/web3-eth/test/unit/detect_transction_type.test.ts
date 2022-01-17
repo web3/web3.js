dfdf768817a8a8631ba1c111df63aab14497cd76
Wyatt Barnes <me@wyatt.email>
2022-01-17 05:34:24 -1000

    Init detectTransactionType and fixture

---
+++ packages/web3-eth/test/unit/detect_transction_type.test.ts
@@ -0,0 +1,41 @@
+import { detectTransactionType, Transaction } from '../../src/eth_tx';
+import {
+	transactionType0x0,
+	transactionType0x1,
+	transactionType0x2,
+	transactionTypeUndefined,
+} from '../fixtures/detect_transaction_type';
+
+describe('should override detectTransactionType method', () => {
+	it('should return 42', () => {
+		const overrideFunction = (transaction: Transaction) => {
+			if (transaction.type !== undefined) return transaction.type;
+			return 42;
+		};
+		expect(detectTransactionType(transactionTypeUndefined[0], overrideFunction)).toBe(42);
+	});
+});
+
+describe('should detect transaction type 0x0', () => {
+	it.each(transactionType0x0)('%s', transaction => {
+		expect(detectTransactionType(transaction)).toBe('0x0');
+	});
+});
+
+describe('should detect transaction type 0x1', () => {
+	it.each(transactionType0x1)('%s', transaction => {
+		expect(detectTransactionType(transaction)).toBe('0x1');
+	});
+});
+
+describe('should detect transaction type 0x2', () => {
+	it.each(transactionType0x2)('%s', transaction => {
+		expect(detectTransactionType(transaction)).toBe('0x2');
+	});
+});
+
+describe('should not be able to detect transaction type, returning undefined', () => {
+	it.each(transactionTypeUndefined)('%s', transaction => {
+		expect(detectTransactionType(transaction)).toBeUndefined();
+	});
+});
