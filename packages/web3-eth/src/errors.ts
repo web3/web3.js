307201f074251474c4fc70e48561ecc296051183
Wyatt Barnes <me@wyatt.email>
2022-01-17 01:40:24 -1000

    Replace inline errors with error constructors

--- packages/web3-eth/src/errors.ts
+++ packages/web3-eth/src/errors.ts
@@ -3,19 +3,124 @@
 import { Web3Error } from 'web3-utils';
 
 export class InvalidTransactionWithSender extends Web3Error {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public constructor(value: any) {
-		// TODO Discuss this naive approach to logging object
-		// Does not account for non JSON properties
+	public constructor(value: unknown) {
 		super(JSON.stringify(value), 'invalid transaction with sender');
 	}
 }
 
 export class InvalidTransactionCall extends Web3Error {
-	// eslint-disable-next-line @typescript-eslint/no-explicit-any
-	public constructor(value: any) {
-		// TODO Discuss this naive approach to logging object
-		// Does not account for non JSON properties
+	public constructor(value: unknown) {
 		super(JSON.stringify(value), 'invalid transaction call');
+	}
+}
+
+export class MissingCustomChainError extends Web3Error {
+	public constructor(value: undefined) {
+		super(value, 'If tx.common is provided it must have tx.common.customChain');
+	}
+}
+
+export class MissingCustomChainIdError extends Web3Error {
+	public constructor(value: undefined) {
+		super(
+			value,
+			'If tx.common is provided it must have tx.common.customChain and tx.common.customChain.chainId',
+		);
+	}
+}
+
+export class ChainIdMismatchError extends Web3Error {
+	public constructor(value: { txChainId: unknown; customChainId: unknown }) {
+		super(
+			JSON.stringify(value),
+			'Chain Id doesnt match in tx.chainId tx.common.customChain.chainId',
+		);
+	}
+}
+
+export class CommonOrChainAndHardforkError extends Web3Error {
+	public constructor() {
+		// TODO - Discuss what to pass as value, would it make sense to pass common, chain, and hardfork for error message here?
+		super(
+			'N/A',
+			'Please provide the @ethereumjs/common object or the chain and hardfork property but not all together.',
+		);
+	}
+}
+
+export class MissingChainOrHardforkError extends Web3Error {
+	public constructor(value: { chain: string | undefined; hardfork: string | undefined }) {
+		// TODO - Discuss what to pass as value
+		super(
+			'N/A',
+			`When specifying chain and hardfork, both values must be defined. Received "chain": ${
+				value.chain ?? 'undefined'
+			}, "hardfork": ${value.hardfork ?? 'undefined'}`,
+		);
+	}
+}
+
+export class MissingGasError extends Web3Error {
+	public constructor(value: unknown) {
+		super(value, '"gas" is missing');
+	}
+}
+
+export class InvalidGasOrGasPrice extends Web3Error {
+	public constructor(value: { gas: unknown; gasPrice: unknown }) {
+		super(JSON.stringify(value), 'Gas or gasPrice is lower than 0');
+	}
+}
+
+export class InvalidMaxPriorityFeePerGasOrMaxFeePerGas extends Web3Error {
+	public constructor(value: { maxPriorityFeePerGas: unknown; maxFeePerGas: unknown }) {
+		super(JSON.stringify(value), 'maxPriorityFeePerGas or maxFeePerGas is lower than 0');
+	}
+}
+
+export class Eip1559GasPriceError extends Web3Error {
+	public constructor(value: unknown) {
+		super(value, "eip-1559 transactions don't support gasPrice");
+	}
+}
+
+export class UnsupportedFeeMarketError extends Web3Error {
+	public constructor(value: { maxPriorityFeePerGas: unknown; maxFeePerGas: unknown }) {
+		super(
+			JSON.stringify(value),
+			"pre-eip-1559 transaction don't support maxFeePerGas/maxPriorityFeePerGas",
+		);
+	}
+}
+
+export class InvalidTransactionObjectError extends Web3Error {
+	public constructor(value: unknown) {
+		super(value, 'invalid transaction obejct');
+	}
+}
+
+export class InvalidNonceOrChainIdError extends Web3Error {
+	public constructor(value: { nonce: unknown; chainId: unknown }) {
+		super(JSON.stringify(value), 'Nonce or chainId is lower than 0');
+	}
+}
+
+export class UnableToPopulateNonceError extends Web3Error {
+	public constructor() {
+		// TODO - Discuss what to pass as value
+		super('N/A', 'unable to populate nonce, no from address available');
+	}
+}
+
+export class Eip1559NotSupportedError extends Web3Error {
+	public constructor() {
+		// TODO - Discuss what to pass as value
+		super('N/A', "Network doesn't support eip-1559");
+	}
+}
+
+export class UnsupportedTransactionTypeError extends Web3Error {
+	public constructor(value: unknown) {
+		super(value, 'unsupported transaction type');
 	}
 }
--- packages/web3-eth/src/eth_tx.ts
+++ packages/web3-eth/src/eth_tx.ts
@@ -13,6 +13,23 @@ import {
 } from 'web3-utils';
 import { privateKeyToAddress } from 'web3-eth-accounts';
 import Web3Eth from '.';
+import {
+	ChainIdMismatchError,
+	CommonOrChainAndHardforkError,
+	Eip1559GasPriceError,
+	Eip1559NotSupportedError,
+	InvalidGasOrGasPrice,
+	InvalidMaxPriorityFeePerGasOrMaxFeePerGas,
+	InvalidNonceOrChainIdError,
+	InvalidTransactionObjectError,
+	MissingChainOrHardforkError,
+	MissingCustomChainError,
+	MissingCustomChainIdError,
+	MissingGasError,
+	UnableToPopulateNonceError,
+	UnsupportedFeeMarketError,
+	UnsupportedTransactionTypeError,
+} from './errors';
 
 export type chain = 'goerli' | 'kovan' | 'mainnet' | 'rinkeby' | 'ropsten' | 'sepolia';
 export type hardfork =
@@ -99,7 +116,7 @@ export function formatTransaction<
 				),
 				chainId: convertToValidType(transaction.common?.customChain.chainId, desiredType),
 			},
-		}, 
+		},
 	};
 	// TODO - TSC is complaining that ReturnType could be instantiated with an
 	// arbitrary type which could be unrelated to 'string | number | bigint | undefined'
@@ -126,19 +143,17 @@ export const detectTransactionType = (
 const validateCustomChainInfo = (transaction: Transaction) => {
 	if (transaction.common !== undefined) {
 		if (transaction.common.customChain === undefined)
-			// TODO - Replace error
-			throw new Error('If tx.common is provided it must have tx.common.customChain');
+			throw new MissingCustomChainError(transaction.common.customChain);
 		if (transaction.common.customChain.chainId === undefined)
-			// TODO - Replace error
-			throw new Error(
-				'If tx.common is provided it must have tx.common.customChain and tx.common.customChain.chainId',
-			);
+			throw new MissingCustomChainIdError(transaction.common.customChain.chainId);
 		if (
 			transaction.chainId !== undefined &&
 			transaction.chainId !== transaction.common.customChain.chainId
 		)
-			// TODO - Replace error
-			throw new Error('Chain Id doesnt match in tx.chainId tx.common.customChain.chainId');
+			throw new ChainIdMismatchError({
+				txChainId: transaction.chainId,
+				customChainId: transaction.common.customChain.chainId,
+			});
 	}
 };
 
@@ -148,18 +163,15 @@ const validateChainInfo = (transaction: Transaction) => {
 		transaction.chain !== undefined &&
 		transaction.hardfork !== undefined
 	)
-		// TODO - Replace error
-		throw new Error(
-			'Please provide the @ethereumjs/common object or the chain and hardfork property but not all together.',
-		);
+		throw new CommonOrChainAndHardforkError();
 	if (
 		(transaction.chain !== undefined && transaction.hardfork === undefined) ||
 		(transaction.hardfork !== undefined && transaction.chain === undefined)
 	)
-		// TODO - Replace error
-		throw new Error(
-			`When specifying chain and hardfork, both values must be defined. Received "chain": ${transaction.chain}, "hardfork": ${transaction.hardfork}`,
-		);
+		throw new MissingChainOrHardforkError({
+			chain: transaction.chain,
+			hardfork: transaction.hardfork,
+		});
 };
 
 const validateGas = (transaction: Transaction<HexString>) => {
@@ -169,47 +181,50 @@ const validateGas = (transaction: Transaction<HexString>) => {
 		transaction.maxPriorityFeePerGas === undefined &&
 		transaction.maxFeePerGas === undefined
 	)
-		// TODO - Replace error
-		throw new Error('"gas" is missing');
+		throw new MissingGasError(transaction.gas);
 	if (transaction.gas !== undefined && transaction.gasPrice !== undefined) {
 		// This check is verifying gas and gasPrice aren't less than 0.
 		// transaction's number properties have been converted to HexStrings.
 		// JavaScript doesn't handle negative hex strings e.g. -0x1, but our
 		// numberToHex method does. -0x1 < 0 would result in false, so we must check if
 		// hex string is negative via the inclusion of -
-		if (transaction.gas.charAt(0) === '-' || transaction.gasPrice.charAt(0) === '-')
-			// TODO - Replace error
-			throw new Error('Gas or gasPrice is lower than 0');
+		if (transaction.gas.startsWith('-') || transaction.gasPrice.startsWith('-'))
+			throw new InvalidGasOrGasPrice({
+				gas: transaction.gas,
+				gasPrice: transaction.gasPrice,
+			});
 	}
 
 	if (transaction.maxFeePerGas !== undefined && transaction.maxPriorityFeePerGas !== undefined) {
 		if (
-			transaction.maxFeePerGas.charAt(0) === '-' ||
-			transaction.maxPriorityFeePerGas.charAt(0) === '-'
+			transaction.maxFeePerGas.startsWith('-') ||
+			transaction.maxPriorityFeePerGas.startsWith('-')
 		)
-			// TODO - Replace error
-			throw new Error('maxPriorityFeePerGas or maxFeePerGas is lower than 0');
+			throw new InvalidMaxPriorityFeePerGasOrMaxFeePerGas({
+				maxPriorityFeePerGas: transaction.maxPriorityFeePerGas,
+				maxFeePerGas: transaction.maxFeePerGas,
+			});
 	}
 
 	const hasEip1559 =
 		transaction.maxFeePerGas !== undefined || transaction.maxPriorityFeePerGas !== undefined;
 	if (transaction.gasPrice !== undefined && (transaction.type === '0x2' || hasEip1559))
-		// TODO - Replace error
-		throw new Error("eip-1559 transactions don't support gasPrice");
+		throw new Eip1559GasPriceError(transaction.gasPrice);
 	if ((transaction.type === '0x0' || transaction.type === '0x1') && hasEip1559)
-		// TODO - Replace error
-		throw new Error("pre-eip-1559 transaction don't support maxFeePerGas/maxPriorityFeePerGas");
+		throw new UnsupportedFeeMarketError({
+			maxPriorityFeePerGas: transaction.maxPriorityFeePerGas,
+			maxFeePerGas: transaction.maxFeePerGas,
+		});
 };
 
 export const validateTransactionForSigning = (
 	transaction: Transaction,
 	overrideMethod?: (transaction: Transaction) => void,
 ) => {
-	if (overrideMethod !== undefined) return overrideMethod(transaction);
+	if (overrideMethod !== undefined) overrideMethod(transaction);
 
 	if (typeof transaction !== 'object' || transaction === null)
-		// TODO - Replace error
-		throw new Error('invalid transaction obejct');
+		throw new InvalidTransactionObjectError(transaction);
 
 	const formattedTransaction = formatTransaction(transaction, ValidTypes.HexString);
 
@@ -218,11 +233,13 @@ export const validateTransactionForSigning = (
 	validateGas(formattedTransaction);
 
 	if (
-		(formattedTransaction.nonce as HexString).charAt(0) === '-' ||
-		(formattedTransaction.chainId as HexString).charAt(0) === '-'
+		(formattedTransaction.nonce as HexString).startsWith('-') ||
+		(formattedTransaction.chainId as HexString).startsWith('-')
 	)
-		// TODO - Replace error
-		throw new Error('Nonce or chainId is lower than 0');
+		throw new InvalidNonceOrChainIdError({
+			nonce: transaction.nonce,
+			chainId: transaction.chainId,
+		});
 };
 
 export interface PopulatedUnsignedBaseTransaction<NumberType = Numbers> {
@@ -240,10 +257,12 @@ export interface PopulatedUnsignedBaseTransaction<NumberType = Numbers> {
 	common: Common<NumberType>;
 	gasLimit: Numbers;
 }
-export interface PopulatedUnsignedEip2930Transaction<NumberType = Numbers> extends PopulatedUnsignedBaseTransaction<NumberType> {
+export interface PopulatedUnsignedEip2930Transaction<NumberType = Numbers>
+	extends PopulatedUnsignedBaseTransaction<NumberType> {
 	accessList: AccessList;
 }
-export interface PopulatedUnsignedEip1559Transaction<NumberType = Numbers> extends PopulatedUnsignedEip2930Transaction<NumberType> {
+export interface PopulatedUnsignedEip1559Transaction<NumberType = Numbers>
+	extends PopulatedUnsignedEip2930Transaction<NumberType> {
 	gasPrice: never;
 	maxFeePerGas: NumberType;
 	maxPriorityFeePerGas: NumberType;
@@ -255,7 +274,7 @@ export type PopulatedUnsignedTransaction<NumberType = Numbers> =
 export async function populateTransaction<
 	DesiredType extends ValidTypes,
 	ReturnType = ValidReturnTypes[DesiredType],
-> (
+>(
 	transaction: Transaction,
 	web3Context: Web3Context<EthExecutionAPI>,
 	desiredType: DesiredType,
@@ -279,9 +298,7 @@ export async function populateTransaction<
 	}
 
 	if (populatedTransaction.nonce === undefined) {
-		// TODO - Replace error
-		if (populatedTransaction.from === undefined)
-			throw new Error('unable to populate nonce, no from address available');
+		if (populatedTransaction.from === undefined) throw new UnableToPopulateNonceError();
 		populatedTransaction.nonce = await web3Eth.getTransactionCount(
 			populatedTransaction.from,
 			BlockTags.PENDING,
@@ -316,40 +333,51 @@ export async function populateTransaction<
 	// If populatedTransaction.type is already defined, no change will be made
 	populatedTransaction.type = detectTransactionType(populatedTransaction);
 	// TODO - After web3Context.defaultTxType is implemented
-	// if (populatedTransaction.type === undefined) populatedTransaction.type = web3Context.defaultTxType;
+	if (populatedTransaction.type === undefined) populatedTransaction.type = '0x0'; // web3Context.defaultTxType;
 
-	if (populatedTransaction.type !== undefined) {
-		const hexTxType = toHex(populatedTransaction.type);
-		if (hexTxType === '0x0' || hexTxType === '0x1') {
-			if (populatedTransaction.gasPrice === undefined)
-				populatedTransaction.gasPrice = await web3Eth.getGasPrice();
-		} else if (hexTxType === '0x1' || hexTxType === '0x2') {
-			if (populatedTransaction.accessList === undefined) populatedTransaction.accessList = [];
-		} else if (hexTxType === '0x2') {
-			// Unless otherwise specified by web3Context.defaultBlock, this defaults to latest
-			const block = await web3Eth.getBlock();
-			if (block.baseFeePerGas === undefined)
-				// TODO - Replace error
-				throw new Error("Network doesn't support eip-1559");
+	const block = await web3Eth.getBlock();
+	const hexTxType = toHex(populatedTransaction.type);
 
-			if (populatedTransaction.gasPrice !== undefined) {
-				// Carry over logic from 1.x
-				populatedTransaction.maxPriorityFeePerGas = populatedTransaction.gasPrice;
-				populatedTransaction.maxFeePerGas = populatedTransaction.gasPrice;
-				populatedTransaction.gasPrice = undefined;
-			} else {
-				if (populatedTransaction.maxPriorityFeePerGas === undefined)
-					populatedTransaction.maxPriorityFeePerGas = toHex('2500000000'); // 2.5 Gwei
-				if (populatedTransaction.maxFeePerGas === undefined)
-					populatedTransaction.maxFeePerGas =
-						BigInt(block.baseFeePerGas) * BigInt(2) +
-						BigInt(populatedTransaction.maxPriorityFeePerGas);
-			}
+	if (hexTxType < '0x0' || hexTxType > '0x2')
+		throw new UnsupportedTransactionTypeError(populatedTransaction.type);
+
+	if (hexTxType === '0x0' || hexTxType === '0x1') {
+		// transaction.type not supported before Berlin hardfork
+		// TODO - Maybe add check for populatedTransaction.hardfork >= Berlin before deleting
+		if (hexTxType === '0x0') populatedTransaction.type = undefined;
+
+		if (populatedTransaction.gasPrice === undefined)
+			populatedTransaction.gasPrice = await web3Eth.getGasPrice();
+	}
+
+	if (hexTxType === '0x1' || hexTxType === '0x2') {
+		if (populatedTransaction.accessList === undefined) populatedTransaction.accessList = [];
+	}
+
+	if (hexTxType === '0x2') {
+		// Unless otherwise specified by web3Context.defaultBlock, this defaults to latest
+		if (block.baseFeePerGas === undefined) throw new Eip1559NotSupportedError();
+
+		if (populatedTransaction.gasPrice !== undefined) {
+			// Logic from 1.x
+			populatedTransaction.maxPriorityFeePerGas = populatedTransaction.gasPrice;
+			populatedTransaction.maxFeePerGas = populatedTransaction.gasPrice;
+			populatedTransaction.gasPrice = undefined;
 		} else {
-			// TODO - Replace error
-			throw new Error('unsupported transaction type');
+			if (populatedTransaction.maxPriorityFeePerGas === undefined)
+				// TODO - Add maxPriorityFeePerGas default to Web3Context
+				populatedTransaction.maxPriorityFeePerGas = toHex('2500000000'); // 2.5 Gwei
+			if (populatedTransaction.maxFeePerGas === undefined)
+				populatedTransaction.maxFeePerGas =
+					BigInt(block.baseFeePerGas) * BigInt(2) +
+					BigInt(populatedTransaction.maxPriorityFeePerGas);
 		}
 	}
 
-	return formatTransaction(populatedTransaction, desiredType) as PopulatedUnsignedTransaction<ReturnType>
-};
+	return formatTransaction(
+		populatedTransaction,
+		desiredType,
+	) as PopulatedUnsignedTransaction<ReturnType>;
+}
+
+// TODO - Replace use of Web3Context with Web3Eth
