06f75753b293f3b73cad251e116eb4185afa23f0
Wyatt Barnes <me@wyatt.email>
2022-01-19 07:19:44 -1000

    Add parent describe to detect_transaction_type test

--- packages/web3-eth/test/unit/detect_transction_type.test.ts
+++ packages/web3-eth/test/unit/detect_transction_type.test.ts
@@ -6,36 +6,38 @@ import {
 	transactionTypeUndefined,
 } from '../fixtures/detect_transaction_type';
 
-describe('should override detectTransactionType method', () => {
-	it('should return 42', () => {
-		// @ts-expect-error - Purposefully not using transaction here,
-		// but must be present to satisfy method signature
-		// eslint-disable-next-line @typescript-eslint/no-unused-vars
-		const overrideFunction = (transaction: Transaction) => 42;
-		expect(detectTransactionType(transactionTypeUndefined[0], overrideFunction)).toBe(42);
+describe('detectTransactionType', () => {
+	describe('should override detectTransactionType method', () => {
+		it('should return 42', () => {
+			// @ts-expect-error - Purposefully not using transaction here,
+			// but must be present to satisfy method signature
+			// eslint-disable-next-line @typescript-eslint/no-unused-vars
+			const overrideFunction = (transaction: Transaction) => 42;
+			expect(detectTransactionType(transactionTypeUndefined[0], overrideFunction)).toBe(42);
+		});
 	});
-});
 
-describe('should detect transaction type 0x0', () => {
-	it.each(transactionType0x0)('%s', transaction => {
-		expect(detectTransactionType(transaction)).toBe('0x0');
+	describe('should detect transaction type 0x0', () => {
+		it.each(transactionType0x0)('%s', transaction => {
+			expect(detectTransactionType(transaction)).toBe('0x0');
+		});
 	});
-});
 
-describe('should detect transaction type 0x1', () => {
-	it.each(transactionType0x1)('%s', transaction => {
-		expect(detectTransactionType(transaction)).toBe('0x1');
+	describe('should detect transaction type 0x1', () => {
+		it.each(transactionType0x1)('%s', transaction => {
+			expect(detectTransactionType(transaction)).toBe('0x1');
+		});
 	});
-});
 
-describe('should detect transaction type 0x2', () => {
-	it.each(transactionType0x2)('%s', transaction => {
-		expect(detectTransactionType(transaction)).toBe('0x2');
+	describe('should detect transaction type 0x2', () => {
+		it.each(transactionType0x2)('%s', transaction => {
+			expect(detectTransactionType(transaction)).toBe('0x2');
+		});
 	});
-});
 
-describe('should not be able to detect transaction type, returning undefined', () => {
-	it.each(transactionTypeUndefined)('%s', transaction => {
-		expect(detectTransactionType(transaction)).toBeUndefined();
+	describe('should not be able to detect transaction type, returning undefined', () => {
+		it.each(transactionTypeUndefined)('%s', transaction => {
+			expect(detectTransactionType(transaction)).toBeUndefined();
+		});
 	});
 });
