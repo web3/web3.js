143922bf35ce19d3759d5cfa33f9993e2c2831d5
Wyatt Barnes <me@wyatt.email>
2022-01-19 07:19:56 -1000

    Add parent describe to format_transaction test

--- packages/web3-eth/test/unit/format_transaction.test.ts
+++ packages/web3-eth/test/unit/format_transaction.test.ts
@@ -7,70 +7,72 @@ import {
 	numberTransaction,
 } from '../fixtures/format_transaction';
 
-describe('should convert hex string properties to expected type', () => {
-	it('numbers', () => {
-		const result = formatTransaction(hexStringTransaction, ValidTypes.Number);
-		expect(result).toStrictEqual(numberTransaction);
+describe('formatTransaction', () => {
+	describe('should convert hex string properties to expected type', () => {
+		it('numbers', () => {
+			const result = formatTransaction(hexStringTransaction, ValidTypes.Number);
+			expect(result).toStrictEqual(numberTransaction);
+		});
+
+		it('number strings', () => {
+			const result = formatTransaction(hexStringTransaction, ValidTypes.NumberString);
+			expect(result).toStrictEqual(numberStringTransaction);
+		});
+
+		it('BigInts', () => {
+			const result = formatTransaction(hexStringTransaction, ValidTypes.BigInt);
+			expect(result).toStrictEqual(bigIntTransaction);
+		});
 	});
 
-	it('number strings', () => {
-		const result = formatTransaction(hexStringTransaction, ValidTypes.NumberString);
-		expect(result).toStrictEqual(numberStringTransaction);
+	describe('should convert number properties to expected type', () => {
+		it('hex string', () => {
+			const result = formatTransaction(numberTransaction, ValidTypes.HexString);
+			expect(result).toStrictEqual(hexStringTransaction);
+		});
+
+		it('number strings', () => {
+			const result = formatTransaction(numberTransaction, ValidTypes.NumberString);
+			expect(result).toStrictEqual(numberStringTransaction);
+		});
+
+		it('BigInts', () => {
+			const result = formatTransaction(numberTransaction, ValidTypes.BigInt);
+			expect(result).toStrictEqual(bigIntTransaction);
+		});
 	});
 
-	it('BigInts', () => {
-		const result = formatTransaction(hexStringTransaction, ValidTypes.BigInt);
-		expect(result).toStrictEqual(bigIntTransaction);
-	});
-});
+	describe('should convert number string properties to expected type', () => {
+		it('hex strings', () => {
+			const result = formatTransaction(numberStringTransaction, ValidTypes.HexString);
+			expect(result).toStrictEqual(hexStringTransaction);
+		});
 
-describe('should convert number properties to expected type', () => {
-	it('hex string', () => {
-		const result = formatTransaction(numberTransaction, ValidTypes.HexString);
-		expect(result).toStrictEqual(hexStringTransaction);
-	});
+		it('numbers', () => {
+			const result = formatTransaction(numberStringTransaction, ValidTypes.Number);
+			expect(result).toStrictEqual(numberTransaction);
+		});
 
-	it('number strings', () => {
-		const result = formatTransaction(numberTransaction, ValidTypes.NumberString);
-		expect(result).toStrictEqual(numberStringTransaction);
-	});
-
-	it('BigInts', () => {
-		const result = formatTransaction(numberTransaction, ValidTypes.BigInt);
-		expect(result).toStrictEqual(bigIntTransaction);
-	});
-});
-
-describe('should convert number string properties to expected type', () => {
-	it('hex strings', () => {
-		const result = formatTransaction(numberStringTransaction, ValidTypes.HexString);
-		expect(result).toStrictEqual(hexStringTransaction);
+		it('BigInts', () => {
+			const result = formatTransaction(numberStringTransaction, ValidTypes.BigInt);
+			expect(result).toStrictEqual(bigIntTransaction);
+		});
 	});
 
-	it('numbers', () => {
-		const result = formatTransaction(numberStringTransaction, ValidTypes.Number);
-		expect(result).toStrictEqual(numberTransaction);
-	});
+	describe('should convert bigint properties to expected type', () => {
+		it('hex strings', () => {
+			const result = formatTransaction(bigIntTransaction, ValidTypes.HexString);
+			expect(result).toStrictEqual(hexStringTransaction);
+		});
 
-	it('BigInts', () => {
-		const result = formatTransaction(numberStringTransaction, ValidTypes.BigInt);
-		expect(result).toStrictEqual(bigIntTransaction);
-	});
-});
+		it('numbers', () => {
+			const result = formatTransaction(bigIntTransaction, ValidTypes.Number);
+			expect(result).toStrictEqual(numberTransaction);
+		});
 
-describe('should convert bigint properties to expected type', () => {
-	it('hex strings', () => {
-		const result = formatTransaction(bigIntTransaction, ValidTypes.HexString);
-		expect(result).toStrictEqual(hexStringTransaction);
-	});
-
-	it('numbers', () => {
-		const result = formatTransaction(bigIntTransaction, ValidTypes.Number);
-		expect(result).toStrictEqual(numberTransaction);
-	});
-
-	it('number strings', () => {
-		const result = formatTransaction(bigIntTransaction, ValidTypes.NumberString);
-		expect(result).toStrictEqual(numberStringTransaction);
+		it('number strings', () => {
+			const result = formatTransaction(bigIntTransaction, ValidTypes.NumberString);
+			expect(result).toStrictEqual(numberStringTransaction);
+		});
 	});
 });
