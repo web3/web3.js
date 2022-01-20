9bbec7a6557a5084151000b860fc7241774450bf
Alex <alex.luu@mail.utoronto.ca>
2022-01-20 10:04:58 -0500

    ENS ABI contract ReverseRegistrar and TestRegistrar  (#4714)
    * adding abi
    
    * adding registry abi
    
    * fixing linter
    
    * adding testregistrar and reverseregistrar
    
    * updating abi
    
    * fixing lint
    
    * updating per feedback
    
    * updating files
    
    * updating abi names
    
    * added links to solidity contracts for the abi

---
+++ packages/web3-eth-ens/src/ABI/ReverseRegistrar.ts
@@ -0,0 +1,141 @@
+// https://github.com/ensdomains/ens/blob/master/contracts/ReverseRegistrar.sol
+const REVERSE_REGISTRAR = [
+	{
+		inputs: [
+			{
+				internalType: 'contract ENS',
+				name: 'ensAddr',
+				type: 'address',
+			},
+			{
+				internalType: 'contract NameResolver',
+				name: 'resolverAddr',
+				type: 'address',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		inputs: [],
+		name: 'ADDR_REVERSE_NODE',
+		outputs: [
+			{
+				internalType: 'bytes32',
+				name: '',
+				type: 'bytes32',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'owner',
+				type: 'address',
+			},
+		],
+		name: 'claim',
+		outputs: [
+			{
+				internalType: 'bytes32',
+				name: '',
+				type: 'bytes32',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'owner',
+				type: 'address',
+			},
+			{
+				internalType: 'address',
+				name: 'resolver',
+				type: 'address',
+			},
+		],
+		name: 'claimWithResolver',
+		outputs: [
+			{
+				internalType: 'bytes32',
+				name: '',
+				type: 'bytes32',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'defaultResolver',
+		outputs: [
+			{
+				internalType: 'contract NameResolver',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'ens',
+		outputs: [
+			{
+				internalType: 'contract ENS',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'address',
+				name: 'addr',
+				type: 'address',
+			},
+		],
+		name: 'node',
+		outputs: [
+			{
+				internalType: 'bytes32',
+				name: '',
+				type: 'bytes32',
+			},
+		],
+		stateMutability: 'pure',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'string',
+				name: 'name',
+				type: 'string',
+			},
+		],
+		name: 'setName',
+		outputs: [
+			{
+				internalType: 'bytes32',
+				name: '',
+				type: 'bytes32',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+] as const;
+
+module.exports = REVERSE_REGISTRAR;
---
+++ packages/web3-eth-ens/src/ABI/TestRegistrar.ts
@@ -0,0 +1,84 @@
+// https://github.com/ensdomains/ens/blob/master/contracts/TestRegistrar.sol
+const TEST_REGISTRAR = [
+	{
+		inputs: [
+			{
+				internalType: 'contract ENS',
+				name: 'ensAddr',
+				type: 'address',
+			},
+			{
+				internalType: 'bytes32',
+				name: 'node',
+				type: 'bytes32',
+			},
+		],
+		stateMutability: 'nonpayable',
+		type: 'constructor',
+	},
+	{
+		inputs: [],
+		name: 'ens',
+		outputs: [
+			{
+				internalType: 'contract ENS',
+				name: '',
+				type: 'address',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'bytes32',
+				name: '',
+				type: 'bytes32',
+			},
+		],
+		name: 'expiryTimes',
+		outputs: [
+			{
+				internalType: 'uint256',
+				name: '',
+				type: 'uint256',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+	{
+		inputs: [
+			{
+				internalType: 'bytes32',
+				name: 'label',
+				type: 'bytes32',
+			},
+			{
+				internalType: 'address',
+				name: 'owner',
+				type: 'address',
+			},
+		],
+		name: 'register',
+		outputs: [],
+		stateMutability: 'nonpayable',
+		type: 'function',
+	},
+	{
+		inputs: [],
+		name: 'rootNode',
+		outputs: [
+			{
+				internalType: 'bytes32',
+				name: '',
+				type: 'bytes32',
+			},
+		],
+		stateMutability: 'view',
+		type: 'function',
+	},
+] as const;
+
+module.exports = TEST_REGISTRAR;
