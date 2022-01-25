4b387714bcd2e4349450b6ea1d0c47c314f957da
Wyatt Barnes <me@wyatt.email>
2022-01-24 23:11:58 -1000

    Update expected errors for validateGas tests

--- packages/web3-eth/test/fixtures/validate_transaction_for_signing.ts
+++ packages/web3-eth/test/fixtures/validate_transaction_for_signing.ts
@@ -395,7 +395,12 @@ export const validateGasData: [
 			r: '0x4f4c17305743700648bc4f6cd3038ec6f6af0df73e31757007b7f59df7bee88d',
 			s: '0x7e1941b264348e80c78c4027afc65a87b0a5e43e86742b8ca0823584c6788fd0',
 		},
-		new InvalidGasOrGasPrice({ gas: undefined, gasPrice: '0x4a817c800' }),
+		new MissingGasError({
+			gas: undefined,
+			gasLimit: '0x5208',
+			maxFeePerGas: undefined,
+			maxPriorityFeePerGas: undefined,
+		}),
 	],
 	[
 		{
@@ -454,7 +459,9 @@ export const validateGasData: [
 			r: '0x4f4c17305743700648bc4f6cd3038ec6f6af0df73e31757007b7f59df7bee88d',
 			s: '0x7e1941b264348e80c78c4027afc65a87b0a5e43e86742b8ca0823584c6788fd0',
 		},
-		new InvalidMaxPriorityFeePerGasOrMaxFeePerGas({
+		new MissingGasError({
+			gas: undefined,
+			gasLimit: '0x5208',
 			maxPriorityFeePerGas: undefined,
 			maxFeePerGas: '0x1229298c00',
 		}),
@@ -476,7 +483,9 @@ export const validateGasData: [
 			r: '0x4f4c17305743700648bc4f6cd3038ec6f6af0df73e31757007b7f59df7bee88d',
 			s: '0x7e1941b264348e80c78c4027afc65a87b0a5e43e86742b8ca0823584c6788fd0',
 		},
-		new InvalidMaxPriorityFeePerGasOrMaxFeePerGas({
+		new MissingGasError({
+			gas: undefined,
+			gasLimit: '0x5208',
 			maxPriorityFeePerGas: '0x49504f80',
 			maxFeePerGas: undefined,
 		}),
