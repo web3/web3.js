07f5cd7bfbb70669da922f80d962705fa1a0b9d6
Wyatt Barnes <me@wyatt.email>
2022-01-25 19:35:52 -1000

    Refactor detectTransactionType

--- packages/web3-eth/src/eth_tx.ts
+++ packages/web3-eth/src/eth_tx.ts
@@ -95,19 +95,27 @@ export function formatTransaction<
 
 export const detectTransactionType = (
 	transaction: Transaction,
-	overrideMethod?: (transaction: Transaction) => Numbers | undefined,
-): Numbers | undefined => {
+	overrideMethod?: (transaction: Transaction) => HexString | undefined,
+): HexString | undefined => {
+	// TODO - Refactor overrideMethod
 	if (overrideMethod !== undefined) return overrideMethod(transaction);
-	if (transaction.type !== undefined) return transaction.type;
+	if (transaction.type !== undefined)
+		return convertToValidType(transaction.type, ValidTypes.HexString);
 
-	if (transaction.maxFeePerGas !== undefined || transaction.maxPriorityFeePerGas !== undefined)
+	if (
+		transaction.maxFeePerGas !== undefined ||
+		transaction.maxPriorityFeePerGas !== undefined ||
+		transaction.hardfork === 'london' ||
+		transaction.common?.hardfork === 'london'
+	)
 		return '0x2';
-	if (transaction.hardfork === 'london') return '0x2';
-	if (transaction.common?.hardfork === 'london') return '0x2';
 
-	if (transaction.accessList !== undefined) return '0x1';
-	if (transaction.hardfork === 'berlin') return '0x1';
-	if (transaction.common?.hardfork === 'berlin') return '0x1';
+	if (
+		transaction.accessList !== undefined ||
+		transaction.hardfork === 'berlin' ||
+		transaction.common?.hardfork === 'berlin'
+	)
+		return '0x1';
 
 	return undefined;
 };
