6a2308133ef7b7fbda0355192aeeeadf663a146a
Alex <alex.luu@mail.utoronto.ca>
2022-01-31 18:06:25 -0500

    adding chainid to transaction

--- packages/web3-common/src/eth_execution_api.ts
+++ packages/web3-common/src/eth_execution_api.ts
@@ -41,6 +41,7 @@ export interface BaseTransaction {
 	readonly gas: Uint;
 	readonly value: Uint;
 	readonly input: HexStringBytes;
+	readonly chainId: Uint;
 }
 
 export interface Transaction1559Unsigned extends BaseTransaction {
--- packages/web3-eth/src/types.ts
+++ packages/web3-eth/src/types.ts
@@ -69,6 +69,7 @@ interface BaseTransactionFormatted<
 	readonly gas: ReturnType;
 	readonly value: ReturnType;
 	readonly input: HexStringBytes;
+	readonly chainId: ReturnType;
 }
 
 interface Transaction1559UnsignedFormatted<
