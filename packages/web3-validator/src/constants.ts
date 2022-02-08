ddecdde7a2f11b8e4f969ec985e5c2c6fefed632
Nazar Hussain <nazarhussain@gmail.com>
2022-02-08 22:02:23 +0500

    `web3-validator` TODOs (#4755)

--- packages/web3-validator/src/constants.ts
+++ packages/web3-validator/src/constants.ts
@@ -1,9 +1,1 @@
-export const VALID_ETH_BASE_TYPES = [
-	'bool',
-	'int',
-	'uint',
-	'bytes',
-	'string',
-	'address',
-	'tuple',
-] as const;
+export const VALID_ETH_BASE_TYPES = ['bool', 'int', 'uint', 'bytes', 'string', 'address', 'tuple'];
