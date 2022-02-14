6421ea157508f969d05039e73a03156db88cc1d7
Vitaly <bikevit2008@yandex.ru>
2022-02-14 19:31:32 +0300

    Optimization for code
    This code works good, but without unnecessary else if

--- packages/web3-utils/src/utils.js
+++ packages/web3-utils/src/utils.js
@@ -84,13 +84,11 @@ var toTwosComplement = function (number) {
  */
 var isAddress = function (address) {
     // check if it has the basic requirements of an address
-    if (!/^(0x)?[0-9a-f]{40}$/i.test(address)) {
-        return false;
-        // If it's ALL lowercase or ALL upppercase
-    } else if (/^(0x|0X)?[0-9a-f]{40}$/.test(address) || /^(0x|0X)?[0-9A-F]{40}$/.test(address)) {
+    //case insensitive regexp
+    if (/^(0x)?[0-9a-f]{40}$/i.test(address)) {
         return true;
-        // Otherwise check each case
     } else {
+        // Otherwise check each case
         return checkAddressChecksum(address);
     }
 };
