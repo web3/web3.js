5f3a2b23ac4b0f97918ba29ee478e965cbae9482
Nazar Hussain <nazarhussain@gmail.com>
2022-02-16 15:52:29 +0500

    :art: Add initial structure

---
+++ packages/web3-eth-personal/src/index.ts
@@ -0,0 +1,4 @@
+import { EthExecutionAPI, TransactionWithSender } from 'web3-common';
+import { SupportedProviders, Web3ConfigOptions, Web3Context } from 'web3-core';
+
+class EthPersonal extends Web3Context<EthExecutionAPI> {}
