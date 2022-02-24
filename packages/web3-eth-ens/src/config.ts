78209d15ef03b3847dacd407b7515f9ceef5ca5d
jdevcs <junaid@chainsafe.io>
2022-02-24 21:13:48 +0100

    get funcs list supported in resolver interfaces

--- packages/web3-eth-ens/src/config.ts
+++ packages/web3-eth-ens/src/config.ts
@@ -1,6 +1,7 @@
 export const registryContractAddress = '0x00000000000C2E074eC69A0dFb2997BA6C7d2e1e';
 
 // https://docs.ens.domains/contract-developer-guide/writing-a-resolver
+// resolver interface Ids
 export const interfaceIds: { [T: string]: string } = {
 	addr: '0x3b3b57de',
 	name: '0x691f3431',
@@ -10,8 +11,12 @@ export const interfaceIds: { [T: string]: string } = {
 	contenthash: '0xbc1c58d1',
 };
 
+// functions list supported in resolver interfaces
 export const methodsInInterface: { [T: string]: string } = {
 	setAddr: 'addr',
+	addr: 'addr',
 	setPubkey: 'pubkey',
+	pubkey: 'pubkey',
 	setContenthash: 'contenthash',
+	contenthash: 'contenthash',
 };
