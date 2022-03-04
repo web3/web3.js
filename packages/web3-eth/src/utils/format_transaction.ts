25338a42f5314e79e611cf6d89e786f68bf44648
Wyatt Barnes <me@wyatt.email>
2022-03-03 23:26:51 -1000

    Refactor format_transaction.ts

--- packages/web3-eth/src/utils/format_transaction.ts
+++ packages/web3-eth/src/utils/format_transaction.ts
@@ -1,22 +1,12 @@
-import { convertToValidType, Numbers, ValidReturnTypes, ValidTypes } from 'web3-utils';
+import { convertToValidType, mergeDeep, Numbers, ValidReturnTypes, ValidTypes } from 'web3-utils';
 import { TransactionDataAndInputError } from '../errors';
 import { Transaction } from '../types';
 
-export function formatTransaction<
+function formatNonNestedProperties<
 	DesiredType extends ValidTypes,
 	NumberType extends ValidReturnTypes[DesiredType] = ValidReturnTypes[DesiredType],
 >(transaction: Transaction, desiredType: DesiredType): Transaction<NumberType> {
-	// TODO - The spread operator performs a shallow copy of transaction.
-	// I tried using Object.assign({}, transaction) which is supposed to perform a deep copy,
-	// but format_transactions.test.ts were still failing due to original nested object properties
-	// being wrongfully updated by this method.
-	const formattedTransaction = { ...transaction };
-	if (transaction.common !== undefined) {
-		formattedTransaction.common = { ...transaction.common };
-		if (transaction.common.customChain !== undefined)
-			formattedTransaction.common.customChain = { ...transaction.common.customChain };
-	}
-
+	const formattedProperties: Transaction<NumberType> = {};
 	const formattableProperties: (keyof Transaction)[] = [
 		'value',
 		'gas',
@@ -28,45 +18,63 @@ export function formatTransaction<
 		'chainId',
 		'gasLimit',
 		'v',
+		// 'to',
+		// 'from',
 	];
-	for (const transactionProperty of formattableProperties) {
-		const formattedProperty = formattedTransaction[transactionProperty];
+	for (const formattableProperty of formattableProperties) {
+		const transactionProperty = transaction[formattableProperty];
 		if (
-			formattedProperty !== undefined &&
-			formattedProperty !== null &&
-			typeof formattedProperty !== 'object' &&
-			!Array.isArray(formattedProperty)
+			transactionProperty !== undefined &&
+			transactionProperty !== null &&
+			typeof transactionProperty !== 'object' &&
+			!Array.isArray(transactionProperty)
 		) {
-			if (transactionProperty === 'value' && formattedProperty === '0x') continue;
-			(formattedTransaction[transactionProperty] as Numbers) = convertToValidType(
-				formattedProperty,
+			// if (transactionProperty === 'to' || transactionProperty === 'from') {
+			// 	// formattedTransaction[formattableProperty] = toChecksumAddress(formattableProperty);
+			// 	continue;
+			// }
+
+			if (formattableProperty === 'value' && transactionProperty === '0x') continue;
+
+			(formattedProperties[formattableProperty] as Numbers) = convertToValidType(
+				transactionProperty,
 				desiredType,
 			);
 		}
 	}
 
-	if (formattedTransaction.common?.customChain !== undefined) {
-		if (formattedTransaction.common.customChain.networkId !== undefined)
-			formattedTransaction.common.customChain.networkId = convertToValidType(
-				formattedTransaction.common.customChain.networkId,
-				desiredType,
-			);
-		if (formattedTransaction.common.customChain.chainId !== undefined)
-			formattedTransaction.common.customChain.chainId = convertToValidType(
-				formattedTransaction.common.customChain.chainId,
-				desiredType,
-			);
+	if (transaction.data !== undefined && transaction.input !== undefined)
+		throw new TransactionDataAndInputError({
+			data: transaction.data,
+			input: transaction.input,
+		});
+	else if (transaction.input !== undefined) {
+		formattedProperties.data = transaction.input;
 	}
 
-	if (formattedTransaction.data !== undefined && formattedTransaction.input !== undefined)
-		throw new TransactionDataAndInputError({
-			data: formattedTransaction.data,
-			input: formattedTransaction.input,
-		});
-	else if (formattedTransaction.input !== undefined) {
-		formattedTransaction.data = formattedTransaction.input;
-		delete formattedTransaction.input;
-	}
+	return formattedProperties;
+}
+
+export function formatTransaction<
+	DesiredType extends ValidTypes,
+	NumberType extends ValidReturnTypes[DesiredType] = ValidReturnTypes[DesiredType],
+>(transaction: Transaction, desiredType: DesiredType): Transaction<NumberType> {
+	const formattedTransaction: Transaction = mergeDeep({}, {
+		...transaction,
+		...formatNonNestedProperties(transaction, desiredType),
+	} as Record<string, unknown>);
+
+	if (formattedTransaction.common?.customChain.networkId !== undefined)
+		formattedTransaction.common.customChain.networkId = convertToValidType(
+			formattedTransaction.common.customChain.networkId,
+			desiredType,
+		);
+
+	if (formattedTransaction.common?.customChain.chainId !== undefined)
+		formattedTransaction.common.customChain.chainId = convertToValidType(
+			formattedTransaction.common.customChain.chainId,
+			desiredType,
+		);
 
 	return formattedTransaction as Transaction<NumberType>;
 }
