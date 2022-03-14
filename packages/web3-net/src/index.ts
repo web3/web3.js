80ee351c15ceb0452e71d456a94133efa3b593fa
Wyatt Barnes <me@wyatt.email>
2022-03-13 17:01:38 -1000

    Add rpcMethods export to web3-net

--- packages/web3-net/src/index.ts
+++ packages/web3-net/src/index.ts
@@ -1,6 +1,7 @@
 import { Web3Net } from './web3_net';
 
 export * from './web3_net';
+export * as rpcMethods from './rpc_methods';
 export * from './rpc_method_wrappers';
 export * from './web3_net_api';
 
