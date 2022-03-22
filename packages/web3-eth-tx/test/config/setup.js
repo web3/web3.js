37f216f1ff1ee9d50edc355b45cf62e9399f2ddb
Nikos Iliakis <nikoulai94@hotmail.com>
2022-03-22 15:13:31 +0200

    Remove Eth Transaction package

--- packages/web3-eth-tx/test/config/setup.js
+++
@@ -1,7 +0,0 @@
-// Have to use `require` because of Jest issue https://jestjs.io/docs/ecmascript-modules
-// eslint-disable-next-line @typescript-eslint/no-require-imports
-require('jest-extended');
-
-// @todo extend jest to have "toHaveBeenCalledOnceWith" matcher.
-
-process.env.NODE_ENV = 'test';
