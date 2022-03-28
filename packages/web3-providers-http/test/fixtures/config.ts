4d6b51def73e9fd837a754e0f9e1ca52c11935a2
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-03-28 18:06:22 +0300

    integration test: request (#4864)
    * integration test: request
    
    * add test:integration script to all packages
    
    * add e2e to build.yml
    
    * move clientUrl to config
    
    * remove chai
    
    * remove web-utils deps
    
    * fix lint
    
    * add matrix strategy to build workflow
    
    * fix typo
    
    * docker geth build and test
    
    * run both geth and ganache
    
    * fix
    
    * test workflow
    
    * fix
    
    * test
    
    * test
    
    * fix condition
    
    * Update .github/workflows/build.yml
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * fix
    
    * Update packages/web3-providers-http/package.json
    
    Co-authored-by: Wyatt Barnes <me@wyatt.email>
    
    * add browser test
    
    * fix lint
    
    * hexToNumber
    
    fix - Should use hexToNumber function from web3-utils
    
    * ganache-cli to ganache
    
    * add fireox tests
    
    * test unit expected
    
    * return matrix unit tests
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    Co-authored-by: Wyatt Barnes <me@wyatt.email>

---
+++ packages/web3-providers-http/test/fixtures/config.ts
@@ -0,0 +1,19 @@
+type Account = {
+	address: string;
+	privateKey: string;
+	balance: string;
+};
+
+export const accounts: Account[] = [
+	{
+		address: '0xdc6bad79dab7ea733098f66f6c6f9dd008da3258',
+		privateKey: '0x4c3758228f536f7a210f8936182fb5b728046970b8e3215d0b5cb4c4faae8a4e',
+		balance: '100',
+	},
+	{
+		address: '0x962f9a9c2a6c092474d24def35eccb3d9363265e',
+		privateKey: '0x34aeb1f338c17e6b440c189655c89fcef148893a24a7f15c0cb666d9cf5eacb3',
+		balance: '100',
+	},
+];
+export const clientUrl = 'http://localhost:8545';
