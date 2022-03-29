9e95d139facc4b31561d583f1bf84835f82f94a0
Nikos Iliakis <nikoulai94@hotmail.com>
2022-03-29 15:27:26 +0300

    Fix errors lint

--- packages/web3-eth-personal/test/config/setup.js
+++ packages/web3-eth-personal/test/config/setup.js
@@ -1,5 +1,3 @@
-// Have to use `require` because of Jest issue https://jestjs.io/docs/ecmascript-modules
-// eslint-disable-next-line @typescript-eslint/no-require-imports
 /*
 This file is part of web3.js.
 
@@ -17,6 +15,8 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+// Have to use `require` because of Jest issue https://jestjs.io/docs/ecmascript-modules
+// eslint-disable-next-line @typescript-eslint/no-require-imports
 require('jest-extended');
 
 // @todo extend jest to have "toHaveBeenCalledOnceWith" matcher.
