9f60ceaf36a2efe4126fa7f99e7de4997b674ce0
Nikos Iliakis <nikoulai94@hotmail.com>
2022-04-04 13:46:10 +0300

    Merge

--- packages/web3/test/integration/string-providers/web3_instance_ws_string.test.ts
+++ packages/web3/test/integration/string-providers/web3_instance_ws_string.test.ts
@@ -1,11 +1,11 @@
 // import { toWei, hexToNumber } from 'web3-utils';
-// import {
-// 	Web3BaseProvider,
-// 	EthExecutionAPI,
-// 	Web3APIPayload,
-// 	// 	// JsonRpcResponse,
-// 	// 	// JsonRpcResponseWithResult,
-// } from 'web3-common';
+import {
+	Web3BaseProvider,
+	// 	EthExecutionAPI,
+	// 	Web3APIPayload,
+	// 	// 	// JsonRpcResponse,
+	// 	// 	// JsonRpcResponseWithResult,
+} from 'web3-common';
 // import { httpStringProvider, wsStringProvider, ipcStringProvider } from '../fixtures/config';
 // import net from 'net';
 import { wsStringProvider } from '../../fixtures/config';
@@ -25,8 +25,24 @@ describe('Web3 instance', () => {
 	// 		params: [accounts[0].address, 'latest'],
 	// 	} as Web3APIPayload<EthExecutionAPI, 'eth_getBalance'>;
 	// });
+	// afterAll(() => mongoose.disconnect());
 
 	describe('Create Web3 class instance with string providers', () => {
+		// jest.setTimeout(35000);
+		// jest.useFakeTimers();
+		let web3: Web3;
+		async function waitForSocketState(web3Inst: Web3, state: string) {
+			return new Promise<void>(resolve => {
+				setTimeout(() => {
+					if ((web3Inst.currentProvider as Web3BaseProvider).getStatus() === state) {
+						resolve();
+					} else {
+						// eslint-disable-next-line @typescript-eslint/no-empty-function
+						waitForSocketState(web3Inst, state).then(resolve, () => {});
+					}
+				}, 5);
+			});
+		}
 		// let jsonRpcPayload: Web3APIPayload<EthExecutionAPI, 'eth_getBalance'>;
 		beforeAll(() => {
 			// jest.spyOn(WebSocket.prototype, 'send');
@@ -38,28 +54,45 @@ describe('Web3 instance', () => {
 			// };
 			// jsonRpcResponse = { ...jsonRpcPayload, result: JSON.stringify(jsonRpcPayload) };
 		});
-		// afterAll(() => server.close());
+		afterEach(async () => {
+			(web3.currentProvider as Web3BaseProvider).disconnect(1000, 'done');
+			await waitForSocketState(web3, 'disconnected');
+			// web3.currentProvider.disconnect();
+			// setTimeout(() => {
+			// 	done();
+			// }, 30000); // delay * (maxAttempts + 1) stop jest from handling
+
+			// jest.advanceTimersByTime(30000);
+		});
 
 		it('should create instance with string of ws provider', async () => {
 			// const web3 = new Web3(wsStringProvider);
 			const wsStringProviderWithPort = `${wsStringProvider}:${process.env.WSPORT ?? 8546}`;
-			const web3 = new Web3(wsStringProviderWithPort);
+			web3 = new Web3(wsStringProviderWithPort);
 			// 	// .on('connect', msg => {
 			// 	// });
 			// 	// (web3.currentProvider as Web3BaseProvider).once();
 			// 	// const curProvider = web3.currentProvider as Web3BaseProvider;
 
-			// 	// (web3.currentProvider as Web3BaseProvider).on('connect', (objs, chainId) => {
-			// 	// 	console.log('^^^^^^^^^^^connect', accounts);
-			// 	// });
-			// 	// eslint-disable-next-line @typescript-eslint/no-misused-promises
+			// (web3.currentProvider as Web3BaseProvider).on('connect', () => {
+			// 	console.log('^^^^^^^^^^^connect');
+			// });
+			// eslint-disable-next-line @typescript-eslint/no-misused-promises
 			// 	(web3.currentProvider as Web3BaseProvider).on('message', async () => {
 			// 		// console.log('^^^^^^^^^^^message', accounts);
-			// 		// console.log((web3.currentProvider as Web3BaseProvider).getStatus());
 			expect(web3).toBeInstanceOf(Web3);
-			// 		(web3.currentProvider as Web3BaseProvider).disconnect();
+			await waitForSocketState(web3, 'connected');
+			// jest.advanceTimersByTime(20000);
+
+			// setTimeout(async () => {
+			// 	console.log('-----------------------------\n--------\n---------\n');
+			// 	(web3.currentProvider as Web3BaseProvider).disconnect(2, 'done');
+			// 	await waitForSocketState(web3, 'disconnected');
+			// 	console.log((web3.currentProvider as Web3BaseProvider).getStatus());
+			// }, 250000);
+
 			// 		// (web3.currentProvider as Web3BaseProvider).
-			// 		// while ((web3.currentProvider as Web3BaseProvider).getStatus() !== 'disconnected') {
+			// 		// while ((web3.`currentProvider as Web3BaseProvider).getStatus() !== 'disconnected') {
 			// 		// await new Promise(r => setTimeout(r, 2000));
 			// 		// }
 			// 	});
