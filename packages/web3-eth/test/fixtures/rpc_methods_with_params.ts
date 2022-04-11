40bf227a966f90f6a3f543fb7867546e28b05277
Wyatt Barnes <me@wyatt.email>
2022-04-10 16:04:12 -1000

    Add input formatting to `rpc_method_wrappers` (#4608) (#4909)
    * Init BlockNumberOrTag type for web3-eth
    
    * Add input formatting to rpc_method_wrappers
    
    * Add validator check to getProof rpc_method
    
    * Move methods to match order of declaration in web3_eth.ts
    
    * Update parameter typings to match rpc_method_wrappers.ts
    
    * Replace use of HexString32Bytes with Bytes for calls with block parameter
    
    * Remove unused imports
    
    * Add ArrayBuffer to ValidInputTypes
    
    * Remove unused imports
    
    * Correct test data to bytes32
    
    * Add comments for clarification

--- packages/web3-eth/test/fixtures/rpc_methods_with_params.ts
+++ packages/web3-eth/test/fixtures/rpc_methods_with_params.ts
@@ -260,7 +260,7 @@ export const getFeeHistoryValidData: [Uint, BlockNumberOrTag, number[]][] = [
 export const getProofValidData: [Address, HexString32Bytes, BlockNumberOrTag, AccountObject][] = [
 	[
 		'0x1234567890123456789012345678901234567890',
-		'0x295a70b2de5e3953354a6a8344e616ed314d7251',
+		'0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
 		'0x1',
 		{
 			accountProof: [
@@ -289,7 +289,7 @@ export const getProofValidData: [Address, HexString32Bytes, BlockNumberOrTag, Ac
 	],
 	[
 		'0x1234567890123456789012345678901234567890',
-		'0x295a70b2de5e3953354a6a8344e616ed314d7251',
+		'0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
 		BlockTags.EARLIEST,
 		{
 			accountProof: [
@@ -318,7 +318,7 @@ export const getProofValidData: [Address, HexString32Bytes, BlockNumberOrTag, Ac
 	],
 	[
 		'0x1234567890123456789012345678901234567890',
-		'0x295a70b2de5e3953354a6a8344e616ed314d7251',
+		'0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
 		BlockTags.LATEST,
 		{
 			accountProof: [
@@ -347,7 +347,7 @@ export const getProofValidData: [Address, HexString32Bytes, BlockNumberOrTag, Ac
 	],
 	[
 		'0x1234567890123456789012345678901234567890',
-		'0x295a70b2de5e3953354a6a8344e616ed314d7251',
+		'0x56e81f171bcc55a6ff8345e692c0f86e5b48e01b996cadc001622fb5e363b421',
 		BlockTags.PENDING,
 		{
 			accountProof: [
