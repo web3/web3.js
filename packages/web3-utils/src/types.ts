969475f621b1f08186fe156ce1a6e31747724aaf
jdevcs <junaid@chainsafe.io>
2022-04-25 15:05:28 +0200

    removing types for fixing cyclic dep

--- packages/web3-utils/src/types.ts
+++ packages/web3-utils/src/types.ts
@@ -1,36 +1,8 @@
-export type HexString = string;
+import { HexString, Numbers } from 'web3-common';
+
 export type ValueTypes = 'address' | 'bool' | 'string' | 'int256' | 'uint256' | 'bytes' | 'bigint';
-export type Bytes = Buffer | Uint8Array | ArrayBuffer | HexString;
-export type Numbers = number | bigint | string | HexString;
-// Hex encoded 32 bytes
-export type HexString32Bytes = HexString;
-// Hex encoded 16 bytes
+
 export type HexString16Bytes = HexString;
-// Hex encoded 8 bytes
-export type HexString8Bytes = HexString;
-// Hex encoded 1 byte
-export type HexStringSingleByte = HexString;
-// Hex encoded 1 byte
-export type HexStringBytes = HexString;
-// Hex encoded 256 byte
-export type HexString256Bytes = HexString;
-// Hex encoded unsigned integer
-export type Uint = HexString;
-// Hex encoded unsigned integer 32 bytes
-export type Uint256 = HexString;
-// Hex encoded address
-export type Address = HexString;
-
-// https://github.com/ethereum/execution-apis/blob/main/src/schemas/filter.json#L59
-export type Topic = HexString32Bytes;
-
-export enum BlockTags {
-	EARLIEST = 'earliest',
-	LATEST = 'latest',
-	PENDING = 'pending',
-}
-export type BlockTag = 'earliest' | 'latest' | 'pending';
-export type BlockNumberOrTag = Uint | BlockTag;
 
 export type Optional<T, K extends keyof T> = Pick<Partial<T>, K> & Omit<T, K>;
 export type EncodingTypes = Numbers | boolean | Numbers[] | boolean[];
@@ -91,11 +63,3 @@ export type JsonEventInterface = {
 	indexed: boolean;
 	anonymous: boolean;
 };
-
-// https://github.com/ethereum/execution-apis/blob/main/src/schemas/filter.json#L28
-export interface Filter {
-	readonly fromBlock?: BlockNumberOrTag;
-	readonly toBlock?: BlockNumberOrTag;
-	readonly address?: Address | Address[];
-	readonly topics?: (Topic | Topic[] | null)[];
-}
