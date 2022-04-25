536aee216103be6c93775ca41dcc79f7009e77ae
jdevcs <junaid@chainsafe.io>
2022-04-25 15:07:56 +0200

    re exporting common functions for external users

--- packages/web3-utils/src/index.ts
+++ packages/web3-utils/src/index.ts
@@ -1,3 +1,22 @@
+export {
+	bytesToBuffer,
+	bytesToHex,
+	hexToBytes,
+	hexToNumber,
+	numberToHex,
+	hexToNumberString,
+	utf8ToHex,
+	fromUtf8,
+	hexToUtf8,
+	toUtf8,
+	toNumber,
+	toBigInt,
+	toChecksumAddress,
+	sha3,
+	sha3Raw,
+	mergeDeep,
+} from 'web3-common';
+
 export * from './converters';
 export * from './errors';
 export * from './validation';
@@ -5,4 +24,3 @@ export * from './types';
 export * from './hash';
 export * from './random';
 export * from './string_manipulation';
-export * from './objects';
