abc7826646cd4a18211c56a741334cab5b0d41b2
Nikos Iliakis <nikoulai94@hotmail.com>
2022-04-27 13:25:49 +0300

    Add some headers

--- packages/web3/test/integration/string-providers/web3_instance_http_string.test.ts
+++ packages/web3/test/integration/string-providers/web3_instance_http_string.test.ts
@@ -1,13 +1,19 @@
-// import { toWei, hexToNumber } from 'web3-utils';
-// import {
-// 	Web3BaseProvider,
-// 	EthExecutionAPI,
-// 	Web3APIPayload,
-// 	// 	// JsonRpcResponse,
-// 	// 	// JsonRpcResponseWithResult,
-// } from 'web3-common';
-// import { httpStringProvider, wsStringProvider, ipcStringProvider } from '../fixtures/config';
-// import net from 'net';
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
 import { httpStringProvider } from '../../fixtures/config';
 
 import { Web3 } from '../../../src/index';
