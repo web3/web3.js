d4b5b6aa22731ee50b4a2d9c3d3630c6bc847504
jdevcs <junaid@chainsafe.io>
2022-04-28 20:23:46 +0200

    error base class

---
+++ packages/web3-errors/src/web3_error_base.ts
@@ -0,0 +1,55 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* eslint-disable max-classes-per-file */
+
+export abstract class Web3Error extends Error {
+	public readonly name: string;
+	public abstract readonly code: number;
+
+	public constructor(msg: string) {
+		super(msg);
+		this.name = this.constructor.name;
+		Error.captureStackTrace(this, Web3Error);
+	}
+
+	public static convertToString(value: unknown, unquotValue = false) {
+		if (value === undefined) return 'undefined';
+
+		const result = JSON.stringify(
+			value,
+			(_, v) => (typeof v === 'bigint' ? v.toString() : v) as unknown,
+		);
+
+		return unquotValue && ['bigint', 'string'].includes(typeof value)
+			? result.replace(/['\\"]+/g, '')
+			: result;
+	}
+	public toJSON() {
+		return { name: this.name, code: this.code, message: this.message };
+	}
+}
+
+export abstract class InvalidValueError extends Web3Error {
+	public readonly name: string;
+
+	public constructor(value: unknown, msg: string) {
+		super(`Invalid value given "${Web3Error.convertToString(value, true)}". Error: ${msg}.`);
+		this.name = this.constructor.name;
+		Error.captureStackTrace(this, Web3Error);
+	}
+}
