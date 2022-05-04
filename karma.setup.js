fc2c53c353a3eca9ddc97e716b125e58826b89a5
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-05-04 20:31:54 +0300

    fix

--- karma.setup.js
+++ karma.setup.js
@@ -1,12 +1,2 @@
-import * as jest from 'jest-mock';
-// import expect from 'expect';
-// window.test = window.it;
-window.it.each = inputs => async (testName, test) => {
-	for (const inp of inputs) {
-		window.it(testName, async () => await test(...inp));
-	}
-};
-window.jest = jest;
-// window.expect = expect;
 global.setImmediate = global.setImmediate || ((fn, ...args) => global.setTimeout(fn, 0, ...args));
 window.setImmediate = global.setImmediate;
