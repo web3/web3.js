f8ce61b8626fde882ba58b2f230654dccffd3dee
Nikos Iliakis <nikoulai94@hotmail.com>
2022-05-06 16:29:02 +0300

    Use http proxy middleware for basic auth test

--- packages/web3-providers-ws/test/integration/web_socket_provider_integration.test.ts
+++ packages/web3-providers-ws/test/integration/web_socket_provider_integration.test.ts
@@ -27,17 +27,28 @@ import {
 	JsonRpcNotification,
 	JsonRpcSubscriptionResult,
 } from 'web3-common';
+// import express, { Express } from 'express';
+import express from 'express';
+import { createProxyMiddleware } from 'http-proxy-middleware';
+// import morgan from 'morgan';
 // import WebSocketProvider from '../../src/index';
 // import { CloseEvent } from 'isomorphic-ws';
+import { Server } from 'http';
 import WebSocketProvider from '../../src/index';
 // import { Web3WSProviderError } from '../../src/errors';
 import { WSRequestItem, OnCloseEvent } from '../../src/types';
 
 // eslint-disable-next-line import/no-relative-packages
-import { clientWsUrl, accounts } from '../../../../.github/test.config';
+// import { clientWsUrl, accounts } from '../../../../.github/test.config';
+import {
+	getSystemTestProvider,
+	// describeIf,
+	getSystemTestAccounts,
+} from '../fixtures/system_test_utils';
 
 describe('WebSocketProvider - implemented methods', () => {
-	jest.setTimeout(30000);
+	let clientWsUrl: string;
+	let accounts: string[];
 	let webSocketProvider: WebSocketProvider;
 	let jsonRpcPayload: Web3APIPayload<EthExecutionAPI, 'eth_getBalance'>;
 	// helper function
@@ -48,8 +59,6 @@ describe('WebSocketProvider - implemented methods', () => {
 			const intervalTime = 5000; // ms
 
 			const interval = setInterval(() => {
-				// console.warn('currentAttemp', currentAttempt);
-				// console.warn(webSocketProvider.getStatus());
 				if (currentAttempt > maxNumberOfAttempts - 1) {
 					clearInterval(interval);
 					reject(new Error('Maximum number of attempts exceeded'));
@@ -62,217 +71,280 @@ describe('WebSocketProvider - implemented methods', () => {
 			}, intervalTime);
 		});
 	};
-	beforeEach(() => {
-		jsonRpcPayload = {
-			jsonrpc: '2.0',
-			id: 42,
-			method: 'eth_getBalance',
-			params: [accounts[0].address, 'latest'],
-		} as Web3APIPayload<EthExecutionAPI, 'eth_getBalance'>;
-		webSocketProvider = new WebSocketProvider(
-			clientWsUrl,
-			{},
-			{ delay: 1, autoReconnect: false, maxAttempts: 1 },
-		);
-		currentAttempt = 0;
+	beforeAll(async () => {
+		clientWsUrl = getSystemTestProvider();
+		accounts = await getSystemTestAccounts();
 	});
-	afterEach(async () => {
-		if (webSocketProvider.getStatus() !== 'disconnected') {
-			// make sure we try to close the connection after it is established
-			await waitForOpenConnection(webSocketProvider);
-			webSocketProvider.disconnect();
-		}
-	});
-
-	describe('websocker provider tests', () => {
-		it.skip('should connect', async () => {
-			await waitForOpenConnection(webSocketProvider);
-			expect(webSocketProvider).toBeInstanceOf(WebSocketProvider);
-			expect(webSocketProvider.getStatus()).toBe('connected');
-		});
-	});
-
-	describe('subscribe event tests', () => {
-		it.skip('should subscribe on message', done => {
-			webSocketProvider.on(
-				'message',
-				(
-					error: Error | null,
-					result?: JsonRpcSubscriptionResult | JsonRpcNotification<any>,
-				) => {
-					if (error) {
-						done.fail(error);
-					}
-					expect(result?.id).toBe(jsonRpcPayload.id);
-					done();
-				},
-			);
-			webSocketProvider.request(jsonRpcPayload).catch(err => {
-				done.fail(err);
-			});
-		});
-
-		// it.skip('should subscribe on error', done => {
-		// 	const errorMsg = 'Custom WebSocket error occured';
-		// 	webSocketProvider.on('error', err => {
-		// 		expect(err?.message).toBe(errorMsg);
-		// 		done();
-		// 	});
-		// 	// Manually emit an error event - accessing private emitter
-		// 	// todo change it
-		// 	webSocketProvider['_wsEventEmitter'].emit('error', new Web3WSProviderError(errorMsg));
-		// });
-
-		// eslint-disable-next-line jest/expect-expect
-		it.skip('should subscribe on connect', done => {
-			webSocketProvider.on('open', () => {
-				done();
-			});
-		});
-		it.skip('should subscribe on close', done => {
-			const code = 1001;
-			const reason = '1001';
-			webSocketProvider.on(
-				'close',
-				(err: Error | null, event: OnCloseEvent | null | undefined) => {
-					if (err) {
-						done.fail(err);
-					}
-					expect(event!.code).toEqual(code);
-					expect(event!.reason).toEqual(reason);
-					done();
-				},
-			);
-			waitForOpenConnection(webSocketProvider)
-				.then(() => {
-					webSocketProvider.disconnect(code, reason);
-				})
-				.catch(() => {
-					done.fail();
-				});
-		});
-	});
-	describe('disconnect and reset test', () => {
-		// eslint-disable-next-line jest/expect-expect
-		it.skip('should disconnect', async () => {
-			// eslint-disable-next-line @typescript-eslint/no-shadow
-			// const webSocketProvider = new WebSocketProvider(
-			// 	clientWsUrl,
-			// 	// {},
-			// 	// { delay: 1, autoReconnect: false, maxAttempts: 1 },
-			// );
-			// console.warn(typeof webSocketProvider.disconnect);
-
-			// // await webSocketProvider.disconnect(1000, 'done');
-			// console.warn(webSocketProvider.getStatus());
-			const provider = new WebSocketProvider(
-				clientWsUrl,
-				{},
-				{ delay: 1, autoReconnect: false, maxAttempts: 1 },
-			);
-			await waitForOpenConnection(provider);
-			provider.disconnect(1000);
-			await waitForOpenConnection(provider, 'disconnected');
-		});
-		it.skip('should reset', async () => {
+	describe('tests', () => {
+		beforeEach(() => {
 			jsonRpcPayload = {
 				jsonrpc: '2.0',
 				id: 42,
 				method: 'eth_getBalance',
-				params: [accounts[0].address, 'latest'],
+				params: [accounts[0], 'latest'],
 			} as Web3APIPayload<EthExecutionAPI, 'eth_getBalance'>;
-			const defPromise = new DeferredPromise<JsonRpcResponse<ResponseType>>();
-
-			const reqItem: WSRequestItem<any, any, any> = {
-				payload: jsonRpcPayload,
-				deferredPromise: defPromise,
-			};
-
-			webSocketProvider['_requestQueue'].set(jsonRpcPayload.id as JsonRpcId, reqItem);
-			expect(webSocketProvider['_requestQueue'].size).toBe(1);
-
-			webSocketProvider['_sentQueue'].set(jsonRpcPayload.id as JsonRpcId, reqItem);
-			expect(webSocketProvider['_sentQueue'].size).toBe(1);
-
-			webSocketProvider.reset();
-			expect(webSocketProvider['_requestQueue'].size).toBe(0);
-			expect(webSocketProvider['_sentQueue'].size).toBe(0);
+			webSocketProvider = new WebSocketProvider(
+				clientWsUrl,
+				{},
+				{ delay: 1, autoReconnect: false, maxAttempts: 1 },
+			);
+			currentAttempt = 0;
 		});
-	});
-
-	describe.skip('getStatus get and validate all status tests', () => {
-		it('test getStatus `connecting`', () => {
-			expect(webSocketProvider.getStatus()).toBe('connecting');
+		afterEach(async () => {
+			if (webSocketProvider.getStatus() !== 'disconnected') {
+				// make sure we try to close the connection after it is established
+				await waitForOpenConnection(webSocketProvider);
+				webSocketProvider.disconnect();
+			}
 		});
 
-		it('test getStatus `connected`', async () => {
-			await waitForOpenConnection(webSocketProvider);
-			expect(webSocketProvider.getStatus()).toBe('connected');
+		describe('websocker provider tests', () => {
+			it.skip('should connect', async () => {
+				await waitForOpenConnection(webSocketProvider);
+				expect(webSocketProvider).toBeInstanceOf(WebSocketProvider);
+				expect(webSocketProvider.getStatus()).toBe('connected');
+			});
 		});
-		it('test getStatus `disconnected`', async () => {
-			await waitForOpenConnection(webSocketProvider);
-			webSocketProvider.disconnect();
-			expect(webSocketProvider.getStatus()).toBe('disconnected');
-		});
-	});
-	describe('send multiple Requests on same connection with valid payload and receive response tests', () => {
-		// eslint-disable-next-line jest/expect-expect
-		let jsonRpcPayload2: Web3APIPayload<EthExecutionAPI, 'eth_mining'>;
-		let jsonRpcPayload3: Web3APIPayload<EthExecutionAPI, 'eth_hashrate'>;
-		beforeAll(() => {
-			jsonRpcPayload2 = {
-				jsonrpc: '2.0',
-				id: 43,
-				method: 'eth_mining',
-			} as Web3APIPayload<EthExecutionAPI, 'eth_mining'>;
-			jsonRpcPayload3 = {
-				jsonrpc: '2.0',
-				id: 44,
-				method: 'eth_hashrate',
-			} as Web3APIPayload<EthExecutionAPI, 'eth_hashrate'>;
-		});
-		it('multiple requests', done => {
-			const prom1 = webSocketProvider.request(jsonRpcPayload);
 
-			const prom2 = webSocketProvider.request(jsonRpcPayload2);
-
-			const prom3 = webSocketProvider.request(jsonRpcPayload3);
-
-			Promise.all([prom1, prom2, prom3])
-				.then(values => {
-					// eslint-disable-next-line jest/no-conditional-expect
-					expect(values).toEqual(
-						expect.arrayContaining([
-							expect.objectContaining({ id: jsonRpcPayload.id }),
-							expect.objectContaining({ id: jsonRpcPayload2.id }),
-							expect.objectContaining({ id: jsonRpcPayload3.id }),
-						]),
-					);
-					// Execute request in connected stated too
-					prom3
-						.then(value => {
-							// eslint-disable-next-line jest/no-conditional-expect
-							expect(value).toEqual(
-								expect.objectContaining({
-									id: jsonRpcPayload3.id,
-								}),
-							);
-							done();
-						})
-						.catch(err => {
-							done.fail(err.message);
-						});
-				})
-				.catch(err => {
-					done.fail(err.message);
+		describe('subscribe event tests', () => {
+			it.skip('should subscribe on message', done => {
+				webSocketProvider.on(
+					'message',
+					(
+						error: Error | null,
+						result?: JsonRpcSubscriptionResult | JsonRpcNotification<any>,
+					) => {
+						if (error) {
+							done.fail(error);
+						}
+						expect(result?.id).toBe(jsonRpcPayload.id);
+						done();
+					},
+				);
+				webSocketProvider.request(jsonRpcPayload).catch(err => {
+					done.fail(err);
 				});
+			});
+
+			// it.skip('should subscribe on error', done => {
+			// 	const errorMsg = 'Custom WebSocket error occured';
+			// 	webSocketProvider.on('error', err => {
+			// 		expect(err?.message).toBe(errorMsg);
+			// 		done();
+			// 	});
+			// 	// Manually emit an error event - accessing private emitter
+			// 	// todo change it
+			// 	webSocketProvider['_wsEventEmitter'].emit('error', new Web3WSProviderError(errorMsg));
+			// });
+
+			// eslint-disable-next-line jest/expect-expect
+			it.skip('should subscribe on connect', done => {
+				webSocketProvider.on('open', () => {
+					done();
+				});
+			});
+			it.skip('should subscribe on close', done => {
+				const code = 1001;
+				const reason = '1001';
+				webSocketProvider.on(
+					'close',
+					(err: Error | null, event: OnCloseEvent | null | undefined) => {
+						if (err) {
+							done.fail(err);
+						}
+						expect(event!.code).toEqual(code);
+						expect(event!.reason).toEqual(reason);
+						done();
+					},
+				);
+				waitForOpenConnection(webSocketProvider)
+					.then(() => {
+						webSocketProvider.disconnect(code, reason);
+					})
+					.catch(() => {
+						done.fail();
+					});
+			});
+		});
+		describe('disconnect and reset test', () => {
+			// eslint-disable-next-line jest/expect-expect
+			it.skip('should disconnect', async () => {
+				// eslint-disable-next-line @typescript-eslint/no-shadow
+				// const webSocketProvider = new WebSocketProvider(
+				// 	clientWsUrl,
+				// 	// {},
+				// 	// { delay: 1, autoReconnect: false, maxAttempts: 1 },
+				// );
+
+				// // await webSocketProvider.disconnect(1000, 'done');
+				// console.warn(webSocketProvider.getStatus());
+				const provider = new WebSocketProvider(
+					clientWsUrl,
+					{},
+					{ delay: 1, autoReconnect: false, maxAttempts: 1 },
+				);
+				await waitForOpenConnection(provider);
+				provider.disconnect(1000);
+				await waitForOpenConnection(provider, 'disconnected');
+			});
+			it.skip('should reset', async () => {
+				jsonRpcPayload = {
+					jsonrpc: '2.0',
+					id: 42,
+					method: 'eth_getBalance',
+					params: [accounts[0], 'latest'],
+				} as Web3APIPayload<EthExecutionAPI, 'eth_getBalance'>;
+				const defPromise = new DeferredPromise<JsonRpcResponse<ResponseType>>();
+
+				const reqItem: WSRequestItem<any, any, any> = {
+					payload: jsonRpcPayload,
+					deferredPromise: defPromise,
+				};
+
+				webSocketProvider['_requestQueue'].set(jsonRpcPayload.id as JsonRpcId, reqItem);
+				expect(webSocketProvider['_requestQueue'].size).toBe(1);
+
+				webSocketProvider['_sentQueue'].set(jsonRpcPayload.id as JsonRpcId, reqItem);
+				expect(webSocketProvider['_sentQueue'].size).toBe(1);
+
+				webSocketProvider.reset();
+				expect(webSocketProvider['_requestQueue'].size).toBe(0);
+				expect(webSocketProvider['_sentQueue'].size).toBe(0);
+			});
+		});
+
+		describe.skip('getStatus get and validate all status tests', () => {
+			it('test getStatus `connecting`', () => {
+				expect(webSocketProvider.getStatus()).toBe('connecting');
+			});
+
+			it('test getStatus `connected`', async () => {
+				await waitForOpenConnection(webSocketProvider);
+				expect(webSocketProvider.getStatus()).toBe('connected');
+			});
+			it('test getStatus `disconnected`', async () => {
+				await waitForOpenConnection(webSocketProvider);
+				webSocketProvider.disconnect();
+				expect(webSocketProvider.getStatus()).toBe('disconnected');
+			});
+		});
+		describe('send multiple Requests on same connection with valid payload and receive response tests', () => {
+			// eslint-disable-next-line jest/expect-expect
+			let jsonRpcPayload2: Web3APIPayload<EthExecutionAPI, 'eth_mining'>;
+			let jsonRpcPayload3: Web3APIPayload<EthExecutionAPI, 'eth_hashrate'>;
+			beforeAll(() => {
+				jsonRpcPayload2 = {
+					jsonrpc: '2.0',
+					id: 43,
+					method: 'eth_mining',
+				} as Web3APIPayload<EthExecutionAPI, 'eth_mining'>;
+				jsonRpcPayload3 = {
+					jsonrpc: '2.0',
+					id: 44,
+					method: 'eth_hashrate',
+				} as Web3APIPayload<EthExecutionAPI, 'eth_hashrate'>;
+			});
+			it('multiple requests', done => {
+				const prom1 = webSocketProvider.request(jsonRpcPayload);
+
+				const prom2 = webSocketProvider.request(jsonRpcPayload2);
+
+				const prom3 = webSocketProvider.request(jsonRpcPayload3);
+
+				Promise.all([prom1, prom2, prom3])
+					.then(values => {
+						// eslint-disable-next-line jest/no-conditional-expect
+						expect(values).toEqual(
+							expect.arrayContaining([
+								expect.objectContaining({ id: jsonRpcPayload.id }),
+								expect.objectContaining({ id: jsonRpcPayload2.id }),
+								expect.objectContaining({ id: jsonRpcPayload3.id }),
+							]),
+						);
+						// Execute request in connected stated too
+						prom3
+							.then(value => {
+								// eslint-disable-next-line jest/no-conditional-expect
+								expect(value).toEqual(
+									expect.objectContaining({
+										id: jsonRpcPayload3.id,
+									}),
+								);
+								done();
+							})
+							.catch(err => {
+								done.fail(err.message);
+							});
+					})
+					.catch(err => {
+						done.fail(err.message);
+					});
+			});
 		});
 	});
-
 	describe('Support of Basic Auth', () => {
+		let server: Server;
+		beforeAll(() => {
+			const app = express();
+			const PORT = 3000;
+			const HOST = 'localhost';
+
+			// const wsProxy = createProxyMiddleware({
+			// 	target: clientWsUrl,
+			// 	ws: true,
+			// 	logLevel: 'debug',
+			// });
+			const wsProxy = createProxyMiddleware({
+				target: clientWsUrl,
+				changeOrigin: true,
+				ws: true,
+				onError: () => {
+					console.warn('************** proxy error');
+				},
+				logLevel: 'silent',
+				// onProxyReqWs: () => {
+				// 	console.warn('socket is destroyed');
+				// },
+			});
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			app.use(wsProxy);
+			server = app.listen(PORT, HOST);
+			// server.on('upgrade', () => {
+			// 	console.warn('***********');
+			// });
+			server.on('upgrade', (req, socket, head) => {
+				if (!req.headers.authorization || !req.headers.authorization?.includes('Basic ')) {
+					socket.emit('error');
+					socket.destroy();
+				}
+				const base64Credentials: string = req.headers.authorization!.split(' ')[1];
+				const credentials: string = Buffer.from(base64Credentials, 'base64').toString(
+					'ascii',
+				);
+				const [username, password] = credentials.split(':');
+				if (username !== 'geth' || password !== 'authpass') {
+					socket.emit('error');
+					socket.destroy();
+				}
+				return wsProxy.upgrade?.(req as any, socket as any, head);
+			});
+		});
+		afterAll(() => {
+			server.close();
+		});
 		// eslint-disable-next-line jest/expect-expect
 		it('should connect with basic auth', async () => {
-			webSocketProvider = new WebSocketProvider('ws://geth:authpas@localhost:80');
+			// await waitForOpenConnection(webSocketProvider);
+			// webSocketProvider.disconnect();
+			webSocketProvider = new WebSocketProvider(
+				'ws://geth:authpass@localhost:3000',
+				{},
+				{ delay: 1, autoReconnect: false, maxAttempts: 1 },
+			);
+
+			await waitForOpenConnection(webSocketProvider);
+			webSocketProvider.disconnect();
 			// webSocketProvider = new WebSocketProvider('ws://localhost:80', {
 			// 	rejectUnauthorized: false,
 			// 	headers: {
@@ -281,7 +353,7 @@ describe('WebSocketProvider - implemented methods', () => {
 			// 	},
 			// });
 			// webSocketProvider = new WebSocketProvider('ws://localhost:8545');
-			await waitForOpenConnection(webSocketProvider);
+			// await waitForOpenConnection(webSocketProvider);
 		});
 	});
 });
