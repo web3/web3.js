82194c377e96338bf53e41fb61d84dcbc077fb96
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-05-09 17:18:06 +0300

    4852 web3 subscribe tests (#4990)
    * test
    
    * test
    
    * test
    
    * tests
    
    * fix build
    
    * private network
    
    * remove sync test
    
    * fix lgtm
    
    * fix
    
    * fix
    
    * fix
    
    * merge
    
    * fix
    
    * fix
    
    * fix lint
    
    * --runInBand to all tests
    
    * add it each
    
    * fix browser tests
    
    * fix
    
    * nonce problem fix
    
    * fix

---
+++ packages/web3-eth/test/integration/helper.ts
@@ -0,0 +1,53 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+import { PromiEvent } from 'web3-common';
+import { ReceiptInfo, SendTransactionEvents, Web3Eth } from '../../src';
+
+type SendFewTxParams = {
+	web3Eth: Web3Eth;
+	to: string;
+	from: string;
+	value: string;
+	times?: number;
+};
+export type Resolve = (value?: unknown) => void;
+export const sendFewTxes = async ({
+	web3Eth,
+	to,
+	value,
+	from,
+	times = 3,
+}: SendFewTxParams): Promise<unknown[]> => {
+	const res = [];
+	for (let i = 0; i < times; i += 1) {
+		const tx: PromiEvent<ReceiptInfo, SendTransactionEvents> = web3Eth.sendTransaction({
+			to,
+			value,
+			from,
+		});
+		res.push(
+			// eslint-disable-next-line no-await-in-loop
+			await new Promise((resolve: Resolve) => {
+				tx.on('receipt', (params: ReceiptInfo) => {
+					expect(params.status).toBe('0x1');
+					resolve(params);
+				});
+			}),
+		);
+	}
+	return res;
+};
