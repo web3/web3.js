a005c1aeef1b557f44cace4ccf384c99586cd845
Alex <alex.luu@mail.utoronto.ca>
2022-05-10 10:21:44 -0400

    using itif

--- packages/web3-eth-personal/test/integration/personal.test.ts
+++ packages/web3-eth-personal/test/integration/personal.test.ts
@@ -22,6 +22,7 @@ import {
 	getSystemTestBackend,
 	getSystemTestAccounts,
 	getSystemTestProvider,
+	itIf,
 } from '../fixtures/system_test_utils';
 
 describe('personal integration tests', () => {
@@ -39,14 +40,12 @@ describe('personal integration tests', () => {
 		expect(isHexStrict(newAccount)).toBe(true);
 	});
 
-	it('ecRecover', async () => {
-		if (getSystemTestBackend() === 'geth') {
-			// ganache does not support ecRecover
-			const signature = await ethPersonal.sign('0x2313', accounts[0], '');
-			const publicKey = await ethPersonal.ecRecover('0x2313', signature); // ecRecover is returning all lowercase
-			// eslint-disable-next-line jest/no-conditional-expect
-			expect(toChecksumAddress(publicKey)).toBe(toChecksumAddress(accounts[0]));
-		}
+	itIf(getSystemTestBackend() === 'geth')('ecRecover', async () => {
+		// ganache does not support ecRecover
+		const signature = await ethPersonal.sign('0x2313', accounts[0], '');
+		const publicKey = await ethPersonal.ecRecover('0x2313', signature); // ecRecover is returning all lowercase
+		// eslint-disable-next-line jest/no-standalone-expect
+		expect(toChecksumAddress(publicKey)).toBe(toChecksumAddress(accounts[0]));
 	});
 
 	it('lock account', async () => {
@@ -60,16 +59,14 @@ describe('personal integration tests', () => {
 		expect(unlockedAccount).toBe(true);
 	});
 
-	it('sign', async () => {
-		if (getSystemTestBackend() === 'geth') {
-			// ganache does not support sign
-			const key = accounts[0];
-			await ethPersonal.unlockAccount(key, '', 100000);
-			const signature = await ethPersonal.sign('0xdeadbeaf', accounts[0], '');
-			const address = await ethPersonal.ecRecover('0xdeadbeaf', signature);
-			// eslint-disable-next-line jest/no-conditional-expect
-			expect(accounts[0]).toBe(address);
-		}
+	// ganache does not support sign
+	itIf(getSystemTestBackend() === 'geth')('sign', async () => {
+		const key = accounts[0];
+		await ethPersonal.unlockAccount(key, '', 100000);
+		const signature = await ethPersonal.sign('0xdeadbeaf', accounts[0], '');
+		const address = await ethPersonal.ecRecover('0xdeadbeaf', signature);
+		// eslint-disable-next-line jest/no-standalone-expect
+		expect(accounts[0]).toBe(address);
 	});
 
 	it('getAccounts', async () => {
@@ -90,26 +87,24 @@ describe('personal integration tests', () => {
 		expect(toChecksumAddress(key)).toBe(importedAccount.address);
 	});
 
-	it('signTransaction', async () => {
-		if (getSystemTestBackend() === 'ganache') {
-			// geth doesn't have signTransaction method
-			const from = accounts[0];
-			await ethPersonal.unlockAccount(from, '', 100000);
-			const tx = {
-				from,
-				to: '0x1337C75FdF978ABABaACC038A1dCd580FeC28ab2',
-				value: '10000',
-				gas: '21000',
-				maxFeePerGas: '0x59682F00',
-				maxPriorityFeePerGas: '0x1DCD6500',
-				nonce: 0,
-			};
-			const signedTx = await ethPersonal.signTransaction(tx, '');
-			const expectedResult =
-				'0x02f86e82053980841dcd65008459682f00825208941337c75fdf978ababaacc038a1dcd580fec28ab282271080c080a0d75090f88d6e3e9525fc6d4b1230726b97b4cb07b7aebd683aa9e5c62bb71220a05b7169e0670f70f62fd25f95fbf90f34decd81bf06b3da6fd5500df9cec83cda';
-			// eslint-disable-next-line jest/no-conditional-expect
-			expect(signedTx).toEqual(expectedResult);
-		}
+	// geth doesn't have signTransaction method
+	itIf(getSystemTestBackend() === 'ganache')('signTransaction', async () => {
+		const from = accounts[0];
+		await ethPersonal.unlockAccount(from, '', 100000);
+		const tx = {
+			from,
+			to: '0x1337C75FdF978ABABaACC038A1dCd580FeC28ab2',
+			value: '10000',
+			gas: '21000',
+			maxFeePerGas: '0x59682F00',
+			maxPriorityFeePerGas: '0x1DCD6500',
+			nonce: 0,
+		};
+		const signedTx = await ethPersonal.signTransaction(tx, '');
+		const expectedResult =
+			'0x02f86e82053980841dcd65008459682f00825208941337c75fdf978ababaacc038a1dcd580fec28ab282271080c080a0d75090f88d6e3e9525fc6d4b1230726b97b4cb07b7aebd683aa9e5c62bb71220a05b7169e0670f70f62fd25f95fbf90f34decd81bf06b3da6fd5500df9cec83cda';
+		// eslint-disable-next-line jest/no-standalone-expect
+		expect(signedTx).toEqual(expectedResult);
 	});
 
 	it('sendTransaction', async () => {
