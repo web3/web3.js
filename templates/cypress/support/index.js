65befcec7f4555506d7ca225b96725355f2fced7
Nazar Hussain <nazarhussain@gmail.com>
2022-05-12 01:56:21 +0200

    Enable the e2e tests for web3-eth-contract (#5010)
    * :art: Enable the skipped tests
    
    * :bug: Fix a geth command line bug
    
    * :green_heart: Remove unused build config
    
    * :art: Add e2e tests support for contracts package
    
    * :art: Update eth package to export the subscriptions
    
    * :white_check_mark: Fix tests for the contract deployment
    
    * :bug: Fix ws provider to handle batch requests
    
    * :bug: Fix deferred promise to fix unhandled rejection
    
    * :art: Update deferred promise to remove setTimeout usage
    
    * :art: Update websocket provider to avoid floating promise
    
    * :white_check_mark: Fix the failing tests
    
    * :bug: Fix error tracing in base error class
    
    * :art: Update error capturing logic

--- templates/cypress/support/index.js
+++ templates/cypress/support/index.js
@@ -24,6 +24,14 @@ import 'cypress-jest-adapter';
 // Enable the hook to match Jest
 global.beforeAll = global.before;
 global.afterAll = global.after;
+
+
+// In e2e tests we just need to use jest mocking API
+global.jest = {
+	fn: global.cy.stub,
+	spyOn: global.cy.spy,
+};
+
 global.it = it;
 global.it.each = data => (describe, test) => {
 	const prs = [];
