319f476818b21a2d07039c387c93cc0046f35471
Alex <alex.luu@mail.utoronto.ca>
2022-05-13 14:33:09 -0400

    modifying lock account testcase

--- packages/web3-eth-personal/test/integration/personal.test.ts
+++ packages/web3-eth-personal/test/integration/personal.test.ts
@@ -14,6 +14,7 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
+import { InvalidResponseError, JsonRpcResponse } from 'web3-common';
 import { isHexStrict } from 'web3-validator';
 import { toChecksumAddress } from 'web3-utils';
 import { EthPersonal } from '../../src/index';
@@ -51,12 +52,45 @@ describe('personal integration tests', () => {
 	it('lock account', async () => {
 		const lockAccount = await ethPersonal.lockAccount(accounts[1]);
 		expect(lockAccount).toBe(true);
+
+		const from = accounts[1];
+		const tx = {
+			from,
+			to: '0x1337C75FdF978ABABaACC038A1dCd580FeC28ab2',
+			value: `0`,
+			gas: '21000',
+			maxFeePerGas: '0x59682F00',
+			maxPriorityFeePerGas: '0x1DCD6500',
+		};
+		// locked accounts will error
+		const errorResponse: JsonRpcResponse = {
+			id: 1,
+			jsonrpc: '2.0',
+			error: { code: 101, message: 'could not decrypt key with given password' },
+		};
+		await expect(ethPersonal.sendTransaction(tx, '')).rejects.toThrow(
+			new InvalidResponseError(errorResponse),
+		);
 	});
 
 	it('unlock account', async () => {
 		const key = accounts[0];
-		const unlockedAccount = await ethPersonal.unlockAccount(key, '', 100000);
+		const unlockedAccount = await ethPersonal.unlockAccount(key, '', 1000);
 		expect(unlockedAccount).toBe(true);
+
+		const from = accounts[0];
+		await ethPersonal.unlockAccount(from, '', 1000);
+		const tx = {
+			from,
+			to: '0x1337C75FdF978ABABaACC038A1dCd580FeC28ab2',
+			value: `0`,
+			gas: '21000',
+			maxFeePerGas: '0x59682F00',
+			maxPriorityFeePerGas: '0x1DCD6500',
+		};
+		const receipt = await ethPersonal.sendTransaction(tx, '');
+
+		expect(isHexStrict(receipt)).toBe(true);
 	});
 
 	// ganache does not support sign
