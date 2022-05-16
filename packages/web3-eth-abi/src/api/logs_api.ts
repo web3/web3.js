0f25197aad59814df9d4ec6b44e4f80e5eede337
Nazar Hussain <nazarhussain@gmail.com>
2022-05-16 17:24:11 +0200

    Create ERC20 and ERC721 integration tests for web3-eth-contract (#5024)
    * :truck: Update the file structure for solidity contracts
    
    * :bug: Fix log decoding bug
    
    * :white_check_mark: Add integration tests for erc20 and erc721

--- packages/web3-eth-abi/src/api/logs_api.ts
+++ packages/web3-eth-abi/src/api/logs_api.ts
@@ -30,39 +30,48 @@ export const decodeLog = <ReturnType extends Record<string, unknown>>(
 	topics: string | string[],
 ) => {
 	const clonedTopics = Array.isArray(topics) ? topics : [topics];
-	const clonedData = data ?? '';
 
-	const notIndexedInputs: Array<string | AbiParameter> = [];
-	const indexedParams: Array<string | unknown> = [];
-	let topicCount = 0;
+	const indexedInputs: Record<number, AbiParameter> = {};
+	const nonIndexedInputs: Record<number, AbiParameter> = {};
+
 	for (const [i, input] of inputs.entries()) {
 		if (input.indexed) {
-			indexedParams[i] = STATIC_TYPES.some(s => input.type.startsWith(s))
-				? (decodeParameter(input.type, clonedTopics[topicCount])[0] as unknown[])
-				: clonedTopics[topicCount];
-
-			topicCount += 1;
+			indexedInputs[i] = input;
 		} else {
-			notIndexedInputs[i] = input as unknown as AbiParameter;
+			nonIndexedInputs[i] = input;
 		}
 	}
 
-	const nonIndexedData = clonedData;
-	const notIndexedParams = nonIndexedData
-		? decodeParametersWith(notIndexedInputs, nonIndexedData, true)
+	const decodedNonIndexedInputs = data
+		? decodeParametersWith(Object.values(nonIndexedInputs), data, true)
 		: [];
+
+	// If topics are more than indexed inputs, that means first topic is the event signature
+	const offset = clonedTopics.length - Object.keys(indexedInputs).length;
+
+	const decodedIndexedInputs = Object.values(indexedInputs).map((input, index) =>
+		STATIC_TYPES.some(s => input.type.startsWith(s))
+			? (decodeParameter(input.type, clonedTopics[index + offset])[0] as unknown[])
+			: clonedTopics[index + offset],
+	);
 
 	const returnValue: { [key: string]: unknown; __length__: number } = { __length__: 0 };
 	returnValue.__length__ = 0;
 
+	let indexedCounter = 0;
+	let nonIndexedCounter = 0;
+
 	for (const [i, res] of inputs.entries()) {
 		returnValue[i] = res.type === 'string' ? '' : null;
 
-		if (notIndexedParams[i]) {
-			returnValue[i] = notIndexedParams[i];
+		if (indexedInputs[i]) {
+			returnValue[i] = decodedIndexedInputs[indexedCounter];
+			indexedCounter += 1;
 		}
-		if (indexedParams[i]) {
-			returnValue[i] = indexedParams[i];
+
+		if (nonIndexedInputs[i]) {
+			returnValue[i] = decodedNonIndexedInputs[nonIndexedCounter];
+			nonIndexedCounter += 1;
 		}
 
 		if (res.name) {
@@ -71,5 +80,6 @@ export const decodeLog = <ReturnType extends Record<string, unknown>>(
 
 		returnValue.__length__ += 1;
 	}
+
 	return returnValue as ReturnType & { __length__: number };
 };
