957cb9584d81cf44f8111e99b7c3440402cceaf7
Wyatt Barnes <me@wyatt.email>
2022-05-17 06:58:10 -1000

    Add `prettier-plugin-solidity` dev dependency v2 (#5023)
    * Add prettier-plugin-solidity dev dependency
    
    * yarn format
    
    * Update packages/web3-eth-contract/test/integration/conract_deploy.test.ts
    
    * Solve merge conflict

--- fixtures/contracts/Greeter.sol
+++ fixtures/contracts/Greeter.sol
@@ -3,22 +3,22 @@
 pragma solidity ^0.8.13;
 
 contract Greeter {
-  string private greeting;
-  event GREETING_CHANGING(string from, string to);
-  event GREETING_CHANGED(string greeting);
+	string private greeting;
+	event GREETING_CHANGING(string from, string to);
+	event GREETING_CHANGED(string greeting);
 
-  constructor(string memory _greeting) {
-      greeting = _greeting;
-  }
+	constructor(string memory _greeting) {
+		greeting = _greeting;
+	}
 
-  function greet() public view returns (string memory) {
-      return greeting;
-  }
+	function greet() public view returns (string memory) {
+		return greeting;
+	}
 
-  function setGreeting(string memory _greeting) public returns (bool, string memory) {
-      emit GREETING_CHANGING(greeting, _greeting);
-      greeting = _greeting;
-      emit GREETING_CHANGED(greeting);
-      return (true, greeting);
-  }
+	function setGreeting(string memory _greeting) public returns (bool, string memory) {
+		emit GREETING_CHANGING(greeting, _greeting);
+		greeting = _greeting;
+		emit GREETING_CHANGED(greeting);
+		return (true, greeting);
+	}
 }
