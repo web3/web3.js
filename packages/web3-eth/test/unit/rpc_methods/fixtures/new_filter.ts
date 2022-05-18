55831fe021e27374e87d12a2bbbcdc14a31be759
Wyatt Barnes <me@wyatt.email>
2022-05-18 00:14:20 -1000

    `web3-eth` `rpc_methods` Test Refactors (#5017)
    * Init BlockNumberOrTag and BlockTag types
    
    * Change BlockNumberOrTag import from web3-utils to web3-common
    
    * WIP rpc_methods test refactors
    
    * Add prettier-plugin-solidity dev dependency
    
    * WIP rpc_methods test refactors
    
    * Finish implementing rpc_methods test refactors
    
    * Remove BlockTag and BlockNumberOrTag type from eth_execution_api.ts
    
    * Update package.json
    
    Co-authored-by: Nazar Hussain <nazarhussain@gmail.com>
    
    * Update comments to match data structures for fixtures
    
    * Update packages/web3-eth-accounts/src/account.ts
    
    * Revert changes to yarn.lock
    
    Co-authored-by: Nazar Hussain <nazarhussain@gmail.com>

---
+++ packages/web3-eth/test/unit/rpc_methods/fixtures/new_filter.ts
@@ -0,0 +1,99 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+import { Filter } from 'web3-utils';
+
+/**
+ * Array consists of:
+ * - Test title
+ * - Input parameters:
+ *     - filter
+ */
+type TestData = [string, [Filter]];
+export const testData: TestData[] = [
+	[
+		'filter = fromBlock',
+		[
+			{
+				fromBlock: '0xc0ff3',
+			},
+		],
+	],
+	[
+		'filter = toBlock',
+		[
+			{
+				toBlock: '0xc0ff3',
+			},
+		],
+	],
+	[
+		'filter = address',
+		[
+			{
+				address: '0x98afe7a8d28bbc88dcf41f8e06d97c74958a47dc',
+			},
+		],
+	],
+	[
+		'filter = address[]',
+		[
+			{
+				address: [
+					'0x98afe7a8d28bbc88dcf41f8e06d97c74958a47dc',
+					'0xdfd5293d8e347dfe59e90efd55b2956a1343963d',
+				],
+			},
+		],
+	],
+	[
+		'filter = topics[]',
+		[
+			{
+				topics: [
+					'0x000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b',
+					null,
+					[
+						'0x000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b',
+						'0x0000000000000000000000000aff3454fce5edbc8cca8697c15331677e6ebccc',
+					],
+				],
+			},
+		],
+	],
+	[
+		'filter = fromBlock, toBlock, address[], topics[]',
+		[
+			{
+				fromBlock: '0xc0ff3',
+				toBlock: '0xc0ff3',
+				address: [
+					'0x98afe7a8d28bbc88dcf41f8e06d97c74958a47dc',
+					'0xdfd5293d8e347dfe59e90efd55b2956a1343963d',
+				],
+				topics: [
+					'0x000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b',
+					null,
+					[
+						'0x000000000000000000000000a94f5374fce5edbc8e2a8697c15331677e6ebf0b',
+						'0x0000000000000000000000000aff3454fce5edbc8cca8697c15331677e6ebccc',
+					],
+				],
+			},
+		],
+	],
+];
---
+++ packages/web3-eth/test/unit/rpc_methods/get_uncle_by_block_hash_and_index.test.ts
@@ -0,0 +1,68 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+// web3.js is free software: you can redistribute it and/or modify
+// it under the terms of the GNU Lesser General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// web3.js is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public License
+// along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+// */
+import { Web3RequestManager } from 'web3-core';
+import { validator } from 'web3-validator';
+
+import { getUncleByBlockHashAndIndex } from '../../../src/rpc_methods';
+import { testData } from './fixtures/get_uncle_by_block_hash_and_index';
+
+jest.mock('web3-validator');
+
+describe('getUncleByBlockHashAndIndex', () => {
+	let requestManagerSendSpy: jest.Mock;
+	let requestManager: Web3RequestManager;
+
+	beforeAll(() => {
+		requestManager = new Web3RequestManager('http://127.0.0.1:8545');
+		requestManagerSendSpy = jest.fn();
+		requestManager.send = requestManagerSendSpy;
+	});
+
+	it.each(testData)(
+		'should call requestManager.send with getUncleByBlockHashAndIndex method and expect parameters\n Title: %s\n Input parameters: %s',
+		async (_, inputParameters) => {
+			await getUncleByBlockHashAndIndex(requestManager, ...inputParameters);
+			expect(requestManagerSendSpy).toHaveBeenCalledWith({
+				method: 'eth_getUncleByBlockHashAndIndex',
+				params: inputParameters,
+			});
+		},
+	);
+
+	it.each(testData)(
+		'should call validator.validate with expected params\n Title: %s\n Input parameters: %s',
+		async (_, inputParameters) => {
+			const validatorSpy = jest.spyOn(validator, 'validate');
+			await getUncleByBlockHashAndIndex(requestManager, ...inputParameters);
+			expect(validatorSpy).toHaveBeenCalledWith(['bytes32', 'hex'], inputParameters);
+		},
+	);
+});
