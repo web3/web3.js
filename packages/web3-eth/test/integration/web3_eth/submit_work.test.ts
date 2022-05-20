912d8e549d5a6c5309f63a394f73c4323caeaf2c
Wyatt Barnes <me@wyatt.email>
2022-05-19 19:42:08 -1000

    Init submit_work integration test

--- packages/web3-eth/test/integration/web3_eth/submit_work.test.ts
+++ packages/web3-eth/test/integration/web3_eth/submit_work.test.ts
@@ -14,3 +14,31 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
+
+import WebSocketProvider from 'web3-providers-ws';
+
+import Web3Eth from '../../../src';
+import { getSystemTestProvider } from '../../fixtures/system_test_utils';
+
+describe('Web3Eth.submitWork', () => {
+	let web3Eth: Web3Eth;
+
+	beforeAll(async () => {
+		web3Eth = new Web3Eth(getSystemTestProvider());
+	});
+
+	afterAll(() => {
+		if (getSystemTestProvider().startsWith('ws')) {
+			(web3Eth.provider as WebSocketProvider).disconnect();
+		}
+	});
+
+	it('should submit work', async () => {
+		const response = await web3Eth.submitWork(
+			'0x0000000000000001',
+			'0x1234567890abcdef1234567890abcdef1234567890abcdef1234567890abcdef',
+			'0xD1FE5700000000000000000000000000D1FE5700000000000000000000000000',
+		);
+		expect(response).toBe(false);
+	});
+});
