9c0a94d7cdc1409f669f1a974fbe8966bd5f258a
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-05-31 17:52:41 +0300

    Update packages/web3-eth/test/integration/defaults.test.ts
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>

--- packages/web3-eth/test/integration/defaults.test.ts
+++ packages/web3-eth/test/integration/defaults.test.ts
@@ -87,7 +87,15 @@ describe('defaults', () => {
 			// check utils
 			expect(getTransactionFromAttr(eth2)).toBe(accounts[1]);
 		});
-		it('handleRevert', () => {
+//TO DO: after handleRevert implementation https://github.com/ChainSafe/web3.js/issues/5069 add following tests in future release
+/* set handleRevert true and test following functions with invalid input tx data and see revert reason present in error details:
+
+web3.eth.call()
+web3.eth.sendTransaction()
+contract.methods.myMethod(…).send(…)
+contract.methods.myMethod(…).call(…)
+
+*/
 			// default
 			expect(web3Eth.handleRevert).toBe(false);
 
