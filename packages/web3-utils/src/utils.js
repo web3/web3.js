8a4eeadb0671571fcbdb62273f80ae3bdb2d54fb
Alex <alex.luu@mail.utoronto.ca>
2022-06-13 20:24:51 -0400

    updating bnWrapped object

--- packages/web3-utils/src/utils.js
+++ packages/web3-utils/src/utils.js
@@ -545,6 +545,15 @@ var BNwrapped = function (value) {
         return new BN(value);
     } 
 };
+BNwrapped.BN = BNwrapped;
+BNwrapped.wordSize = 26;
+BNwrapped.isBN = BN.isBN;
+BNwrapped.max = BN.max;
+BNwrapped.min = BN.min;
+BNwrapped.mont = BN.mont;
+BNwrapped.red = BN.red;
+BNwrapped._prime = BN._prime;
+Object.setPrototypeOf(BNwrapped.prototype, BN.prototype);
 
 module.exports = {
     BN: BNwrapped,
