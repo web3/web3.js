76763374ba4bd4a09ec5ba7a8b43dafc83bf92ee
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-06-14 14:00:26 +0300

    move download script to build

--- scripts/download_geth.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-wget https://gethstore.blob.core.windows.net/builds/geth-linux-386-1.10.18-de23cf91.tar.gz
-tar -xf ./geth-linux-386-1.10.18-de23cf91.tar.gz -C ./scripts
-mv ./scripts/geth-linux-386-1.10.18-de23cf91/geth ./scripts/geth
-rm ./geth-linux-386-1.10.18-de23cf91.tar.gz
-rm -rf ./scripts/geth-linux-386-1.10.18-de23cf91
