dcb408bca49160356a236b771ec7333133d0d5f4
Nazar Hussain <nazarhussain@gmail.com>
2022-06-22 22:35:50 +0200

    :art: Update github reference

--- docs/docusaurus.config.js
+++ docs/docusaurus.config.js
@@ -95,7 +95,7 @@ const config = {
 					sidebarPath: require.resolve('./sidebars.js'),
 					// Please change this to your repo.
 					// Remove this to remove the "edit this page" links.
-					editUrl: 'https://github.com/ChainSafe/web3.js/tree/main/website/',
+					editUrl: 'https://github.com/ChainSafe/web3.js/tree/4.x/docs',
 				},
 				theme: {
 					customCss: require.resolve('./src/css/custom.css'),
@@ -126,7 +126,7 @@ const config = {
 						position: 'left',
 					},
 					{
-						href: 'https://github.com/ChainSafe/web3.js',
+						href: 'https://github.com/ChainSafe/web3.js/tree/4.x/',
 						label: 'GitHub',
 						position: 'right',
 					},
