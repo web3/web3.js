a566423bacd4abb2475823222660ee774b6a6389
Nazar Hussain <nazarhussain@gmail.com>
2022-06-28 15:53:56 +0200

    Contract API Documentation (#5162)
    * :art: Add generic interface for the Web3Eth
    
    * :memo: Add documentation for web3-eth-contract
    
    * Apply suggestions from code review
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update docs/docs/glossary/json_interface.md
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * :art: Update the the usage 'var' with the 'const'
    
    * Apply suggestions from code review
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex  <alex.luu@mail.utoronto.ca>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    Co-authored-by: Alex  <alex.luu@mail.utoronto.ca>

--- packages/web3-eth-contract/src/index.ts
+++ packages/web3-eth-contract/src/index.ts
@@ -15,11 +15,37 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * The `web3.eth.Contract` object makes it easy to interact with smart contracts on the Ethereum blockchain.
+ * When you create a new contract object you give it the JSON interface of the respective smart contract and
+ * web3 will auto convert all calls into low level ABI calls over RPC for you.
+ * This allows you to interact with smart contracts as if they were JavaScript objects.
+ *
+ * To use it standalone:
+ *
+ * ```ts
+ * const Contract = require('web3-eth-contract');
+ *
+ * // set provider for all later instances to use
+ * Contract.setProvider('ws://localhost:8546');
+ *
+ * const contract = new Contract(jsonInterface, address);
+ *
+ * contract.methods.somFunc().send({from: ....})
+ * .on('receipt', function(){
+ *    ...
+ * });
+ * ```
+ */
+/**
+ * This comment _supports3_ [Markdown](https://marked.js.org/)
+ */
 import { Contract } from './contract';
 
 export * from './encoding';
 
 export * from './contract';
+export * from './log_subscription';
 export * from './types';
 
 export default Contract;
