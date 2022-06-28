734cf1f27d6f30c24d2a3e796a3ccda05a398d96
Nazar Hussain <nazarhussain@gmail.com>
2022-06-28 15:53:56 +0200

    Contract API Documentation (#5162)
    * :art: Add generic interface for the Web3Eth
    
    * :memo: Add documentation for web3-eth-contract
    
    * Apply suggestions from code review
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update docs/docs/glossary/json_interface.md
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * :art: Update the the usage 'var' with the 'const'
    
    * Apply suggestions from code review
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Apply suggestions from code review
    
    Co-authored-by: Alex  <alex.luu@mail.utoronto.ca>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    * Update packages/web3-eth-contract/src/types.ts
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    Co-authored-by: Alex  <alex.luu@mail.utoronto.ca>

--- packages/web3-eth-contract/src/log_subscription.ts
+++ packages/web3-eth-contract/src/log_subscription.ts
@@ -20,18 +20,84 @@ import { Web3RequestManager, Web3Subscription } from 'web3-core';
 import { AbiEventFragment, encodeEventSignature, jsonInterfaceMethodToString } from 'web3-eth-abi';
 import { HexString, Topic } from 'web3-utils';
 import { decodeEventABI } from './encoding';
+import { EventLog } from './types';
 
+/**
+ * LogSubscription to be used to subscribe to events logs.
+ *
+ * Following events are supported and can be accessed with either {@link LogsSubscription.once} or ${@link LogsSubscription.on} methods.
+ *
+ * - **connected**: Emitted when the subscription is connected.
+ * - **data**: Fires on each incoming event with the event object as argument.
+ * - **changed**: Fires on each event which was removed from the blockchain. The event will have the additional property `removed: true`.
+ * - **error**: Fires on each error.
+ *
+ * ```ts
+ * const subscription = await myContract.events.MyEvent({
+ *   filter: {myIndexedParam: [20,23], myOtherIndexedParam: '0x123456789...'}, // Using an array means OR: e.g. 20 or 23
+ *   fromBlock: 0
+ * });
+ *
+ * subscription.on("connected", function(subscriptionId){
+ *   console.log(subscriptionId);
+ * });
+ *
+ * subscription.on('data', function(event){
+ *   console.log(event); // same results as the optional callback above
+ * });
+ *
+ * subscription.on('changed', function(event){
+ *   // remove event from local database
+ * })
+ *
+ * subscription.on('error', function(error, receipt) { // If the transaction was rejected by the network with a receipt, the second parameter will be the receipt.
+ *   ...
+ * });
+ *
+ * // event output example
+ * > {
+ *   returnValues: {
+ *       myIndexedParam: 20,
+ *       myOtherIndexedParam: '0x123456789...',
+ *       myNonIndexParam: 'My String'
+ *   },
+ *   raw: {
+ *       data: '0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385',
+ *       topics: ['0xfd43ade1c09fade1c0d57a7af66ab4ead7c2c2eb7b11a91ffdd57a7af66ab4ead7', '0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385']
+ *   },
+ *   event: 'MyEvent',
+ *   signature: '0xfd43ade1c09fade1c0d57a7af66ab4ead7c2c2eb7b11a91ffdd57a7af66ab4ead7',
+ *   logIndex: 0,
+ *   transactionIndex: 0,
+ *   transactionHash: '0x7f9fade1c0d57a7af66ab4ead79fade1c0d57a7af66ab4ead7c2c2eb7b11a91385',
+ *   blockHash: '0xfd43ade1c09fade1c0d57a7af66ab4ead7c2c2eb7b11a91ffdd57a7af66ab4ead7',
+ *   blockNumber: 1234,
+ *   address: '0xde0B295669a9FD93d5F28D9Ec85E40f4cb697BAe'
+ * }
+ * ```
+ */
 export class LogsSubscription extends Web3Subscription<
 	{
 		error: Error;
 		connected: number;
-		data: ReturnType<typeof decodeEventABI>;
-		changed: ReturnType<typeof decodeEventABI> & { removed: true };
+		data: EventLog;
+		changed: EventLog & { removed: true };
 	},
 	{ address?: HexString; topics?: (Topic | Topic[])[]; abi: AbiEventFragment }
 > {
+	/**
+	 * Address of tye contract
+	 */
 	public readonly address?: HexString;
+
+	/**
+	 * The list of topics subscribed
+	 */
 	public readonly topics?: (Topic | Topic[])[];
+
+	/**
+	 * The {@doclink glossary/json_interface | JSON Interface} of the event.
+	 */
 	public readonly abi: AbiEventFragment;
 
 	public constructor(
