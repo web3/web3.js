6aeabed1adbf7310fa76dddb6d02d5b3b741a06a
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-06-28 08:28:01 +0300

    cypress

---
+++ packages/web3-eth/cypress.config.js
@@ -0,0 +1,12 @@
+export default {
+	screenshotOnRunFailure: false,
+	video: false,
+	e2e: {
+		// We've imported your old cypress plugins here.
+		// You may want to clean this up later by importing these.
+		setupNodeEvents(on, config) {
+			return require('./cypress/plugins/index.js')(on, config);
+		},
+		specPattern: 'test/integration/**/**/*.test.ts',
+	},
+};
