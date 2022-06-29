fd4d2601d9a2a75a965a5452f1a057784b791ba7
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-06-29 14:40:07 +0300

    revert

--- packages/web3-eth/test/integration/setup.js
+++ packages/web3-eth/test/integration/setup.js
@@ -19,6 +19,6 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require('../config/setup');
 
-const jestTimeout = 25000;
+const jestTimeout = 35000;
 
 jest.setTimeout(jestTimeout);
