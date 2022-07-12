e0b0148b0724f1ca4af780fbc3a0bc4260547f79
Alex <alex.luu@mail.utoronto.ca>
2022-07-12 12:35:11 -0400

    fixing testcase

--- packages/web3-eth-accounts/test/unit/account.test.ts
+++ packages/web3-eth-accounts/test/unit/account.test.ts
@@ -43,7 +43,6 @@ import {
 	validHashMessageData,
 	validPrivateKeytoAccountData,
 	validPrivateKeyToAddressData,
-	signatureObjectRecoverData,
 } from '../fixtures/account';
 
 describe('accounts', () => {
