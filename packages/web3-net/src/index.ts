4f65b9c613c728cf8970fa8520693d6da9f3ac36
Alex <alex.luu@mail.utoronto.ca>
2022-07-14 15:46:12 -0400

    adding docs for net

--- packages/web3-net/src/index.ts
+++ packages/web3-net/src/index.ts
@@ -15,6 +15,35 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+/**
+ * The web3-net package allows you to interact with an Ethereum node’s network properties.
+ *
+ * ```ts
+ * import Net from 'web3-net';
+ *
+ * const net = new Net(Net.givenProvider || 'ws://some.local-or-remote.node:8546');
+ * // or using the web3 umbrella package
+ * import Web3 from 'web3';
+ * const web3 = new Web3(Web3.givenProvider || 'ws://some.local-or-remote.node:8546');
+ *
+ * // -> web3.eth.net
+ *
+ * // get the ID of the network
+ * web3.eth.net.getId();
+ * > 1
+ *
+ * // get the peer count
+ * web3.eth.net.getPeerCount();
+ * > 0
+ *
+ * // Check if the node is listening for peers
+ * web3.eth.net.getPeerCount();
+ * ```
+ */
+
+/**
+ *
+ */
 import { Net } from './net';
 
 export * from './net';
