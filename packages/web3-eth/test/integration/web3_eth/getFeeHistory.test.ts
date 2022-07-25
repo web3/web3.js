a61a898ce6261d163add65c88a9af35e9bec75cd
Nikos Iliakis <nikoulai94@hotmail.com>
2022-07-25 19:19:24 +0300

    Fix test import

--- packages/web3-eth/test/integration/web3_eth/getFeeHistory.test.ts
+++ packages/web3-eth/test/integration/web3_eth/getFeeHistory.test.ts
@@ -16,7 +16,7 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 import WebSocketProvider from 'web3-providers-ws';
 
-import { BlockNumberOrTag, Numbers } from 'web3-utils';
+import { BlockNumberOrTag, Numbers } from 'web3-types';
 
 import Web3Eth from '../../../src';
 import {
