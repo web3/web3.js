3654b0b1ca6b98310217ecf7f9acdae7a50ff04a
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-08-02 15:25:13 +0300

    add ssl cypress

--- templates/cypress.config.js
+++ templates/cypress.config.js
@@ -1,7 +1,20 @@
 module.exports = {
 	screenshotOnRunFailure: false,
 	video: false,
+	clientCertificates: [
+		{
+			url: 'https://web3.js',
+			certs: [
+				{
+					cert: './cypress/.cert/cert.pem',
+					key: './cypress/.cert/key.pem',
+				},
+			],
+		},
+	],
 	e2e: {
+		port: 8888,
+		baseUrl: 'https://localhost:8888',
 		// We've imported your old cypress plugins here.
 		// You may want to clean this up later by importing these.
 		setupNodeEvents(on, config) {
