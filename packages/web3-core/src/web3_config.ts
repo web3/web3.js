612e5b18ce9093582bb67da29c9911c4900bf8c9
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2022-08-10 23:42:12 +0200

    updating comment and whitespaces

--- packages/web3-core/src/web3_config.ts
+++ packages/web3-core/src/web3_config.ts
@@ -260,7 +260,7 @@ export abstract class Web3Config
 	}
 	/**
 	 * Used over HTTP connections. This option defines the number of seconds Web3 will wait for a receipt which confirms that a transaction was mined by the network. Note: If this method times out, the transaction may still be pending.
-	 * Default is `2000` ms.
+	 * Default is `750` seconds (12.5 minutes).
 	 */
 	public get transactionPollingTimeout() {
 		return this._config.transactionPollingTimeout;
