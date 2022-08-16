e775a96c7bc88e333fca895496c059874bcac1e2
Your Name <you@example.com>
2022-08-16 15:31:37 +0300

    Log Web3.version

--- blackbox_test/basic_usage.ts
+++ blackbox_test/basic_usage.ts
@@ -69,6 +69,8 @@ async function main() {
 
 	if (isWs(backendMode)) log('^^^^^^^^^', await waitForOpenConnection(web3, backendMode));
 
+	log('Web3.version:', Web3.version);
+
 	log();
 	log('>>>>>>');
 	log(`${backendMode?.toUpperCase()}:MAINNET getBlock`);
