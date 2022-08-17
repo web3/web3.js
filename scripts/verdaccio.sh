e6c59da347028b5bb99e8587eee54b9203ad04da
Wyatt Barnes <me@wyatt.email>
2022-08-17 12:08:38 -0400

    Rename verdaccio script

--- scripts/verdaccio.sh
+++ scripts/verdaccio.sh
@@ -3,7 +3,7 @@
 ORIGARGS=("$@")
 
 helpFunction() {
-	echo "Usage: $0 [start|stop] [background]"
+	echo "Usage: $0 [start|stop|publish|startAndPublish] [background]"
 	exit 1 # Exit script after printing help
 }
 
@@ -22,43 +22,54 @@ stop() {
     docker ps -q --filter ancestor="verdaccio/verdaccio" | xargs -r docker stop
 }
 
-publish() {
-    echo "Publishing to verdaccio ..."
-
-    npx wait-port 4873
-
-    # `npm add user`
+createVerdaccioNPMUser() {
     curl -XPUT \
         -H "Content-type: application/json" \
         -d '{ "name": "test", "password": "test" }' \
         'http://localhost:4873/-/user/org.couchdb.user:test'
+}
 
-    # `npm login`
+loginNPMUser() {
     npm-auth-to-token \
         -u test \
         -p test \
         -e test@test.com \
         -r http://localhost:4873
+}
 
+lernaUpdatePackageVersions() {
     lerna version 5.0.0 \
         --ignore-scripts \
         --no-push \
         --no-private \
         --no-git-tag-version \
         --yes
+}
 
-    yarn build
-
-    git add .
-    git commit -m "Comitting for black box publish"
-
-    # Lerna publish to alpha tag
+lernaPublish() {
     lerna publish from-package \
         --no-git-tag-version \
         --no-push \
         --registry http://localhost:4873 \
         --ignore-scripts \
         --yes
+}
+
+publish() {
+    echo "Publishing to verdaccio ..."
+
+    npx wait-port 4873
+
+    createVerdaccioNPMUser
+    loginNPMUser
+    lernaUpdatePackageVersions
+
+    yarn build
+
+    git add .
+    git commit -m "Comitting for black box publish"
+
+    lernaPublish
 }
 
 startAndPublish() {
@@ -69,6 +80,10 @@ case $1 in
 start) start ;;
 stop) stop ;;
 publish) publish ;;
-startAndPublish) startAndPublishpublish ;;
+startAndPublish) startAndPublish ;;
+createVerdaccioNPMUser) createVerdaccioNPMUser ;;
+loginNPMUser) loginNPMUser ;;
+lernaUpdatePackageVersions) lernaUpdatePackageVersions ;;
+lernaPublish) lernaPublish ;;
 *) helpFunction ;; # Print helpFunction in case parameter is non-existent
 esac
