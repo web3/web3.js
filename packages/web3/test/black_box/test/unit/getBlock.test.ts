d6b97783ec0055c80dddedf973636622e85079c7
Wyatt Barnes <me@wyatt.email>
2022-08-17 12:08:40 -0400

    WIP black box test

--- packages/web3/test/black_box/test/unit/getBlock.test.ts
+++ packages/web3/test/black_box/test/unit/getBlock.test.ts
@@ -14,20 +14,40 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 import Web3 from 'web3';
+import { validator } from 'web3-validator';
+import { blockSchema } from 'web3-eth';
 
-// import { blockSchema } from 'web3-eth';
+import { getSystemTestBackend } from '../../../shared_fixtures/system_tests_utils';
 
-describe('Black Box Unit Tests', () => {
+export const describeIf = (condition: (() => boolean) | boolean) =>
+	(typeof condition === 'function' ? condition() : condition) ? describe : describe.skip;
+
+describeIf(getSystemTestBackend() === 'http')('Black Box Unit Tests - HTTP - getBlock', () => {
 	let web3: Web3;
 
 	beforeAll(() => {
 		web3 = new Web3('http://localhost:8545');
 	});
 
-	it('should get the latest block', async () => {
-		const response = await web3.eth.getBlock('latest');
-		expect(response).toBeDefined();
+	it('should get the latest block and validate it against blockSchema', async () => {
+		expect(validator.validateJSONSchema(blockSchema, await web3.eth.getBlock('latest')));
+	});
+});
+
+describeIf(getSystemTestBackend() === 'ws')('Black Box Unit Tests - WS - getBlock', () => {
+	let web3: Web3;
+
+	beforeAll(async () => {
+		web3 = new Web3('ws://localhost:8545');
+
+        let status = '';
+        while (status !== 'connected') {
+            console.log(web3.provider);
+        }
+	});
+
+	it('should get the latest block and validate it against blockSchema', async () => {
+		expect(validator.validateJSONSchema(blockSchema, await web3.eth.getBlock('latest')));
 	});
 });
