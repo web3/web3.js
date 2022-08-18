28ac55e057e821ebd8507c161c754f50138be056
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2022-08-18 15:04:53 +0200

    add a missing import

--- packages/web3-eth/src/errors.ts
+++ packages/web3-eth/src/errors.ts
@@ -42,6 +42,7 @@ import {
 	ERR_TX_RECEIPT_MISSING_BLOCK_NUMBER,
 	ERR_SIGNATURE_FAILED,
 	InvalidValueError,
+	Web3Error,
 } from 'web3-errors';
 import { Bytes, HexString, Numbers, TransactionReceipt } from 'web3-types';
 
@@ -271,10 +272,11 @@ export class TransactionSendTimeoutError extends Web3Error {
 
 	public constructor(value: { numberOfSeconds: number; transactionHash?: Bytes }) {
 		super(
-			`transactionHash: ${
+			`The connected Ethereum Node did not respond within ${
+				value.numberOfSeconds
+			} seconds, please make sure your transaction was properly sent and you are connected to a healthy Node. Be aware that transaction might still be pending or mined!\n\tTransaction Hash: ${
 				value.transactionHash ? value.transactionHash.toString() : 'not available'
 			}`,
-			`The connected Ethereum Node did not respond within ${value.numberOfSeconds} seconds, please make sure your transaction was properly sent and you are connected to a healthy Node. Be aware that transaction might still be pending or mined!`,
 		);
 	}
 }
@@ -284,8 +286,9 @@ export class TransactionPollingTimeoutError extends Web3Error {
 
 	public constructor(value: { numberOfSeconds: number; transactionHash: Bytes }) {
 		super(
-			`transactionHash: ${value.transactionHash.toString()}`,
-			`Transaction was not mined within ${value.numberOfSeconds} seconds, please make sure your transaction was properly sent. Be aware that it might still be pending or mined!`,
+			`Transaction was not mined within ${
+				value.numberOfSeconds
+			} seconds, please make sure your transaction was properly sent. Be aware that it might still be pending or mined!\n\tTransaction Hash: ${value.transactionHash.toString()}`,
 		);
 	}
 }
