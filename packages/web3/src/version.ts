c35e3d5e5e8be05db0b02e45eaddb6d5b1167028
Junaid <86780488+jdevcs@users.noreply.github.com>
2022-08-24 23:20:27 +0200

    Release/4.0.0 alpha.0 (#5375)
    * web3 package version fix
    
    * change log update

--- packages/web3/src/version.ts
+++ packages/web3/src/version.ts
@@ -1,1 +1,1 @@
-/* eslint-disable header/header */ export const Web3PkgInfo = { version: '4.0.0-alpha.1' };
+/* eslint-disable header/header */ export const Web3PkgInfo = { version: '4.0.0-alpha.0' };
