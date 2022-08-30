40bbb33083dc74505bf2b7f9006be5bb671b00bd
Wyatt Barnes <me@wyatt.email>
2022-08-29 17:09:22 -1000

    Update packages/web3-plugin-example/test/unit/plugin.test.ts

--- packages/web3-plugin-example/test/unit/plugin.test.ts
+++ packages/web3-plugin-example/test/unit/plugin.test.ts
@@ -22,7 +22,7 @@ import { AggregatorV3InterfaceABI } from '../../src/aggregator_v3_interface_abi'
 const aggregatorAddress = '0xECe365B379E1dD183B20fc5f022230C044d51404';
 
 describe('Chainlink Plugin Tests', () => {
-	it('should do something', async () => {
+	it('should register ChainlinkPlugin and make the getPrice call', async () => {
 		const web3 = new Web3('https://rpc.ankr.com/eth_rinkeby');
 		web3.registerPlugin(new ChainlinkPlugin(AggregatorV3InterfaceABI, aggregatorAddress));
 		const price = await web3.chainlink.getPrice();
