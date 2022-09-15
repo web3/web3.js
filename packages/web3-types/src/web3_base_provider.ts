fb1770402092957dcaa328c7ee97d71a52394199
Wyatt Barnes <me@wyatt.email>
2022-09-14 15:20:00 -1000

    Move eth_execution_api to web3-types

--- packages/web3-types/src/web3_base_provider.ts
+++ packages/web3-types/src/web3_base_provider.ts
@@ -14,10 +14,10 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
-
 import { Socket } from 'net';
+
 import { Web3Error } from './error_types';
-import { EthExecutionAPI } from './eth_execution_api';
+import { EthExecutionAPI } from './apis/eth_execution_api';
 import {
 	JsonRpcNotification,
 	JsonRpcPayload,
