b3172c6c4f8b1387eeb8c263e4e85dfbd07a8763
Nikos Iliakis <nikoulai@users.noreply.github.com>
2022-09-22 16:58:45 +0300

    Nikos/5240/ens resolver 4.x system tests (#5437)
    * Add initial setup
    
    * Add contract artifacts
    
    * Fix contracts setup
    
    * Finish tests
    
    * Delete setUpENS
    
    * Tests refactoring
    
    * Use different builds for ens internal contracts
    
    * Revert files deletion
    
    * Remove try catch
    
    * Fix test script
    
    * Restore try catch
    
    * Create dedicated bytecode files for tests
    
    * Init tests
    
    * Add some tests
    
    * Add contentHash tests
    
    * Add addr functions
    
    * Fix ens tests
    
    * Address feedback
    
    * Address feedback
    
    * Namewrapper changes
    
    * Fix abi typo
    
    * Update packages/web3-eth-ens/test/fixtures/ens/bytecode/NameWrapperBytecode.ts
    
    Co-authored-by: Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
    
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>
    Co-authored-by: Muhammad Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
    Co-authored-by: Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>

---
+++ packages/web3-eth-ens/test/integration/resolver.test.ts
@@ -0,0 +1,226 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { Contract, PayableTxOptions } from 'web3-eth-contract';
+import { sha3, DEFAULT_RETURN_FORMAT } from 'web3-utils';
+import Web3Eth from 'web3-eth';
+
+import { Address, Bytes } from 'web3-types';
+import { ENS } from '../../src';
+import { namehash } from '../../src/utils';
+
+import {
+	getSystemTestAccounts,
+	getSystemTestProvider,
+	isWs,
+	isIpc,
+	closeOpenConnection,
+} from '../fixtures/system_tests_utils';
+
+import { ENSRegistryAbi } from '../../src/abi/ens/ENSRegistry';
+import { ENSRegistryBytecode } from '../fixtures/ens/bytecode/ENSRegistryBytecode';
+import { NameWrapperAbi } from '../fixtures/ens/abi/NameWrapper';
+import { NameWrapperBytecode } from '../fixtures/ens/bytecode/NameWrapperBytecode';
+import { PublicResolverAbi } from '../../src/abi/ens/PublicResolver';
+import { PublicResolverBytecode } from '../fixtures/ens/bytecode/PublicResolverBytecode';
+
+describe('ens', () => {
+	let registry: Contract<typeof ENSRegistryAbi>;
+	let resolver: Contract<typeof PublicResolverAbi>;
+	let nameWrapper: Contract<typeof NameWrapperAbi>;
+
+	let Resolver: Contract<typeof PublicResolverAbi>;
+
+	let sendOptions: PayableTxOptions;
+
+	const domain = 'test';
+	const domainNode = namehash(domain);
+	const node = namehash('resolver');
+	const label = sha3('resolver') as string;
+
+	let web3Eth: Web3Eth;
+
+	let accounts: string[];
+	let ens: ENS;
+	let defaultAccount: string;
+	let accountOne: string;
+
+	const ZERO_NODE: Bytes = '0x0000000000000000000000000000000000000000000000000000000000000000';
+	const addressOne: Address = '0x0000000000000000000000000000000000000001';
+
+	const contentHash = '0x0000000000000000000000000000000000000000000000000000000000000001';
+
+	const DEFAULT_COIN_TYPE = 60;
+
+	beforeAll(async () => {
+		accounts = await getSystemTestAccounts();
+
+		[defaultAccount, accountOne] = accounts;
+
+		sendOptions = { from: defaultAccount, gas: '10000000' };
+
+		const Registry = new Contract(ENSRegistryAbi, undefined, {
+			provider: getSystemTestProvider(),
+		});
+
+		const NameWrapper = new Contract(NameWrapperAbi, undefined, {
+			provider: getSystemTestProvider(),
+		});
+
+		Resolver = new Contract(PublicResolverAbi, undefined, {
+			provider: getSystemTestProvider(),
+		});
+
+		registry = await Registry.deploy({ data: ENSRegistryBytecode }).send(sendOptions);
+
+		nameWrapper = await NameWrapper.deploy({ data: NameWrapperBytecode }).send(sendOptions);
+
+		resolver = await Resolver.deploy({
+			data: PublicResolverBytecode,
+			arguments: [
+				registry.options.address as string,
+				nameWrapper.options.address as string,
+				accountOne,
+				defaultAccount,
+			],
+		}).send(sendOptions);
+
+		await registry.methods.setSubnodeOwner(ZERO_NODE, label, defaultAccount).send(sendOptions);
+		await registry.methods
+			.setResolver(node, resolver.options.address as string)
+			.send(sendOptions);
+		await resolver.methods.setAddr(node, addressOne).send(sendOptions);
+
+		await registry.methods
+			.setSubnodeOwner(ZERO_NODE, sha3(domain) as string, defaultAccount)
+			.send(sendOptions);
+
+		const clientUrl = getSystemTestProvider();
+		let provider;
+		if (isIpc) provider = new ENS.providers.IpcProvider(clientUrl);
+		else if (isWs) provider = new ENS.providers.WebsocketProvider(clientUrl);
+		else provider = new ENS.providers.HttpProvider(clientUrl);
+
+		ens = new ENS(registry.options.address, provider);
+
+		web3Eth = new Web3Eth(provider);
+		const block = await web3Eth.getBlock('latest', false, DEFAULT_RETURN_FORMAT);
+		const gas = block.gasLimit.toString();
+
+		// Increase gas for contract calls
+		sendOptions = {
+			...sendOptions,
+			gas,
+		};
+	});
+
+	afterAll(async () => {
+		if (isWs || isIpc) {
+			await closeOpenConnection(ens);
+			await closeOpenConnection(ens['_registry']['contract']);
+			await closeOpenConnection(registry);
+			await closeOpenConnection(resolver);
+			await closeOpenConnection(nameWrapper);
+		}
+	});
+	beforeEach(async () => {
+		// set up subnode
+		await registry.methods
+			.setSubnodeOwner(ZERO_NODE, sha3('eth') as string, defaultAccount)
+			.send(sendOptions);
+	});
+
+	it('supports known interfaces', async () => {
+		await expect(ens.supportsInterface('resolver', '0x3b3b57de')).resolves.toBeTruthy(); // IAddrResolver
+		await expect(ens.supportsInterface('resolver', '0xf1cb7e06')).resolves.toBeTruthy(); // IAddressResolver
+		await expect(ens.supportsInterface('resolver', '0x691f3431')).resolves.toBeTruthy(); // INameResolver
+		await expect(ens.supportsInterface('resolver', '0x2203ab56')).resolves.toBeTruthy(); // IABIResolver
+		await expect(ens.supportsInterface('resolver', '0xc8690233')).resolves.toBeTruthy(); // IPubkeyResolver
+		await expect(ens.supportsInterface('resolver', '0x59d1d43c')).resolves.toBeTruthy(); // ITextResolver
+		await expect(ens.supportsInterface('resolver', '0xbc1c58d1')).resolves.toBeTruthy(); // IContentHashResolver
+		await expect(ens.supportsInterface('resolver', '0xa8fa5682')).resolves.toBeTruthy(); // IDNSRecordResolver
+		await expect(ens.supportsInterface('resolver', '0x5c98042b')).resolves.toBeTruthy(); // IDNSZoneResolver
+		await expect(ens.supportsInterface('resolver', '0x01ffc9a7')).resolves.toBeTruthy(); // IInterfaceResolver
+	});
+
+	it('does not support a random interface', async () => {
+		await expect(ens.supportsInterface('resolver', '0x3b3b57df')).resolves.toBeFalsy();
+	});
+
+	it('fetch pubkey', async () => {
+		await ens.setResolver(
+			domain,
+			resolver.options.address as string,
+			sendOptions,
+			DEFAULT_RETURN_FORMAT,
+		);
+
+		const res = await ens.getPubkey(domain);
+
+		expect(res[0]).toBe('0x0000000000000000000000000000000000000000000000000000000000000000');
+		expect(res[1]).toBe('0x0000000000000000000000000000000000000000000000000000000000000000');
+	});
+
+	it('permits setting public key by owner', async () => {
+		const x = '0x1000000000000000000000000000000000000000000000000000000000000000';
+		const y = '0x2000000000000000000000000000000000000000000000000000000000000000';
+
+		await ens.setPubkey(domain, x, y, sendOptions);
+
+		const result = await ens.getPubkey(domain);
+
+		expect(result[0]).toBe(namehash(x));
+		expect(result[1]).toBe(namehash(y));
+	});
+
+	it('sets contenthash', async () => {
+		await ens.setContenthash(domain, contentHash, sendOptions);
+
+		const res = await resolver.methods.contenthash(domainNode).call(sendOptions);
+		expect(res).toBe(contentHash);
+	});
+
+	it('fetches contenthash', async () => {
+		await resolver.methods.setContenthash(domainNode, contentHash).call(sendOptions);
+
+		const res = await ens.getContenthash(domain);
+		expect(res).toBe(contentHash);
+	});
+
+	it('sets address', async () => {
+		await registry.methods
+			.setResolver(domainNode, resolver.options.address as string)
+			.send(sendOptions);
+
+		await ens.setAddress(domain, accounts[1], sendOptions, DEFAULT_RETURN_FORMAT);
+
+		const res = await resolver.methods.addr(domainNode, DEFAULT_COIN_TYPE).call(sendOptions);
+		expect(res).toBe(accounts[1]);
+	});
+
+	it('fetches address', async () => {
+		await registry.methods
+			.setResolver(domainNode, resolver.options.address as string)
+			.send(sendOptions);
+
+		await resolver.methods.setAddr(domainNode, accountOne).send(sendOptions);
+
+		const resultAddress = await ens.getAddress(domain);
+		expect(resultAddress).toBe(accountOne);
+	});
+});
