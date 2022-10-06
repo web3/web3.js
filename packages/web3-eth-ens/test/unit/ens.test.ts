d6ddd64a20350c1aecf79569628e37b8da2d9612
Nikos Iliakis <nikoulai94@hotmail.com>
2022-10-06 12:41:46 +0300

    Add ens tests

--- packages/web3-eth-ens/test/unit/ens.test.ts
+++ packages/web3-eth-ens/test/unit/ens.test.ts
@@ -15,27 +15,36 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Web3Context, Web3ContextObject, Web3EventMap, Web3PromiEvent } from 'web3-core';
-import { NonPayableMethodObject, Contract } from 'web3-eth-contract';
-import { ResolverMethodMissingError } from 'web3-errors';
-import { sha3Raw, sha3, DEFAULT_RETURN_FORMAT, DataFormat } from 'web3-utils';
-import { Address, Bytes } from 'web3-types';
-import { Registry } from '../../src/registry';
-import { ENSRegistryAbi } from '../../src/abi/ens/ENSRegistry';
-import { methodsInInterface, interfaceIds, registryAddresses } from '../../src/config';
-import { namehash } from '../../src/utils';
-import { ENS } from '../../src/ens';
+import { Web3Context, Web3ContextObject, Web3PromiEvent } from 'web3-core';
+import { Contract } from 'web3-eth-contract';
+import { ENSNetworkNotSyncedError, ENSUnsupportedNetworkError } from 'web3-errors';
+import { sha3Raw, DEFAULT_RETURN_FORMAT } from 'web3-utils';
+import { Block } from 'web3-types';
+import { registryAddresses } from '../../src/config';
 import { PublicResolverAbi } from '../../src/abi/ens/PublicResolver';
-import { Resolver } from '../../src/resolver';
 
-/* eslint-disable @typescript-eslint/no-unused-vars */
+import { ENS } from '../../src/ens';
+
+jest.useFakeTimers().setSystemTime(new Date('2020-01-01'));
+
+jest.mock('web3-eth', () => ({
+	__esModule: true,
+	getBlock: jest.fn(),
+}));
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+const { getBlock } = require('web3-eth');
+
+const expectedNetworkId = 'main';
+jest.mock('web3-net', () => ({
+	getId: jest.fn(),
+}));
+// eslint-disable-next-line @typescript-eslint/no-require-imports, @typescript-eslint/no-var-requires
+const { getId } = require('web3-net');
+
 describe('ens', () => {
 	const TTL = 3600;
 	let object: Web3ContextObject;
-	let registry: Registry;
-	// let contract: Contract<typeof PublicResolverAbi>;
-	let resolver: Resolver;
-	let contract: Contract<typeof ENSRegistryAbi>;
 	let resolverContract: Contract<typeof PublicResolverAbi>;
 	const mockAddress = '0x0000000000000000000000000000000000000000';
 	const ENS_NAME = 'web3js.eth';
@@ -47,16 +56,9 @@ describe('ens', () => {
 		const context = new Web3Context('http://test.com');
 		object = context.getContextObject() as Web3ContextObject;
 
-		registry = new Registry(object);
-		resolver = new Resolver(registry);
-		contract = new Contract(ENSRegistryAbi, mockAddress);
 		resolverContract = new Contract(PublicResolverAbi, mockAddress);
 		ens = new ENS(registryAddresses.main, object);
 	});
-
-	// afterEach(() => {
-	// 	jest.clearAllMocks();
-	// });
 
 	describe('Resolver', () => {
 		it('setResolver', async () => {
@@ -69,7 +71,7 @@ describe('ens', () => {
 
 			const sendOptions = { from: mockAddress };
 			await ens.setResolver(ENS_NAME, mockAddress, sendOptions);
-			// await registry.setOwner(ENS_NAME, mockAddress, sendOptions);
+
 			expect(setResolverMock).toHaveBeenCalledWith(
 				ENS_NAME,
 				mockAddress,
@@ -117,7 +119,6 @@ describe('ens', () => {
 		});
 		it('setSubnodeOwner', async () => {
 			const label = sha3Raw(ENS_NAME);
-			const node = namehash(ENS_NAME);
 
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
 			const send = jest.spyOn({ send: () => {} }, 'send');
@@ -160,7 +161,6 @@ describe('ens', () => {
 
 			const sendOptions = { from: mockAddress };
 			await ens.setApprovalForAll(ENS_NAME, true, sendOptions);
-			// await registry.setOwner(ENS_NAME, mockAddress, sendOptions);
 			expect(setApprovalForAllMock).toHaveBeenCalledWith(ENS_NAME, true, sendOptions);
 		});
 
@@ -218,8 +218,6 @@ describe('ens', () => {
 
 	describe('ttl', () => {
 		it('setTTL', async () => {
-			// const checkInteraface = jest.spyOn(resolver, 'checkInterfaceSupport');
-
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
 			const send = jest.spyOn({ send: () => {} }, 'send');
 
@@ -247,8 +245,6 @@ describe('ens', () => {
 
 	describe('owner', () => {
 		it('setOwner', async () => {
-			// const checkInteraface = jest.spyOn(resolver, 'checkInterfaceSupport');
-
 			// eslint-disable-next-line @typescript-eslint/no-empty-function
 			const send = jest.spyOn({ send: () => {} }, 'send');
 
@@ -337,7 +333,6 @@ describe('ens', () => {
 
 		describe('Contenthash', () => {
 			it('setContenthash', async () => {
-				const setContenthashMethod = methodsInInterface.setContenthash;
 				const hash = sha3Raw('justToHash');
 
 				// eslint-disable-next-line @typescript-eslint/no-empty-function
@@ -378,10 +373,68 @@ describe('ens', () => {
 
 		await ens.supportsInterface(ENS_NAME, interfaceId);
 
-		expect(supportsInterfaceMock).toHaveBeenCalledWith(ENS_NAME, methodsInInterface);
+		expect(supportsInterfaceMock).toHaveBeenCalledWith(ENS_NAME, interfaceId);
 	});
 
 	describe('CheckNetwork', () => {
-		it('checkNetwork', async () => {});
+		beforeEach(() => {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getId.mockReset();
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getBlock.mockReset();
+		});
+		it('Not last sync/ENSNetworkNotSyncedError', async () => {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getId.mockImplementation(() => expectedNetworkId);
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getBlock.mockImplementation(() => {
+				return {
+					timestamp: BigInt(Date.now() / 1000 - 3601),
+				} as unknown as Block;
+			});
+			await expect(ens.checkNetwork()).rejects.toThrow(new ENSNetworkNotSyncedError());
+		});
+
+		it('Threshold exceeded from previous check/ENSNetworkNotSyncedError', async () => {
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getId.mockImplementation(() => expectedNetworkId);
+
+			// An initial check, in order to update `_lastSyncCheck`
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getBlock.mockImplementation(() => {
+				return {
+					timestamp: BigInt(Date.now() / 1000),
+				} as unknown as Block;
+			});
+			// update `_lastSyncCheck`
+			await ens.checkNetwork();
+
+			// now - this._lastSyncCheck > 3600)
+			jest.useFakeTimers().setSystemTime(new Date('2020-01-01').getTime() + 3601000); // (3600 + 1) * 1000
+			await expect(ens.checkNetwork()).resolves.not.toThrow();
+		});
+
+		it('ENSUnsupportedNetworkError', async () => {
+			// reset from previous check
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			ens['_detectedAddress'] = undefined;
+
+			const network = 'AnUnsupportedNetwork';
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getId.mockImplementation(() => network);
+
+			// eslint-disable-next-line @typescript-eslint/no-unsafe-call
+			getBlock.mockImplementation(() => {
+				return {
+					timestamp: BigInt(111111111),
+				} as unknown as Block;
+			});
+
+			await expect(ens.checkNetwork()).rejects.toThrow(
+				new ENSUnsupportedNetworkError(network),
+			);
+		});
 	});
 });
