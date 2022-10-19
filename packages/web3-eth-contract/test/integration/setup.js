6df768b3e304c156fd846a517cee23a6904e3a03
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-10-18 21:21:52 -0400

    ipc timeout 20s

--- packages/web3-eth-contract/test/integration/setup.js
+++ packages/web3-eth-contract/test/integration/setup.js
@@ -18,7 +18,6 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 // Have to use `require` because of Jest issue https://jestjs.io/docs/ecmascript-modules
 // eslint-disable-next-line @typescript-eslint/no-require-imports
 require('../config/setup');
-
-const jestTimeout = 15000;
+const jestTimeout = String(process.env.WEB3_SYSTEM_TEST_PROVIDER).includes('ipc') ? 20000 : 15000;
 
 jest.setTimeout(jestTimeout);
