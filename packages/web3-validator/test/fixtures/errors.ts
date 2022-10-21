bfce27fbc27b0db0261e874044c7df5d338023e6
Nikos Iliakis <nikoulai@users.noreply.github.com>
2022-10-21 15:39:13 +0200

    Nikos/5430/validator unit test coverage (#5525)
    * Add codePoint tests
    
    * Add hexToNumber tests
    
    * Add default validator test
    
    * Add invalid bloom test
    
    * Add error tests
    
    * Add index tests
    
    * Add more tests
    
    * Remove comments

---
+++ packages/web3-validator/test/fixtures/errors.ts
@@ -0,0 +1,69 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+export const fullErrors: any[] = [
+	{
+		instancePath: '',
+		schemaPath: '#/minItems',
+		keyword: 'minItems',
+		params: { limit: 1 },
+		message: 'must NOT have fewer than 1 items',
+	},
+	{
+		instancePath: '',
+		schemaPath: '#/minItems',
+		keyword: 'minItems',
+		params: { limit: 2 },
+		message: 'must NOT have fewer than 2 items',
+	},
+	{
+		instancePath: '',
+		schemaPath: '#/maxItems',
+		keyword: 'maxItems',
+		params: { limit: 1 },
+		message: 'must NOT have more than 1 items',
+	},
+];
+
+export const fullErrorsWithInstance: any[] = [
+	{
+		message: 'must pass "uint" validation',
+		keyword: 'eth',
+		params: { value: -1 },
+		instancePath: '/0',
+		schemaPath: '#/items/0/eth',
+	},
+];
+
+export const errorsWithInstanceNoParams: any[] = [
+	{
+		message: 'must pass "uint" validation',
+		keyword: 'eth',
+		instancePath: '/0',
+		schemaPath: '#/items/0/eth',
+	},
+];
+
+export const errorsWithInstanceNoParamsNoMessage: any[] = [
+	{
+		keyword: 'eth',
+		instancePath: '/0',
+		schemaPath: '#/items/0/eth',
+	},
+];
+
+export const unspecifiedErrors: any[] = [{}];
