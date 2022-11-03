67827030b11c375116a2b9e8032f2e69deb6ccf8
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-11-03 11:41:12 -0400

    fix pure in IF

--- packages/web3-eth-contract/src/contract.ts
+++ packages/web3-eth-contract/src/contract.ts
@@ -1015,7 +1015,31 @@ export class Contract<Abi extends ContractAbi>
 					throw new Web3ValidatorError(errors);
 				}
 			}
-
+			if (methodAbi.stateMutability === 'payable') {
+				return {
+					arguments: abiParams,
+					call: async (options?: PayableCallOptions, block?: BlockNumberOrTag) =>
+						this._contractMethodCall(methodAbi, abiParams, errorsAbis, options, block),
+					send: (options?: PayableTxOptions) =>
+						this._contractMethodSend(methodAbi, abiParams, options), // TODO: refactor to parse errorsAbi #5587
+					estimateGas: async <
+						ReturnFormat extends DataFormat = typeof DEFAULT_RETURN_FORMAT,
+					>(
+						options?: PayableCallOptions,
+						returnFormat: ReturnFormat = DEFAULT_RETURN_FORMAT as ReturnFormat,
+					) =>
+						this._contractMethodEstimateGas({
+							abi: methodAbi,
+							params: abiParams,
+							returnFormat,
+							options,
+						}),
+					encodeABI: () => encodeMethodABI(methodAbi, abiParams),
+				} as unknown as PayableMethodObject<
+					ContractMethod<T>['Inputs'],
+					ContractMethod<T>['Outputs']
+				>;
+			}
 			return {
 				arguments: abiParams,
 				call: async (options?: NonPayableCallOptions, block?: BlockNumberOrTag) =>
