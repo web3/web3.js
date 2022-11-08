45e223a49a8049329db0dc7560641e52a9be06ff
Nikos Iliakis <nikoulai94@hotmail.com>
2022-11-08 12:24:21 +0200

    Proper skip test

--- templates/cypress.config.js
+++ templates/cypress.config.js
@@ -1,3 +1,19 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
 const config = {
 	screenshotOnRunFailure: false,
 	video: false,
@@ -5,9 +21,13 @@ const config = {
 		// We've imported your old cypress plugins here.
 		// You may want to clean this up later by importing these.
 		setupNodeEvents(on, config) {
-			return require('./cypress/plugins/index.js')(on, config);
+			const pluginConfig = require('./cypress/plugins/index.js')(on, config);
+			return {
+				...pluginConfig,
+			};
 		},
 		specPattern: 'test/integration/**/**/*.test.ts',
+		excludeSpecPattern: ['**/contract_defaults_extra.test.ts'],
 	},
 };
 
