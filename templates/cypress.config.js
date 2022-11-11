4fadb192b0d5840c94b41ff9c117ec040895b540
Nikos Iliakis <nikoulai@users.noreply.github.com>
2022-11-11 14:11:34 +0200

    Nikos/5096/improve the integration test for contracts default attributes (#5585)
    * Find right schema of oneOf
    
    * Update changelog
    
    * Store oneOf option
    
    * Use new variable
    
    * Refactor check
    
    * Add check for transaction
    
    * Init tests
    
    * Add more tests
    
    * Add handleRevert
    
    * Cleanup
    
    * Fix web3-eth-accounts package version
    
    * Refactor test
    
    * cypress check
    
    * Try exiting
    
    * Fix wrong access
    
    * Test
    
    * try catch
    
    * Try to skip
    
    * package json
    
    * Remove package to see if that's responsible
    
    * Move package to top level
    
    * new package
    
    * Proper skip test
    
    * Remove package
    
    * Remove web3-rpc-methods

--- templates/cypress.config.js
+++ templates/cypress.config.js
@@ -1,3 +1,19 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
 const config = {
 	screenshotOnRunFailure: false,
 	video: false,
@@ -8,6 +24,7 @@ const config = {
 			return require('./cypress/plugins/index.js')(on, config);
 		},
 		specPattern: 'test/integration/**/**/*.test.ts',
+		excludeSpecPattern: ['**/contract_defaults_extra.test.ts'],
 	},
 };
 
