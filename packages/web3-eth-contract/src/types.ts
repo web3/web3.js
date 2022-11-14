adc370471f098a5e01f1f23042e512d4659a6e8c
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2022-11-13 20:33:17 -0500

    call special output type

--- packages/web3-eth-contract/src/types.ts
+++ packages/web3-eth-contract/src/types.ts
@@ -227,7 +227,11 @@ export interface NonPayableMethodObject<Inputs = unknown[], Outputs = unknown[]>
 	 * @param block - If you pass this parameter it will not use the default block set with contract.defaultBlock. Pre-defined block numbers as `earliest`, `latest`, and `pending` can also be used. Useful for requesting data from or replaying transactions in past blocks.
 	 * @returns - The return value(s) of the smart contract method. If it returns a single value, it’s returned as is. If it has multiple return values they are returned as an object with properties and indices.
 	 */
-	call(tx?: NonPayableCallOptions, block?: BlockNumberOrTag): Promise<Outputs>;
+
+	call<SpecialOutput = undefined>(
+		tx?: NonPayableCallOptions,
+		block?: BlockNumberOrTag,
+	): Promise<SpecialOutput extends undefined ? Outputs : SpecialOutput>;
 
 	/**
 	 * This will send a transaction to the smart contract and execute its method. Note this can alter the smart contract state.
@@ -384,7 +388,10 @@ export interface PayableMethodObject<Inputs = unknown[], Outputs = unknown[]> {
 	 * @param block - If you pass this parameter it will not use the default block set with contract.defaultBlock. Pre-defined block numbers as `earliest`, `latest`, and `pending` can also be used. Useful for requesting data from or replaying transactions in past blocks.
 	 * @returns - The return value(s) of the smart contract method. If it returns a single value, it’s returned as is. If it has multiple return values they are returned as an object with properties and indices.
 	 */
-	call(tx?: PayableCallOptions, block?: BlockNumberOrTag): Promise<Outputs>;
+	call<SpecialOutput = undefined>(
+		tx?: PayableCallOptions,
+		block?: BlockNumberOrTag,
+	): Promise<SpecialOutput extends undefined ? Outputs : SpecialOutput>;
 
 	/**
 	 * Will send a transaction to the smart contract and execute its method. Note this can alter the smart contract state.
