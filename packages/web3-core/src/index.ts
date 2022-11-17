c0e711d98a6d277801ac585da75010275956f76c
Wyatt Barnes <me@wyatt.email>
2022-11-16 19:53:08 -1000

    Manually export web3-core exports

--- packages/web3-core/src/index.ts
+++ packages/web3-core/src/index.ts
@@ -14,18 +14,117 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
+/* eslint deprecation/deprecation: 0 */
 
-export * from './web3_config';
-export * from './web3_request_manager';
-export * from './web3_subscription_manager';
-export * from './web3_subscriptions';
-export * from './web3_context';
-export * from './web3_batch_request';
-export * from './utils';
-export * from './types';
-export * from './formatters';
-export * from './web3_promi_event';
-export * from './web3_event_emitter';
+import { Web3ConfigOptions, Web3ConfigEvent, Web3Config } from './web3_config';
+import { Web3RequestManagerEvent, Web3RequestManager } from './web3_request_manager';
+import { Web3SubscriptionManager } from './web3_subscription_manager';
+import { Web3Subscription, Web3SubscriptionConstructor } from './web3_subscriptions';
+import {
+	Web3ContextObject,
+	Web3ContextInitOptions,
+	Web3ContextConstructor,
+	Web3ContextFactory,
+	Web3Context,
+	TransactionBuilder,
+	Web3PluginBase,
+	Web3EthPluginBase,
+} from './web3_context';
+import { DEFAULT_BATCH_REQUEST_TIMEOUT, Web3BatchRequest } from './web3_batch_request';
+import {
+	isWeb3Provider,
+	isLegacyRequestProvider,
+	isEIP1193Provider,
+	isLegacySendProvider,
+	isLegacySendAsyncProvider,
+	isSupportedProvider,
+	isSupportSubscriptions,
+} from './utils';
+import { TransactionTypeParser } from './types';
+import {
+	inputStorageKeysFormatter,
+	outputProofFormatter,
+	outputBigIntegerFormatter,
+	inputBlockNumberFormatter,
+	inputDefaultBlockNumberFormatter,
+	inputAddressFormatter,
+	txInputOptionsFormatter,
+	inputCallFormatter,
+	inputTransactionFormatter,
+	inputSignFormatter,
+	outputTransactionFormatter,
+	inputTopicFormatter,
+	inputLogFormatter,
+	outputLogFormatter,
+	outputTransactionReceiptFormatter,
+	outputBlockFormatter,
+	inputPostFormatter,
+	outputPostFormatter,
+	outputSyncingFormatter,
+} from './formatters';
+import { PromiseExecutor, Web3PromiEvent } from './web3_promi_event';
+import {
+	Web3EventMap,
+	Web3EventKey,
+	Web3EventCallback,
+	Web3Emitter,
+	Web3EventEmitter,
+} from './web3_event_emitter';
+
+export {
+	Web3ConfigOptions,
+	Web3ConfigEvent,
+	Web3Config,
+	Web3RequestManagerEvent,
+	Web3RequestManager,
+	Web3SubscriptionManager,
+	Web3Subscription,
+	Web3SubscriptionConstructor,
+	Web3ContextObject,
+	Web3ContextInitOptions,
+	Web3ContextConstructor,
+	Web3ContextFactory,
+	Web3Context,
+	TransactionBuilder,
+	Web3PluginBase,
+	Web3EthPluginBase,
+	DEFAULT_BATCH_REQUEST_TIMEOUT,
+	Web3BatchRequest,
+	isWeb3Provider,
+	isLegacyRequestProvider,
+	isEIP1193Provider,
+	isLegacySendProvider,
+	isLegacySendAsyncProvider,
+	isSupportedProvider,
+	isSupportSubscriptions,
+	TransactionTypeParser,
+	inputStorageKeysFormatter,
+	outputProofFormatter,
+	outputBigIntegerFormatter,
+	inputBlockNumberFormatter,
+	inputDefaultBlockNumberFormatter,
+	inputAddressFormatter,
+	txInputOptionsFormatter,
+	inputCallFormatter,
+	inputTransactionFormatter,
+	inputSignFormatter,
+	outputTransactionFormatter,
+	inputTopicFormatter,
+	inputLogFormatter,
+	outputLogFormatter,
+	outputTransactionReceiptFormatter,
+	outputBlockFormatter,
+	inputPostFormatter,
+	outputPostFormatter,
+	outputSyncingFormatter,
+	PromiseExecutor,
+	Web3PromiEvent,
+	Web3EventMap,
+	Web3EventKey,
+	Web3EventCallback,
+	Web3Emitter,
+	Web3EventEmitter,
+};
 
 // For backward usability export as namespace
 export * as formatters from './formatters';
