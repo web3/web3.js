272669fc0aeb8f853f15debe55a57cbf83ca30b2
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2022-11-25 00:06:46 +0100

    improve how legacy providers, such as hardhat, is used

--- packages/web3-core/src/utils.ts
+++ packages/web3-core/src/utils.ts
@@ -41,11 +41,7 @@ export const isEIP1193Provider = <API extends Web3APISpec>(
 ): provider is EIP1193Provider<API> =>
 	typeof provider !== 'string' &&
 	'request' in provider &&
-	(provider.request.constructor.name === 'AsyncFunction' ||
-		// Some providers that follow a previous drafted version of EIP1193 has a `request` function
-		//	that is not defined as `async` (but it returns a promise).
-		// like: https://github.dev/NomicFoundation/hardhat/blob/62bea2600785595ba36f2105564076cf5cdf0fd8/packages/hardhat-core/src/internal/core/providers/backwards-compatibility.ts#L19
-		provider.request.constructor.name === 'Function');
+	provider.request.constructor.name === 'AsyncFunction';
 
 export const isLegacySendProvider = <API extends Web3APISpec>(
 	provider: SupportedProviders<API>,
