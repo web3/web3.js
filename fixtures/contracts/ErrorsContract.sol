196a952c05715bdba0fe03548ddd5640101e59e7
Nikos Iliakis <nikoulai94@hotmail.com>
2022-11-29 14:48:28 +0200

    Add updated contract and artifacts

--- fixtures/contracts/ErrorsContract.sol
+++ fixtures/contracts/ErrorsContract.sol
@@ -3,6 +3,7 @@
 pragma solidity ^0.8.13;
 
 error Unauthorized();
+error CustomError(string);
 
 contract ErrorsContract {
 	address payable owner = payable(0x0);
@@ -11,6 +12,12 @@ contract ErrorsContract {
 
 	function unauthorize() public {
 		if (msg.sender != owner) revert Unauthorized();
+
+		owner.transfer(address(this).balance);
+	}
+
+	function badRequire() public {
+		if (1 < 2) revert CustomError('reverted using custom Error');
 
 		owner.transfer(address(this).balance);
 	}
