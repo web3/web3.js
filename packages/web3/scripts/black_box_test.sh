9ec7298dbc8a927bd165eb550b81352e70175ded
Wyatt Barnes <me@wyatt.email>
2023-01-12 09:21:00 -1000

    Update black box script to allow TEST_TYPE and BROWSER variables

--- packages/web3/scripts/black_box_test.sh
+++ packages/web3/scripts/black_box_test.sh
@@ -5,22 +5,34 @@ ORIGARGS=("$@")
 . scripts/env.sh
 
 helpFunction() {
-	echo "Usage: $0 <ganache | geth | infura> <http | ws>"
+	echo "Usage: $0 <e2e> <ganache | geth | infura> <http | ws> <chrome | firefox | electron>"
 	exit 1 # Exit script after printing help
 }
 
-BACKEND=${ORIGARGS[0]}
-MODE=${ORIGARGS[1]}
-PROVIDER_URL=${ORIGARGS[2]}
+TEST_TYPE=${ORIGARGS[0]}
+BACKEND=${ORIGARGS[1]}
+MODE=${ORIGARGS[2]}
+PROVIDER_URL=${ORIGARGS[3]}
+BROWSER=${ORIGARGS[4]}
 
+SUPPORTED_TEST_TYPE=("" "e2e")
 SUPPORTED_BACKENDS=("ganache" "geth" "infura")
 SUPPORTED_MODE=("http" "ws")
+SUPPORTED_BROWSERS=("chrome" "firefox" "electron")
+
+if [[ ! " ${SUPPORTED_TEST_TYPE[*]} " =~ " ${TEST_TYPE} " ]]; then
+	helpFunction
+fi
 
 if [[ ! " ${SUPPORTED_BACKENDS[*]} " =~ " ${BACKEND} " ]]; then
 	helpFunction
 fi
 
 if [[ ! " ${SUPPORTED_MODE[*]} " =~ " ${MODE} " ]]; then
+	helpFunction
+fi
+
+if [[ ! " ${SUPPORTED_BROWSERS[*]} " =~ " ${BROWSER} " ]]; then
 	helpFunction
 fi
 
@@ -34,6 +46,13 @@ cd test/black_box
 yarn --update-checksums
 yarn
 
+TEST_STRING = "test"
+
+if [ $TEST_TYPE == "e2e"]
+then
+    TEST_STRING = "$TEST_STRING:e2e"
+fi
+
 if [[ ${BACKEND} == "infura" ]]
 then
     if [ ! $INFURA_HTTP ] || [ ! $INFURA_WSS ]
@@ -42,10 +61,25 @@ then
         exit 1
     elif [ $MODE == "http" ]
     then
-        WEB3_SYSTEM_TEST_PROVIDER=$INFURA_HTTP yarn "test:$BACKEND:$MODE"
+        if [ $TEST_TYPE == "e2e"]
+        then
+            WEB3_SYSTEM_TEST_PROVIDER=$INFURA_HTTP yarn "$TEST_STRING:$BACKEND:$MODE:$BROWSER"
+        else
+            WEB3_SYSTEM_TEST_PROVIDER=$INFURA_HTTP yarn "$TEST_STRING:$BACKEND:$MODE"
+        fi
     else
-        WEB3_SYSTEM_TEST_PROVIDER=$INFURA_WSS yarn "test:$BACKEND:$MODE"
+        if [ $TEST_TYPE == "e2e"]
+        then
+            WEB3_SYSTEM_TEST_PROVIDER=$INFURA_WSS yarn "$TEST_STRING:$BACKEND:$MODE:$BROWSER"
+        else
+            WEB3_SYSTEM_TEST_PROVIDER=$INFURA_WSS yarn "$TEST_STRING:$BACKEND:$MODE"
+        fi
     fi
 else
-    yarn "test:$BACKEND:$MODE"
+    if [ $TEST_TYPE == "e2e"]
+    then
+        yarn "$TEST_STRING:$BACKEND:$MODE:$BROWSER"
+    else
+        yarn "$TEST_STRING:$BACKEND:$MODE"
+    fi
 fi
