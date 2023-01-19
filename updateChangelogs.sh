4577f7b9230d600d7fca680c1dc613ff1fc4d427
Nikos Iliakis <nikoulai94@hotmail.com>
2023-01-19 20:18:50 +0200

    Fix formatting

--- updateChangelogs.sh
+++ updateChangelogs.sh
@@ -20,94 +20,96 @@ echo "" >>$changelog
 
 # Find all $($changelog) files in the packages/**/ folders
 for file in $(find packages -name $changelog -not -path "*node_modules*"); do
-
 	lines_count=$(awk '/\[Unreleased\]/,EOF' $file | awk 'NF' | wc -l)
 
-	# Check if there are any non-empty lines, meaning we have
+	# Check if there are any non-empty lines, meaning we have updates
 	if [ $lines_count -gt 1 ]; then
 		# echo "There are $lines_count non-empty lines under the [Unreleased] header in $package_name"
 
 		# Extract the package name from the file path
 		package_name=$(echo $file | awk -F '/' '{print $(NF-1)}')
-
 		sed -i "" "s/\[Unreleased\]/$header_name/g" $file
 
 		unreleased_changes=$(awk "/$header_name/,EOF" $file)
-
 		#     echo $unreleased_changes | wc -l
-		# Replace the [Unreleased] header with the new header name
+		# Replace the [Unreleased] header with the new header name, add at
 		unreleased_changes=${unreleased_changes//## $header_name/}
 
+		# echo $unreleased_changes
 		IFS=$'\n' read -d '' -r -a lines <<<"$unreleased_changes"
 		lines+=("EOF")
 
+		index=-1 # the position of the header in the `headers_of_updates` array (Cannot use key-value ds in v3 bash)
+		current_changes=""
 		for line in "${lines[@]}"; do
 
 			if [[ $line =~ ^\#\#\# || $line == EOF ]]; then
 				# Just found a header of updates, check if we have already found it in a previous file
 				# echo "Current changes for previous header $index"
-				# echo $current_changes
+				# echo $line
+				# echo 111111111
+				# echo "${headers_of_updates[@]}"
+				# echo 111111111
 
+				################
 				# If have found a previous header append the changes there (meaning: we found a new ### header, append to array all gathered ## headers)
 				if [[ $index -ge 0 ]]; then
-					# IFS=$'\n'
-					changes[$index]+=$current_changes
-					echo ${#changes[@]}
-					# changes[$index]+=""
-					# echo "********"
-					# echo $changes[$index]
-					# echo "********"
-				fi
-				index=-1 # the position of the header in the `headers_of_updates` array (Cannot use key-value ds in v3 bash)
-				current_changes=""
-				# Remove the `###` characters and trim whitespaces
+					echo "changes to add:"
+					echo $current_changes
+					echo "---------"
+					# for i in "${!changes[@]}"; do
+					# 	echo -e ${changes[i]}
+					# 	echo ***
+					# done
+					# echo ")))))"
+					# echo inserted ${line}
 
+					# IFS=$'\n'
+					# changes[$index]+=" #### $package_name \n"
+					# changes[$index]+=""
+					changes[$index]+=$current_changes
+					changes[$index]+=$"\n"
+					# changes[$index]+=$'\n'
+				# echo ${#changes[@]}
+				# changes[$index]+=""
+				# echo "********"
+				# echo $changes[$index]
+				fi
 				if [[ $line == EOF ]]; then
 					# echo "This is the end of the file"
 					break
 				fi
+				index=-1 # the position of the header in the `headers_of_updates` array (Cannot use key-value ds in v3 bash)
+				################
+
+				current_changes="\n#### $package_name \n"
+
 				#todo maybe revert this comment
 				# header=$(echo "$line" | sed 's/\#\#\#//g' | xargs)
 				IFS=$'\n'
 				header=$line
-				# header+=$'\n'
-				# header+=$'\n\n'
-				# echo -e $header
 
 				for i in "${!headers_of_updates[@]}"; do
+					# echo "Item already exists at position $index in the array"
 					if [[ "${headers_of_updates[i]}" == "$header" ]]; then
 						index=$i
 						break
 					fi
 				done
 				if ! [[ $index -ge 0 ]]; then
-					# echo "Item already exists at position $index in the array"
-					# else
-					# Add the item to the array
 					headers_of_updates+=("$header")
 					index=$(expr ${#headers_of_updates[@]} - 1)
-					# changes+=("")
-					# echo "Item added at position $index to the array"
 				fi
-				# echo "${headers_of_updates[@]}"
-				current_changes+="#### $package_name \n"
-				# current_changes+=$'\n'
-				# echo -e $current_changes
+			elif [[ $line == -* ]]; then # True if line starts with a -
+				current_changes+="\n$line"
 			else
-				if [[ $line == -* ]]; then # True if line starts with a -
-					# current_changes+=$'\n'
-					# echo "***"
-					# echo $line
-					# echo "***"
-					current_changes+="\n$line"
-				else
-					current_changes+=$line
-				fi
-
+				current_changes+=$line
 			fi
+
 		done
 
 		echo "" >>$file
+		# echo without new line
 		echo -ne "[Unreleased]" >>$file
 	fi
 
@@ -119,9 +121,11 @@ for i in "${!changes[@]}"; do
 	#   echo "Index: $i, Item: ${headers_of_updates[i]}"
 	#   echo "*****"
 	echo -e ${headers_of_updates[i]} >>$changelog
-	echo -e "" >>$changelog
+	# echo -e "" >>$changelog
 	echo -e ${changes[i]} >>$changelog
-	echo -e "" >>$changelog
+	# echo -e "" >>$changelog
 done
 
 echo "[Unreleased]" >>$changelog
+echo "${#changes[@]}"
+echo "${#headers_of_updates[@]}"
