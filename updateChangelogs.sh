2553973f2b002321a1d7400bee65c2abc39fabd3
Nikos Iliakis <nikoulai94@hotmail.com>
2023-01-19 17:35:17 +0200

    Check point, need some formatting

--- updateChangelogs.sh
+++ updateChangelogs.sh
@@ -4,91 +4,124 @@ header_name=$1
 
 # Check if the header name is provided
 if [ -z "$header_name" ]; then
-  echo "Please provide a header name as the first argument"
-  exit 1
+	echo "Please provide a header name as the first argument"
+	exit 1
 fi
 changelog="CHANGELOG.md"
 headers_of_updates=() #this will track which kinds of headers are met in changelog.mds (Added, fixed, etc...)
-# declare -A arr
-declare -A my_array
-
+changes=()
+# echo ${#changes[@]}
 
 # Append the new header name to the main ($changelog) file
 # echo "" >> $changelog
 # echo "$header_name" >> $changelog
-sed -i "" "s/\[Unreleased\]/$header_name/g" $changelog
-echo "" >> $changelog
+sed -i "" "s/\[Unreleased\]/"\[$header_name\]"/g" $changelog
+echo "" >>$changelog
 
 # Find all $($changelog) files in the packages/**/ folders
 for file in $(find packages -name $changelog -not -path "*node_modules*"); do
 
-    lines_count=$(awk '/\[Unreleased\]/,EOF' $file | awk 'NF' | wc -l)
-    # Check if there are any non-empty lines
+	lines_count=$(awk '/\[Unreleased\]/,EOF' $file | awk 'NF' | wc -l)
 
-    if [ $lines_count -gt 1 ]; then
-  	echo "There are $lines_count non-empty lines under the [Unreleased] header in $package_name"
+	# Check if there are any non-empty lines, meaning we have
+	if [ $lines_count -gt 1 ]; then
+		# echo "There are $lines_count non-empty lines under the [Unreleased] header in $package_name"
 
-    	# Extract the package name from the file path
-    	package_name=$(echo $file | awk -F '/' '{print $(NF-1)}')
+		# Extract the package name from the file path
+		package_name=$(echo $file | awk -F '/' '{print $(NF-1)}')
 
-	sed -i "" "s/\[Unreleased\]/$header_name/g" $file
+		sed -i "" "s/\[Unreleased\]/$header_name/g" $file
 
-	# Create new Unreleased header in changelog.md of package
-	# echo "" >> $file
-    	# echo "## [Unreleased]" >> $file
+		unreleased_changes=$(awk "/$header_name/,EOF" $file)
 
-	# echo "######"
-	# echo $lines_count
-	# echo "### $package_name" >> $changelog
-	# echo "" >> $changelog
+		#     echo $unreleased_changes | wc -l
+		# Replace the [Unreleased] header with the new header name
+		unreleased_changes=${unreleased_changes//## $header_name/}
 
+		IFS=$'\n' read -d '' -r -a lines <<<"$unreleased_changes"
+		lines+=("EOF")
 
-	# Extract the unreleased changes from the ($changelog) file
-	echo $header_name
-	# unreleased_changes=$(awk '/\[$header_name\]/,EOF' $file)
-	unreleased_changes=$(awk "/$header_name/,EOF" $file)
-	echo $unreleased_changes
-	#     echo $unreleased_changes | wc -l 
-	# Replace the [Unreleased] header with the new header name
-	unreleased_changes=${unreleased_changes//## $header_name}
-	echo "The unreleased changes after pattern delete $unreleased_changes"
-	echo '----'
-	IFS=$'\n' read -d '' -r -a lines <<< "$unreleased_changes"
-	# echo $lines
-	for line in "${lines[@]}"; do
-		echo $line
-		index=-1
-		if [[ $line =~ ^\#\#\# ]]; then
-        		# Remove the `###` characters and trim whitespaces
-        		header=$(echo "$line" | sed 's/\#\#\#//g' | xargs)
-			echo "%%%%%%"
-			echo $header
-			echo "%%%%%%"
-		fi
-	# 	if [[ $line =~ ^\#+ ]]; then
-	# 	header=$line
-	# 	#     echo "" >> changelog.md
-	# 	echo "$header" 
+		for line in "${lines[@]}"; do
 
-	# 	# else
-	# 	#     echo "  $line" >> changelog.md
-	# 	fi
-	done
+			if [[ $line =~ ^\#\#\# || $line == EOF ]]; then
+				# Just found a header of updates, check if we have already found it in a previous file
+				# echo "Current changes for previous header $index"
+				# echo $current_changes
 
-	# exit
+				# If have found a previous header append the changes there (meaning: we found a new ### header, append to array all gathered ## headers)
+				if [[ $index -ge 0 ]]; then
+					# IFS=$'\n'
+					changes[$index]+=$current_changes
+					echo ${#changes[@]}
+					# changes[$index]+=""
+					# echo "********"
+					# echo $changes[$index]
+					# echo "********"
+				fi
+				index=-1 # the position of the header in the `headers_of_updates` array (Cannot use key-value ds in v3 bash)
+				current_changes=""
+				# Remove the `###` characters and trim whitespaces
 
-	# Append the modified unreleased changes to the main ($changelog)s file
-	echo "$unreleased_changes" >> $changelog
-	echo "" >> $changelog
+				if [[ $line == EOF ]]; then
+					# echo "This is the end of the file"
+					break
+				fi
+				#todo maybe revert this comment
+				# header=$(echo "$line" | sed 's/\#\#\#//g' | xargs)
+				IFS=$'\n'
+				header=$line
+				# header+=$'\n'
+				# header+=$'\n\n'
+				# echo -e $header
 
-#     else
-  	# echo "There are no non-empty lines under the [Unreleased] header"
-    fi
+				for i in "${!headers_of_updates[@]}"; do
+					if [[ "${headers_of_updates[i]}" == "$header" ]]; then
+						index=$i
+						break
+					fi
+				done
+				if ! [[ $index -ge 0 ]]; then
+					# echo "Item already exists at position $index in the array"
+					# else
+					# Add the item to the array
+					headers_of_updates+=("$header")
+					index=$(expr ${#headers_of_updates[@]} - 1)
+					# changes+=("")
+					# echo "Item added at position $index to the array"
+				fi
+				# echo "${headers_of_updates[@]}"
+				current_changes+="#### $package_name \n"
+				# current_changes+=$'\n'
+				# echo -e $current_changes
+			else
+				if [[ $line == -* ]]; then # True if line starts with a -
+					# current_changes+=$'\n'
+					# echo "***"
+					# echo $line
+					# echo "***"
+					current_changes+="\n$line"
+				else
+					current_changes+=$line
+				fi
+
+			fi
+		done
+
+		echo "" >>$file
+		echo -ne "[Unreleased]" >>$file
+	fi
 
 done
-declare -a my_array
+# echo "$unreleased_changes" >> $changelog
+# echo "" >> $changelog
+for i in "${!changes[@]}"; do
+	#   echo "*****"
+	#   echo "Index: $i, Item: ${headers_of_updates[i]}"
+	#   echo "*****"
+	echo -e ${headers_of_updates[i]} >>$changelog
+	echo -e "" >>$changelog
+	echo -e ${changes[i]} >>$changelog
+	echo -e "" >>$changelog
+done
 
-echo "changelogs have been appended to $changelog"
-
-
-
+echo "[Unreleased]" >>$changelog
