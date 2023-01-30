6eb71e74f635207fa56df0020a5b6ddf03dbd2d4
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-01-30 12:26:19 -0500

    revert

--- packages/web3/src/types.ts
+++ packages/web3/src/types.ts
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Address, Bytes, EthExecutionAPI, SupportedProviders, Transaction } from 'web3-types';
+import { EthExecutionAPI, SupportedProviders, Address, Bytes, Transaction } from 'web3-types';
 import Eth from 'web3-eth';
 import {
 	ContractAbi,
