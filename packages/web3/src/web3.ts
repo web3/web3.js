d26bbf6ab806824185e0c173a6671e708bf3958e
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-01-30 12:21:30 -0500

    fix a problem

--- packages/web3/src/web3.ts
+++ packages/web3/src/web3.ts
@@ -16,7 +16,7 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 // eslint-disable-next-line max-classes-per-file
 import { Web3Context } from 'web3-core';
-import Web3Eth from 'web3-eth';
+import Web3Eth, { registeredSubscriptions } from 'web3-eth';
 import { ContractAbi } from 'web3-eth-abi';
 import Contract, { ContractInitOptions } from 'web3-eth-contract';
 import { ENS, registryAddresses } from 'web3-eth-ens';
@@ -47,7 +47,7 @@ export class Web3 extends Web3Context<EthExecutionAPI> {
 	public eth: Web3EthInterface;
 
 	public constructor(provider?: SupportedProviders<EthExecutionAPI> | string) {
-		super({ provider });
+		super({ provider, registeredSubscriptions });
 
 		if (isNullish(provider) || (typeof provider === 'string' && provider.trim() === '')) {
 			console.warn(
