c8e1dcf2bf1d33f506ae2509146b4555d7228ca8
Nikos Iliakis <nikoulai94@hotmail.com>
2023-01-31 14:50:16 +0200

    Revert "Remove setTimeouts from helper functions"
    This reverts commit 1fc1059be0e7439a1de575127a995a4032c706b9.

--- packages/web3-eth/test/integration/helper.ts
+++ packages/web3-eth/test/integration/helper.ts
@@ -39,6 +39,12 @@ export const sendFewTxes = async ({
 }: SendFewTxParams): Promise<TransactionReceipt[]> => {
 	const res: TransactionReceipt[] = [];
 	for (let i = 0; i < times; i += 1) {
+		// @TODO: Investigate why we need timeout here #5730
+		// eslint-disable-next-line no-await-in-loop
+		await new Promise<void>(resolve => {
+			setTimeout(resolve, 500);
+		});
+
 		const tx: Web3PromiEvent<
 			TransactionReceipt,
 			SendTransactionEvents<typeof DEFAULT_RETURN_FORMAT>
@@ -80,8 +86,13 @@ export const sendFewTxesWithoutReceipt = async ({
 		TransactionReceipt,
 		SendTransactionEvents<typeof DEFAULT_RETURN_FORMAT>
 	>[] = [];
-
 	for (let i = 0; i < times; i += 1) {
+		// @TODO: Investigate why we need timeout here #5730
+		// eslint-disable-next-line no-await-in-loop
+		await new Promise<void>(resolve => {
+			setTimeout(resolve, 500);
+		});
+
 		res.push(
 			web3Eth.sendTransaction({
 				to,
