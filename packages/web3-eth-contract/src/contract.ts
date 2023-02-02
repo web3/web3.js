f7647da3701649b61e012ca96c1a87911b2feb24
Nikos Iliakis <nikoulai94@hotmail.com>
2023-02-02 23:04:27 +0200

    Remove unused import

--- packages/web3-eth-contract/src/contract.ts
+++ packages/web3-eth-contract/src/contract.ts
@@ -15,7 +15,6 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { EventEmitter } from 'events';
 import { Web3Context, Web3EventEmitter, Web3PromiEvent, Web3ConfigEvent } from 'web3-core';
 import { ContractExecutionError, SubscriptionError, Web3ContractError } from 'web3-errors';
 import {
