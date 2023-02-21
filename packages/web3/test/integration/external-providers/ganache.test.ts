f859e12ca8d1377edf9664ab4b954619118acffd
Alex <alex.luu@mail.utoronto.ca>
2023-02-21 13:17:42 -0500

    add ganache tests

--- packages/web3/test/integration/external-providers/ganache.test.ts
+++ packages/web3/test/integration/external-providers/ganache.test.ts
@@ -16,20 +16,56 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 // eslint-disable-next-line import/no-extraneous-dependencies
-import ganache from 'ganache';
+import ganache, {Server} from 'ganache';
+import WebSocketProvider from 'web3-providers-ws';
 
 import { performBasicRpcCalls } from './helper';
 
 import { getSystemTestMnemonic } from '../../shared_fixtures/system_tests_utils';
 
-describe('compatibility with `ganache` provider', () => {
-	it('should initialize Web3, get accounts & block number and send a transaction', async () => {
-		const { provider } = ganache.server({
-			wallet: {
-				mnemonic: getSystemTestMnemonic(),
-			},
+describe ('ganache tests', () => {
+	describe('compatibility with `ganache` provider', () => {
+		it('should initialize Web3, get accounts & block number and send a transaction', async () => {
+			const { provider } = ganache.server({
+				wallet: {
+					mnemonic: getSystemTestMnemonic(),
+				},
+			});
+
+			await performBasicRpcCalls(provider);
+		});
+	});
+
+	
+	describe ('WebSocketProvider - ganache', () => {
+		let server: Server;
+
+		afterEach(async () => {
+			try {
+				await server.close();
+			} catch (error) {
+				console.log(error)
+			}
 		});
 
-		await performBasicRpcCalls(provider);
+		// const ganacheOptions = { Chain: {hardfork: 'muirGlacier'}, server: { ws: true } };
+
+		const port = 7545;
+		const host = `ws://localhost:${port}`;
+			server = ganache.server();
+			await server.listen(port);
+			// test connection to server
+			const webSocketProvider = new WebSocketProvider(host);
+			expect(webSocketProvider).toBeDefined();
+
+		});
+
+		it('"error" handler fires if the client closes unilaterally', async () => {
+
+			server = ganache.server();
+			await server.listen(port);
+			const webSocketProvider = new WebSocketProvider(host);
+			expect(webSocketProvider).toBeDefined();
+
+		});
 	});
-});
