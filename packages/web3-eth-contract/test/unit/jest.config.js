6df64c1fa9c02dd31cd18475ec364c3cb2682867
Junaid <86780488+jdevcs@users.noreply.github.com>
2023-02-28 13:43:10 +0100

    5747 codecov (#5843)
    * code cov integration for unit tests
    
    * removed cov threshold
    
    * code cov token
    
    * by default collect coverage
    
    * codecov with existing unittests
    
    * jest config
    
    * only analyze coverage for n v16 unit test runs
    
    * node ver
    
    * codecov paths config
    
    * added layout
    
    * common config
    
    * flag names in ci
    
    * flags under proj
    
    * min CI for test
    
    * build and test for u14 and 16
    
    * validated yml
    
    * components usage
    
    * comment moved at end
    
    * updated comments
    
    * testing 90% cov target
    
    * component cov target
    
    * patch cov
    
    * enable actions + remove old coverage check actions
    
    * codecov config change
    
    * remove unused scripts

--- packages/web3-eth-contract/test/unit/jest.config.js
+++ packages/web3-eth-contract/test/unit/jest.config.js
@@ -4,6 +4,15 @@ module.exports = {
 	...base,
 	testMatch: ['<rootDir>/test/unit/**/*.(spec|test).(js|ts)'],
 
-	coverageDirectory: '.coverage/unit',
+	coverageDirectory: '../../.coverage/unit',
 	collectCoverageFrom: ['src/**'],
+	collectCoverage: true,
+	coverageReporters: [
+		[
+			'json',
+			{
+				file: 'web3-eth-contract-unit-coverage.json',
+			},
+		],
+	],
 };
