9e8ad71262cda3ad255e62e7de8b8060acb98b19
Alex <alex.luu@mail.utoronto.ca>
2023-02-28 14:15:16 -0500

    add await

--- packages/web3-eth/test/integration/helper.ts
+++ packages/web3-eth/test/integration/helper.ts
@@ -38,35 +38,13 @@ export const sendFewTxes = async ({
 }: SendFewTxParams): Promise<TransactionReceipt[]> => {
 	const res: TransactionReceipt[] = [];
 	for (let i = 0; i < times; i += 1) {
-		// @TODO: Investigate why we need timeout here #5730
-		// eslint-disable-next-line no-await-in-loop
-		await new Promise<void>(resolve => {
-			setTimeout(resolve, 500);
-		});
-
-		const tx: Web3PromiEvent<
-			TransactionReceipt,
-			SendTransactionEvents<typeof DEFAULT_RETURN_FORMAT>
-			// eslint-disable-next-line no-await-in-loop
-		> = await web3Eth.sendTransaction({
-			to,
-			value,
-			from,
-		});
 		res.push(
 			// eslint-disable-next-line no-await-in-loop
-			(await new Promise((resolve: Resolve, reject) => {
-				// tx promise is handled separately
-				// eslint-disable-next-line no-void
-				void tx.on('receipt', (params: TransactionReceipt) => {
-					expect(params.status).toBe(BigInt(1));
-					resolve(params);
-				});
-				// eslint-disable-next-line no-void
-				void tx.on('error', (error: any) => {
-					reject(error);
-				});
-			})) as TransactionReceipt,
+			await web3Eth.sendTransaction({
+				to,
+				value,
+				from,
+			}),
 		);
 	}
 
