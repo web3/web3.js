165164b031089b9b41c8c1f37d533a75ada41b7e
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-03-02 19:03:43 +0100

    fix a class import at test

--- packages/web3-providers-ipc/test/unit/ipc_provider.test.ts
+++ packages/web3-providers-ipc/test/unit/ipc_provider.test.ts
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { Socket } from 'dgram';
+import { Socket } from 'net';
 import * as fs from 'fs';
 import { ConnectionError, InvalidClientError } from 'web3-errors';
 import IpcProvider from '../../src/index';
