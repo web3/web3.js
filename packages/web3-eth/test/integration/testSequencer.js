cc7d5f9a22603353bee425e2a0437111228f0179
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-03-07 23:01:32 -0500

    order

--- packages/web3-eth/test/integration/testSequencer.js
+++ packages/web3-eth/test/integration/testSequencer.js
@@ -1,8 +1,31 @@
-const Sequencer = require('@jest/test-sequencer').default;
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/* eslint-disable @typescript-eslint/explicit-member-accessibility */
+/* eslint-disable  @typescript-eslint/no-var-requires */
+
+// eslint-disable-next-line @typescript-eslint/no-require-imports
+const { default: Sequencer } = require('@jest/test-sequencer');
 
 class CustomSequencer extends Sequencer {
+	// eslint-disable-next-line class-methods-use-this
 	sort(tests) {
 		const copyTests = Array.from(tests);
+		// eslint-disable-next-line @typescript-eslint/no-unsafe-return
 		return copyTests.sort((testA, testB) => (testA.path > testB.path ? 1 : -1));
 	}
 }
