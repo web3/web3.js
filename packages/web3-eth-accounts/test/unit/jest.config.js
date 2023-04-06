99ac66d9a7f905f1d43bb021dc965fddf2e29660
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-04-06 12:11:41 -0400

    fix json error

--- packages/web3-eth-accounts/test/unit/jest.config.js
+++ packages/web3-eth-accounts/test/unit/jest.config.js
@@ -5,7 +5,7 @@ module.exports = {
 	testMatch: ['<rootDir>/test/unit/**/*.(spec|test).(js|ts)'],
 
 	coverageDirectory: '../../.coverage/unit',
-	collectCoverageFrom: ['src/**'],
+	collectCoverageFrom: ['src/**/*.ts'],
 	collectCoverage: true,
 	coverageReporters: [
 		[
