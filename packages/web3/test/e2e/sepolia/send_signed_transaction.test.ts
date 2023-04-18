bec53aa90f5d8bafaa3c42cddac8755b7ae90930
Wyatt Barnes <me@wyatt.email>
2023-04-17 15:50:56 -1000

    Init remaining typed sendSignedTransaction tests

--- packages/web3/test/e2e/sepolia/send_signed_transaction.test.ts
+++ packages/web3/test/e2e/sepolia/send_signed_transaction.test.ts
@@ -27,7 +27,7 @@ import {
 	getSystemE2ETestProvider,
 } from '../e2e_utils';
 
-describe(`${getSystemTestBackend()} tests - getCode`, () => {
+describe(`${getSystemTestBackend()} tests - sendSignedTransaction`, () => {
 	const provider = getSystemE2ETestProvider();
 
 	let web3: Web3;
@@ -40,40 +40,125 @@ describe(`${getSystemTestBackend()} tests - getCode`, () => {
 		await closeOpenConnection(web3);
 	});
 
-	itIf(getAllowedSendTransaction())('should send a simple value transfer', async () => {
-		const transaction: Transaction = {
-			from: getE2ETestAccountAddress(),
-			to: getE2ETestAccountAddress(),
-			value: 1,
-			gas: 21000,
-		};
-		const signedTransaction = await web3.eth.accounts.signTransaction(
-			transaction,
-			getE2ETestAccountPrivateKey(),
-		);
-		const result = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+	itIf(getAllowedSendTransaction())(
+		'should send a simple value transfer - type 0x0',
+		async () => {
+			const transaction: Transaction = {
+				from: getE2ETestAccountAddress(),
+				to: getE2ETestAccountAddress(),
+				value: 1,
+				gas: 21000,
+				type: 0,
+			};
+			const signedTransaction = await web3.eth.accounts.signTransaction(
+				transaction,
+				getE2ETestAccountPrivateKey(),
+			);
+			const result = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
 
-		// TODO This should work, but throws a type error
-		// for root not being included in expected object.
-		// However, root is not apart of result object.
-		// expect(result).toMatchObject<TransactionReceipt>({
-		// eslint-disable-next-line jest/no-standalone-expect
-		expect(result).toMatchObject({
-			// root: expect.any(String),
-			blockHash: expect.any(String),
-			blockNumber: expect.any(BigInt),
-			cumulativeGasUsed: expect.any(BigInt),
-			effectiveGasPrice: expect.any(BigInt),
-			from: getE2ETestAccountAddress().toLowerCase(),
-			gasUsed: BigInt(21000),
-			logs: [],
-			logsBloom:
-				'0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
-			status: BigInt(1),
-			to: getE2ETestAccountAddress().toLowerCase(),
-			transactionHash: expect.any(String),
-			transactionIndex: expect.any(BigInt),
-			type: BigInt(2),
-		});
-	});
+			// TODO This should work, but throws a type error
+			// for root not being included in expected object.
+			// However, root is not apart of result object.
+			// expect(result).toMatchObject<TransactionReceipt>({
+			// eslint-disable-next-line jest/no-standalone-expect
+			expect(result).toMatchObject({
+				// root: expect.any(String),
+				blockHash: expect.any(String),
+				blockNumber: expect.any(BigInt),
+				cumulativeGasUsed: expect.any(BigInt),
+				effectiveGasPrice: expect.any(BigInt),
+				from: getE2ETestAccountAddress().toLowerCase(),
+				gasUsed: BigInt(21000),
+				logs: [],
+				logsBloom:
+					'0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+				status: BigInt(1),
+				to: getE2ETestAccountAddress().toLowerCase(),
+				transactionHash: expect.any(String),
+				transactionIndex: expect.any(BigInt),
+				type: BigInt(0),
+			});
+		},
+	);
+
+	itIf(getAllowedSendTransaction())(
+		'should send a simple value transfer - type 0x1',
+		async () => {
+			const transaction: Transaction = {
+				from: getE2ETestAccountAddress(),
+				to: getE2ETestAccountAddress(),
+				value: 1,
+				gas: 21000,
+				type: 1,
+			};
+			const signedTransaction = await web3.eth.accounts.signTransaction(
+				transaction,
+				getE2ETestAccountPrivateKey(),
+			);
+			const result = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+
+			// TODO This should work, but throws a type error
+			// for root not being included in expected object.
+			// However, root is not apart of result object.
+			// expect(result).toMatchObject<TransactionReceipt>({
+			// eslint-disable-next-line jest/no-standalone-expect
+			expect(result).toMatchObject({
+				// root: expect.any(String),
+				blockHash: expect.any(String),
+				blockNumber: expect.any(BigInt),
+				cumulativeGasUsed: expect.any(BigInt),
+				effectiveGasPrice: expect.any(BigInt),
+				from: getE2ETestAccountAddress().toLowerCase(),
+				gasUsed: BigInt(21000),
+				logs: [],
+				logsBloom:
+					'0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+				status: BigInt(1),
+				to: getE2ETestAccountAddress().toLowerCase(),
+				transactionHash: expect.any(String),
+				transactionIndex: expect.any(BigInt),
+				type: BigInt(1),
+			});
+		},
+	);
+
+	itIf(getAllowedSendTransaction())(
+		'should send a simple value transfer - type 0x2',
+		async () => {
+			const transaction: Transaction = {
+				from: getE2ETestAccountAddress(),
+				to: getE2ETestAccountAddress(),
+				value: 1,
+				gas: 21000,
+			};
+			const signedTransaction = await web3.eth.accounts.signTransaction(
+				transaction,
+				getE2ETestAccountPrivateKey(),
+			);
+			const result = await web3.eth.sendSignedTransaction(signedTransaction.rawTransaction);
+
+			// TODO This should work, but throws a type error
+			// for root not being included in expected object.
+			// However, root is not apart of result object.
+			// expect(result).toMatchObject<TransactionReceipt>({
+			// eslint-disable-next-line jest/no-standalone-expect
+			expect(result).toMatchObject({
+				// root: expect.any(String),
+				blockHash: expect.any(String),
+				blockNumber: expect.any(BigInt),
+				cumulativeGasUsed: expect.any(BigInt),
+				effectiveGasPrice: expect.any(BigInt),
+				from: getE2ETestAccountAddress().toLowerCase(),
+				gasUsed: BigInt(21000),
+				logs: [],
+				logsBloom:
+					'0x00000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000',
+				status: BigInt(1),
+				to: getE2ETestAccountAddress().toLowerCase(),
+				transactionHash: expect.any(String),
+				transactionIndex: expect.any(BigInt),
+				type: BigInt(2),
+			});
+		},
+	);
 });
