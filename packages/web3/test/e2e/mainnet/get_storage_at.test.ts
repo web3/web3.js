be06d3e2be985717b90d7484f33ae9074e582058
Wyatt Barnes <me@wyatt.email>
2023-04-20 06:15:54 -1000

    Init sepolia and mainnet testing (#5990)
    * Init sepolia and mainnet testing
    
    Signed-off-by: Wyatt Barnes <me@wyatt.email>
    
    * Refactor secrets template
    
    Signed-off-by: Wyatt Barnes <me@wyatt.email>
    
    * Init getAccounts and getBlockNumber tests
    
    Signed-off-by: Wyatt Barnes <me@wyatt.email>
    
    * Init sepolia call test
    
    Signed-off-by: Wyatt Barnes <me@wyatt.email>
    
    * Init init script to create .secrets.json on prebuild
    
    * Remove getSystemE2ETestProvider from shared utils file, moved into standalone file
    
    * Replace e2e_utils
    
    * Init more tests
    
    * Add getAllowedSendTransaction e2e util
    
    * Init more tests
    
    * Add TODO to get uncle test
    
    * Add ALLOWED_SEND_TRANSACTION to secrets template
    
    * Finish adding Infura RPC tests
    
    * Update get_storage_at to handle Nethermind vs Geth
    
    * Update test to handle mainnet vs sepolia
    
    * Remove getDeployedStorageContractAddress util func
    
    * Init mainnet specific tests
    
    * Init contract sepolia tests
    
    * Init remaining typed sendSignedTransaction tests
    
    * Update imports
    
    * Init e2e test block fixtures
    
    * Update imports
    
    * Update imports
    
    * Add E2E test fixtures
    
    * Update tests to test for specific data for blockHash and blockNumber
    
    * Init e2e_network_test Github workflow
    
    * Testing GH workflow
    
    * Testing GH workflow
    
    * GH workflow syntax fix
    
    * GH workflow syntax fix
    
    * Update sepolia block fixture
    
    * Update address in GH workflow for mainnet
    
    * Fix test values for mainnet
    
    * Add TEST_ACCOUNT_PRIVATE_KEY to GH workflow
    
    * Change sepolia contract test to only load private key if allowed send transaction is true
    
    * GH workflow syntax fix
    
    * Remove chainId from expected properties for getTransactionFromBlock tests
    
    * Testing GH workflow bool fix
    
    * Replace hardcoded ALLOWED_SEND_TRANSACTION with secrets ENV. Revert test change so tests only run on release branch for 4.x
    
    * Update default address for mainnet in secrets template
    
    * Update RELEASE.md to include info about E2E testing
    
    ---------
    
    Signed-off-by: Wyatt Barnes <me@wyatt.email>
    Co-authored-by: Junaid <86780488+jdevcs@users.noreply.github.com>

---
+++ packages/web3/test/e2e/mainnet/get_storage_at.test.ts
@@ -0,0 +1,82 @@
+/*
+This file is part of web3.js.
+
+web3.js is free software: you can redistribute it and/or modify
+it under the terms of the GNU Lesser General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+web3.js is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU Lesser General Public License for more details.
+
+You should have received a copy of the GNU Lesser General Public License
+along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
+*/
+import Web3, { Numbers } from '../../../src';
+import { getSystemE2ETestProvider } from '../e2e_utils';
+import {
+	closeOpenConnection,
+	getSystemTestBackend,
+} from '../../shared_fixtures/system_tests_utils';
+import { toAllVariants } from '../../shared_fixtures/utils';
+import { mainnetBlockData } from '../fixtures/mainnet';
+
+describe(`${getSystemTestBackend()} tests - getStorageAt`, () => {
+	const provider = getSystemE2ETestProvider();
+
+	let web3: Web3;
+
+	beforeAll(() => {
+		web3 = new Web3(provider);
+	});
+
+	afterAll(async () => {
+		await closeOpenConnection(web3);
+	});
+
+	it.each(
+		toAllVariants<{
+			storageSlot: Numbers;
+			block:
+				| 'earliest'
+				| 'latest'
+				| 'pending'
+				| 'finalized'
+				| 'safe'
+				| 'blockHash'
+				| 'blockNumber';
+		}>({
+			storageSlot: ['0x1', '1', 1, BigInt(1)],
+			block: [
+				'earliest',
+				'latest',
+				'pending',
+				'safe',
+				'finalized',
+				'blockHash',
+				'blockNumber',
+			],
+		}),
+	)('getStorageAt', async ({ storageSlot, block }) => {
+		const result = await web3.eth.getStorageAt(
+			'0xdAC17F958D2ee523a2206206994597C13D831ec7',
+			storageSlot,
+			mainnetBlockData[block],
+		);
+
+		if (mainnetBlockData[block] === 'earliest') {
+			// eslint-disable-next-line jest/no-conditional-expect
+			expect(result).toMatch(/0[xX][0-9a-fA-F]{64}/i);
+		} else if (block === 'blockHash' || block === 'blockNumber') {
+			// eslint-disable-next-line jest/no-conditional-expect
+			expect(result).toBe(
+				'0x000000000000000000000000000000000000000000000000007d5a864f06b2d5',
+			);
+		} else {
+			// eslint-disable-next-line jest/no-conditional-expect
+			expect(result).toMatch(/0[xX][0-9a-fA-F]{64}/i);
+		}
+	});
+});
