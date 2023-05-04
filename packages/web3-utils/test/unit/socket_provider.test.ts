a34601e0e51a16dbb4508103072e41e6d0d47af6
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-05-04 19:32:21 +0200

    add more tests for SocketProvider

--- packages/web3-utils/test/unit/socket_provider.test.ts
+++ packages/web3-utils/test/unit/socket_provider.test.ts
@@ -15,6 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
+import { EventEmitter } from 'stream';
 import { Web3APIPayload, EthExecutionAPI, JsonRpcResponse, Web3ProviderStatus } from 'web3-types';
 // eslint-disable-next-line import/no-relative-packages
 import { sleep } from '../../../../fixtures/utils';
@@ -66,86 +67,165 @@ describe('SocketProvider', () => {
 	const socketPath = `some_path`;
 	const socketOption = { dummyOption: true } as const;
 
-	describe('constructor', () => {
-		it('should construct the instance of the provider', () => {
-			const provider = new TestProvider(socketPath, socketOption);
-			expect(provider).toBeInstanceOf(SocketProvider);
-			expect(provider.SocketConnection).toEqual(dummySocketConnection);
-		});
-	});
 	describe('socket_provider unit tests', () => {
-		it('should not call method reconnect', () => {
-			const reconnectOptions = { autoReconnect: false };
-			const provider = new TestProvider(socketPath, socketOption, reconnectOptions);
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_reconnect').mockReturnValue('');
-			provider.message('');
-			// @ts-expect-error run protected method
-			expect(provider._reconnect).not.toHaveBeenCalled();
-		});
-		it('should call method reconnect', () => {
-			const reconnectOptions = { autoReconnect: true };
-			const provider = new TestProvider(socketPath, socketOption, reconnectOptions);
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_reconnect').mockReturnValue('');
-			provider.message('');
-			// @ts-expect-error run protected method
-			expect(provider._reconnect).toHaveBeenCalled();
-		});
-		it('should call method reconnect in case of error at _openSocketConnection', async () => {
-			const provider = new TestProvider(socketPath, socketOption);
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_openSocketConnection').mockImplementation(() => {
-				throw new Error();
+		describe('constructor', () => {
+			it('should construct the instance of the provider', () => {
+				const provider = new TestProvider(socketPath, socketOption);
+				expect(provider).toBeInstanceOf(SocketProvider);
+				expect(provider.SocketConnection).toEqual(dummySocketConnection);
 			});
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_reconnect').mockReturnValue('');
-			// @ts-expect-error run protected method
-			provider.isReconnecting = true;
-			provider.connect();
-
-			await sleep(100);
-
-			// @ts-expect-error run protected method
-			expect(provider._reconnect).toHaveBeenCalled();
 		});
-		it('should call method reconnect in case of error at _addSocketListeners', async () => {
-			const provider = new TestProvider(socketPath, socketOption);
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_addSocketListeners').mockImplementation(() => {
-				throw new Error();
+		describe('testing _reconnect() method', () => {
+			it('should not be called when { autoReconnect: false }', () => {
+				const reconnectOptions = { autoReconnect: false };
+				const provider = new TestProvider(socketPath, socketOption, reconnectOptions);
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_reconnect').mockReturnValue('');
+				provider.message('');
+				// @ts-expect-error run protected method
+				expect(provider._reconnect).not.toHaveBeenCalled();
 			});
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_reconnect').mockReturnValue('');
-			// @ts-expect-error run protected method
-			provider.isReconnecting = true;
-			provider.connect();
-
-			await sleep(100);
-
-			// @ts-expect-error run protected method
-			expect(provider._reconnect).toHaveBeenCalled();
+			it('should be called when { autoReconnect: true }', () => {
+				const reconnectOptions = { autoReconnect: true };
+				const provider = new TestProvider(socketPath, socketOption, reconnectOptions);
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_reconnect').mockReturnValue('');
+				provider.message('');
+				// @ts-expect-error run protected method
+				expect(provider._reconnect).toHaveBeenCalled();
+			});
 		});
-		it('should throw "Error while connecting..." in case of error inside `connect()`', () => {
-			const dummyError = new Error('error');
-			const reconnectOptions = { autoReconnect: true };
-			const provider = new TestProvider(socketPath, socketOption, reconnectOptions);
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_addSocketListeners').mockImplementation(() => {
-				throw dummyError;
+
+		describe('testing connect() method', () => {
+			it('should call method reconnect in case of error at _openSocketConnection', async () => {
+				const provider = new TestProvider(socketPath, socketOption);
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_openSocketConnection').mockImplementation(() => {
+					throw new Error();
+				});
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_reconnect').mockReturnValue('');
+				// @ts-expect-error run protected method
+				provider.isReconnecting = true;
+				provider.connect();
+
+				await sleep(100);
+
+				// @ts-expect-error run protected method
+				expect(provider._reconnect).toHaveBeenCalled();
 			});
-			expect(() => provider.connect()).toThrow(
-				`Error while connecting to ${socketPath}. Reason: ${dummyError.message}`,
-			);
+			it('should call method reconnect in case of error at _addSocketListeners', async () => {
+				const provider = new TestProvider(socketPath, socketOption);
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_addSocketListeners').mockImplementation(() => {
+					throw new Error();
+				});
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_reconnect').mockReturnValue('');
+				// @ts-expect-error run protected method
+				provider.isReconnecting = true;
+				provider.connect();
+
+				await sleep(100);
+
+				// @ts-expect-error run protected method
+				expect(provider._reconnect).toHaveBeenCalled();
+			});
+			it('should throw "Error while connecting..." in case of error inside `connect()`', () => {
+				const dummyError = new Error('error');
+				const provider = new TestProvider(socketPath, socketOption);
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_addSocketListeners').mockImplementation(() => {
+					throw dummyError;
+				});
+				expect(() => provider.connect()).toThrow(
+					`Error while connecting to ${socketPath}. Reason: ${dummyError.message}`,
+				);
+			});
+			it('should throw "Client URL ... is invalid" in case of error with no message inside `connect()`', () => {
+				const provider = new TestProvider(socketPath, socketOption);
+				// @ts-expect-error run protected method
+				jest.spyOn(provider, '_addSocketListeners').mockImplementation(() => {
+					throw new Error();
+				});
+				expect(() => provider.connect()).toThrow(`Client URL "${socketPath}" is invalid.`);
+			});
 		});
-		it('should throw "Client URL ... is invalid" in case of error with no message inside `connect()`', () => {
-			const reconnectOptions = { autoReconnect: true };
-			const provider = new TestProvider(socketPath, socketOption, reconnectOptions);
-			// @ts-expect-error run protected method
-			jest.spyOn(provider, '_addSocketListeners').mockImplementation(() => {
-				throw new Error();
+
+		describe('testing supportsSubscriptions() function', () => {
+			it('should returns false when calling `supportsSubscriptions()`', async () => {
+				const provider = new TestProvider(socketPath, socketOption);
+				expect(provider.supportsSubscriptions()).toBe(true);
 			});
-			expect(() => provider.connect()).toThrow(`Client URL "${socketPath}" is invalid.`);
+		});
+
+		describe('testing on() method', () => {
+			it('should internally call `_eventEmitter.on`', async () => {
+				const provider = new TestProvider(socketPath, socketOption);
+
+				// @ts-expect-error run protected method
+				const funcBSpy = jest.spyOn(provider._eventEmitter, 'on').mockReturnValue();
+				const event = 'message';
+				const func = () => {
+					// ...
+				};
+				provider.on(event, func);
+				expect(funcBSpy).toHaveBeenCalledWith(event, func);
+			});
+		});
+
+		describe('testing once() method', () => {
+			it('should internally call `_eventEmitter.once`', async () => {
+				const provider = new TestProvider(socketPath, socketOption);
+
+				// @ts-expect-error run protected method
+				const funcBSpy = jest.spyOn(provider._eventEmitter, 'once').mockReturnValue();
+				const event = 'message';
+				const func = () => {
+					// ...
+				};
+				provider.once(event, func);
+				expect(funcBSpy).toHaveBeenCalledWith(event, func);
+			});
+		});
+
+		describe('testing removeListener() method', () => {
+			it('should internally call `_eventEmitter.removeListener`', async () => {
+				const provider = new TestProvider(socketPath, socketOption);
+
+				const funcBSpy = jest
+					// @ts-expect-error run protected method
+					.spyOn(provider._eventEmitter, 'removeListener')
+					.mockReturnValue(new EventEmitter());
+				const event = 'message';
+				const func = () => {
+					// ...
+				};
+				provider.removeListener(event, func);
+				expect(funcBSpy).toHaveBeenCalledWith(event, func);
+			});
+		});
+
+		describe('testing disconnect() method', () => {
+			it('should internally call `super._onDisconnect` and change the connectionStatus to `disconnected`', async () => {
+				const provider = new TestProvider(socketPath, socketOption);
+
+				const funcBSpy = jest
+					// spy on provider.super._onDisconnect
+					.spyOn(
+						Object.getPrototypeOf(
+							Object.getPrototypeOf(Object.getPrototypeOf(provider)),
+						),
+						'_onDisconnect',
+					)
+					.mockReturnValue(new EventEmitter());
+				const code = 0;
+				const data = '0x0';
+				provider.disconnect(code, data);
+				// @ts-expect-error run protected method
+				expect(provider._connectionStatus).toBe('disconnected');
+				expect(funcBSpy).toHaveBeenCalledWith(code, data);
+			});
 		});
 	});
 });
