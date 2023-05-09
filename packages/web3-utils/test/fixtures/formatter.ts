3b918bbb5d25572db069bf9bc91b8622e6448973
luu-alex <alex.luu@mail.utoronto.ca>
2023-05-09 15:16:04 -0400

    update formatter unit tests

--- packages/web3-utils/test/fixtures/formatter.ts
+++ packages/web3-utils/test/fixtures/formatter.ts
@@ -34,6 +34,7 @@ export const convertScalarValueValid: [[any, any, any], any][] = [
 		'0x0e84375b25f38de0e68f7f4884b7342e0814747143c790f48088d22e802cf7a3',
 	],
 	[[100, 'int', { number: FMT_NUMBER.NUMBER, bytes: FMT_BYTES.HEX }], 100],
+	[[100, 'int', { number: 'unknown', bytes: FMT_BYTES.HEX }], 100],
 	[[100, 'uint', { number: FMT_NUMBER.HEX, bytes: FMT_BYTES.HEX }], '0x64'],
 	[[64, 'uint8', { number: FMT_NUMBER.STR }], '64'],
 	[
@@ -43,6 +44,10 @@ export const convertScalarValueValid: [[any, any, any], any][] = [
 	[
 		[new Uint8Array(hexToBytes('FF')), 'bytes32', { bytes: FMT_BYTES.HEX }],
 		'0x00000000000000000000000000000000000000000000000000000000000000ff',
+	],
+	[
+		[new Uint8Array(hexToBytes('FF')), 'unknown', { bytes: FMT_BYTES.HEX }],
+		new Uint8Array(hexToBytes('FF')),
 	],
 	[
 		[new Uint8Array(hexToBytes('FF')), 'bytes32', { bytes: FMT_BYTES.UINT8ARRAY }],
