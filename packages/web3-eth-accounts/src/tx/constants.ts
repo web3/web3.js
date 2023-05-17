9342906b45a21950a20e788755a3e3204deac212
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-05-17 17:23:38 -0400

    fix e2e tests

--- packages/web3-eth-accounts/src/tx/constants.ts
+++ packages/web3-eth-accounts/src/tx/constants.ts
@@ -14,8 +14,9 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
-import { secp256k1 } from 'ethereum-cryptography/secp256k1';
 
+// eslint-disable-next-line import/extensions
+import { secp256k1 } from 'ethereum-cryptography/secp256k1.js';
 /**
  * 2^64-1
  */
