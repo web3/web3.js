5d37d01a351e41cde8178aaccc5532059a27fe19
Wyatt Barnes <me@wyatt.email>
2023-06-07 21:37:11 -1000

    Init simple overload contract test (#6140)

---
+++ packages/web3-eth-contract/test/fixtures/contracts/SimpleOverloaded.sol
@@ -0,0 +1,51 @@
+pragma solidity 0.8.19;
+
+contract SimpleOverload {
+	uint256 public secret;
+	string public someString;
+
+	constructor() {
+		secret = 42;
+	}
+
+	function getSecret() public view returns (uint256) {
+		return secret;
+	}
+
+	function getSecret(uint256 numToAdd) public view returns (uint256) {
+		return secret + numToAdd;
+	}
+
+	function getSecret(
+		uint256 numToAdd,
+		string calldata _someString
+	) public view returns (uint256, string memory) {
+		return (secret + numToAdd, string.concat(someString, _someString));
+	}
+
+	function setSecret() public {
+		secret = 42;
+	}
+
+	function setSecret(uint256 numToAdd) public {
+		secret += numToAdd;
+	}
+
+	function setSecret(uint256 numToAdd, string calldata _someString) public {
+		secret += numToAdd;
+		someString = _someString;
+	}
+
+	function multicall(bytes[] calldata datas) external {
+		for (uint i = 0; i < datas.length; i++) {
+			address(this).call(datas[i]);
+		}
+	}
+
+	function multicall(uint deadline, bytes[] calldata datas) external {
+		require(block.timestamp <= deadline);
+		for (uint i = 0; i < datas.length; i++) {
+			address(this).call(datas[i]);
+		}
+	}
+}
