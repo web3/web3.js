a8f99b8aa111588c6cc59ba2704d0f22f2e9c362
luu-alex <alex.luu@mail.utoronto.ca>
2023-06-13 10:46:19 -0400

    update types

--- packages/web3-utils/types/tests/check-address-checksum-test.ts
+++ packages/web3-utils/types/tests/check-address-checksum-test.ts
@@ -25,10 +25,7 @@ import {checkAddressChecksum} from 'web3-utils';
 
 // $ExpectType boolean
 checkAddressChecksum('0x8ee7f17bb3f88b01247c21ab6603880b64ae53e811f5e01138822e558cf1ab51');
-// $ExpectType boolean
-checkAddressChecksum('0xFb6916095CA1dF60bb79CE92ce3Ea74C37c5D359', 31);
-// $ExpectType boolean
-checkAddressChecksum('0xFb6916095CA1dF60bb79CE92ce3Ea74C37c5D359', undefined);
+
 
 // $ExpectError
 checkAddressChecksum([4]);
