f19f2780b7ef540eff386856cdcc1511fe394f5b
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-07-07 01:05:09 +0200

    fix a type at an integration test

--- packages/web3-eth/test/fixtures/example_subscription.ts
+++ packages/web3-eth/test/fixtures/example_subscription.ts
@@ -14,10 +14,10 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
-import { Web3Subscription } from 'web3-core';
+import { CommonSubscriptionEvents, Web3Subscription } from 'web3-core';
 
 export class NewHeadsSubscription extends Web3Subscription<
-	{ data: string },
+	CommonSubscriptionEvents & { data: string },
 	{ param1: string },
 	{ eth_subscribe: (newHeads: string) => void }
 > {
