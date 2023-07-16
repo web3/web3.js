930d2b46367d1724db37818a895abfbe9ee03598
Wyatt Barnes <me@wyatt.email>
2023-07-15 14:50:13 -1000

    Add itIf to signTypedData integration test

--- packages/web3-eth/test/integration/web3_eth/sign_typed_data.test.ts
+++ packages/web3-eth/test/integration/web3_eth/sign_typed_data.test.ts
@@ -22,7 +22,9 @@ import Web3Eth from '../../../src';
 import {
 	closeOpenConnection,
 	createTempAccount,
+	getSystemTestBackend,
 	getSystemTestProvider,
+	itIf,
 } from '../../fixtures/system_test_utils';
 
 describe('Web3Eth.signTypedData', () => {
@@ -38,83 +40,87 @@ describe('Web3Eth.signTypedData', () => {
 		await closeOpenConnection(web3Eth);
 	});
 
-	it('should sign the typed data, return the signature, and recover the correct ETH address', async () => {
-		const typedData = {
-			types: {
-				EIP712Domain: [
-					{
-						name: 'name',
-						type: 'string',
-					},
-					{
-						name: 'version',
-						type: 'string',
-					},
-					{
-						name: 'chainId',
-						type: 'uint256',
-					},
-					{
-						name: 'verifyingContract',
-						type: 'address',
-					},
-				],
-				Person: [
-					{
-						name: 'name',
-						type: 'string',
-					},
-					{
-						name: 'wallet',
-						type: 'address',
-					},
-				],
-				Mail: [
-					{
-						name: 'from',
-						type: 'Person',
-					},
-					{
-						name: 'to',
-						type: 'Person',
-					},
-					{
-						name: 'contents',
-						type: 'string',
-					},
-				],
-			},
-			primaryType: 'Mail',
-			domain: {
-				name: 'Ether Mail',
-				version: '1',
-				chainId: 1,
-				verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-			},
-			message: {
-				from: {
-					name: 'Cow',
-					wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+	itIf(getSystemTestBackend() === 'ganache')(
+		'should sign the typed data, return the signature, and recover the correct ETH address',
+		async () => {
+			const typedData = {
+				types: {
+					EIP712Domain: [
+						{
+							name: 'name',
+							type: 'string',
+						},
+						{
+							name: 'version',
+							type: 'string',
+						},
+						{
+							name: 'chainId',
+							type: 'uint256',
+						},
+						{
+							name: 'verifyingContract',
+							type: 'address',
+						},
+					],
+					Person: [
+						{
+							name: 'name',
+							type: 'string',
+						},
+						{
+							name: 'wallet',
+							type: 'address',
+						},
+					],
+					Mail: [
+						{
+							name: 'from',
+							type: 'Person',
+						},
+						{
+							name: 'to',
+							type: 'Person',
+						},
+						{
+							name: 'contents',
+							type: 'string',
+						},
+					],
 				},
-				to: {
-					name: 'Bob',
-					wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+				primaryType: 'Mail',
+				domain: {
+					name: 'Ether Mail',
+					version: '1',
+					chainId: 1,
+					verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
 				},
-				contents: 'Hello, Bob!',
-			},
-		};
-		const encodedTypedDataHash = getEncodedEip712Message(typedData, true);
-		const signature = await web3Eth.signTypedData(tempAcc.address, typedData);
-		const r = toUint8Array(signature.slice(0, 66));
-		const s = toUint8Array(`0x${signature.slice(66, 130)}`);
-		const v = BigInt(hexToNumber(`0x${signature.slice(130, 132)}`));
-		const recoveredPublicKey = Buffer.from(
-			ecrecover(toUint8Array(encodedTypedDataHash), v, r, s),
-		).toString('hex');
+				message: {
+					from: {
+						name: 'Cow',
+						wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
+					},
+					to: {
+						name: 'Bob',
+						wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
+					},
+					contents: 'Hello, Bob!',
+				},
+			};
+			const encodedTypedDataHash = getEncodedEip712Message(typedData, true);
+			const signature = await web3Eth.signTypedData(tempAcc.address, typedData);
+			const r = toUint8Array(signature.slice(0, 66));
+			const s = toUint8Array(`0x${signature.slice(66, 130)}`);
+			const v = BigInt(hexToNumber(`0x${signature.slice(130, 132)}`));
+			const recoveredPublicKey = Buffer.from(
+				ecrecover(toUint8Array(encodedTypedDataHash), v, r, s),
+			).toString('hex');
 
-		const recoveredAddress = `0x${keccak256(Buffer.from(recoveredPublicKey, 'hex')).slice(
-			-40,
-		)}`;
-		expect(recoveredAddress).toBe(tempAcc.address);
-	});
+			const recoveredAddress = `0x${keccak256(Buffer.from(recoveredPublicKey, 'hex')).slice(
+				-40,
+			)}`;
+			// eslint-disable-next-line jest/no-standalone-expect
+			expect(recoveredAddress).toBe(tempAcc.address);
+		},
+	);
 });
