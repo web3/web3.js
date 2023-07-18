a7b12c43ecadede224c074a71ff2fe9b9d7721e4
Wyatt Barnes <me@wyatt.email>
2023-07-17 14:28:54 -1000

    Add additional test cases

--- packages/web3-eth-abi/test/unit/get_encoded_eip712_message.test.ts
+++ packages/web3-eth-abi/test/unit/get_encoded_eip712_message.test.ts
@@ -15,94 +15,17 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 import { getEncodedEip712Message } from '../../src/index';
+import { erroneousTestData, testData } from '../fixtures/get_encoded_eip712_message';
 
 describe('getEncodedEip712Message', () => {
-	const typedData = {
-		types: {
-			EIP712Domain: [
-				{
-					name: 'name',
-					type: 'string',
-				},
-				{
-					name: 'version',
-					type: 'string',
-				},
-				{
-					name: 'chainId',
-					type: 'uint256',
-				},
-				{
-					name: 'verifyingContract',
-					type: 'address',
-				},
-			],
-			Person: [
-				{
-					name: 'name',
-					type: 'string',
-				},
-				{
-					name: 'wallet',
-					type: 'address',
-				},
-			],
-			Mail: [
-				{
-					name: 'from',
-					type: 'Person',
-				},
-				{
-					name: 'to',
-					type: 'Person',
-				},
-				{
-					name: 'contents',
-					type: 'string',
-				},
-			],
-		},
-		primaryType: 'Mail',
-		domain: {
-			name: 'Ether Mail',
-			version: '1',
-			chainId: 1,
-			verifyingContract: '0xCcCCccccCCCCcCCCCCCcCcCccCcCCCcCcccccccC',
-		},
-		message: {
-			from: {
-				name: 'Cow',
-				wallet: '0xCD2a3d9F938E13CD947Ec05AbC7FE734Df8DD826',
-			},
-			to: {
-				name: 'Bob',
-				wallet: '0xbBbBBBBbbBBBbbbBbbBbbbbBBbBbbbbBbBbbBBbB',
-			},
-			contents: 'Hello, Bob!',
-		},
-	};
-
-	it('should get encoded message without hashing, hash = undefined', () => {
-		const expectedEncodedMessage =
-			'0x1901f2cee375fa42b42143804025fc449deafd50cc031ca257e0b194a650a912090fc52c0ee5d84264471806290a3f2c4cecfc5490626bf912d01f240d7a274b371e';
-		const encodedMessage = getEncodedEip712Message(typedData);
-
-		expect(encodedMessage).toBe(expectedEncodedMessage);
+	it.each(testData)('%s', (_, typedData, hashEncodedData, expectedResponse) => {
+		const encodedMessage = getEncodedEip712Message(typedData, hashEncodedData);
+		expect(encodedMessage).toBe(expectedResponse);
 	});
 
-	it('should get encoded message without hashing, hash = false', () => {
-		const expectedEncodedMessage =
-			'0x1901f2cee375fa42b42143804025fc449deafd50cc031ca257e0b194a650a912090fc52c0ee5d84264471806290a3f2c4cecfc5490626bf912d01f240d7a274b371e';
-		const encodedMessage = getEncodedEip712Message(typedData);
-
-		expect(encodedMessage).toBe(expectedEncodedMessage);
-	});
-
-	it('should get the hashed encoded message, hash = true', () => {
-		const expectedHashedMessage =
-			'0xbe609aee343fb3c4b28e1df9e632fca64fcfaede20f02e86244efddf30957bd2';
-		const encodedMessage = getEncodedEip712Message(typedData, true);
-
-		expect(encodedMessage).toBe(expectedHashedMessage);
+	it.each(erroneousTestData)('%s', (_, typedData, hashEncodedData, expectedError) => {
+		expect(() => getEncodedEip712Message(typedData, hashEncodedData)).toThrowError(
+			expectedError,
+		);
 	});
 });
