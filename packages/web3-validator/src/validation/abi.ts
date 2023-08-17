68df3b8237c3e4c28e14bf98f431bb04526335c9
jdevcs <junaid@chainsafe.io>
2023-08-17 16:02:00 +0200

    import path fix ESM

--- packages/web3-validator/src/validation/abi.ts
+++ packages/web3-validator/src/validation/abi.ts
@@ -16,7 +16,7 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { AbiParameter } from 'web3-types';
-import { ShortValidationSchema } from '../types';
+import { ShortValidationSchema } from '../types.js';
 
 export const isAbiParameterSchema = (
 	schema: string | ShortValidationSchema | AbiParameter,
