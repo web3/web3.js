071effb9010ffd9c9375b1799d987f3439b2a969
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-11-09 11:22:42 -0500

    fix ipc tests

--- scripts/stressTests/data/start.sh
+++ scripts/stressTests/data/start.sh
@@ -1,13 +1,16 @@
 #!/usr/bin/env bash
 . scripts/env.sh
 
-yarn geth-binary:start:background
-export WEB3_SYSTEM_TEST_PROVIDER=$IPC_PATH
 export WEB3_SYSTEM_TEST_BACKEND="geth"
+
+./scripts/geth_binary.sh stressStart
+
 yarn generate:accounts
+
+export WEB3_SYSTEM_TEST_PROVIDER=$IPC_PATH
 node ./scripts/stressTests/data/index.js
 
 export WEB3_SYSTEM_TEST_PROVIDER=ws://127.0.0.1:8545
 node ./scripts/stressTests/data/index.js
 
-yarn geth-binary:stop
+./scripts/geth_binary.sh stop
