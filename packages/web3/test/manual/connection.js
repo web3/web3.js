d5d2c680eb6fe4c5955aac500328fc0918467c99
luu-alex <alex.luu@mail.utoronto.ca>
2023-11-10 15:07:46 -0500

    add eslint disable

--- packages/web3/test/manual/connection.js
+++ packages/web3/test/manual/connection.js
@@ -14,7 +14,7 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
-
+/* eslint-disable */
 const { Web3 } = require('../../');
 const secrets  = require('../../../../.secrets.json');
 
