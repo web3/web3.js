faba5aacbf8f5bad251f1edb822b5bd0df174465
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-11-14 10:20:59 -0500

    browser tests

--- packages/web3/test/stress/startRequests.sh
+++ packages/web3/test/stress/startRequests.sh
@@ -4,7 +4,7 @@
 export WEB3_SYSTEM_TEST_BACKEND="geth"
 
 ./scripts/geth_binary.sh stressStart
-export PARALLEL_COUNT=400
+export PARALLEL_COUNT=10
 export WEB3_SYSTEM_TEST_PROVIDER=$IPC_PATH
 node ./packages/web3/test/stress/requests.js
 ./scripts/geth_binary.sh stop
@@ -16,7 +16,7 @@ node ./packages/web3/test/stress/requests.js
 
 
 ./scripts/geth_binary.sh stressStart
-export PARALLEL_COUNT=500
+export PARALLEL_COUNT=100
 export WEB3_SYSTEM_TEST_PROVIDER=http://127.0.0.1:8545
 node ./packages/web3/test/stress/requests.js
 ./scripts/geth_binary.sh stop
