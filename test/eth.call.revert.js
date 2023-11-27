3c6b804ef88fb0bfcffc6b2a642e31764dc243c7
Nicolas COURTILLER <nicolas.courtiller@free.fr>
2023-11-27 23:40:49 +0100

    add unit test case 'Insufficient funds Errors on Polygon PoS through MetaMask' to cover new logic in sendTxCallback() previously modified

--- test/eth.call.revert.js
+++ test/eth.call.revert.js
@@ -94,5 +94,50 @@ describe('call revert', function () {
             assert.equal(error.reason, 'ImproperState');
         }
     });
+
+    it('Insufficient funds Errors on Polygon PoS through MetaMask', async function() {
+        let insufficientFundsErrorMsg = "err: insufficient funds for gas * price + value: address 0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b have 78119475604576697598 want 100000000000000000000 (supplied gas 600000000)";
+        provider.injectRawError({
+            "code": -32603,
+            "message": "Internal JSON-RPC error.",
+            "data": {
+              "code": -32000,
+              "message": insufficientFundsErrorMsg
+            },
+            "stack": "Error@moz-extension://8de06240-0fd5-4f6d-8dca-318e48de4a2c/runtime-lavamoat.js:7554:17\n(...)\nr.createFetchMiddleware/<@moz-extension://8de06240-0fd5-4f6d-8dca-318e48de4a2c/background-0.js:7:45979\n"
+        });
+        provider.injectValidation(function (payload) {
+            assert.equal(payload.jsonrpc, '2.0');
+            assert.equal(payload.method, 'eth_call');
+            assert.deepEqual(
+                payload.params,
+                [{
+                    to: '0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b',
+                    data: '0x23455654',
+                    value: web3.utils.toHex('100000000000000000000'),
+                    gas: '0xb',
+                    gasPrice: '0xb'
+                }, 'latest']
+            );
+        });
+
+        var options = {
+            to: '0xa94f5374fce5edbc8e2a8697c15331677e6ebf0b',
+            data: '0x23455654',
+            value: '100000000000000000000',
+            gas: 11,
+            gasPrice: 11
+        };
+
+        try {
+            await web3.eth.call(options, 'latest');
+            assert.fail('call should have failed!');
+        } catch (error) {
+            assert(error.message.startsWith("Internal JSON-RPC error."));
+            // recover internal data
+            let data = JSON.parse(error.message.slice(24));
+            assert.equal(data.message, insufficientFundsErrorMsg);
+        }
+    });
     
 });
