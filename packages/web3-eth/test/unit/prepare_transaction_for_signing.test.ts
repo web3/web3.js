f124da77f6454c33a4cbd1dbfbd9ad448d93aede
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-11-28 15:42:04 -0500

    increase coverage

--- packages/web3-eth/test/unit/prepare_transaction_for_signing.test.ts
+++ packages/web3-eth/test/unit/prepare_transaction_for_signing.test.ts
@@ -15,20 +15,27 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { EthExecutionAPI } from 'web3-types';
+import {
+	Common,
+	EthExecutionAPI,
+	HexString,
+	Web3NetAPI,
+	Transaction as TransactionType,
+} from 'web3-types';
 import { Web3Context } from 'web3-core';
 import HttpProvider from 'web3-providers-http';
 import { isNullish } from 'web3-validator';
-import {
-	AccessListEIP2930Transaction,
-	FeeMarketEIP1559Transaction,
-	Transaction,
-} from 'web3-eth-accounts';
+
 import { ethRpcMethods } from 'web3-rpc-methods';
 
 import { bytesToHex, hexToBytes } from 'web3-utils';
 import { prepareTransactionForSigning } from '../../src/utils/prepare_transaction_for_signing';
 import { validTransactions } from '../fixtures/prepare_transaction_for_signing';
+import {
+	AccessListEIP2930Transaction,
+	FeeMarketEIP1559Transaction,
+	Transaction,
+} from 'web3-eth-accounts';
 
 describe('prepareTransactionForSigning', () => {
 	const web3Context = new Web3Context<EthExecutionAPI>({
@@ -36,6 +43,74 @@ describe('prepareTransactionForSigning', () => {
 		config: { defaultNetworkId: '0x1' },
 	});
 
+	describe('default', () => {
+		it('use default common', async () => {
+			const context = new Web3Context<EthExecutionAPI>({
+				provider: new HttpProvider('http://127.0.0.1'),
+				config: { defaultNetworkId: '0x1' },
+			});
+			context.defaultChain = 'mainnet';
+			// context.setConfig({ defaultHardfork: 'london' });
+			// @ts-ignore
+			context.defaultCommon = {
+				customChain: {
+					name: 'test',
+					networkId: 457,
+					chainId: 1458,
+				},
+				baseChain: 'mainnet',
+			};
+
+			async function transactionBuilder<ReturnType = TransactionType>(options: {
+				transaction: TransactionType;
+				web3Context: Web3Context<EthExecutionAPI & Web3NetAPI>;
+				privateKey?: HexString | Uint8Array;
+				fillGasPrice?: boolean;
+				fillGasLimit?: boolean;
+			}): Promise<ReturnType> {
+				if (isNullish(options.transaction.common)) {
+					if (options.web3Context.defaultCommon) {
+						const common = options.web3Context.defaultCommon as unknown as Common;
+						const chainId = common.customChain.chainId as string;
+						const networkId = common.customChain.networkId as string;
+						const name = common.customChain.name as string;
+						options.transaction.common = {
+							...common,
+							customChain: { chainId, networkId, name },
+						};
+					}
+				}
+				return options.transaction as unknown as ReturnType;
+			}
+
+			// @ts-ignore
+			context.transactionBuilder = transactionBuilder;
+
+			// (i.e. requestManager, blockNumber, hydrated params), but that doesn't matter for the test
+			jest.spyOn(ethRpcMethods, 'estimateGas').mockImplementation(
+				// @ts-expect-error - Mocked implementation doesn't have correct method signature
+				() => expectedTransaction.gas,
+			);
+			// @ts-expect-error - Mocked implementation doesn't have correct method signature
+			jest.spyOn(ethRpcMethods, 'getBlockByNumber').mockImplementation(() => mockBlock);
+
+			const ethereumjsTx = await prepareTransactionForSigning(
+				{
+					chainId: 1458,
+					nonce: 1,
+					gasPrice: BigInt(20000000000),
+					gas: BigInt(21000),
+					to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
+					from: '0x2c7536E3605D9C16a7a3D7b1898e529396a65c23',
+					value: '1000000000',
+					input: '',
+				},
+				context,
+			);
+			expect(Number(ethereumjsTx.common.networkId())).toBe(457);
+			expect(ethereumjsTx.common.chainName()).toBe('test');
+		});
+	});
 	describe('should return an web3-utils/tx instance with expected properties', () => {
 		it.each(validTransactions)(
 			'mockBlock: %s\nexpectedTransaction: %s\nexpectedPrivateKey: %s\nexpectedAddress: %s\nexpectedRlpEncodedTransaction: %s\nexpectedTransactionHash: %s\nexpectedMessageToSign: %s\nexpectedV: %s\nexpectedR: %s\nexpectedS: %s',
