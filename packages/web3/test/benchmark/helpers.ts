0d2b1d085b8d11c3acbb0f39839aff643269e07d
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2023-12-11 19:45:50 -0500

    add tests. run few tests

--- packages/web3/test/benchmark/helpers.ts
+++ packages/web3/test/benchmark/helpers.ts
@@ -14,43 +14,10 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
-import Benchmark from 'benchmark';
 // eslint-disable-next-line import/no-extraneous-dependencies
-import Web3Eth, { prepareTransactionForSigning } from 'web3-eth';
-import accounts from '../fixtures/accounts.json';
+import { Web3Context } from 'web3-core';
 
-const eth = new Web3Eth({
+export const context = new Web3Context({
 	// eslint-disable-next-line @typescript-eslint/no-empty-function
 	request: () => {},
-});
-
-const bench = new Benchmark(
-	'requests',
-	async () => {
-		return prepareTransactionForSigning(
-			{
-				from: accounts[0].address,
-				to: accounts[1].address,
-				value: BigInt(1),
-				nonce: 0,
-				chainId: 0,
-				gas: BigInt(100000),
-				gasPrice: BigInt(10000),
-			},
-			eth,
-			undefined,
-			false,
-			false,
-		);
-	},
-	{
-		onComplete(event: any) {
-			// eslint-disable-next-line no-console
-			console.log(String(event.target));
-		},
-	},
-);
-
-bench.run({
-	async: true,
 });
