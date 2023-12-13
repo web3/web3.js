5541151fe59b4432788cbb013e3596eeaadca766
Muhammad-Altabba <24407834+Muhammad-Altabba@users.noreply.github.com>
2023-12-13 23:32:17 +0100

    add test cases

--- packages/web3-eth/test/unit/prepare_transaction_for_signing.test.ts
+++ packages/web3-eth/test/unit/prepare_transaction_for_signing.test.ts
@@ -104,22 +104,13 @@ describe('prepareTransactionForSigning', () => {
 		});
 	});
 
-	it('use read Hardfork from context.defaultHardfork', async () => {
+	it('should be able to read Hardfork from context.defaultHardfork', async () => {
 		const context = new Web3Context<EthExecutionAPI>({
 			provider: new HttpProvider('http://127.0.0.1'),
 			config: { defaultNetworkId: '0x9' },
 		});
 		context.defaultChain = 'mainnet';
-		context.defaultCommon = {
-			customChain: {
-				name: 'test',
-				networkId: 457,
-				chainId: 1458,
-			},
-		} as any;
 		context.defaultHardfork = Hardfork.Istanbul;
-
-		// context.defaultChain = 'sepolia';
 
 		async function transactionBuilder<ReturnType = TransactionType>(options: {
 			transaction: TransactionType;
@@ -144,12 +135,119 @@ describe('prepareTransactionForSigning', () => {
 				from: '0x2c7536E3605D9C16a7a3D7b1898e529396a65c23',
 				value: '1000000000',
 				input: '',
-				networkId: 457,
-				// chain: 'sepolia',
+				networkId: 999,
 			},
 			context,
 		);
 		expect(ethereumjsTx.common.hardfork()).toBe(Hardfork.Istanbul);
+		expect(ethereumjsTx.common.networkId().toString()).toBe('999');
+	});
+
+	it('should be able to read Hardfork from context.config.defaultHardfork and context.defaultCommon.hardfork', async () => {
+		const context = new Web3Context<EthExecutionAPI>({
+			provider: new HttpProvider('http://127.0.0.1'),
+			config: { defaultNetworkId: '0x9' },
+		});
+		context.defaultChain = 'mainnet';
+
+		// if the value here is different from the one in context.defaultCommon.hardfork
+		// Then an error will be thrown:
+		// "ConfigHardforkMismatchError: Web3Config hardfork doesnt match in defaultHardfork london and common.hardfork istanbul"
+		context.config.defaultHardfork = Hardfork.Istanbul;
+		context.defaultCommon = {
+			customChain: {
+				name: 'test',
+				networkId: 111,
+				chainId: 1458,
+			},
+			hardfork: Hardfork.Istanbul,
+			baseChain: 'mainnet',
+		} as any;
+
+		async function transactionBuilder<ReturnType = TransactionType>(options: {
+			transaction: TransactionType;
+			web3Context: Web3Context<EthExecutionAPI & Web3NetAPI>;
+			privateKey?: HexString | Uint8Array;
+			fillGasPrice?: boolean;
+			fillGasLimit?: boolean;
+		}): Promise<ReturnType> {
+			const tx = { ...options.transaction };
+			return tx as unknown as ReturnType;
+		}
+
+		context.transactionBuilder = transactionBuilder;
+
+		const ethereumjsTx = await prepareTransactionForSigning(
+			{
+				chainId: 1458,
+				nonce: 1,
+				gasPrice: BigInt(20000000000),
+				gas: BigInt(21000),
+				to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
+				from: '0x2c7536E3605D9C16a7a3D7b1898e529396a65c23',
+				value: '1000000000',
+				input: '',
+			},
+			context,
+		);
+		expect(ethereumjsTx.common.hardfork()).toBe(Hardfork.Istanbul);
+		expect(ethereumjsTx.common.networkId().toString()).toBe('111');
+	});
+
+	it('should give priorities to tx.hardfork and tx.networkId over values from context', async () => {
+		const context = new Web3Context<EthExecutionAPI>({
+			provider: new HttpProvider('http://127.0.0.1'),
+			config: { defaultNetworkId: '0x9' },
+		});
+		context.defaultChain = 'mainnet';
+
+		// if the value here is different from the one in context.defaultCommon.hardfork
+		// Then an error will be thrown:
+		// "ConfigHardforkMismatchError: Web3Config hardfork doesnt match in defaultHardfork london and common.hardfork istanbul"
+		context.config.defaultHardfork = Hardfork.Istanbul;
+		context.defaultCommon = {
+			customChain: {
+				name: 'test',
+				networkId: 111,
+				chainId: 1458,
+			},
+			hardfork: Hardfork.Istanbul,
+			baseChain: 'mainnet',
+		} as any;
+
+		async function transactionBuilder<ReturnType = TransactionType>(options: {
+			transaction: TransactionType;
+			web3Context: Web3Context<EthExecutionAPI & Web3NetAPI>;
+			privateKey?: HexString | Uint8Array;
+			fillGasPrice?: boolean;
+			fillGasLimit?: boolean;
+		}): Promise<ReturnType> {
+			const tx = { ...options.transaction };
+			return tx as unknown as ReturnType;
+		}
+
+		context.transactionBuilder = transactionBuilder;
+
+		// context.transactionBuilder = defaultTransactionBuilder;
+
+		const ethereumjsTx = await prepareTransactionForSigning(
+			{
+				chainId: 1458,
+				nonce: 1,
+				gasPrice: BigInt(20000000000),
+				gas: BigInt(21000),
+				to: '0xF0109fC8DF283027b6285cc889F5aA624EaC1F55',
+				from: '0x2c7536E3605D9C16a7a3D7b1898e529396a65c23',
+				value: '1000000000',
+				input: '',
+				networkId: 999,
+				hardfork: Hardfork.Chainstart,
+				chain: 'mainnet',
+			},
+			context,
+		);
+		expect(ethereumjsTx.common.hardfork()).toBe(Hardfork.Chainstart);
+		expect(ethereumjsTx.common.networkId().toString()).toBe('999');
 	});
 
 	describe('should return an web3-utils/tx instance with expected properties', () => {
