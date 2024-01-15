a7984f9484663307d46d8bed15b9e3bd6de9277a
luu-alex <alex.luu@mail.utoronto.ca>
2024-01-15 13:29:06 -0500

    add eslint

--- packages/web3/test/e2e/sepolia/get_storage_at.test.ts
+++ packages/web3/test/e2e/sepolia/get_storage_at.test.ts
@@ -78,6 +78,7 @@ describe(`${getSystemTestBackend()} tests - getStorageAt`, () => {
 					result === '0x0000000000000000000000000000000000000000000000000000000000000000',
 			).toBeTruthy();
 		} else {
+			// eslint-disable-next-line
 			console.log(block);
 			// eslint-disable-next-line jest/no-conditional-expect
 			expect(result).toBe(
