e923c4e19dfb09fae4d4c33309b32283ba7753d4
luu-alex <alex.luu@mail.utoronto.ca>
2024-02-05 12:44:28 -0500

    update ocnfig

---
+++ scripts/hardhat.config.js
@@ -0,0 +1,12 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+
+module.exports = {
+  solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      chainId: 1337,
+      accounts: {
+      }
+    }
+  }
+};
