29982fa346f6154b129c5d3e8e4f8e0597b0227f
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2024-02-28 10:24:06 -0500

    add space

--- packages/web3-utils/src/index.ts
+++ packages/web3-utils/src/index.ts
@@ -14,6 +14,7 @@ GNU Lesser General Public License for more details.
 You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
+
 export * from './converters.js';
 export * from './event_emitter.js';
 export * from './validation.js';
