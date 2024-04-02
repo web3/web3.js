f59e840ff5c8d8b6648f39c4b9508e1f5fa9a12d
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2024-04-02 14:43:31 +0200

    fix

--- packages/web3-eth/test/integration/format.test.ts
+++ packages/web3-eth/test/integration/format.test.ts
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { SupportedProviders, FMT_BYTES, FMT_NUMBER, DEFAULT_RETURN_FORMAT } from 'web3-types';
+import { SupportedProviders, FMT_BYTES, FMT_NUMBER } from 'web3-types';
 // eslint-disable-next-line import/no-extraneous-dependencies
 import { Contract } from 'web3-eth-contract';
 import { numberToHex } from 'web3-utils';
--- packages/web3/test/integration/web3.format.test.ts
+++ packages/web3/test/integration/web3.format.test.ts
@@ -15,7 +15,7 @@ You should have received a copy of the GNU Lesser General Public License
 along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
-import { SupportedProviders, DEFAULT_RETURN_FORMAT } from 'web3-types';
+import { SupportedProviders } from 'web3-types';
 import { numberToHex } from 'web3-utils';
 import { describe } from 'node:test';
 import { Web3, Contract, FMT_BYTES, FMT_NUMBER } from '../../src';
