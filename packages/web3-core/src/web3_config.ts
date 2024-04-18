7b347d388243951a71621f6bba7418de63397ca3
Oleksii Kosynskyi <oleksii.kosynskyi@gmail.com>
2024-04-17 21:49:11 -0400

    fix

--- packages/web3-core/src/web3_config.ts
+++ packages/web3-core/src/web3_config.ts
@@ -18,11 +18,11 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 import { Numbers, HexString, BlockNumberOrTag, Common } from 'web3-types';
 import { ConfigHardforkMismatchError, ConfigChainMismatchError } from 'web3-errors';
 import { isNullish, toHex } from 'web3-utils';
+import { ValidationSchemaInput } from 'web3-validator';
 import { TransactionTypeParser } from './types.js';
 // eslint-disable-next-line import/no-cycle
 import { TransactionBuilder } from './web3_context.js';
 import { Web3EventEmitter } from './web3_event_emitter.js';
-import { ValidationSchemaInput } from 'web3-validator';
 
 // To avoid cycle dependency declare this
 export interface Web3ConfigOptions {
