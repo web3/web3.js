51a10d4fc98639e18da0d11dac53d56e31aa0370
luu-alex <98a.lexluu@gmail.com>
2024-07-27 12:57:32 -0400

    update

--- packages/web3/test/e2e/mainnet/get_storage_at.test.ts
+++ packages/web3/test/e2e/mainnet/get_storage_at.test.ts
@@ -80,7 +80,7 @@ describe(`${getSystemTestBackend()} tests - getStorageAt`, () => {
 		} else if (block === 'blockHash' || block === 'blockNumber') {
 			// eslint-disable-next-line jest/no-conditional-expect
 			expect(result).toBe(
-				'0x000000000000000000000000000000000000000000000000007d5a864f06b2d5',
+				'0x00000000000000000000000000000000000000000000000000b8b61e3be91403',
 			);
 		} else {
 			// eslint-disable-next-line jest/no-conditional-expect
