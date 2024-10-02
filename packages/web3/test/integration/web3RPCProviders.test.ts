1d5c7365769de69f3bc72a54c79c739a07f67dfe
Mario Rivera <marioriveravazquez@gmail.com>
2024-10-02 14:26:54 -0400

    Testing

--- packages/web3/test/integration/web3RPCProviders.test.ts
+++ packages/web3/test/integration/web3RPCProviders.test.ts
@@ -16,6 +16,7 @@ along with web3.js.  If not, see <http://www.gnu.org/licenses/>.
 */
 
 import { mainnet, Network, QuickNodeProvider, Transport } from 'web3-rpc-providers';
+import { AlchemyProvider } from 'web3-rpc-providers/src';
 import { Web3 } from '../../src/index';
 
 describe('Web3 RPC Provider Integration tests', () => {
@@ -36,6 +37,18 @@ describe('Web3 RPC Provider Integration tests', () => {
 		networks.forEach(network => {
 			it(`QuickNodeProvider should work with ${transport} transport and ${network} network`, async () => {
 				const provider = new QuickNodeProvider(network, transport);
+				const web3 = new Web3(provider);
+				const result = await web3.eth.getBlockNumber();
+
+				expect(typeof result).toBe('bigint');
+				expect(result > 0).toBe(true);
+
+				if (transport === Transport.WebSocket) {
+					web3.provider?.disconnect();
+				}
+			});
+			it(`AlchemyProvider should work with ${transport} transport and ${network} network`, async () => {
+				const provider = new AlchemyProvider(network, transport);
 				const web3 = new Web3(provider);
 				const result = await web3.eth.getBlockNumber();
 
